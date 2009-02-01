745538d180adf363f06a5ac7d5b501e8c19e01dc
Alexander Heinecke <heinecke@in.tum.de>
2009-02-01 17:19:15 +0000

    - added alexgames_alpha.in that contains the alpha values for a 2D parable - modified alexgames.py, add function evaluation
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@119 4eea3252-f0fb-4393-894d-40516dce545b

--- bin/alexgames.py
+++ bin/alexgames.py
@@ -27,9 +27,79 @@ except:
 def callback_deprecated(option, opt, value, parser):
     print "Warning: Option %s is deprecated." % (option)
 
+## Reads in an ARFF file:
+# The data is stored in lists. There is a value list for every dimension of the data set. e.g. 
+# [[2, 3],[1, 1]] are the data points P_1(2,1) and P_2(3,1)
+#
+# @param filename the file's filename that should be read
+# @return returns a set of a array with the data (named data), a array with the classes (named classes) and the filename named as filename
+def readDataARFF(filename):
+    fin = open(filename, "r")
+    data = []
+    classes = []
+    hasclass = False
+
+    # get the different section of ARFF-File
+    for line in fin:
+        sline = line.strip().lower()
+        if sline.startswith("%") or len(sline) == 0:
+            continue
+
+        if sline.startswith("@data"):
+            break
+        
+        if sline.startswith("@attribute"):
+            value = sline.split()
+            if value[1].startswith("class"):
+                hasclass = True
+            else:
+                data.append([])
+    
+    #read in the data stored in the ARFF file
+    for line in fin:
+        sline = line.strip()
+        if sline.startswith("%") or len(sline) == 0:
+            continue
+
+        values = sline.split(",")
+        if hasclass:
+            classes.append(float(values[-1]))
+            values = values[:-1]
+        for i in xrange(len(values)):
+            data[i].append(float(values[i]))
+            
+    # cleaning up and return
+    fin.close()
+    return {"data":data, "classes":classes, "filename":filename}
+
+
+#-------------------------------------------------------------------------------
+## Builds the training data vector
+# 
+# @param data a list of lists that contains the points a the training data set, coordinate-wise
+# @return a instance of a DataVector that stores the training data
+def buildTrainingVector(data):
+    dim = len(data["data"])
+    training = DataVector(len(data["data"][0]), dim)
+    
+    # i iterates over the data points, d over the dimension of one data point
+    for i in xrange(len(data["data"][0])):
+        for d in xrange(dim):
+            training[i*dim + d] = data["data"][d][i]
+    
+    return training
+
+
+# Alex is playing with python and sg++
+#
+# This test routine creates a grid and evaluates a function
+# on this grid
 def run_test():
+    alpha = None
+    gridpoint = None
     dim = 2
     level = 3
+    res = 9
     
     grid = Grid.createLinearGrid(dim)
     generator  = grid.createGridGenerator()
@@ -37,10 +107,27 @@ def run_test():
     
     #print grid to console (base functions)
     print grid.serialize()
+    print "Size of Grid = Number of Gridpoints is:"
+    print grid.getStorage().size()
     
-    #apply function to grid
+    #evaluating parable on sparse grid
+    eval = grid.createOperationEval()
+    gridpoint = DataVector(1, dim)
     
+    # read the alphas
+    alpha = buildTrainingVector(readDataARFF("alexgames_alpha.in"))
     
+    fout = file("alexgames_eval.out", "w")
+    
+    for x in xrange(res):
+        for y in xrange(res):
+            gridpoint[0] = float(x) / (res - 1)
+            gridpoint[1] = float(y) / (res - 1)
+            gp_value = eval.eval(alpha, gridpoint)
+            fout.write("%f %f %f\n" % (gridpoint[0], gridpoint[1], gp_value))
+    fout.close()
+    return
+      
 #===============================================================================
 # Main
 #===============================================================================
