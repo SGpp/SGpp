c5973b6e7ee58fe52043b5247860db92a57869bc
Alexander Heinecke <heinecke@in.tum.de>
2009-02-05 13:15:13 +0000

    moved definition of basis into basis/
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@135 4eea3252-f0fb-4393-894d-40516dce545b

--- sg++/basis/modwavelet/modified_wavelet_base.hpp
+++ sg++/basis/modwavelet/modified_wavelet_base.hpp
@@ -2,6 +2,7 @@
 /* This file is part of sg++, a program package making use of spatially      */
 /* adaptive sparse grids to solve numerical problems                         */
 /*                                                                           */
+/* Copyright (C) 2008 Jörg Blank (blankj@in.tum.de)                          */
 /* Copyright (C) 2009 Alexander Heinecke (Alexander.Heinecke@mytum.de)       */
 /*                                                                           */
 /* sg++ is free software; you can redistribute it and/or modify              */
@@ -20,50 +21,9 @@
 /* or see <http://www.gnu.org/licenses/>.                                    */
 /*****************************************************************************/
 
-#include "basis/linear/operation/OperationHierarchisationLinear.hpp"
-#include "basis/linear/algorithm_sweep/HierarchisationLinear.hpp"
-#include "basis/linear/algorithm_sweep/DehierarchisationLinear.hpp"
+#ifndef MODIFIED_WAVELET_BASE_HPP
+#define MODIFIED_WAVELET_BASE_HPP
 
-#include "sgpp.hpp"
-#include "algorithms.hpp"
-#include "basis/basis.hpp"
-#include "data/DataVector.h"
 
-namespace sg
-{
 
-/**
- * Implements the hierarchisation on a sprase grid with linear base functions
- *
- * @param node_values the functions values in the node base
- */
-void OperationHierarchisationLinear::doHierarchisation(DataVector& node_values)
-{
-	detail::HierarchisationLinear func(this->storage);
-	sweep<detail::HierarchisationLinear> s(func, this->storage);
-
-	// Execute hierarchisation in every dimension of the grid
-	for (size_t i = 0; i < this->storage->dim(); i++)
-	{
-		s.sweep1D(node_values, node_values, i);
-	}
-}
-
-/**
- * Implements the dehierarchisation on a sprase grid with linear base functions
- *
- * @param alpha the coefficients of the sparse grid's base functions
- */
-void OperationHierarchisationLinear::doDehierarchisation(DataVector& alpha)
-{
-	detail::DehierarchisationLinear func(this->storage);
-	sweep<detail::DehierarchisationLinear> s(func, this->storage);
-
-	// Execute hierarchisation in every dimension of the grid
-	for (size_t i = 0; i < this->storage->dim(); i++)
-	{
-		s.sweep1D(alpha, alpha, i);
-	}
-}
-
-}
+#endif /* MODIFIED_WAVELET_BASE_HPP */
