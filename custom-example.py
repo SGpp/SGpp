45f52a3346ce2399ffc58caf375c76ee6ba13ca3
Alexander Heinecke <heinecke@in.tum.de>
2009-02-11 13:49:18 +0000

    changed GPL
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@141 4eea3252-f0fb-4393-894d-40516dce545b

--- custom-example.py
+++ custom-example.py
@@ -1,19 +1,27 @@
-# pyclass is a set of tools utilizing sparse grids to solve numerical problems
-# Copyright (C) 2007  Joerg Blank (blankj@in.tum.de), Richard Roettger (roettger@in.tum.de), Dirk Pflueger (pflueged@in.tum.de)
-#               2008  Dirk Pflueger (pflueged@in.tum.de)
-# 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#############################################################################
+# This file is part of pysgpp, a program package making use of spatially    #
+# adaptive sparse grids to solve numerical problems                         #
+#                                                                           #
+# Copyright (C) 2007 Jörg Blank (blankj@in.tum.de)                          #
+# Copyright (C) 2007 Richard Röttger (roettger@in.tum.de)                   #
+# Copyright (C) 2008 Dirk Plüger (pflueged@in.tum.de)                       #
+# Copyright (C) 2009 Alexander Heinecke (Alexander.Heinecke@mytum.de)       #
+#                                                                           #
+# pysgpp is free software; you can redistribute it and/or modify            #
+# it under the terms of the GNU General Public License as published by      #
+# the Free Software Foundation; either version 3 of the License, or         #
+# (at your option) any later version.                                       #
+#                                                                           #
+# pysgpp is distributed in the hope that it will be useful,                 #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
+# GNU General Public License for more details.                              #
+#                                                                           #
+# You should have received a copy of the GNU General Public License         #
+# along with pysgpp; if not, write to the Free Software                     #
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA #
+# or see <http://www.gnu.org/licenses/>.                                    #
+#############################################################################
 
 import os
 
