8b2c6c0b1c82cee345ba5bbba39a2e88222bfbca
Alexander Heinecke <heinecke@in.tum.de>
2009-02-11 13:43:44 +0000

    added function for determining coordinates of a grid point in HashGridIndex.hpp added unittest for hierarchisation extended hierarchisation.py
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@140 4eea3252-f0fb-4393-894d-40516dce545b

--- bin/hierarchisation.py
+++ bin/hierarchisation.py
@@ -26,6 +26,7 @@ from tools import *
 from pysgpp import *
 from math import sqrt
 import random
+import re
 
 from array import array
 
@@ -54,7 +55,7 @@ def callback_deprecated(option, opt, value, parser):
 #-------------------------------------------------------------------------------
 # Builds the data vector that hold the coefficients fo either the ansatzfuctions
 # or the node base
-# 
+# re.sub(regex, replacement, subject)
 # @param filename name of the ARFF file that contains the coefficients
 # @return a instance of a DataVector that stores coefficients
 def buildCoefficientVectorFromFile(filename):
@@ -119,37 +120,90 @@ def doDehierarchisation(alpha, grid):
     return alpha
     
     
+# evalutes a given function
+# @param function a string the gives the function; x1...xn must be the names of the placeholders
+# @param points sorted list of the coordinates (x1...xn) of evaluation point
+# @return returns the function value at points
+def evalFunction(function, points):
+    for i in xrange(len(points)):
+        function = re.sub("x" + str(i+1), points[i], function)
+            
+    return eval(function)    
+    
+
+# tests the correctness of the hierarchisation and dehierachisation
+# @param node1 the vector of the node base values before hierarchisation and dehierarchisation
+# @param node2 the vector of the node base values after hierarchisation and dehierarchisation
+# @return maximum error during the transformations
+def testHierarchisationResults(node1, node2):
+    error = 0.0
+    
+    for i in xrange(len(node1)):
+        if abs(abs(node1[i])-abs(node2[i])) > error:
+            error = abs(abs(node1[i])-abs(node2[i]))
+            
+    return error
+    
+    
+# build parable test function over [0,1]^d
+# @param dim dimension of the parable's space
+# @return returns a string that contains the function as string
+def buildParable(dim):
+    function = ""
+    
+    function = str(pow(4.0,dim))
+    
+    for i in xrange(dim):
+        function = function + "*x" + str(i+1) + "*(1-" + "x" + str(i+1) + ")"
+        
+    return function    
+    
+    
 #-------------------------------------------------------------------------------    
 # tests the hierarchisation and dehierarchisation routine of sg++ with a sparse
 # grid of dimension 2 and level 3
-def runHierarchisationDehierarchisationLinearRegularTest():
+def runHierarchisationDehierarchisationLinearRegularTest(dim, level):
     node_values = None
     node_values_back = None
     alpha = None
+    points = None
 
-    dim = 2
-    level = 3
+    function = buildParable(dim)
+    
+    print "The test function is:"
+    print function
     
     # generate a regular test grid
     grid = Grid.createLinearGrid(dim)
     generator  = grid.createGridGenerator()
     generator.regular(level)
-        
-    # read in the node base values
-    node_values = buildNodevalueVector("hierarchisation_nodevalues.in")
     
-    print node_values
+    # generate the node_values vector
+    storage = grid.getStorage()
+    
+    node_values = DataVector(storage.size(), 1)
+    
+    for n in xrange(storage.size()):
+        points = storage.get(n).getCoordinates().split()
+        node_values[n] = evalFunction(function, points)
+        
+        
+    #print node_values
     
     # do hierarchisation
     alpha = doHierarchisation(node_values, grid)
     
-    print alpha
+    #print alpha
     
     # do dehierarchisation
     node_values_back = doDehierarchisation(alpha, grid)
      
     #print result
-    print node_values_back
+    #print node_values_back
+    
+    # test hierarchisation and dehierarchisation
+    print "The maximum error during hierarchisation and dehierarchisation was:"
+    print testHierarchisationResults(node_values, node_values_back)
     
     return
     
@@ -161,4 +215,4 @@ def runHierarchisationDehierarchisationLinearRegularTest():
 # check so that file can also be imported in other files
 if __name__=='__main__':
     #start the test programm
-    runHierarchisationDehierarchisationLinearRegularTest()
+    runHierarchisationDehierarchisationLinearRegularTest(20, 4)
