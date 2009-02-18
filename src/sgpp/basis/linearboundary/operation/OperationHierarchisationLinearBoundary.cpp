9aa07522845ab2ab8c999240a9bf38c62d4da278
Alexander Heinecke <heinecke@in.tum.de>
2009-02-18 11:58:27 +0000

    added missing files
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@152 4eea3252-f0fb-4393-894d-40516dce545b

---
+++ src/sgpp/basis/linearboundary/operation/OperationHierarchisationLinearBoundary.cpp
@@ -0,0 +1,69 @@
+/*****************************************************************************/
+/* This file is part of sgpp, a program package making use of spatially      */
+/* adaptive sparse grids to solve numerical problems                         */
+/*                                                                           */
+/* Copyright (C) 2009 Alexander Heinecke (Alexander.Heinecke@mytum.de)       */
+/*                                                                           */
+/* sgpp is free software; you can redistribute it and/or modify              */
+/* it under the terms of the GNU General Public License as published by      */
+/* the Free Software Foundation; either version 3 of the License, or         */
+/* (at your option) any later version.                                       */
+/*                                                                           */
+/* sgpp is distributed in the hope that it will be useful,                   */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of            */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             */
+/* GNU General Public License for more details.                              */
+/*                                                                           */
+/* You should have received a copy of the GNU General Public License         */
+/* along with sgpp; if not, write to the Free Software                       */
+/* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+/* or see <http://www.gnu.org/licenses/>.                                    */
+/*****************************************************************************/
+
+#include "basis/linearboundary/operation/OperationHierarchisationLinearBoundary.hpp"
+#include "basis/linearboundary/algorithm_sweep/HierarchisationLinearBoundary.hpp"
+#include "basis/linearboundary/algorithm_sweep/DehierarchisationLinearBoundary.hpp"
+
+#include "sgpp.hpp"
+
+#include "basis/basis.hpp"
+#include "data/DataVector.h"
+
+namespace sg
+{
+
+/**
+ * Implements the hierarchisation on a sprase grid with linear base functions and boundaries
+ *
+ * @param node_values the functions values in the node base
+ */
+void OperationHierarchisationLinearBoundary::doHierarchisation(DataVector& node_values)
+{
+	detail::HierarchisationLinearBoundary func(this->storage);
+	sweep<detail::HierarchisationLinearBoundary> s(func, this->storage);
+
+	// Execute hierarchisation in every dimension of the grid
+	for (size_t i = 0; i < this->storage->dim(); i++)
+	{
+		s.sweep1D(node_values, node_values, i);
+	}
+}
+
+/**
+ * Implements the dehierarchisation on a sprase grid with linear base functions and boundaries
+ *
+ * @param alpha the coefficients of the sparse grid's base functions
+ */
+void OperationHierarchisationLinearBoundary::doDehierarchisation(DataVector& alpha)
+{
+	detail::DehierarchisationLinearBoundary func(this->storage);
+	sweep<detail::DehierarchisationLinearBoundary> s(func, this->storage);
+
+	// Execute hierarchisation in every dimension of the grid
+	for (size_t i = 0; i < this->storage->dim(); i++)
+	{
+		s.sweep1D(alpha, alpha, i);
+	}
+}
+
+}
---
+++ src/sgpp/basis/linearboundary/operation/OperationLaplaceLinearBoundary.hpp
@@ -0,0 +1,110 @@
+/*****************************************************************************/
+/* This file is part of sgpp, a program package making use of spatially      */
+/* adaptive sparse grids to solve numerical problems                         */
+/*                                                                           */
+/* Copyright (C) 2009 Alexander Heinecke (Alexander.Heinecke@mytum.de)       */
+/*                                                                           */
+/* sgpp is free software; you can redistribute it and/or modify              */
+/* it under the terms of the GNU General Public License as published by      */
+/* the Free Software Foundation; either version 3 of the License, or         */
+/* (at your option) any later version.                                       */
+/*                                                                           */
+/* sgpp is distributed in the hope that it will be useful,                   */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of            */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             */
+/* GNU General Public License for more details.                              */
+/*                                                                           */
+/* You should have received a copy of the GNU General Public License         */
+/* along with sgpp; if not, write to the Free Software                       */
+/* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+/* or see <http://www.gnu.org/licenses/>.                                    */
+/*****************************************************************************/
+
+#ifndef OPERATIONLAPLACELINEARBOUNDARY_HPP
+#define OPERATIONLAPLACELINEARBOUNDARY_HPP
+
+//#include "basis/linearboundary/algorithm_sweep/LaplaceDownLinear.hpp"
+//#include "basis/linearboundary/algorithm_sweep/LaplaceUpLinear.hpp"
+
+#include "operation/OperationMatrix.hpp"
+
+#include "algorithm/UnidirGradient.hpp"
+#include "algorithm/sweep.hpp"
+
+#include "grid/GridStorage.hpp"
+#include "data/DataVector.h"
+
+namespace sg
+{
+
+/**
+ * Implementation for linear functions
+ */
+class OperationLaplaceLinearBoundary: public OperationMatrix, public UnidirGradient
+{
+public:
+	OperationLaplaceLinearBoundary(GridStorage* storage) : UnidirGradient(storage)
+	{
+	}
+
+	virtual ~OperationLaplaceLinearBoundary() {}
+
+	/*
+	virtual void mult(DataVector& alpha, DataVector& result)
+	{
+		this->updown(alpha, result);
+	}
+
+protected:
+	virtual void gradient(DataVector& alpha, DataVector& result, size_t dim, size_t gradient_dim)
+	{
+		// In direction gradient_dim we only calculate the norm of the gradient
+		// The up-part is empty, thus omitted
+		if(dim > 0)
+		{
+			DataVector temp(alpha.getSize());
+			updown(alpha, temp, dim-1, gradient_dim);
+			downGradient(temp, result, gradient_dim);
+		}
+		else
+		{
+			// Terminates dimension recursion
+			downGradient(alpha, result, gradient_dim);
+		}
+	}
+
+	virtual void up(DataVector& alpha, DataVector& result, size_t dim)
+	{
+		detail::LaplaceUpLinear func(this->storage);
+		sweep<detail::LaplaceUpLinear> s(func, this->storage);
+		s.sweep1D(alpha, result, dim);
+	}
+
+	virtual void down(DataVector& alpha, DataVector& result, size_t dim)
+	{
+		detail::LaplaceDownLinear func(this->storage);
+		sweep<detail::LaplaceDownLinear> s(func, this->storage);
+		s.sweep1D(alpha, result, dim);
+	}
+
+	virtual void downGradient(DataVector& alpha, DataVector& result, size_t dim)
+	{
+		// traverse all basis function by sequence number
+		for(size_t i = 0; i < storage->size(); i++)
+		{
+			GridStorage::index_type::level_type level;
+			GridStorage::index_type::index_type index;
+			(*storage)[i]->get(dim, level, index);
+			//only affects the diagonal of the stiffness matrix
+			result[i] = alpha[i]*pow(2.0, level+1);
+		}
+	}
+
+	virtual void upGradient(DataVector& alpha, DataVector& result, size_t dim) {}
+	*/
+
+};
+
+}
+
+#endif /* OPERATIONLAPLACELINEAR_HPP */
