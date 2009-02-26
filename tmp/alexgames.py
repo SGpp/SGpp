21f5e74c498f8993ea1760dc2a06fd68d3f86030
Alexander Heinecke <heinecke@in.tum.de>
2009-02-26 17:59:14 +0000

    added some new test routines for grid refinement
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@164 4eea3252-f0fb-4393-894d-40516dce545b

--- tmp/alexgames.py
+++ tmp/alexgames.py
@@ -90,42 +90,121 @@ def buildTrainingVector(data):
     return training
 
 
+def refinement2d():
+    factory = Grid.createLinearBoundaryGrid(2)
+    storage = factory.getStorage()
+    
+    gen = factory.createGridGenerator()
+    gen.regular(1)
+    
+    alpha = DataVector(9)
+    alpha[0] = 0.0
+    alpha[1] = 0.0
+    alpha[2] = 0.0
+    alpha[3] = 0.0
+    alpha[4] = 0.0
+    alpha[5] = 0.0
+    alpha[6] = 0.0
+    alpha[7] = 0.0
+    alpha[8] = 1.0
+    func = SurplusRefinementFunctor(alpha)
+    
+    #for n in xrange(factory.getStorage().size()):
+    #    print factory.getStorage().get(n).getCoordinates()
+    
+    gen.refine(func)
+
+    for n in xrange(factory.getStorage().size()):
+        print factory.getStorage().get(n).getCoordinates()    
+
+
+def refinement3d():
+    factory = Grid.createLinearBoundaryGrid(3)
+    storage = factory.getStorage()
+    
+    gen = factory.createGridGenerator()
+    gen.regular(1)
+    
+    alpha = DataVector(27)
+    
+    for i in xrange(len(alpha)):
+        alpha[i] = 0.0
+
+    alpha[26] = 1.0
+    func = SurplusRefinementFunctor(alpha)
+    
+    #for n in xrange(factory.getStorage().size()):
+    #    print factory.getStorage().get(n).getCoordinates()
+        
+    gen.refine(func)
+
+    for n in xrange(factory.getStorage().size()):
+        print factory.getStorage().get(n).getCoordinates()
+        
+            
 # Alex is playing with python and sgpp
 #
 # This test routine creates a grid and evaluates a function
 # on this grid
 def run_test():
-    alpha = None
-    gridpoint = None
-    dim = 2
-    level = 3
-    res = 9
+    factory = Grid.createLinearBoundaryGrid(3)
+    storage = factory.getStorage()
     
-    grid = Grid.createLinearGrid(dim)
-    generator  = grid.createGridGenerator()
-    generator.regular(level)
+    gen = factory.createGridGenerator()
+    gen.regular(1)
+    
+    alpha = DataVector(27)
+    
+    for i in xrange(len(alpha)):
+        alpha[i] = 0.0
+
+    alpha[26] = 1.0
+    func = SurplusRefinementFunctor(alpha)
+    
+    #for n in xrange(factory.getStorage().size()):
+    #    print factory.getStorage().get(n).getCoordinates()
+        
+    gen.refine(func)
+
+    for n in xrange(factory.getStorage().size()):
+        print factory.getStorage().get(n).getCoordinates()
+        
+    #print factory.getStorage().size()
+    
+    #alpha = None
+    #gridpoint = None
+    #dim = 3
+    #level = 3
+    #res = 9
+    
+    #grid = Grid.createLinearBoundaryGrid(dim)
+    #generator = grid.createGridGenerator()
+    #generator.regularBoundaries(level)
     
     #print grid to console (base functions)
-    print grid.serialize()
-    print "Size of Grid = Number of Gridpoints is:"
-    print grid.getStorage().size()
+    #print grid.serialize()
+    #print "Size of Grid = Number of Gridpoints is:"
+    #print grid.getStorage().size()
+    
+    #for n in xrange(grid.getStorage().size()):
+    #    print grid.getStorage().get(n).getCoordinates()
     
     #evaluating parable on sparse grid
-    eval = grid.createOperationEval()
-    gridpoint = DataVector(1, dim)
+    #eval = grid.createOperationEval()
+    #gridpoint = DataVector(1, dim)
     
     # read the alphas
-    alpha = buildTrainingVector(readDataARFF("alexgames_alpha.in"))
+    #alpha = buildTrainingVector(readDataARFF("alexgames_alpha.in"))
     
-    fout = file("alexgames_eval.out", "w")
+    #fout = file("alexgames_eval.out", "w")
     
-    for x in xrange(res):
-        for y in xrange(res):
-            gridpoint[0] = float(x) / (res - 1)
-            gridpoint[1] = float(y) / (res - 1)
-            gp_value = eval.eval(alpha, gridpoint)
-            fout.write("%f %f %f\n" % (gridpoint[0], gridpoint[1], gp_value))
-    fout.close()
+    #for x in xrange(res):
+    #    for y in xrange(res):
+    #        gridpoint[0] = float(x) / (res - 1)
+    #        gridpoint[1] = float(y) / (res - 1)
+    #        gp_value = eval.eval(alpha, gridpoint)
+    #        fout.write("%f %f %f\n" % (gridpoint[0], gridpoint[1], gp_value))
+    #fout.close()
     return
       
 #===============================================================================
