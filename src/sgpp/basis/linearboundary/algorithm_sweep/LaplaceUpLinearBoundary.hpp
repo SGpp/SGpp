4208fe7f8d4ad414b8f03a73bafaf7ff40502bbd
Alexander Heinecke <heinecke@in.tum.de>
2009-03-16 23:24:29 +0000

    added comments about the requirements for the ansatzfunctions on level zero
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@208 4eea3252-f0fb-4393-894d-40516dce545b

--- src/sgpp/basis/linearboundary/algorithm_sweep/LaplaceUpLinearBoundary.hpp
+++ src/sgpp/basis/linearboundary/algorithm_sweep/LaplaceUpLinearBoundary.hpp
@@ -50,6 +50,19 @@ public:
 	{
 	}
 
+	/**
+	 * This operations performs the calculation of up in the direction of dimension <i>dim</i>
+	 *
+	 * For level zero it's assumed, that both ansatz-functions do exist: 0,0 and 0,1
+	 * If one is missing this code might produce some bad errors (segmentation fault, wrong calculation
+	 * result)
+	 * So please assure that both functions do exist!
+	 *
+	 * @param source DataVector that contains the gridpoint's coefficients (values from the vector of the laplace operation)
+	 * @param result DataVector that contains the result of the up operation
+	 * @param index a iterator object of the grid
+	 * @param dim current fixed dimension of the 'execution direction'
+	 */
 	void operator()(DataVector& source, DataVector& result, grid_iterator& index, size_t dim)
 	{
 		// get boundary values
