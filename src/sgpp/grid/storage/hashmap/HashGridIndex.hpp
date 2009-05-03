3c12223fe516dd8adaaf54020abc4baef03f01cc
Alexander Heinecke <heinecke@in.tum.de>
2009-05-03 20:16:10 +0000

    using std::pow
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@285 4eea3252-f0fb-4393-894d-40516dce545b

--- src/sgpp/grid/storage/hashmap/HashGridIndex.hpp
+++ src/sgpp/grid/storage/hashmap/HashGridIndex.hpp
@@ -431,7 +431,7 @@ public:
     		}
     		else
     		{
-    			return_stream << (pow(0.5, level[i])*index[i]);
+    			return_stream << (std::pow(0.5, level[i])*index[i]);
     		}
 
     		if (i < DIM-1)
