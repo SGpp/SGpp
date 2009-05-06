58f4455dcb8a4a725e1ece8a923c6f02d9d841e7
Alexander Heinecke <heinecke@in.tum.de>
2009-05-06 08:11:21 +0000

    update of Alex's test things
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@293 4eea3252-f0fb-4393-894d-40516dce545b

--- tmp/batchjob_gnuplot.sh
+++ tmp/batchjob_gnuplot.sh
@@ -2,9 +2,9 @@
 
 LEVELS="3 4 5 6 7 8"
 LAMBDAS="1 0.1 0.01 0.001 0.0001 0.00001 0.000001"
-GRIDS="complete-boundary uscaled-boundary no-boundary"
+GRIDS="complete-boundary uscaled-boundary no-boundary border"
 
-TESTDATA="twospirals.wieland.arff.gz"
+TESTDATA="../datasets/twospirals/twospirals.wieland.arff.gz"
 
 #generate gnuplot data
 for level in $LEVELS
@@ -19,17 +19,8 @@ do
   				"no-boundary") python classifier.py --data $TESTDATA --level $level --zeh laplace --mode normal --gnuplot $GNUFILE --resolution 200 --lambda $lambda -i 2000;;
   				"uscaled-boundary") python classifier.py --data $TESTDATA --level $level --zeh laplace --mode normal --gnuplot $GNUFILE --resolution 200 --lambda $lambda --$grid -i 2000;;
   				"complete-boundary") python classifier.py --data $TESTDATA --level $level --zeh laplace --mode normal --gnuplot $GNUFILE --resolution 200 --lambda $lambda --$grid -i 2000;;
+  				"border") python classifier.py --data $TESTDATA --level $level --zeh laplace --mode normal --gnuplot $GNUFILE --resolution 200 --lambda $lambda --$grid -i 2000;;
   			esac
 		done
 	done
-done
-
-#generate heatmaps with generated data
-for file in *.gnuplot
-do
-	cp $file temp.gnu
-	gnuplot print_gnuplot_heatmaps
-	cp temp.png ${file}.png
-	rm temp.gnu
-	rm temp.png
 done
--- tmp/batchjob_stats_foldr.sh
+++ tmp/batchjob_stats_foldr.sh
@@ -2,7 +2,7 @@
 
 LEVELS="4 5 6 7 8"
 LAMBDAS="0.1 0.01 0.001 0.0001 0.000001"
-GRIDS="complete-boundary uscaled-boundary no-boundary"
+GRIDS="complete-boundary uscaled-boundary no-boundary border"
 FOLDLEVEL="10"
 TESTDATA="twospirals.wieland.arff.gz"
 
@@ -18,6 +18,7 @@ do
   				"no-boundary") python classifier.py --data $TESTDATA --level $level --zeh laplace --mode foldr --foldlevel $FOLDLEVEL --lambda $lambda -i 2000 --stats $STATSFILE;;
   				"uscaled-boundary") python classifier.py --data $TESTDATA --level $level --zeh laplace --mode foldr --foldlevel $FOLDLEVEL --lambda $lambda --$grid -i 2000 --stats $STATSFILE;;
   				"complete-boundary") python classifier.py --data $TESTDATA --level $level --zeh laplace --mode foldr --foldlevel $FOLDLEVEL --lambda $lambda --$grid -i 2000 --stats $STATSFILE;;
+  				"border") python classifier.py --data $TESTDATA --level $level --zeh laplace --mode foldr --foldlevel $FOLDLEVEL --lambda $lambda --$grid -i 2000 --stats $STATSFILE;;
   			esac
 		done
 	done
