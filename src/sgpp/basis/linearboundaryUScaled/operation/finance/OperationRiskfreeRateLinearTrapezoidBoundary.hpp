993b89d6bbb318b2d12d587715157e5be80b3000
Alexander Heinecke <heinecke@in.tum.de>
2009-08-26 14:12:05 +0000

    implemented OperationRiskfreeRate on the LinearTrapezoidBoundary Grid
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@390 4eea3252-f0fb-4393-894d-40516dce545b

--- src/sgpp/basis/linearboundaryUScaled/operation/finance/OperationRiskfreeRateLinearTrapezoidBoundary.hpp
+++ src/sgpp/basis/linearboundaryUScaled/operation/finance/OperationRiskfreeRateLinearTrapezoidBoundary.hpp
@@ -52,12 +52,42 @@ public:
 
 	virtual void mult(DataVector& alpha, DataVector& result);
 
-protected:
+private:
 	typedef GridStorage::grid_iterator grid_iterator;
 
 	/// Pointer to the grid's storage object
 	GridStorage* storage;
 
+	/**
+	 * Recursive procedure for updown
+	 *
+	 * @param dim the current dimension
+	 * @param alpha vector of coefficients
+	 * @param result vector to store the results in
+	 */
+	void updown(DataVector& alpha, DataVector& result, size_t dim);
+
+	/**
+	 * Up-step in dimension <i>dim</i> for \f$(\phi_i(x),\phi_j(x))_{L_2}\f$.
+	 * Applies the up-part of the one-dimensional mass matrix in one dimension.
+	 * Computes \f[\int_{x=0}^1  \phi_i(x) \sum_{j, l_i < l_j} \alpha_j \phi_j(x) dx.\f]
+	 *
+	 * @param dim dimension in which to apply the up-part
+	 * @param alpha vector of coefficients
+	 * @param result vector to store the results in
+	 */
+	void up(DataVector& alpha, DataVector& result, size_t dim);
+
+	/**
+	 * Down-step in dimension <i>dim</i> for \f$(\phi_i(x),\phi_j(x))_{L_2}\f$.
+	 * Applies the down-part of the one-dimensional mass matrix in one dimension.
+	 * Computes \f[\int_{x=0}^1  \phi_i(x) \sum_{j, l_i\geq l_j} \alpha_j \phi_j(x) dx.\f]
+	 *
+	 * @param dim dimension in which to apply the down-part
+	 * @param alpha vector of coefficients
+	 * @param result vector to store the results in
+	 */
+	void down(DataVector& alpha, DataVector& result, size_t dim);
 };
 
 }
