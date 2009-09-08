49cc29c9001590b52b38ade6d60001596ff8baa8
Alexander Heinecke <heinecke@in.tum.de>
2009-09-08 14:27:35 +0000

    error correction in NativeCppBlackScholesSolver
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@419 4eea3252-f0fb-4393-894d-40516dce545b

--- src/sgpp/solver/euler/ExplicitEuler.cpp
+++ src/sgpp/solver/euler/ExplicitEuler.cpp
@@ -21,6 +21,7 @@
 /*****************************************************************************/
 
 #include "solver/euler/ExplicitEuler.hpp"
+#include "solver/cg/ConjugateGradients.hpp"
 
 namespace sg
 {
@@ -36,6 +37,8 @@ ExplicitEuler::~ExplicitEuler()
 
 void ExplicitEuler::solve(OperationMatrix& SystemMatrix, DataVector& alpha, bool verbose)
 {
+	// expliziter Euler
+	////////////////////
 	DataVector temp(alpha.getSize());
 
 	for (size_t i = 0; i < this->nMaxIterations; i++)
@@ -43,10 +46,71 @@ void ExplicitEuler::solve(OperationMatrix& SystemMatrix, DataVector& alpha, bool
 		temp.setAll(0.0);
 
 		SystemMatrix.mult(alpha, temp);
+
+		alpha.axpy(this->myEpsilon, temp);
+
+		//std::cout << temp.toString() << std::endl;
+		//std::cout << alpha.toString() << std::endl;
+		//std::cout << this->myEpsilon << std::endl;
+	}
+
+	// Heun ??
+	////////////////////
+	/*DataVector temp(alpha.getSize());
+	DataVector alphaEuler(alpha.getSize());
+	DataVector alphaHeun(alpha.getSize());
+
+	for (size_t i = 0; i < this->nMaxIterations; i++)
+	{
+		double r = alpha.get(1);
+		double l = alpha.get(0);
+		temp.setAll(0.0);
+		alphaEuler = alpha;
+		alphaHeun = alpha;
+
+		// Euler step:
+		SystemMatrix.mult(alphaEuler, temp);
+		alphaEuler.axpy(this->myEpsilon, temp);
+		alphaEuler.mult(static_cast<double>(i+1)*(this->myEpsilon));
+
+		alphaHeun.mult(static_cast<double>(i)*(this->myEpsilon));
+		alphaHeun.add(alphaEuler);
+		alphaHeun.mult(this->myEpsilon*0.5);
+
+		alpha.add(alphaHeun);
+
+		alpha.set(0,l);
+		alpha.set(1,r);
+
+		//std::cout << temp.toString() << std::endl;
+		//std::cout << alpha.toString() << std::endl;
+		//std::cout << this->myEpsilon << std::endl;
+	}*/
+
+	// Crank Nicholson???
+	//////////////////////
+	/*DataVector temp(alpha.getSize());
+	DataVector cgresult(alpha.getSize());
+	DataVector rhs(alpha.getSize());
+    ConjugateGradients myCG(this->nMaxIterations, this->myEpsilon);
+
+	for (size_t i = 0; i < this->nMaxIterations; i++)
+	{
+		temp.setAll(0.0);
+		rhs = alpha;
+
+		SystemMatrix.mult(alpha, temp);
 		//std::cout << temp.toString() << std::endl;
 		//std::cout << this->myEpsilon << std::endl;
-		alpha.axpy(this->myEpsilon, temp);
-	}
+		rhs.axpy(this->myEpsilon, temp);
+
+		alpha.setAll(0.0);
+		//std::cout << alpha.toString() << std::endl;
+		//std::cout << rhs.toString() << std::endl;
+
+	    // slove the system of linear equations, SystemMatrix is wrong Matrix ??!!!!!
+	    myCG.solve(SystemMatrix, alpha, rhs, false, true, -1.0);
+	}*/
 }
 
 }
