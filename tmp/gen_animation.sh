4f73199237953a1964083d9591748f80e4453bdd
Alexander Heinecke <heinecke@in.tum.de>
2009-09-20 16:54:12 +0000

    added script for mpeg generation
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@457 4eea3252-f0fb-4393-894d-40516dce545b

---
+++ tmp/gen_animation.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# generate all frames (jpeg) that are needed to build 
+# an animation
+for file in *.gnuplot
+do
+	cp $file solution.gnuplot
+	gnuplot gen_animation_frame.gplt
+	cp temp.jpg ${file}.jpg
+	rm solution.gnuplot
+	rm temp.jpg
+done
+# now build an mpeg movie form the jpeg files
