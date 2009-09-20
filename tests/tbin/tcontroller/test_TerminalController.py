d276f4c3b6b8aa5910067ad9cd0486b214cc9665
Valeriy Khakhutskyy <khakhutv@in.tum.de>
2009-09-20 16:11:44 +0000

    controllers test
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@455 4eea3252-f0fb-4393-894d-40516dce545b

---
+++ tests/tbin/tcontroller/test_TerminalController.py
@@ -0,0 +1,53 @@
+##############################################################################
+# This file is part of pysgpp, a program package making use of spatially    #
+# adaptive sparse grids to solve numerical problems                         #
+#                                                                           #
+# Copyright (C) 2009 Valeriy Khakhutskyy (khakhutv@in.tum.de)               #
+#                                                                           #
+# pysgpp is free software; you can redistribute it and/or modify            #
+# it under the terms of the GNU General Public License as published by      #
+# the Free Software Foundation; either version 3 of the License, or         #
+# (at your option) any later version.                                       #
+#                                                                           #
+# pysgpp is distributed in the hope that it will be useful,                 #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
+# GNU Lesser General Public License for more details.                       #
+#                                                                           #
+# You should have received a copy of the GNU General Public License         #
+# along with pysgpp; if not, write to the Free Software                     #
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA #
+# or see <http://www.gnu.org/licenses/>.                                    #
+#############################################################################
+
+import unittest
+
+#correct the syspath, so python looks for packages in the root directory of SGpp
+import sys, os
+pathname = os.path.dirname(__file__)
+pathlocal = os.path.abspath(pathname)
+if pathlocal not in sys.path: sys.path.append(pathlocal)
+pathsgpp = os.path.abspath(pathname) + '/../../..'
+if pathsgpp not in sys.path: sys.path.append(pathsgpp)
+
+from bin.controller.TerminalController import TerminalController
+
+import os
+from subprocess import *
+
+class TestTerminalController(unittest.TestCase):
+    
+    def testConstructObjectsFromFile(self,):
+        jobfile = 'testsettings.job'
+        
+        command = "python ../../../bin/controller/TerminalController.py --jobfile ./" + jobfile
+        [out,err] = Popen(command.split(" "), stdout = PIPE).communicate()
+        if err:
+            print err
+        if out:
+            print out
+            
+if __name__=="__main__":
+    unittest.main()
+        
+    
