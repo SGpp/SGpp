f26d36849b674ba5d78bf1f662a98472a1e38813
Alexander Heinecke <heinecke@in.tum.de>
2009-10-22 06:39:01 +0000

    added new test cases for Black Scholes
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@501 4eea3252-f0fb-4393-894d-40516dce545b

---
+++ tests/data/BlackScholes/test2D_zins.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+OMP_NUM_THREADS=8
+export OMP_NUM_THREADS
+./NativeCppBlackScholesSolver solveND 2 2 BStest2DMC.bound BStest2DMC_zins.stoch BStest2D.strike avgM 0.05 1.0 0.1 CrNic 16000 0.00001 > test2D_level_2_zins_8CPU.out
+./NativeCppBlackScholesSolver solveND 2 3 BStest2DMC.bound BStest2DMC_zins.stoch BStest2D.strike avgM 0.05 1.0 0.1 CrNic 16000 0.00001 > test2D_level_3_zins_8CPU.out
+./NativeCppBlackScholesSolver solveND 2 4 BStest2DMC.bound BStest2DMC_zins.stoch BStest2D.strike avgM 0.05 1.0 0.1 CrNic 16000 0.00001 > test2D_level_4_zins_8CPU.out
+./NativeCppBlackScholesSolver solveND 2 5 BStest2DMC.bound BStest2DMC_zins.stoch BStest2D.strike avgM 0.05 1.0 0.1 CrNic 16000 0.00001 > test2D_level_5_zins_8CPU.out
+./NativeCppBlackScholesSolver solveND 2 6 BStest2DMC.bound BStest2DMC_zins.stoch BStest2D.strike avgM 0.05 1.0 0.1 CrNic 16000 0.00001 > test2D_level_6_zins_8CPU.out
+./NativeCppBlackScholesSolver solveND 2 7 BStest2DMC.bound BStest2DMC_zins.stoch BStest2D.strike avgM 0.05 1.0 0.1 CrNic 16000 0.00001 > test2D_level_7_zins_8CPU.out
+./NativeCppBlackScholesSolver solveND 2 8 BStest2DMC.bound BStest2DMC_zins.stoch BStest2D.strike avgM 0.05 1.0 0.1 CrNic 16000 0.00001 > test2D_level_8_zins_8CPU.out
+./NativeCppBlackScholesSolver solveND 2 9 BStest2DMC.bound BStest2DMC_zins.stoch BStest2D.strike avgM 0.05 1.0 0.1 CrNic 16000 0.00001 > test2D_level_9_zins_8CPU.out
+./NativeCppBlackScholesSolver solveND 2 10 BStest2DMC.bound BStest2DMC_zins.stoch BStest2D.strike avgM 0.05 1.0 0.1 CrNic 16000 0.00001 > test2D_level_10_zins_8CPU.out
+OMP_NUM_THREADS=4
+export OMP_NUM_THREADS
+./NativeCppBlackScholesSolver solveND 2 2 BStest2DMC.bound BStest2DMC_zins.stoch BStest2D.strike avgM 0.05 1.0 0.1 CrNic 16000 0.00001 > test2D_level_2_zins_4CPU.out
+./NativeCppBlackScholesSolver solveND 2 3 BStest2DMC.bound BStest2DMC_zins.stoch BStest2D.strike avgM 0.05 1.0 0.1 CrNic 16000 0.00001 > test2D_level_3_zins_4CPU.out
+./NativeCppBlackScholesSolver solveND 2 4 BStest2DMC.bound BStest2DMC_zins.stoch BStest2D.strike avgM 0.05 1.0 0.1 CrNic 16000 0.00001 > test2D_level_4_zins_4CPU.out
+./NativeCppBlackScholesSolver solveND 2 5 BStest2DMC.bound BStest2DMC_zins.stoch BStest2D.strike avgM 0.05 1.0 0.1 CrNic 16000 0.00001 > test2D_level_5_zins_4CPU.out
+./NativeCppBlackScholesSolver solveND 2 6 BStest2DMC.bound BStest2DMC_zins.stoch BStest2D.strike avgM 0.05 1.0 0.1 CrNic 16000 0.00001 > test2D_level_6_zins_4CPU.out
+./NativeCppBlackScholesSolver solveND 2 7 BStest2DMC.bound BStest2DMC_zins.stoch BStest2D.strike avgM 0.05 1.0 0.1 CrNic 16000 0.00001 > test2D_level_7_zins_4CPU.out
+./NativeCppBlackScholesSolver solveND 2 8 BStest2DMC.bound BStest2DMC_zins.stoch BStest2D.strike avgM 0.05 1.0 0.1 CrNic 16000 0.00001 > test2D_level_8_zins_4CPU.out
+./NativeCppBlackScholesSolver solveND 2 9 BStest2DMC.bound BStest2DMC_zins.stoch BStest2D.strike avgM 0.05 1.0 0.1 CrNic 16000 0.00001 > test2D_level_9_zins_4CPU.out
+./NativeCppBlackScholesSolver solveND 2 10 BStest2DMC.bound BStest2DMC_zins.stoch BStest2D.strike avgM 0.05 1.0 0.1 CrNic 16000 0.00001 > test2D_level_10_zins_4CPU.out
+OMP_NUM_THREADS=2
+export OMP_NUM_THREADS
+./NativeCppBlackScholesSolver solveND 2 2 BStest2DMC.bound BStest2DMC_zins.stoch BStest2D.strike avgM 0.05 1.0 0.1 CrNic 16000 0.00001 > test2D_level_2_zins_2CPU.out
+./NativeCppBlackScholesSolver solveND 2 3 BStest2DMC.bound BStest2DMC_zins.stoch BStest2D.strike avgM 0.05 1.0 0.1 CrNic 16000 0.00001 > test2D_level_3_zins_2CPU.out
+./NativeCppBlackScholesSolver solveND 2 4 BStest2DMC.bound BStest2DMC_zins.stoch BStest2D.strike avgM 0.05 1.0 0.1 CrNic 16000 0.00001 > test2D_level_4_zins_2CPU.out
+./NativeCppBlackScholesSolver solveND 2 5 BStest2DMC.bound BStest2DMC_zins.stoch BStest2D.strike avgM 0.05 1.0 0.1 CrNic 16000 0.00001 > test2D_level_5_zins_2CPU.out
+./NativeCppBlackScholesSolver solveND 2 6 BStest2DMC.bound BStest2DMC_zins.stoch BStest2D.strike avgM 0.05 1.0 0.1 CrNic 16000 0.00001 > test2D_level_6_zins_2CPU.out
+./NativeCppBlackScholesSolver solveND 2 7 BStest2DMC.bound BStest2DMC_zins.stoch BStest2D.strike avgM 0.05 1.0 0.1 CrNic 16000 0.00001 > test2D_level_7_zins_2CPU.out
+./NativeCppBlackScholesSolver solveND 2 8 BStest2DMC.bound BStest2DMC_zins.stoch BStest2D.strike avgM 0.05 1.0 0.1 CrNic 16000 0.00001 > test2D_level_8_zins_2CPU.out
+./NativeCppBlackScholesSolver solveND 2 9 BStest2DMC.bound BStest2DMC_zins.stoch BStest2D.strike avgM 0.05 1.0 0.1 CrNic 16000 0.00001 > test2D_level_9_zins_2CPU.out
+./NativeCppBlackScholesSolver solveND 2 10 BStest2DMC.bound BStest2DMC_zins.stoch BStest2D.strike avgM 0.05 1.0 0.1 CrNic 16000 0.00001 > test2D_level_10_zins_2CPU.out
+OMP_NUM_THREADS=1
+export OMP_NUM_THREADS
+./NativeCppBlackScholesSolver solveND 2 2 BStest2DMC.bound BStest2DMC_zins.stoch BStest2D.strike avgM 0.05 1.0 0.1 CrNic 16000 0.00001 > test2D_level_2_zins_1CPU.out
+./NativeCppBlackScholesSolver solveND 2 3 BStest2DMC.bound BStest2DMC_zins.stoch BStest2D.strike avgM 0.05 1.0 0.1 CrNic 16000 0.00001 > test2D_level_3_zins_1CPU.out
+./NativeCppBlackScholesSolver solveND 2 4 BStest2DMC.bound BStest2DMC_zins.stoch BStest2D.strike avgM 0.05 1.0 0.1 CrNic 16000 0.00001 > test2D_level_4_zins_1CPU.out
+./NativeCppBlackScholesSolver solveND 2 5 BStest2DMC.bound BStest2DMC_zins.stoch BStest2D.strike avgM 0.05 1.0 0.1 CrNic 16000 0.00001 > test2D_level_5_zins_1CPU.out
+./NativeCppBlackScholesSolver solveND 2 6 BStest2DMC.bound BStest2DMC_zins.stoch BStest2D.strike avgM 0.05 1.0 0.1 CrNic 16000 0.00001 > test2D_level_6_zins_1CPU.out
+./NativeCppBlackScholesSolver solveND 2 7 BStest2DMC.bound BStest2DMC_zins.stoch BStest2D.strike avgM 0.05 1.0 0.1 CrNic 16000 0.00001 > test2D_level_7_zins_1CPU.out
+./NativeCppBlackScholesSolver solveND 2 8 BStest2DMC.bound BStest2DMC_zins.stoch BStest2D.strike avgM 0.05 1.0 0.1 CrNic 16000 0.00001 > test2D_level_8_zins_1CPU.out
+./NativeCppBlackScholesSolver solveND 2 9 BStest2DMC.bound BStest2DMC_zins.stoch BStest2D.strike avgM 0.05 1.0 0.1 CrNic 16000 0.00001 > test2D_level_9_zins_1CPU.out
+./NativeCppBlackScholesSolver solveND 2 10 BStest2DMC.bound BStest2DMC_zins.stoch BStest2D.strike avgM 0.05 1.0 0.1 CrNic 16000 0.00001 > test2D_level_10_zins_1CPU.out
