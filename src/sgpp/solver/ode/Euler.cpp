508089c72bd2cac396e7020597e095f87bec0570
Alexander Heinecke <heinecke@in.tum.de>
2009-11-24 21:07:47 +0000

    version labeling: NativeBlackScholesSolver 1.0.0 RC4
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@526 4eea3252-f0fb-4393-894d-40516dce545b

--- src/sgpp/solver/ode/Euler.cpp
+++ src/sgpp/solver/ode/Euler.cpp
@@ -84,13 +84,13 @@ void Euler::solve(OperationODESolverMatrix& SystemMatrix, DataVector& alpha, boo
 	    {
 	    	if (myScreen == NULL)
 	    	{
-	    		std::cout << "Final residuum " << myCG.residuum << "; with " << myCG.getNumberIterations() << " Iterations (Sum Iterations: " << allIter << ")" << std::endl;
+	    		std::cout << "Final residuum " << myCG.residuum << "; with " << myCG.getNumberIterations() << " Iterations (Total Iter.: " << allIter << ")" << std::endl;
 	    	}
 	    }
 	    if (myScreen != NULL)
     	{
     		std::stringstream soutput;
-    		soutput << "Final residuum " << myCG.residuum << "; with " << myCG.getNumberIterations() << " Iterations (Sum Iterations: " << allIter << ")";
+    		soutput << "Final residuum " << myCG.residuum << "; with " << myCG.getNumberIterations() << " Iterations (Total Iter.: " << allIter << ")";
 
     		if (i < this->nMaxIterations-1)
     		{
