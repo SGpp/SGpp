4046a37a0e34395e4b968d9be0a099d97b2460e1
Alexander Heinecke <heinecke@in.tum.de>
2010-03-15 10:25:34 +0000

    first step in up down refactoring
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@599 4eea3252-f0fb-4393-894d-40516dce545b

---
+++ src/sgpp/algorithm/pde/StdUpDown.cpp
@@ -0,0 +1,146 @@
+/*****************************************************************************/
+/* This file is part of sgpp, a program package making use of spatially      */
+/* adaptive sparse grids to solve numerical problems                         */
+/*                                                                           */
+/* Copyright (C) 2010 Alexander Heinecke (Alexander.Heinecke@mytum.de)       */
+/*                                                                           */
+/* sgpp is free software; you can redistribute it and/or modify              */
+/* it under the terms of the GNU Lesser General Public License as published  */
+/* by the Free Software Foundation; either version 3 of the License, or      */
+/* (at your option) any later version.                                       */
+/*                                                                           */
+/* sgpp is distributed in the hope that it will be useful,                   */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of            */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             */
+/* GNU Lesser General Public License for more details.                       */
+/*                                                                           */
+/* You should have received a copy of the GNU Lesser General Public License  */
+/* along with sgpp; if not, write to the Free Software                       */
+/* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+/* or see <http://www.gnu.org/licenses/>.                                    */
+/*****************************************************************************/
+
+#include "algorithm/pde/StdUpDown.hpp"
+
+namespace sg
+{
+
+StdUpDown::StdUpDown(GridStorage* storage)
+{
+	this->storage = storage;
+}
+
+StdUpDown::~StdUpDown()
+{
+}
+
+void StdUpDown::mult(DataVector& alpha, DataVector& result)
+{
+	DataVector beta(result.getSize());
+	result.setAll(0.0);
+#ifdef USEOMP
+#ifdef USEOMPTHREE
+	#pragma omp parallel
+	{
+#ifndef AIX_XLC
+		#pragma omp single nowait
+#endif
+#ifdef AIX_XLC
+		#pragma omp single
+#endif
+		{
+			this->updown_parallel(alpha, beta, storage->dim() - 1);
+		}
+	}
+	result.add(beta);
+#endif
+#ifndef USEOMPTHREE
+	this->updown(alpha, beta, storage->dim() - 1);
+
+	result.add(beta);
+#endif
+#endif
+#ifndef USEOMP
+	this->updown(alpha, beta, storage->dim() - 1);
+
+	result.add(beta);
+#endif
+}
+
+#ifndef USEOMPTHREE
+void StdUpDown::updown(DataVector& alpha, DataVector& result, size_t dim)
+{
+	//Unidirectional scheme
+	if(dim > 0)
+	{
+		// Reordering ups and downs
+		DataVector temp(alpha.getSize());
+		up(alpha, temp, dim);
+		updown(temp, result, dim-1);
+
+		DataVector result_temp(alpha.getSize());
+		updown(alpha, temp, dim-1);
+		down(temp, result_temp, dim);
+
+		result.add(result_temp);
+	}
+	else
+	{
+		// Terminates dimension recursion
+		up(alpha, result, dim);
+
+		DataVector temp(alpha.getSize());
+		down(alpha, temp, dim);
+
+		result.add(temp);
+	}
+}
+#endif
+
+#ifdef USEOMPTHREE
+void StdUpDown::updown_parallel(DataVector& alpha, DataVector& result, size_t dim)
+{
+	//Unidirectional scheme
+	if(dim > 0)
+	{
+		// Reordering ups and downs
+		DataVector temp(alpha.getSize());
+		DataVector result_temp(alpha.getSize());
+		DataVector temp_two(alpha.getSize());
+
+		#pragma omp task shared(alpha, temp, result)
+		{
+			up(alpha, temp, dim);
+			updown_parallel(temp, result, dim-1);
+		}
+
+
+		#pragma omp task shared(alpha, temp_two, result_temp)
+		{
+			updown_parallel(alpha, temp_two, dim-1);
+			down(temp_two, result_temp, dim);
+		}
+
+		#pragma omp taskwait
+
+		result.add(result_temp);
+	}
+	else
+	{
+		// Terminates dimension recursion
+		DataVector temp(alpha.getSize());
+
+		#pragma omp task shared(alpha, result)
+		up(alpha, result, dim);
+
+		#pragma omp task shared(alpha, temp)
+		down(alpha, temp, dim);
+
+		#pragma omp taskwait
+
+		result.add(temp);
+	}
+}
+#endif
+
+}
--- src/sgpp/algorithm/pde/StdUpDown.hpp
+++ src/sgpp/algorithm/pde/StdUpDown.hpp
@@ -2,7 +2,7 @@
 /* This file is part of sgpp, a program package making use of spatially      */
 /* adaptive sparse grids to solve numerical problems                         */
 /*                                                                           */
-/* Copyright (C) 2009-2010 Alexander Heinecke (Alexander.Heinecke@mytum.de)  */
+/* Copyright (C) 2010 Alexander Heinecke (Alexander.Heinecke@mytum.de)       */
 /*                                                                           */
 /* sgpp is free software; you can redistribute it and/or modify              */
 /* it under the terms of the GNU Lesser General Public License as published  */
@@ -20,20 +20,28 @@
 /* or see <http://www.gnu.org/licenses/>.                                    */
 /*****************************************************************************/
 
-#ifndef OPERATIONLTWODOTPRODUCTLINEAR_HPP
-#define OPERATIONLTWODOTPRODUCTLINEAR_HPP
+#ifndef STDUPDOWN_HPP
+#define STDUPDOWN_HPP
 
-#include "algorithm/pde/StdUpDown.hpp"
+#include "grid/GridStorage.hpp"
+
+#include "operation/common/OperationMatrix.hpp"
+
+#include "data/DataVector.hpp"
+
+#ifdef USEOMPTHREE
+#include <omp.h>
+#endif
 
 namespace sg
 {
 
 /**
- * Implements the standard L 2 scalar product on linear grids (no boundaries)
+ * Implements a standard Up/Down Schema without any operation dim.
  *
  * @version $HEAD$
  */
-class OperationLTwoDotProductLinear: public StdUpDown
+class StdUpDown: public OperationMatrix
 {
 public:
 	/**
@@ -41,37 +49,63 @@ public:
 	 *
 	 * @param storage the grid's GridStorage object
 	 */
-	OperationLTwoDotProductLinear(GridStorage* storage);
+	StdUpDown(GridStorage* storage);
 
 	/**
 	 * Destructor
 	 */
-	virtual ~OperationLTwoDotProductLinear();
+	virtual ~StdUpDown();
+
+
+	virtual void mult(DataVector& alpha, DataVector& result);
 
 protected:
+	typedef GridStorage::grid_iterator grid_iterator;
+
+	/// Pointer to the grid's storage object
+	GridStorage* storage;
+
+#ifndef USEOMPTHREE
 	/**
-	 * Up-step in dimension <i>dim</i> for \f$(\phi_i(x),\phi_j(x))_{L_2}\f$.
-	 * Applies the up-part of the one-dimensional mass matrix in one dimension.
-	 * Computes \f[\int_{x=0}^1  \phi_i(x) \sum_{j, l_i < l_j} \alpha_j \phi_j(x) dx.\f]
+	 * Recursive procedure for updown
+	 *
+	 * @param dim the current dimension
+	 * @param alpha vector of coefficients
+	 * @param result vector to store the results in
+	 */
+	void updown(DataVector& alpha, DataVector& result, size_t dim);
+#endif
+
+#ifdef USEOMPTHREE
+	/**
+	 * Recursive procedure for updown, parallel version using OpenMP 3
+	 *
+	 * @param dim the current dimension
+	 * @param alpha vector of coefficients
+	 * @param result vector to store the results in
+	 */
+	void updown_parallel(DataVector& alpha, DataVector& result, size_t dim);
+#endif
+
+	/**
+	 * 1D up Operation
 	 *
 	 * @param dim dimension in which to apply the up-part
 	 * @param alpha vector of coefficients
 	 * @param result vector to store the results in
 	 */
-	virtual void up(DataVector& alpha, DataVector& result, size_t dim);
+	virtual void up(DataVector& alpha, DataVector& result, size_t dim) = 0;
 
 	/**
-	 * Down-step in dimension <i>dim</i> for \f$(\phi_i(x),\phi_j(x))_{L_2}\f$.
-	 * Applies the down-part of the one-dimensional mass matrix in one dimension.
-	 * Computes \f[\int_{x=0}^1  \phi_i(x) \sum_{j, l_i\geq l_j} \alpha_j \phi_j(x) dx.\f]
+	 * 1D down Operation
 	 *
 	 * @param dim dimension in which to apply the down-part
 	 * @param alpha vector of coefficients
 	 * @param result vector to store the results in
 	 */
-	virtual void down(DataVector& alpha, DataVector& result, size_t dim);
+	virtual void down(DataVector& alpha, DataVector& result, size_t dim) = 0;
 };
 
 }
 
-#endif /* OPERATIONLTWODOTPRODUCTLINEAR_HPP */
+#endif /* STDUPDOWN_HPP */
