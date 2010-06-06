b60557cbe5fe6cbc71871f5541fb4424f948611b
Dirk Pflueger <dirk.pflueger@ipvs.uni-stuttgart.de>
2010-06-06 19:18:58 +0000

    Update Copyright bei tests
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/branches/pre-release2@700 4eea3252-f0fb-4393-894d-40516dce545b

--- tests/test_pysgpp.py
+++ tests/test_pysgpp.py
@@ -1,25 +1,9 @@
-#############################################################################
-# This file is part of pysgpp, a program package making use of spatially    #
-# adaptive sparse grids to solve numerical problems                         #
-#                                                                           #
-# Copyright (C) 2007 Joerg Blank (blankj@in.tum.de)                         #
-# Copyright (C) 2009 Alexander Heinecke (Alexander.Heinecke@mytum.de)       #
-#                                                                           #
-# pysgpp is free software; you can redistribute it and/or modify            #
-# it under the terms of the GNU Lesser General Public License as published  #
-# by the Free Software Foundation; either version 3 of the License, or      #
-# (at your option) any later version.                                       #
-#                                                                           #
-# pysgpp is distributed in the hope that it will be useful,                 #
-# but WITHOUT ANY WARRANTY; without even the implied warranty of            #
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
-# GNU Lesser General Public License for more details.                       #
-#                                                                           #
-# You should have received a copy of the GNU Lesser General Public License  #
-# along with pysgpp; if not, write to the Free Software                     #
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA #
-# or see <http://www.gnu.org/licenses/>.                                    #
-#############################################################################
+###############################################################################
+# Copyright (C) 2009 Technische Universitaet Muenchen                         #
+# This file is part of the SG++ project. For conditions of distribution and   #
+# use, please see the copyright notice at http://www5.in.tum.de/SGpp          #
+###############################################################################
+## @author Joerg Blank (blankj@in.tum.de), Alexander Heinecke (Alexander.Heinecke@mytum.de)####################################################################
 
 import unittest, sys, toolsKbhitCountdown
 
