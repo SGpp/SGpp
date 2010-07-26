c4b2828c2dc65ffa48d37f383d5faa0c39d7fc2a
Alexander Heinecke <heinecke@in.tum.de>
2010-07-26 13:14:22 +0000

    added simple iterative evaluation algorithm
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@749 4eea3252-f0fb-4393-894d-40516dce545b

--- src/sgpp/basis/linear/noboundary/linear_base.hpp
+++ src/sgpp/basis/linear/noboundary/linear_base.hpp
@@ -9,6 +9,7 @@
 #define LINEAR_BASE_HPP
 
 #include <cmath>
+#include <algorithm>
 
 namespace sg
 {
@@ -28,6 +29,18 @@ public:
 	double eval(LT level, IT index, double p)
 	{
 		return 1.0 - fabs((1<<level) * p - index);
+	}
+
+	/**
+	 * Evaluate a base functions.
+	 * Has a dependence on the absolute position of grid point and support.
+	 *
+	 * This version catches errors, that occur if a basis function
+	 * is evaluated outside its domain
+	 */
+	double evalSave(LT level, IT index, double p)
+	{
+		return std::max(1.0 - fabs((1<<level) * p - index), 0.0);
 	}
 };
 
