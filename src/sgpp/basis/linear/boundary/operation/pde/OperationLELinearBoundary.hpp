5800be455fa5beda2e239df09b515241cac7e61a
qic <qic>
2010-08-10 14:43:03 +0000

    add for the matrix E for hull white model
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@817 4eea3252-f0fb-4393-894d-40516dce545b

---
+++ src/sgpp/basis/linear/boundary/operation/pde/OperationLELinearBoundary.hpp
@@ -0,0 +1,62 @@
+/******************************************************************************
+* Copyright (C) 2009 Technische Universitaet Muenchen                         *
+* This file is part of the SG++ project. For conditions of distribution and   *
+* use, please see the copyright notice at http://www5.in.tum.de/SGpp          *
+******************************************************************************/
+// @author Alexander Heinecke (Alexander.Heinecke@mytum.de)
+
+#ifndef OPERATIONLELINEARBOUNDARY_HPP
+#define OPERATIONLELINEARBOUNDARY_HPP
+
+#include "algorithm/pde/StdUpDown.hpp"
+
+namespace sg
+{
+
+/**
+ * Implements the standard L 2 scalar product on linear boundary grids
+ *
+ * @version $HEAD$
+ */
+class OperationLELinearBoundary: public StdUpDown
+{
+public:
+	/**
+	 * Constructor
+	 *
+	 * @param storage the grid's GridStorage object
+	 */
+	OperationLELinearBoundary(GridStorage* storage);
+
+	/**
+	 * Destructor
+	 */
+	virtual ~OperationLELinearBoundary();
+
+protected:
+	/**
+	 * Up-step in dimension <i>dim</i> for \f$(\phi_i(x),\phi_j(x))_{L_2}\f$.
+	 * Applies the up-part of the one-dimensional mass matrix in one dimension.
+	 * Computes \f[\int_{x=0}^1  \phi_i(x) \sum_{j, l_i < l_j} \alpha_j \phi_j(x) dx.\f]
+	 *
+	 * @param dim dimension in which to apply the up-part
+	 * @param alpha vector of coefficients
+	 * @param result vector to store the results in
+	 */
+	virtual void up(DataVector& alpha, DataVector& result, size_t dim);
+
+	/**
+	 * Down-step in dimension <i>dim</i> for \f$(\phi_i(x),\phi_j(x))_{L_2}\f$.
+	 * Applies the down-part of the one-dimensional mass matrix in one dimension.
+	 * Computes \f[\int_{x=0}^1  \phi_i(x) \sum_{j, l_i\geq l_j} \alpha_j \phi_j(x) dx.\f]
+	 *
+	 * @param dim dimension in which to apply the down-part
+	 * @param alpha vector of coefficients
+	 * @param result vector to store the results in
+	 */
+	virtual void down(DataVector& alpha, DataVector& result, size_t dim);
+};
+
+}
+
+#endif /* OPERATIONLTWODOTPRODUCTLINEARBOUNDARY_HPP */
