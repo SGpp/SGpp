412cf184715a1fda0047b679203c626dc8cca339
qic <qic>
2010-08-10 14:45:08 +0000

    add for the matrix D for hull white model
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@822 4eea3252-f0fb-4393-894d-40516dce545b

---
+++ src/sgpp/basis/linear/noboundary/operation/pde/OperationLDLinear.cpp
@@ -0,0 +1,44 @@
+/******************************************************************************
+* Copyright (C) 2009 Technische Universitaet Muenchen                         *
+* This file is part of the SG++ project. For conditions of distribution and   *
+* use, please see the copyright notice at http://www5.in.tum.de/SGpp          *
+******************************************************************************/
+// @author Alexander Heinecke (Alexander.Heinecke@mytum.de)
+
+#include "basis/linear/noboundary/operation/pde/OperationLDLinear.hpp"
+
+#include "basis/linear/noboundary/algorithm_sweep/XPhiPhiDownBBLinear.hpp"
+#include "basis/linear/noboundary/algorithm_sweep/XPhiPhiUpBBLinear.hpp"
+
+#include "algorithm/common/sweep.hpp"
+
+namespace sg
+{
+
+OperationLDLinear::OperationLDLinear(GridStorage* storage) : StdUpDown(storage)
+{
+}
+
+OperationLDLinear::~OperationLDLinear()
+{
+}
+
+void OperationLDLinear::up(DataVector& alpha, DataVector& result, size_t dim)
+{
+	// X * phi * phi
+	detail::XPhiPhiUpBBLinear func(this->storage);
+	sweep<detail::XPhiPhiUpBBLinear> s(func, this->storage);
+
+	s.sweep1D(alpha, result, dim);
+}
+
+void OperationLDLinear::down(DataVector& alpha, DataVector& result, size_t dim)
+{
+	// X * phi * phi
+	detail::XPhiPhiDownBBLinear func(this->storage);
+	sweep<detail::XPhiPhiDownBBLinear> s(func, this->storage);
+
+	s.sweep1D(alpha, result, dim);
+}
+
+}
