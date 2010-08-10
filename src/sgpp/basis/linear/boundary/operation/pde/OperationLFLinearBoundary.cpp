324c9f3e83412b8027948547747794f6ff54a092
qic <qic>
2010-08-10 14:43:14 +0000

    add for the matrix F for hull white model
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@818 4eea3252-f0fb-4393-894d-40516dce545b

---
+++ src/sgpp/basis/linear/boundary/operation/pde/OperationLFLinearBoundary.cpp
@@ -0,0 +1,44 @@
+/******************************************************************************
+* Copyright (C) 2009 Technische Universitaet Muenchen                         *
+* This file is part of the SG++ project. For conditions of distribution and   *
+* use, please see the copyright notice at http://www5.in.tum.de/SGpp          *
+******************************************************************************/
+// @author Alexander Heinecke (Alexander.Heinecke@mytum.de)
+
+#include "basis/linear/boundary/operation/pde/OperationLFLinearBoundary.hpp"
+
+#include "basis/linear/boundary/algorithm_sweep/XdPhiPhiDownBBLinearBoundary.hpp"
+#include "basis/linear/boundary/algorithm_sweep/XdPhiPhiUpBBLinearBoundary.hpp"
+
+#include "algorithm/common/sweep.hpp"
+
+namespace sg
+{
+
+OperationLFLinearBoundary::OperationLFLinearBoundary(GridStorage* storage) : StdUpDown(storage)
+{
+}
+
+OperationLFLinearBoundary::~OperationLFLinearBoundary()
+{
+}
+
+void OperationLFLinearBoundary::up(DataVector& alpha, DataVector& result, size_t dim)
+{
+	// X * dphi * phi
+	detail::XdPhiPhiUpBBLinearBoundary func(this->storage);
+	sweep<detail::XdPhiPhiUpBBLinearBoundary> s(func, this->storage);
+
+	s.sweep1D_Boundary(alpha, result, dim);
+}
+
+void OperationLFLinearBoundary::down(DataVector& alpha, DataVector& result, size_t dim)
+{
+	// X * dphi * phi
+	detail::XdPhiPhiDownBBLinearBoundary func(this->storage);
+	sweep<detail::XdPhiPhiDownBBLinearBoundary> s(func, this->storage);
+
+	s.sweep1D_Boundary(alpha, result, dim);
+}
+
+}
