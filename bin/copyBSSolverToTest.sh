bf5ca2ee484080a04842a4ad9b46319d19a31728
Alexander Heinecke <heinecke@in.tum.de>
2010-09-09 20:05:16 +0000

    added script to copy current BSSolve into right test dir
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@944 4eea3252-f0fb-4393-894d-40516dce545b

---
+++ bin/copyBSSolverToTest.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cp -rdfv BSSolver ./../tests/BlackScholes_TestData/1d/cart/
+cp -rdfv BSSolver ./../tests/BlackScholes_TestData/1d/log/
+cp -rdfv BSSolver ./../tests/BlackScholes_TestData/2d/cart/
+cp -rdfv BSSolver ./../tests/BlackScholes_TestData/2d/log/
+cp -rdfv BSSolver ./../tests/BlackScholes_TestData/3d/cart/
+cp -rdfv BSSolver ./../tests/BlackScholes_TestData/3d/log/
+cp -rdfv BSSolver ./../tests/BlackScholes_TestData/4d/cart/
+cp -rdfv BSSolver ./../tests/BlackScholes_TestData/4d/log/
+cp -rdfv BSSolver ./../tests/BlackScholes_TestData/5d/cart/
+cp -rdfv BSSolver ./../tests/BlackScholes_TestData/5d/log/
