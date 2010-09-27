e14bff1c60d789dcd181443210e627dbbdd85a41
nagy <nagy>
2010-09-27 09:35:13 +0000

    Combi removed
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@980 4eea3252-f0fb-4393-894d-40516dce545b

---
+++ src/sgpp/grid/type/SquareRootGrid.cpp
@@ -0,0 +1,73 @@
+/*
+ * SquareRootGrid.cpp
+ *
+ *  Created on: Aug 4, 2010
+ *      Author: Aliz Nagy
+ */
+
+/******************************************************************************
+* Copyright (C) 2009 Technische Universitaet Muenchen                         *
+* This file is part of the SG++ project. For conditions of distribution and   *
+* use, please see the copyright notice at http://www5.in.tum.de/SGpp          *
+******************************************************************************/
+// @author Alexander Heinecke (Alexander.Heinecke@mytum.de)
+
+#include "grid/Grid.hpp"
+#include "grid/type/SquareRootGrid.hpp"
+
+#include "grid/generation/SquareRootGridGenerator.hpp"
+#include "basis/linear/boundary/operation/common/OperationEvalLinearBoundary.hpp"
+#include "basis/linear/boundary/operation/common/OperationHierarchisationLinearBoundary.hpp"
+#include "sgpp.hpp"
+
+#include <iostream>
+
+namespace sg
+{
+
+SquareRootGrid::SquareRootGrid(std::istream& istr) : Grid(istr)
+{
+
+}
+
+SquareRootGrid::SquareRootGrid(size_t dim)
+{
+	this->storage = new GridStorage(dim);
+}
+
+SquareRootGrid::SquareRootGrid(BoundingBox& BB)
+{
+	this->storage = new GridStorage(BB);
+}
+
+SquareRootGrid::~SquareRootGrid()
+{
+}
+
+const char* SquareRootGrid::getType()
+{
+	return "squareRoot";
+}
+Grid* SquareRootGrid::unserialize(std::istream& istr)
+{
+	return new SquareRootGrid(istr);
+}
+/**
+ * Creates new GridGenerator
+ * This must be changed if we add other storage types
+ */
+GridGenerator* SquareRootGrid::createGridGenerator()
+{
+	return new SquareRootGridGenerator(this->storage);
+}
+OperationHierarchisation* SquareRootGrid::createOperationHierarchisation()
+{
+	return new OperationHierarchisationLinearBoundary(this->storage);
+}
+OperationEval* SquareRootGrid::createOperationEval()
+{
+	return new OperationEvalLinearBoundary(this->storage);
+}
+
+}
+
