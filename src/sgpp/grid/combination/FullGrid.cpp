2aa4a3f05ad303bb389b7eac3c310e4a5bb91c38
benk <benk>
2011-01-07 10:25:19 +0000

    small fix in the Combi Technique / Full Grid
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@1119 4eea3252-f0fb-4393-894d-40516dce545b

--- src/sgpp/grid/combination/FullGrid.cpp
+++ src/sgpp/grid/combination/FullGrid.cpp
@@ -3,15 +3,18 @@
 using namespace std;
 namespace sg{
 
+
     FullGrid* FullGrid::createLinearFullGrid(size_t dim, vector<level_t> *inlevel)
     {
         return new LinearFullGrid(dim,inlevel);
     }
 
+
     FullGrid* FullGrid::createLinearBoundaryFullGrid(size_t dim, vector<level_t> *inlevel)
     {
         return new FullGrid(dim,inlevel);
     }
+
 
    void FullGrid::getCoords(size_t index,DataVector &v)
     {
@@ -37,6 +40,7 @@ namespace sg{
         }
     }
 
+
    std::string FullGrid::getCoordsString(size_t index)
     {
     	std::stringstream return_stream;
@@ -57,4 +61,63 @@ namespace sg{
 
     	return return_stream.str();
     }
+
+
+   double FullGrid::eval(DataVector& p)
+   {
+  	 int ii,i,tmp_val,vv,nr;
+  	 int jj;
+  	 double dist,baseVal,ofs,len;
+  	 double normcoord;
+  	 /**If the boundingBox of the fullgrid is null we consider the default [0,1]^d hypercube*/
+  	 if (boundingBox!=0){
+  		 for ( ii = dim-1 ; ii >=0; ii--){
+  			 ofs=boundingBox->getIntervalOffset(ii);
+				 len=boundingBox->getIntervalWidth(ii);
+  		     // scale to the reference [0,1] interval the intersection point
+  		     normcoord=(p[ii]-ofs)*powOfTwo[level[ii]]/len;
+  		     aindex[ii] = floor(normcoord);
+  		     aindex[ii] = (aindex[ii] < 0) ? 0 : aindex[ii];
+			 aindex[ii] = (aindex[ii] >= (int)(this->length(ii) - 1)) ? (int)(this->length(ii)-2) : aindex[ii];
+  		     dist = aindex[ii]+1-normcoord;
+  		     intersect[2*ii] = dist;
+  		     intersect[2*ii+1] = 1- dist;
+  		  }
+  	 }
+  	 else
+  	 {
+			 for ( ii = dim-1 ; ii >=0; ii--){
+			     // scale to the reference [0,1] interval the intersection point
+				 normcoord=p[ii]*powOfTwo[level[ii]];
+				 aindex[ii]=floor(normcoord);
+				 aindex[ii] = (aindex[ii] < 0) ? 0 : aindex[ii];
+				 aindex[ii] = (aindex[ii] >= (int)(this->length(ii) - 1)) ? (int)(this->length(ii)-2) : aindex[ii];
+				 dist = aindex[ii]+1-normcoord;
+				 intersect[2*ii] = dist;
+				 intersect[2*ii+1] = 1 - dist;
+			 }
+  	 }
+  	 value = 0.0;
+  	 nr=powOfTwo[dim];
+  	 for (ii=0; ii < nr; ii++){
+  	        baseVal = 1;//we will store here the coefficient for the next corner point of the cuboid
+  	        i = 0;
+  	        tmp_val = ii;
+  	        vv = 0;
+  	        // compute the "dim" dimensional basis function value(for one node) and the corresponding vector index
+  	        for (jj = dim-1 ; jj >=0; jj--){
+  	              vv = (tmp_val & 1); // tmp % 2 ;
+  	              baseVal = baseVal * intersect[2*jj+vv];
+  	              i = i*this->length(jj)+aindex[jj]+vv;
+  	              tmp_val = tmp_val >> 1; //tmp_val / 2;
+  	              //std::cout << "FullGrid::eval jj:" << jj << " , this->length(jj):" << this->length(jj) << " , i:" << i
+  	            //		  << ", aindex[jj]:" << aindex[jj] << " , vv:" << vv<< std::endl;
+  	        }
+  	        // multiply the basis function value with the coefficient
+  	        //if (baseVal!=0) //todo:
+  	        value += baseVal * vec[i];
+  	  }
+  	  // just return the value
+  	  return value;
+   }
 }
