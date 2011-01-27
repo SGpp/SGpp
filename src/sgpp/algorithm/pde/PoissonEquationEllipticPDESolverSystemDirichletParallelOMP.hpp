4eac934ef64b5c9707116a44fb53d815b7002b39
Alexander Heinecke <heinecke@in.tum.de>
2011-01-27 07:52:50 +0000

    added parallelized version of Poisson equation solver
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@1148 4eea3252-f0fb-4393-894d-40516dce545b

---
+++ src/sgpp/algorithm/pde/PoissonEquationEllipticPDESolverSystemDirichletParallelOMP.hpp
@@ -0,0 +1,51 @@
+/******************************************************************************
+* Copyright (C) 2011 Technische Universitaet Muenchen                         *
+* This file is part of the SG++ project. For conditions of distribution and   *
+* use, please see the copyright notice at http://www5.in.tum.de/SGpp          *
+******************************************************************************/
+// @author Alexander Heinecke (Alexander.Heinecke@mytum.de)
+
+#ifndef POISSONEQUATIONELLIPTICPDESOLVERSYSTEMDIRICHLETPARALLELOMP_HPP
+#define POISSONEQUATIONELLIPTICPDESOLVERSYSTEMDIRICHLETPARALLELOMP_HPP
+
+#include "operation/pde/OperationEllipticPDESolverSystemDirichlet.hpp"
+
+namespace sg
+{
+
+/**
+ * This class uses OperationEllipticPDESolverSystemDirichlet
+ * to define a solver system for the Poission Equation.
+ *
+ * For the mult-routine only the Laplace-Operator is required
+ *
+ * There is a parallelization over all operators, required
+ * to solve the poisson equation.
+ */
+class PoissonEquationEllipticPDESolverSystemDirichletParallelOMP : public OperationEllipticPDESolverSystemDirichlet
+{
+protected:
+	OperationMatrix* Laplace_Inner;
+	OperationMatrix* Laplace_Complete;
+
+	void applyLOperatorComplete(DataVector& alpha, DataVector& result);
+
+	void applyLOperatorInner(DataVector& alpha, DataVector& result);
+
+public:
+	/**
+	 * Constructor
+	 *
+	 * @param rhs the right hand side for solving the elliptic PDE
+	 */
+	PoissonEquationEllipticPDESolverSystemDirichletParallelOMP(Grid& SparseGrid, DataVector& rhs);
+
+	/**
+	 * Destructor
+	 */
+	virtual ~PoissonEquationEllipticPDESolverSystemDirichletParallelOMP();
+};
+
+}
+
+#endif /* POISSONEQUATIONELLIPTICPDESOLVERSYSTEMDIRICHLET_HPP */
