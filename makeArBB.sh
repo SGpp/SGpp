cfe62b3386f9d6ad27c1d7bef53996e6a8b675a5
Alexander Heinecke <heinecke@in.tum.de>
2011-02-13 16:00:10 +0000

    removed not needed files for DataMining extensions (Intel ArBB and OpenCL) bug fix for Hybrid OCL SSE single percision OperationMultipleEval
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@1181 4eea3252-f0fb-4393-894d-40516dce545b

--- makeArBB.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-mkdir -p tmp/build_native/ClassifyBenchmark_ArBB
-make -f ./../../../makefile_classify_ArBB --directory=./tmp/build_native/ClassifyBenchmark_ArBB
--- makeOCL.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-mkdir -p tmp/build_native/ClassifyBenchmark_OCL
-make -f ./../../../makefile_classify_OCL --directory=./tmp/build_native/ClassifyBenchmark_OCL
