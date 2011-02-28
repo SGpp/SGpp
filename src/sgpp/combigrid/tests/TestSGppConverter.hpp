3addd69fad7e8cc499fe7a254255141b473f2c81
benk <benk>
2011-02-28 18:00:33 +0000

    new (combi grid) test cases added
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@1217 4eea3252-f0fb-4393-894d-40516dce545b

--- src/sgpp/combigrid/tests/TestSGppConverter.hpp
+++ src/sgpp/combigrid/tests/TestSGppConverter.hpp
@@ -10,6 +10,7 @@
 
 #include "combigrid/combigrid/SerialCombiGrid.hpp"
 #include "combigrid/combischeme/CombiTS_CT.hpp"
+#include "combigrid/combischeme/CombiS_CT.hpp"
 #include "combigrid/converter/CombiSGppConverter.hpp"
 
 namespace combigrid{
@@ -27,6 +28,8 @@ public:
 
     	// 3D test
     	test2();
+    	test21(); // S-CT
+    	test22(); // S-CT without boundary
     }
 
     /** test function for the grid, this should be exactly interpolated */
@@ -216,6 +219,188 @@ public:
     	COMBIGRID_ERROR_TEST_EQUAL( combigrid->eval(coords) , testfunction3D1( coords[0] , coords[1] , coords[2] ) , 1e-4 , "" );
     	coords[0] = 0.999; coords[1] = 0.001; coords[2] = 0.999;
     	COMBIGRID_ERROR_TEST_EQUAL( combigrid->eval(coords) , testfunction3D1( coords[0] , coords[1] , coords[2] ) , 1e-4 , "" );
+
+    	// delete the allocated variables
+    	delete alphas;
+    	delete minAlpha;
+    	delete maxAlpha;
+    	delete gridstorageSGpp;
+    	delete combigrid;
+    	delete combischeme;
+    }
+
+    static void test21(){
+
+    	std::vector<double> coords( 3 , 0.0);
+    	std::vector<int> levels( 3 , 9);
+    	std::vector<int> levels_trunc( 3 , 3);
+    	levels[0] = 9; levels[1] = 6; levels[2] = 8;
+    	levels_trunc[0] = 1; levels_trunc[1] = 3; levels_trunc[2] = 1;
+    	CombiSchemeBasis* combischeme = new S_CT( 3 , levels , levels_trunc );
+    	AbstractCombiGrid* combigrid = new SerialCombiGrid(combischeme);
+    	sg::GridStorage* gridstorageSGpp;
+    	DataVector *alphas , *minAlpha , *maxAlpha ;
+
+    	// creates the full grids in the combination grid
+    	combigrid->createFullGrids();
+
+    	// for each full grid set the values
+    	for( int fg = 0 ; fg < combigrid->getNrFullGrid() ; fg++){
+    		FullGridD* fgp = combigrid->getFullGrid(fg);
+    		// for this full grid set the values
+        	for (int i=0 ; i < fgp->getNrElements() ; i++){
+        		fgp->getCoords( i , coords );
+        		fgp->getElementVector()[i] = testfunction3D1( coords[0] , coords[1] , coords[2]);
+        	}
+    	}
+
+    	// create the grid storage
+    	gridstorageSGpp = combigrid->createSGppGridStorage();
+
+    	// create the coefficient vectors for the SGpp grid
+    	alphas = new DataVector(gridstorageSGpp->size());
+    	minAlpha = new DataVector(gridstorageSGpp->size());
+    	maxAlpha = new DataVector(gridstorageSGpp->size());
+
+    	// recompose full grids -> SGpp
+    	combigrid->reCompose( gridstorageSGpp , alphas , minAlpha , maxAlpha );
+    	combigrid->reCompose( gridstorageSGpp , alphas );
+
+    	double minA, maxA, minMin , maxMax;
+    	maxMax = (*maxAlpha)[0]; minMin = (*minAlpha)[0]; minA = maxA = (*alphas)[0];
+    	for (int ind = 1 ; ind < (int)gridstorageSGpp->size() ; ind++){
+    		maxMax = ( (*maxAlpha)[ind] > maxMax) ? (*maxAlpha)[ind] : maxMax;
+    		minMin = ( (*minAlpha)[ind] < minMin) ? (*minAlpha)[ind] : minMin;
+    		minA = ( (*minAlpha)[ind] < minA) ? (*minAlpha)[ind] : minA;
+    		maxA = ( (*minAlpha)[ind] > maxA) ? (*minAlpha)[ind] : maxA;
+    	}
+
+    	coords[0] = 0.0; coords[1] = 0.0; coords[2] = 0.0;
+    	COMBIGRID_ERROR_TEST_EQUAL( minMin , testfunction3D1( coords[0] , coords[1] , coords[2]) , 1e-10 , "" );
+    	COMBIGRID_ERROR_TEST_EQUAL( minA , testfunction3D1( coords[0] , coords[1] , coords[2]) , 1e-10 , "" );
+    	coords[0] = 1.0; coords[1] = 1.0; coords[2] = 1.0;
+    	COMBIGRID_ERROR_TEST_EQUAL( maxMax , testfunction3D1( coords[0] , coords[1] , coords[2]) , 1e-10 , "" );
+    	COMBIGRID_ERROR_TEST_EQUAL( maxA , testfunction3D1( coords[0] , coords[1] , coords[2]) , 1e-10 , "" );
+
+       	// for each full grid set the values
+        for( int fg = 0 ; fg < combigrid->getNrFullGrid() ; fg++){
+        	FullGridD* fgp = combigrid->getFullGrid(fg);
+           	for (int i=0 ; i < fgp->getNrElements() ; i++){
+           		// set all the full grid values with dummy values
+           		// so that we can test if the decomposition really works
+           		fgp->getElementVector()[i] = 912.34;
+           	}
+        }
+
+        // --- decompose SGpp -> full grids
+        combigrid->deCompose( gridstorageSGpp , alphas );
+
+    	coords[0] = 1.0/3.0; coords[1] = 1.0/3.0; coords[2] = 1.0/3.0;
+    	COMBIGRID_ERROR_TEST_EQUAL( combigrid->eval(coords) , testfunction3D1( coords[0] , coords[1] , coords[2] ) , 1e-4 , "" );
+    	coords[0] = 0.0001; coords[1] = 1.0/3.0; coords[2] = 1.0/3.0;
+    	COMBIGRID_ERROR_TEST_EQUAL( combigrid->eval(coords) , testfunction3D1( coords[0] , coords[1] , coords[2] ) , 1e-4 , "" );
+    	coords[0] = 1.0/3.0; coords[1] = 0.0001; coords[2] = 0.0001;
+    	COMBIGRID_ERROR_TEST_EQUAL( combigrid->eval(coords) , testfunction3D1( coords[0] , coords[1] , coords[2] ) , 1e-4 , "" );
+    	coords[0] = 0.0; coords[1] = 0.0; coords[2] =0.0;
+    	COMBIGRID_ERROR_TEST_EQUAL( combigrid->eval(coords) , testfunction3D1( coords[0] , coords[1] , coords[2] ) , 1e-4 , "" );
+    	coords[0] = 1.0; coords[1] = 1.0; coords[2] =1.0;
+    	COMBIGRID_ERROR_TEST_EQUAL( combigrid->eval(coords) , testfunction3D1( coords[0] , coords[1] , coords[2] ) , 1e-4 , "" );
+    	coords[0] = 0.999; coords[1] = 0.95; coords[2] = 0.95;
+    	COMBIGRID_ERROR_TEST_EQUAL( combigrid->eval(coords) , testfunction3D1( coords[0] , coords[1] , coords[2] ) , 1e-4 , "" );
+    	coords[0] = 0.999; coords[1] = 0.001; coords[2] = 0.999;
+    	COMBIGRID_ERROR_TEST_EQUAL( combigrid->eval(coords) , testfunction3D1( coords[0] , coords[1] , coords[2] ) , 1e-4 , "" );
+
+    	// delete the allocated variables
+    	delete alphas;
+    	delete minAlpha;
+    	delete maxAlpha;
+    	delete gridstorageSGpp;
+    	delete combigrid;
+    	delete combischeme;
+    }
+
+    static void test22(){
+
+    	std::vector<double> coords( 3 , 0.0);
+    	std::vector<int> levels( 3 , 9);
+    	std::vector<int> levels_trunc( 3 , 3);
+    	levels[0] = 8; levels[1] = 6; levels[2] = 8;
+    	levels_trunc[0] = 1; levels_trunc[1] = 3; levels_trunc[2] = 1;
+    	CombiSchemeBasis* combischeme = new S_CT( 3 , levels , levels_trunc );
+    	AbstractCombiGrid* combigrid = new SerialCombiGrid( combischeme , false );
+    	sg::GridStorage* gridstorageSGpp;
+    	DataVector *alphas , *minAlpha , *maxAlpha ;
+
+    	// creates the full grids in the combination grid
+    	combigrid->createFullGrids();
+
+    	// for each full grid set the values
+    	for( int fg = 0 ; fg < combigrid->getNrFullGrid() ; fg++){
+    		FullGridD* fgp = combigrid->getFullGrid(fg);
+    		// for this full grid set the values
+        	for (int i=0 ; i < fgp->getNrElements() ; i++){
+        		fgp->getCoords( i , coords );
+        		fgp->getElementVector()[i] = testfunction3D1( coords[0] , coords[1] , coords[2]);
+        	}
+    	}
+
+    	// create the grid storage
+    	gridstorageSGpp = combigrid->createSGppGridStorage();
+
+    	// create the coefficient vectors for the SGpp grid
+    	alphas = new DataVector(gridstorageSGpp->size());
+    	minAlpha = new DataVector(gridstorageSGpp->size());
+    	maxAlpha = new DataVector(gridstorageSGpp->size());
+
+    	// recompose full grids -> SGpp
+    	combigrid->reCompose( gridstorageSGpp , alphas , minAlpha , maxAlpha );
+    	combigrid->reCompose( gridstorageSGpp , alphas );
+
+    	double minA, maxA, minMin , maxMax;
+    	maxMax = (*maxAlpha)[0]; minMin = (*minAlpha)[0]; minA = maxA = (*alphas)[0];
+    	for (int ind = 1 ; ind < (int)gridstorageSGpp->size() ; ind++){
+    		maxMax = ( (*maxAlpha)[ind] > maxMax) ? (*maxAlpha)[ind] : maxMax;
+    		minMin = ( (*minAlpha)[ind] < minMin) ? (*minAlpha)[ind] : minMin;
+    		minA = ( (*minAlpha)[ind] < minA) ? (*minAlpha)[ind] : minA;
+    		maxA = ( (*minAlpha)[ind] > maxA) ? (*minAlpha)[ind] : maxA;
+    	}
+
+    	// since this grid has no boundary points the tolerance has to be higher
+    	coords[0] = 0.0; coords[1] = 0.0; coords[2] = 0.0;
+    	COMBIGRID_ERROR_TEST_EQUAL( minMin , testfunction3D1( coords[0] , coords[1] , coords[2]) , 1e-1 , "" );
+    	COMBIGRID_ERROR_TEST_EQUAL( minA , testfunction3D1( coords[0] , coords[1] , coords[2]) , 1e-1 , "" );
+    	coords[0] = 1.0; coords[1] = 1.0; coords[2] = 1.0;
+    	COMBIGRID_ERROR_TEST_EQUAL( maxMax , testfunction3D1( coords[0] , coords[1] , coords[2]) , 3e-0 , "" );
+    	COMBIGRID_ERROR_TEST_EQUAL( maxA , testfunction3D1( coords[0] , coords[1] , coords[2]) , 3e-0 , "" );
+
+       	// for each full grid set the values
+        for( int fg = 0 ; fg < combigrid->getNrFullGrid() ; fg++){
+        	FullGridD* fgp = combigrid->getFullGrid(fg);
+           	for (int i=0 ; i < fgp->getNrElements() ; i++){
+           		// set all the full grid values with dummy values
+           		// so that we can test if the decomposition really works
+           		fgp->getElementVector()[i] = 912.34;
+           	}
+        }
+
+        // --- decompose SGpp -> full grids
+        combigrid->deCompose( gridstorageSGpp , alphas );
+
+    	// since this grid has no boundary points the tolerance has to be higher
+    	coords[0] = 1.0/3.0; coords[1] = 1.0/3.0; coords[2] = 1.0/3.0;
+    	COMBIGRID_ERROR_TEST_EQUAL( combigrid->eval(coords) , testfunction3D1( coords[0] , coords[1] , coords[2] ) , 1e-4 , "" );
+    	coords[0] = 0.0001; coords[1] = 1.0/3.0; coords[2] = 1.0/3.0;
+    	COMBIGRID_ERROR_TEST_EQUAL( combigrid->eval(coords) , testfunction3D1( coords[0] , coords[1] , coords[2] ) , 1e-3 , "" );
+    	coords[0] = 1.0/3.0; coords[1] = 0.0001; coords[2] = 0.0001;
+    	COMBIGRID_ERROR_TEST_EQUAL( combigrid->eval(coords) , testfunction3D1( coords[0] , coords[1] , coords[2] ) , 1e-3 , "" );
+    	coords[0] = 0.0; coords[1] = 0.0; coords[2] =0.0;
+    	COMBIGRID_ERROR_TEST_EQUAL( combigrid->eval(coords) , testfunction3D1( coords[0] , coords[1] , coords[2] ) , 1e-3 , "" );
+    	coords[0] = 1.0; coords[1] = 1.0; coords[2] =1.0;
+    	COMBIGRID_ERROR_TEST_EQUAL( combigrid->eval(coords) , testfunction3D1( coords[0] , coords[1] , coords[2] ) , 3e-3 , "" );
+    	coords[0] = 0.999; coords[1] = 0.95; coords[2] = 0.95;
+    	COMBIGRID_ERROR_TEST_EQUAL( combigrid->eval(coords) , testfunction3D1( coords[0] , coords[1] , coords[2] ) , 3e-3 , "" );
+    	coords[0] = 0.999; coords[1] = 0.001; coords[2] = 0.999;
+    	COMBIGRID_ERROR_TEST_EQUAL( combigrid->eval(coords) , testfunction3D1( coords[0] , coords[1] , coords[2] ) , 3e-3 , "" );
 
     	// delete the allocated variables
     	delete alphas;
