1df77eaba6bdb54a3595ea9e42c86938edc8268f
Alexander Heinecke <heinecke@in.tum.de>
2011-03-14 19:56:38 +0000

    increased treshold for recursive Up/down parallelization
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@1243 4eea3252-f0fb-4393-894d-40516dce545b

--- src/sgpp/algorithm/pde/UpDownTwoOpDims.hpp
+++ src/sgpp/algorithm/pde/UpDownTwoOpDims.hpp
@@ -93,7 +93,7 @@ protected:
 	/// number of algorithmic dimensions
 	const size_t numAlgoDims_;
 	/// max number of parallel stages (dimension recursive calls)
-	static const size_t maxParallelDims_ = 2;
+	static const size_t maxParallelDims_ = 3;
 
 	/**
 	 * Recursive procedure for updown, parallel version using OpenMP 3
