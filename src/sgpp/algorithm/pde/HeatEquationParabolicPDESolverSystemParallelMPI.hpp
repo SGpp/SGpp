bf6aff5826fed8fc1ade95063485f51934886a22
Alexander Heinecke <heinecke@in.tum.de>
2011-03-14 21:16:45 +0000

    added HeatEquation Parabolic Solver System that supports MPI
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@1244 4eea3252-f0fb-4393-894d-40516dce545b

---
+++ src/sgpp/algorithm/pde/HeatEquationParabolicPDESolverSystemParallelMPI.hpp
@@ -0,0 +1,73 @@
+/******************************************************************************
+* Copyright (C) 2009 Technische Universitaet Muenchen                         *
+* This file is part of the SG++ project. For conditions of distribution and   *
+* use, please see the copyright notice at http://www5.in.tum.de/SGpp          *
+******************************************************************************/
+// @author Alexander Heinecke (Alexander.Heinecke@mytum.de)
+
+#ifndef HEATEQUATIONPARABOLICPDESOLVERSYSTEMPARALLELMPI_HPP
+#define HEATEQUATIONPARABOLICPDESOLVERSYSTEMPARALLELMPI_HPP
+
+#include "data/DataVector.hpp"
+#include "grid/Grid.hpp"
+#include "operation/pde/OperationParabolicPDESolverSystemDirichlet.hpp"
+
+namespace sg
+{
+
+/**
+ * This class implements the ParabolicPDESolverSystem for the
+ * Heat Equation parallelized with MPI.
+ */
+class HeatEquationParabolicPDESolverSystemParallelMPI : public OperationParabolicPDESolverSystemDirichlet
+{
+private:
+	/// the heat coefficient
+	double a;
+	/// the Laplace Operation (Stiffness Matrix), on boundary grid
+	OperationMatrix* OpLaplaceBound;
+	/// the LTwoDotProduct Operation (Mass Matrix), on boundary grid
+	OperationMatrix* OpMassBound;
+	/// the Laplace Operation (Stiffness Matrix), on inner grid
+	OperationMatrix* OpLaplaceInner;
+	/// the LTwoDotProduct Operation (Mass Matrix), on inner grid
+	OperationMatrix* OpMassInner;
+
+	void applyMassMatrixComplete(DataVector& alpha, DataVector& result);
+
+	void applyLOperatorComplete(DataVector& alpha, DataVector& result);
+
+	void applyMassMatrixInner(DataVector& alpha, DataVector& result);
+
+	void applyLOperatorInner(DataVector& alpha, DataVector& result);
+
+public:
+	/**
+	 * Std-Constructor
+	 *
+	 * @param SparseGrid reference to the sparse grid
+	 * @param the sparse grid's coefficients
+	 * @param a the heat coefficient
+	 * @param TimestepSize the size of one timestep used in the ODE Solver
+	 * @param OperationMode specifies in which solver this matrix is used, valid values are: ExEul for explicit Euler,
+	 *  							ImEul for implicit Euler, CrNic for Crank Nicolson solver
+	 */
+	HeatEquationParabolicPDESolverSystemParallelMPI(Grid& SparseGrid, DataVector& alpha, double a, double TimestepSize, std::string OperationMode = "ExEul");
+
+	/**
+	 * Std-Destructor
+	 */
+	virtual ~HeatEquationParabolicPDESolverSystemParallelMPI();
+
+	void finishTimestep(bool isLastTimestep = false);
+
+	void startTimestep();
+
+	virtual void mult(DataVector& alpha, DataVector& result);
+
+	virtual DataVector* generateRHS();
+};
+
+}
+
+#endif /* HEATEQUATIONPARABOLICPDESOLVERSYSTEMPARALLELMPI_HPP */
