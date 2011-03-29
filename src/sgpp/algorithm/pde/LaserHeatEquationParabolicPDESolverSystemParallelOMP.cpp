5794dd01602428fa9bc2ee7338093596a5fe0752
Alexander Heinecke <heinecke@in.tum.de>
2011-03-29 12:58:25 +0000

    added first try of circle function for laser rotation
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@1312 4eea3252-f0fb-4393-894d-40516dce545b

--- src/sgpp/algorithm/pde/LaserHeatEquationParabolicPDESolverSystemParallelOMP.cpp
+++ src/sgpp/algorithm/pde/LaserHeatEquationParabolicPDESolverSystemParallelOMP.cpp
@@ -11,6 +11,10 @@
 #include "grid/generation/SurplusRefinementFunctor.hpp"
 #include "tools/common/StdNormalDistribution.hpp"
 
+#include <string>
+#include <sstream>
+#include <cmath>
+
 #ifndef PI
 #define PI 3.14159265
 #endif
@@ -29,6 +33,8 @@ LaserHeatEquationParabolicPDESolverSystemParallelOMP::~LaserHeatEquationParaboli
 void LaserHeatEquationParabolicPDESolverSystemParallelOMP::finishTimestep(bool isLastTimestep)
 {
 	double heat = 4.0;
+	double xadd = 0.0;
+	bool y_add = true;
 
 	// Replace the inner coefficients on the boundary grid
 	this->GridConverter->updateBoundaryCoefs(*this->alpha_complete, *this->alpha_inner);
@@ -38,51 +44,78 @@ void LaserHeatEquationParabolicPDESolverSystemParallelOMP::finishTimestep(bool i
 	OperationHierarchisation* myHierarchisation = this->BoundGrid->createOperationHierarchisation();
 	myHierarchisation->doDehierarchisation(*this->alpha_complete);
 
-//	DataVector laser_update(this->alpha_complete->getSize());
-//
-//	this->laser_x_offset_ += (this->beam_velocity_*this->TimestepSize);
-//	double pos_x = this->laser_x_start_ + sin(this->laser_x_offset_*2.0*PI);
-//	if ((pos_x > 0.5) && (this->laser_x_last_ <= 0.5))
-//	{
-//		this->y_sign_switch++;
-//	}
-//	if ((pos_x < 0.5) && (this->laser_x_last_ >= 0.5))
-//	{
-//		this->y_sign_switch++;
-//	}
-//	double pos_y = sqrt((0.25*0.25)-((pos_x-0,5)*(pos_x-0,5)));
-//	pos_y = pos_y*(pow(-1.0, (double)this->y_sign_switch));
-//	pos_y += 0.5;
-//
-//	double* dblFuncValues = new double[2];
-//	for (size_t i = 0; i < this->BoundGrid->getStorage()->size(); i++)
-//	{
-//		std::string coords = this->BoundGrid->getStorage()->get(i)->getCoordsStringBB(*(this->BoundGrid->getStorage()->getBoundingBox()));
-//		std::stringstream coordsStream(coords);
-//
-//		for (size_t j = 0; j < 2; j++)
-//		{
-//			coordsStream >> dblFuncValues[j];
-//		}
-//
-//		// check if coordinates at starting point of laser
-//		laser_update[i] =  heat*(myNormDistr.getDensity(dblFuncValues[0], pos_x, this->heat_sigma_)*myNormDistr.getDensity(dblFuncValues[1], pos_y, this->heat_sigma_));
-//
-//		//boundaries are set to zero
-//		if (dblFuncValues[0] == 0.0 || dblFuncValues[1] == 0.0)
-//		{
-//			laser_update[i] = 0.0;
-//		}
-//	}
-//	delete[] dblFuncValues;
-//
-//	this->laser_x_last_ = pos_x;
-//
-//	// combine last solution and laser update
-//	for (size_t i = 0; this->alpha_complete->getSize(); i++)
-//	{
-//		this->alpha_complete->set(i, std::max< double >(this->alpha_complete->get(i), laser_update[i]));
-//	}
+	DataVector laser_update(this->BoundGrid->getStorage()->size());
+	BoundingBox* myBoundingBox = new BoundingBox(*(this->BoundGrid->getStorage()->getBoundingBox()));
+
+	this->laser_x_offset_ += (this->beam_velocity_*this->TimestepSize);
+
+	int int_x_offset = (int)laser_x_offset_;
+	if ((this->laser_x_offset_ - ((double)int_x_offset)) <= 0.25)
+	{
+		y_add = true;
+		xadd = 0.0;
+	}
+	if ((this->laser_x_offset_ - ((double)int_x_offset)) > 0.25)
+	{
+		y_add = true;
+		xadd = 0.25;
+	}
+	if ((this->laser_x_offset_ - ((double)int_x_offset)) > 0.5)
+	{
+		y_add = false;
+		xadd = 0.5;
+	}
+	if ((this->laser_x_offset_ - ((double)int_x_offset)) > 0.75)
+	{
+		y_add = false;
+		xadd = 0.25;
+	}
+
+	double pos_x = this->laser_x_start_ + xadd + (0.25*sin(this->laser_x_offset_*2.0*PI));
+	double pos_y = sqrt((0.25*0.25)-((pos_x-0.5)*(pos_x-0.5)));
+	pos_y = pos_y*(pow(-1.0, (double)this->y_sign_switch));
+
+	if (y_add == true)
+	{
+		pos_y = pos_y + 0.5;
+	}
+	else
+	{
+		pos_y = 0.5 - pos_y;
+	}
+
+	std::cout << std::endl << std::endl << pos_x << " " << pos_y << std::endl << std::endl;
+
+	double* dblFuncValues = new double[2];
+	for (size_t i = 0; i < this->BoundGrid->getStorage()->size(); i++)
+	{
+		std::string coords = this->BoundGrid->getStorage()->get(i)->getCoordsStringBB(*(myBoundingBox));
+		std::stringstream coordsStream(coords);
+
+		for (size_t j = 0; j < 2; j++)
+		{
+			coordsStream >> dblFuncValues[j];
+		}
+
+		// check if coordinates at starting point of laser
+		laser_update[i] =  heat*(myNormDistr.getDensity(dblFuncValues[0], pos_x, this->heat_sigma_)*myNormDistr.getDensity(dblFuncValues[1], pos_y, this->heat_sigma_));
+
+		//boundaries are set to zero
+		if (dblFuncValues[0] == 0.0 || dblFuncValues[1] == 0.0)
+		{
+			laser_update[i] = 0.0;
+		}
+	}
+	delete[] dblFuncValues;
+	delete myBoundingBox;
+
+	this->laser_x_last_ = pos_x;
+
+	// combine last solution and laser update
+	for (size_t i = 0; i < this->BoundGrid->getStorage()->size(); i++)
+	{
+		this->alpha_complete->set(i, std::max< double >(this->alpha_complete->get(i), laser_update[i]));
+	}
 
 	// do hierarchisation
 	myHierarchisation->doHierarchisation(*this->alpha_complete);
@@ -98,7 +131,7 @@ void LaserHeatEquationParabolicPDESolverSystemParallelOMP::finishTimestep(bool i
 	GridGenerator* myGenerator = this->BoundGrid->createGridGenerator();
 
 	size_t numRefines = myGenerator->getNumberOfRefinablePoints();
-	SurplusRefinementFunctor* myRefineFunc = new SurplusRefinementFunctor(this->alpha_complete, numRefines, 0.0001);
+	SurplusRefinementFunctor* myRefineFunc = new SurplusRefinementFunctor(this->alpha_complete, numRefines, 0.01);
 	myGenerator->refineMaxLevel(myRefineFunc, this->max_level_);
 	this->alpha_complete->resizeZero(this->BoundGrid->getStorage()->size());
 	delete myRefineFunc;
@@ -106,7 +139,7 @@ void LaserHeatEquationParabolicPDESolverSystemParallelOMP::finishTimestep(bool i
 	if (this->BoundGrid->getStorage()->getNumInnerPoints() > 100)
 	{
 		size_t numCoarsen = myGenerator->getNumberOfRemoveablePoints();
-		SurplusCoarseningFunctor* myCoarsenFunctor = new SurplusCoarseningFunctor(this->alpha_complete, numCoarsen, 0.00001);
+		SurplusCoarseningFunctor* myCoarsenFunctor = new SurplusCoarseningFunctor(this->alpha_complete, numCoarsen, 0.001);
 		myGenerator->coarsenNFirstOnly(myCoarsenFunctor, this->alpha_complete, originalGridSize);
 		delete myCoarsenFunctor;
 	}
