dd668e4c5edc043f35241cdf119b2f5bf2d3957c
Alexander Heinecke <heinecke@in.tum.de>
2011-04-11 21:26:39 +0000

    added convergence plots to MPI paralleliyzed Heat Eaution Solver
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@1361 4eea3252-f0fb-4393-894d-40516dce545b

--- src/application_cpp/NativeCppHeatEquationSolverMPI.cpp
+++ src/application_cpp/NativeCppHeatEquationSolverMPI.cpp
@@ -116,7 +116,8 @@ void writeHelp()
 	mySStream << "HeatEquation" << std::endl << "------" << std::endl;
 	mySStream << "the following options must be specified:" << std::endl;
 	mySStream << "	dim: the number of dimensions of Sparse Grid" << std::endl;
-	mySStream << "	level: number of levels within the Sparse Grid" << std::endl;
+	mySStream << "	start_level: number of start levels of the Sparse Grid" << std::endl;
+	mySStream << "	end_level: number of max. levels of the Sparse Grid" << std::endl;
 	mySStream << "	left_bound: x_i of left boundary" << std::endl;
 	mySStream << "	right_bound: x_i of right boundary" << std::endl;
 	mySStream << "	a: thermal diffusivity" << std::endl;
@@ -128,7 +129,7 @@ void writeHelp()
 	mySStream << "	CGIterations: Maxmimum number of iterations used in CG mehtod" << std::endl;
 	mySStream << std::endl;
 	mySStream << "Example:" << std::endl;
-	mySStream << "HESolver HeatEquation 3 5 0.0 3.0 1.0 smooth 1.0 0.1 ImEul 0.00001 400" << std::endl;
+	mySStream << "HESolver HeatEquation 3 2 5 0.0 3.0 1.0 smooth 1.0 0.1 ImEul 0.00001 400" << std::endl;
 	mySStream << std::endl;
 	mySStream << "Remark: This test generates following files (gnuplot):" << std::endl;
 	mySStream << "	heatStart.gnuplot: the start condition" << std::endl;
@@ -157,119 +158,189 @@ void writeHelp()
 	std::cout << mySStream.str() << std::endl;
 }
 
-void testHeatEquation(size_t dim, size_t level, double bound_left, double bound_right, double a,
+void testHeatEquation(size_t dim, size_t start_level, size_t end_level, double bound_left, double bound_right, double a,
 						std::string initFunc, double T, double dt, std::string ODESolver,
 						double cg_eps, size_t cg_its)
 {
 	size_t timesteps = (size_t)(T/dt);
 	sg::HeatEquationSolverMPI* myHESolver = new sg::HeatEquationSolverMPI();
 	DataVector* alpha = NULL;
+	DataMatrix EvalPoints(1, dim);
+	std::string tFileEvalCuboid = "EvalPointsHeatEquationMPI";
+	std::string tFileEvalCuboidValues = "EvalValuesHeatEquationMPI";
+	size_t evalPoints = NUMEVALPOINTS;
+	std::vector<DataVector> results;
 
-	if (sg::myGlobalMPIComm->getMyRank() == 0)
+	for (size_t l = start_level; l <= end_level; l++)
 	{
-		sg::DimensionBoundary* myBoundaries = new sg::DimensionBoundary[dim];
-
-		// set the bounding box
-		for (size_t i = 0; i < dim; i++)
+		if (sg::myGlobalMPIComm->getMyRank() == 0)
 		{
-			myBoundaries[i].leftBoundary = bound_left;
-			myBoundaries[i].rightBoundary = bound_right;
-			myBoundaries[i].bDirichletLeft = true;
-			myBoundaries[i].bDirichletRight = true;
+			sg::DimensionBoundary* myBoundaries = new sg::DimensionBoundary[dim];
+
+			// set the bounding box
+			for (size_t i = 0; i < dim; i++)
+			{
+				myBoundaries[i].leftBoundary = bound_left;
+				myBoundaries[i].rightBoundary = bound_right;
+				myBoundaries[i].bDirichletLeft = true;
+				myBoundaries[i].bDirichletRight = true;
+			}
+
+			sg::BoundingBox* myBoundingBox = new sg::BoundingBox(dim, myBoundaries);
+			delete[] myBoundaries;
+
+			// in first iteration -> calculate the evaluation points
+			if (l == start_level)
+			{
+				sg::EvalCuboidGenerator* myEvalCuboidGen = new sg::EvalCuboidGenerator();
+				myEvalCuboidGen->getEvaluationCuboid(EvalPoints, *myBoundingBox, evalPoints);
+				writeDataMatrix(EvalPoints, tFileEvalCuboid);
+				delete myEvalCuboidGen;
+			}
+
+			// init Screen Object
+			myHESolver->initScreen();
+
+			// Construct a grid
+			myHESolver->constructGrid(*myBoundingBox, l);
+
+			// init the basis functions' coefficient vector (start solution)
+			alpha = new DataVector(myHESolver->getNumberGridPoints());
+			if (initFunc == "smooth")
+			{
+				myHESolver->initGridWithSmoothHeat(*alpha, bound_right, bound_right/DIV_SIGMA, DISTRI_FACTOR);
+			}
+			else
+			{
+				writeHelp();
+				sg::myGlobalMPIComm->Abort();
+			}
+
+			delete myBoundingBox;
 		}
 
-
-		sg::BoundingBox* myBoundingBox = new sg::BoundingBox(dim, myBoundaries);
-		delete[] myBoundaries;
-
-		// init Screen Object
-		myHESolver->initScreen();
-
-		// Construct a grid
-		myHESolver->constructGrid(*myBoundingBox, level);
-
-		// init the basis functions' coefficient vector (start solution)
-		alpha = new DataVector(myHESolver->getNumberGridPoints());
-		if (initFunc == "smooth")
+		// Communicate grid
+		if (sg::myGlobalMPIComm->getMyRank() == 0)
 		{
-			myHESolver->initGridWithSmoothHeat(*alpha, bound_right, bound_right/DIV_SIGMA, DISTRI_FACTOR);
+			std::string serialized_grid = myHESolver->getGrid();
+
+			sg::myGlobalMPIComm->broadcastGrid(serialized_grid);
 		}
 		else
 		{
-			writeHelp();
-			sg::myGlobalMPIComm->Abort();
+			// Now receive the grid
+			std::string serialized_grid = "";
+
+			sg::myGlobalMPIComm->receiveGrid(serialized_grid);
+			myHESolver->setGrid(serialized_grid);
+
+			alpha = new DataVector(myHESolver->getNumberGridPoints());
 		}
 
-		delete myBoundingBox;
-	}
+		sg::myGlobalMPIComm->Barrier();
 
-	// Communicate grid
-	if (sg::myGlobalMPIComm->getMyRank() == 0)
-	{
-		std::string serialized_grid = myHESolver->getGrid();
-
-		sg::myGlobalMPIComm->broadcastGrid(serialized_grid);
-	}
-	else
-	{
-		// Now receive the grid
-		std::string serialized_grid = "";
-
-		sg::myGlobalMPIComm->receiveGrid(serialized_grid);
-		myHESolver->setGrid(serialized_grid);
-
-		alpha = new DataVector(myHESolver->getNumberGridPoints());
-	}
-
-	sg::myGlobalMPIComm->Barrier();
-
-	// Communicate coefficients
-	if (sg::myGlobalMPIComm->getMyRank() == 0)
-	{
-		sg::myGlobalMPIComm->broadcastGridCoefficients(*alpha);
-	}
-	else
-	{
-		sg::myGlobalMPIComm->receiveGridCoefficients(*alpha);
-	}
-
-	sg::myGlobalMPIComm->Barrier();
-
-	// Print initial grid only on rank 0
-	if (sg::myGlobalMPIComm->getMyRank() == 0)
-	{
-		// Print the initial heat function into a gnuplot file
-		if (dim < 3)
+		// Communicate coefficients
+		if (sg::myGlobalMPIComm->getMyRank() == 0)
 		{
-			myHESolver->printGrid(*alpha, GUNPLOT_RESOLUTION, "heatStart.gnuplot");
+			sg::myGlobalMPIComm->broadcastGridCoefficients(*alpha);
 		}
-	}
-
-	// set heat coefficient
-	myHESolver->setHeatCoefficient(a);
-
-	// Start solving the Heat Equation
-	if (ODESolver == "ExEul")
-	{
-		myHESolver->solveExplicitEuler(timesteps, dt, cg_its, cg_eps, *alpha, true, false, std::max(timesteps/SOLUTION_FRAMES,(size_t)1));
-	}
-	else if (ODESolver == "ImEul")
-	{
-		myHESolver->solveImplicitEuler(timesteps, dt, cg_its, cg_eps, *alpha, true, false, std::max(timesteps/SOLUTION_FRAMES,(size_t)1));
-	}
-	else if (ODESolver == "CrNic")
-	{
-		myHESolver->solveCrankNicolson(timesteps, dt, cg_its, cg_eps, *alpha, CRNIC_IMEUL_STEPS);
-	}
-
-	// print solved grid only on rank 0
-	if (sg::myGlobalMPIComm->getMyRank() == 0)
-	{
-		// Print the solved Heat Equation into a gnuplot file
-		if (dim < 3)
+		else
 		{
-			myHESolver->printGrid(*alpha, GUNPLOT_RESOLUTION, "heatSolved.gnuplot");
+			sg::myGlobalMPIComm->receiveGridCoefficients(*alpha);
 		}
+
+		sg::myGlobalMPIComm->Barrier();
+
+		// Print initial grid only on rank 0
+		if (sg::myGlobalMPIComm->getMyRank() == 0)
+		{
+			// Print the initial heat function into a gnuplot file
+			if (dim < 3)
+			{
+				myHESolver->printGrid(*alpha, GUNPLOT_RESOLUTION, "heatStartMPI.gnuplot");
+			}
+		}
+
+		// set heat coefficient
+		myHESolver->setHeatCoefficient(a);
+
+		// Start solving the Heat Equation
+		if (ODESolver == "ExEul")
+		{
+			myHESolver->solveExplicitEuler(timesteps, dt, cg_its, cg_eps, *alpha, true, false, std::max(timesteps/SOLUTION_FRAMES,(size_t)1));
+		}
+		else if (ODESolver == "ImEul")
+		{
+			myHESolver->solveImplicitEuler(timesteps, dt, cg_its, cg_eps, *alpha, true, false, std::max(timesteps/SOLUTION_FRAMES,(size_t)1));
+		}
+		else if (ODESolver == "CrNic")
+		{
+			myHESolver->solveCrankNicolson(timesteps, dt, cg_its, cg_eps, *alpha, CRNIC_IMEUL_STEPS);
+		}
+
+		// print solved grid only on rank 0
+		if (sg::myGlobalMPIComm->getMyRank() == 0)
+		{
+			// Print the solved Heat Equation into a gnuplot file
+			if (dim < 3)
+			{
+				myHESolver->printGrid(*alpha, GUNPLOT_RESOLUTION, "heatSolvedMPI.gnuplot");
+			}
+
+			// Calculate Norms
+			// Evaluate Cuboid
+			DataVector PoisEvals(EvalPoints.getNrows());
+			myHESolver->evaluateCuboid(*alpha, PoisEvals, EvalPoints);
+			results.push_back(PoisEvals);
+
+			// write solution in a additional file
+			std::stringstream level_string;
+			level_string << l;
+			writeDataVector(PoisEvals, tFileEvalCuboidValues+".level_"+ level_string.str());
+			writeDataVector(PoisEvals, tFileEvalCuboidValues);
+
+			if (l > start_level)
+			{
+				std::cout << "=====================================================================" << std::endl;
+				std::cout << "=====================================================================" << std::endl << std::endl;
+				std::cout << "Calculating norms of relative errors to a grid" << std::endl;
+				std::cout << "with " << l << " levels and testing-coboid" << std::endl;
+
+				double oldMaxNorm = 0.0;
+				double oldTwoNorm = 0.0;
+
+				// Calculate relative errors and some norms
+				for (size_t j = 0; j < l-start_level; j++)
+				{
+					DataVector maxLevel(results[l-start_level]);
+					DataVector relError(results[j]);
+					double maxNorm = 0.0;
+					double l2Norm = 0.0;
+
+					// calculate relative error
+					relError.sub(maxLevel);
+					relError.componentwise_div(maxLevel);
+
+					// calculate max. norm of relative error
+					maxNorm = relError.maxNorm();
+
+					// calculate two norm of relative error
+					l2Norm = relError.RMSNorm();
+
+					// Printing norms
+					std::cout << "Level " << j + start_level << ": max-norm(rel-error)=" << maxNorm << "; two-norm(rel-error)=" << l2Norm << "; rate max-norm: " << log(oldMaxNorm/maxNorm) << "; rate two-norm: " << log(oldTwoNorm/l2Norm) << std::endl;
+
+					oldMaxNorm = maxNorm;
+					oldTwoNorm = l2Norm;
+				}
+			}
+			std::cout << std::endl << std::endl;
+		}
+
+		myHESolver->deleteGrid();
+
+		delete alpha;
+		alpha = NULL;
 	}
 
 	delete myHESolver;
@@ -470,7 +541,7 @@ int main(int argc, char *argv[])
 
 	if (option == "HeatEquation")
 	{
-		if (argc != 13)
+		if (argc != 14)
 		{
 			if (mpi_myid == 0)
 			{
@@ -481,7 +552,8 @@ int main(int argc, char *argv[])
 		}
 
 		size_t dim;
-		size_t level;
+		size_t start_level;
+		size_t end_level;
 		double bound_left;
 		double bound_right;
 		double a;
@@ -493,18 +565,19 @@ int main(int argc, char *argv[])
 		size_t cg_its;
 
 		dim = atoi(argv[2]);
-		level = atoi(argv[3]);
-		bound_left = atof(argv[4]);
-		bound_right = atof(argv[5]);
-		a = atof(argv[6]);
-		initFunc.assign(argv[7]);
-		T = atof(argv[8]);
-		dt = atof(argv[9]);
-		ODESolver.assign(argv[10]);
-		cg_eps = atof(argv[11]);
-		cg_its = atoi(argv[12]);
+		start_level = atoi(argv[3]);
+		end_level = atoi(argv[4]);
+		bound_left = atof(argv[5]);
+		bound_right = atof(argv[6]);
+		a = atof(argv[7]);
+		initFunc.assign(argv[8]);
+		T = atof(argv[9]);
+		dt = atof(argv[10]);
+		ODESolver.assign(argv[11]);
+		cg_eps = atof(argv[12]);
+		cg_its = atoi(argv[13]);
 
-		testHeatEquation(dim, level, bound_left, bound_right, a, initFunc, T, dt, ODESolver, cg_eps, cg_its);
+		testHeatEquation(dim, start_level, end_level, bound_left, bound_right, a, initFunc, T, dt, ODESolver, cg_eps, cg_its);
 	}
 	else if (option == "PoissonEquation")
 	{
