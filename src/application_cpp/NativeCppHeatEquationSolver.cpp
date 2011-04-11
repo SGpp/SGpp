7c44021b71fa5d73b4c035307cf9aa16b59fe8a1
Alexander Heinecke <heinecke@in.tum.de>
2011-04-11 19:45:10 +0000

    added convergence check to Heat Equationen solver
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@1357 4eea3252-f0fb-4393-894d-40516dce545b

--- src/application_cpp/NativeCppHeatEquationSolver.cpp
+++ src/application_cpp/NativeCppHeatEquationSolver.cpp
@@ -13,6 +13,8 @@
 #define DIV_SIGMA 4.0
 #define DISTRI_FACTOR 5.0
 
+#define NUMEVALPOINTS 21
+
 //#define EXPORT_MATRIX_FILES
 
 #include <cstdlib>
@@ -115,7 +117,8 @@ void writeHelp()
 	mySStream << "HeatEquation" << std::endl << "------" << std::endl;
 	mySStream << "the following options must be specified:" << std::endl;
 	mySStream << "	dim: the number of dimensions of Sparse Grid" << std::endl;
-	mySStream << "	level: number of levels within the Sparse Grid" << std::endl;
+	mySStream << "	start_level: number of start level of the Sparse Grid" << std::endl;
+	mySStream << "	end_level: number of max levels of the Sparse Grid" << std::endl;
 	mySStream << "	left_bound: x_i of left boundary" << std::endl;
 	mySStream << "	right_bound: x_i of right boundary" << std::endl;
 	mySStream << "	a: thermal diffusivity" << std::endl;
@@ -127,7 +130,7 @@ void writeHelp()
 	mySStream << "	CGIterations: Maxmimum number of iterations used in CG mehtod" << std::endl;
 	mySStream << std::endl;
 	mySStream << "Example:" << std::endl;
-	mySStream << "HESolver HeatEquation 3 5 0.0 3.0 1.0 smooth 1.0 0.1 ImEul 0.00001 400" << std::endl;
+	mySStream << "HESolver HeatEquation 3 2 5 0.0 3.0 1.0 smooth 1.0 0.1 ImEul 0.00001 400" << std::endl;
 	mySStream << std::endl;
 	mySStream << "Remark: This test generates following files (gnuplot):" << std::endl;
 	mySStream << "	heatStart.gnuplot: the start condition" << std::endl;
@@ -146,7 +149,7 @@ void writeHelp()
 	mySStream << "	CGIterations: Maxmimum number of iterations used in CG mehtod" << std::endl;
 	mySStream << std::endl;
 	mySStream << "Example:" << std::endl;
-	mySStream << "HESolver PoissonEquation 3 3 5 0.0 3.0 smooth 0.00001 400" << std::endl;
+	mySStream << "HESolver PoissonEquation 3 2 5 0.0 3.0 smooth 0.00001 400" << std::endl;
 	mySStream << std::endl;
 	mySStream << "Remark: This test generates following files (gnuplot):" << std::endl;
 	mySStream << "	poissonStart.gnuplot: the start condition" << std::endl;
@@ -156,13 +159,17 @@ void writeHelp()
 	std::cout << mySStream.str() << std::endl;
 }
 
-void testHeatEquation(size_t dim, size_t level, double bound_left, double bound_right, double a,
+void testHeatEquation(size_t dim, size_t start_level, size_t end_level, double bound_left, double bound_right, double a,
 						std::string initFunc, double T, double dt, std::string ODESolver,
 						double cg_eps, size_t cg_its)
 {
 	size_t timesteps = (size_t)(T/dt);
-
 	sg::DimensionBoundary* myBoundaries = new sg::DimensionBoundary[dim];
+	DataMatrix EvalPoints(1, dim);
+	std::string tFileEvalCuboid = "EvalPointsHeatEquation";
+	std::string tFileEvalCuboidValues = "EvalValuesHeatEquation";
+	size_t evalPoints = NUMEVALPOINTS;
+	std::vector<DataVector> results;
 
 	// set the bounding box
 	for (size_t i = 0; i < dim; i++)
@@ -177,133 +184,203 @@ void testHeatEquation(size_t dim, size_t level, double bound_left, double bound_
 	sg::BoundingBox* myBoundingBox = new sg::BoundingBox(dim, myBoundaries);
 	delete[] myBoundaries;
 
+	sg::EvalCuboidGenerator* myEvalCuboidGen = new sg::EvalCuboidGenerator();
+
 	// init Screen Object
 	myHESolver->initScreen();
 
-	// Construct a grid
-	myHESolver->constructGrid(*myBoundingBox, level);
+	for (size_t l = start_level; l <= end_level; l++)
+	{
+		// Construct a grid
+		myHESolver->constructGrid(*myBoundingBox, l);
 
-	// init the basis functions' coefficient vector (start solution)
-	DataVector* alpha = new DataVector(myHESolver->getNumberGridPoints());
-	if (initFunc == "smooth")
-	{
-		myHESolver->initGridWithSmoothHeat(*alpha, bound_right, bound_right/DIV_SIGMA, DISTRI_FACTOR);
-	}
-	else
-	{
-		writeHelp();
-	}
+		// in first iteration -> calculate the evaluation points
+		if (l == start_level)
+		{
+			myEvalCuboidGen->getEvaluationCuboid(EvalPoints, *myBoundingBox, evalPoints);
 
-	// Print the initial heat function into a gnuplot file
-	if (dim < 3)
-	{
-		myHESolver->printGrid(*alpha, GUNPLOT_RESOLUTION, "heatStart.gnuplot");
-	}
+			writeDataMatrix(EvalPoints, tFileEvalCuboid);
+		}
 
-	// set heat coefficient
-	myHESolver->setHeatCoefficient(a);
+		// init the basis functions' coefficient vector (start solution)
+		DataVector* alpha = new DataVector(myHESolver->getNumberGridPoints());
+		if (initFunc == "smooth")
+		{
+			myHESolver->initGridWithSmoothHeat(*alpha, bound_right, bound_right/DIV_SIGMA, DISTRI_FACTOR);
+		}
+		else
+		{
+			writeHelp();
+		}
 
-	// Start solving the Heat Equation
-	if (ODESolver == "ExEul")
-	{
-		myHESolver->solveExplicitEuler(timesteps, dt, cg_its, cg_eps, *alpha, true, false, std::max(timesteps/SOLUTION_FRAMES,(size_t)1));
-	}
-	else if (ODESolver == "ImEul")
-	{
-		myHESolver->solveImplicitEuler(timesteps, dt, cg_its, cg_eps, *alpha, true, false, std::max(timesteps/SOLUTION_FRAMES,(size_t)1));
-	}
-	else if (ODESolver == "CrNic")
-	{
-		myHESolver->solveCrankNicolson(timesteps, dt, cg_its, cg_eps, *alpha, CRNIC_IMEUL_STEPS);
-	}
+		// Print the initial heat function into a gnuplot file
+		if (dim < 3)
+		{
+			myHESolver->printGrid(*alpha, GUNPLOT_RESOLUTION, "heatStart.gnuplot");
+		}
 
-	// Print the solved Heat Equation into a gnuplot file
-	if (dim < 3)
-	{
-		myHESolver->printGrid(*alpha, GUNPLOT_RESOLUTION, "heatSolved.gnuplot");
-	}
+		// set heat coefficient
+		myHESolver->setHeatCoefficient(a);
+
+		// Start solving the Heat Equation
+		if (ODESolver == "ExEul")
+		{
+			myHESolver->solveExplicitEuler(timesteps, dt, cg_its, cg_eps, *alpha, true, false, std::max(timesteps/SOLUTION_FRAMES,(size_t)1));
+		}
+		else if (ODESolver == "ImEul")
+		{
+			myHESolver->solveImplicitEuler(timesteps, dt, cg_its, cg_eps, *alpha, true, false, std::max(timesteps/SOLUTION_FRAMES,(size_t)1));
+		}
+		else if (ODESolver == "CrNic")
+		{
+			myHESolver->solveCrankNicolson(timesteps, dt, cg_its, cg_eps, *alpha, CRNIC_IMEUL_STEPS);
+		}
+
+		// Print the solved Heat Equation into a gnuplot file
+		if (dim < 3)
+		{
+			myHESolver->printGrid(*alpha, GUNPLOT_RESOLUTION, "heatSolved.gnuplot");
+		}
+
+		// Calculate Norms
+		// Evaluate Cuboid
+		DataVector PoisEvals(EvalPoints.getNrows());
+		myHESolver->evaluateCuboid(*alpha, PoisEvals, EvalPoints);
+		results.push_back(PoisEvals);
+
+		// write solution in a additional file
+		std::stringstream level_string;
+		level_string << l;
+		writeDataVector(PoisEvals, tFileEvalCuboidValues+".level_"+ level_string.str());
+		writeDataVector(PoisEvals, tFileEvalCuboidValues);
+
+		if (l > start_level)
+		{
+			std::cout << "=====================================================================" << std::endl;
+			std::cout << "=====================================================================" << std::endl << std::endl;
+			std::cout << "Calculating norms of relative errors to a grid" << std::endl;
+			std::cout << "with " << l << " levels and testing-coboid" << std::endl;
+			std::cout << "with the bounding box:" << std::endl;
+			for (size_t j = 0; j < dim; j++)
+			{
+				std::cout << myBoundingBox->getBoundary(j).leftBoundary << " " << myBoundingBox->getBoundary(j).rightBoundary << std::endl;
+			}
+			std::cout << std::endl << std::endl;
+
+			double oldMaxNorm = 0.0;
+			double oldTwoNorm = 0.0;
+
+			// Calculate relative errors and some norms
+			for (size_t j = 0; j < l-start_level; j++)
+			{
+				DataVector maxLevel(results[l-start_level]);
+				DataVector relError(results[j]);
+				double maxNorm = 0.0;
+				double l2Norm = 0.0;
+
+				// calculate relative error
+				relError.sub(maxLevel);
+				relError.componentwise_div(maxLevel);
+
+				// calculate max. norm of relative error
+				maxNorm = relError.maxNorm();
+
+				// calculate two norm of relative error
+				l2Norm = relError.RMSNorm();
+
+				// Printing norms
+				std::cout << "Level " << j + start_level << ": max-norm(rel-error)=" << maxNorm << "; two-norm(rel-error)=" << l2Norm << "; rate max-norm: " << log(oldMaxNorm/maxNorm) << "; rate two-norm: " << log(oldTwoNorm/l2Norm) << std::endl;
+
+				oldMaxNorm = maxNorm;
+				oldTwoNorm = l2Norm;
+			}
+		}
+		std::cout << std::endl << std::endl;
 
 #ifdef EXPORT_MATRIX_FILES
-	// print inner matrix
-	std::stringstream mtxFile;
-	mtxFile.clear();
-	alpha->setAll(0.0);
-	if (initFunc == "smooth")
-	{
-		myHESolver->initGridWithSmoothHeat(*alpha, bound_right, bound_right/DIV_SIGMA, DISTRI_FACTOR);
-	}
-	else
-	{
-		writeHelp();
-	}
-	mtxFile << "SG_HeatEquation_InnerMatrix_" << dim << "d_" << level << "l.mtx";
-	myHESolver->storeInnerMatrix(*alpha, mtxFile.str(), dt);
+		// print inner matrix
+		std::stringstream mtxFile;
+		mtxFile.clear();
+		alpha->setAll(0.0);
+		if (initFunc == "smooth")
+		{
+			myHESolver->initGridWithSmoothHeat(*alpha, bound_right, bound_right/DIV_SIGMA, DISTRI_FACTOR);
+		}
+		else
+		{
+			writeHelp();
+		}
+		mtxFile << "SG_HeatEquation_InnerMatrix_" << dim << "d_" << level << "l.mtx";
+		myHESolver->storeInnerMatrix(*alpha, mtxFile.str(), dt);
 
-	// print inner matrix, diagonal
-	std::stringstream mtxFileDiagonal;
-	mtxFileDiagonal.clear();
-	alpha->setAll(0.0);
-	if (initFunc == "smooth")
-	{
-		myHESolver->initGridWithSmoothHeat(*alpha, bound_right, bound_right/DIV_SIGMA, DISTRI_FACTOR);
-	}
-	else
-	{
-		writeHelp();
-	}
-	mtxFileDiagonal << "SG_HeatEquation_InnerMatrixDiagonal_" << dim << "d_" << level << "l.mtx";
-	myHESolver->storeInnerMatrixDiagonal(*alpha, mtxFileDiagonal.str(), dt);
+		// print inner matrix, diagonal
+		std::stringstream mtxFileDiagonal;
+		mtxFileDiagonal.clear();
+		alpha->setAll(0.0);
+		if (initFunc == "smooth")
+		{
+			myHESolver->initGridWithSmoothHeat(*alpha, bound_right, bound_right/DIV_SIGMA, DISTRI_FACTOR);
+		}
+		else
+		{
+			writeHelp();
+		}
+		mtxFileDiagonal << "SG_HeatEquation_InnerMatrixDiagonal_" << dim << "d_" << level << "l.mtx";
+		myHESolver->storeInnerMatrixDiagonal(*alpha, mtxFileDiagonal.str(), dt);
 
-	// print inner matrix, diagonal row sum
-	std::stringstream mtxFileDiagonalRowSum;
-	mtxFileDiagonalRowSum.clear();
-	alpha->setAll(0.0);
-	if (initFunc == "smooth")
-	{
-		myHESolver->initGridWithSmoothHeat(*alpha, bound_right, bound_right/DIV_SIGMA, DISTRI_FACTOR);
-	}
-	else
-	{
-		writeHelp();
-	}
-	mtxFileDiagonalRowSum << "SG_HeatEquation_InnerMatrixDiagonalRowSum_" << dim << "d_" << level << "l.mtx";
-	myHESolver->storeInnerMatrixDiagonalRowSum(*alpha, mtxFileDiagonalRowSum.str(), dt);
+		// print inner matrix, diagonal row sum
+		std::stringstream mtxFileDiagonalRowSum;
+		mtxFileDiagonalRowSum.clear();
+		alpha->setAll(0.0);
+		if (initFunc == "smooth")
+		{
+			myHESolver->initGridWithSmoothHeat(*alpha, bound_right, bound_right/DIV_SIGMA, DISTRI_FACTOR);
+		}
+		else
+		{
+			writeHelp();
+		}
+		mtxFileDiagonalRowSum << "SG_HeatEquation_InnerMatrixDiagonalRowSum_" << dim << "d_" << level << "l.mtx";
+		myHESolver->storeInnerMatrixDiagonalRowSum(*alpha, mtxFileDiagonalRowSum.str(), dt);
 
-	// print inner rhs
-	std::stringstream rhsFile;
-	rhsFile.clear();
-	alpha->setAll(0.0);
-	if (initFunc == "smooth")
-	{
-		myHESolver->initGridWithSmoothHeat(*alpha, bound_right, bound_right/DIV_SIGMA, DISTRI_FACTOR);
-	}
-	else
-	{
-		writeHelp();
-	}
-	rhsFile << "SG_HeatEquation_InnerRHS_" << dim << "d_" << level << "l.vec";
-	myHESolver->storeInnerRHS(*alpha, rhsFile.str(), dt);
+		// print inner rhs
+		std::stringstream rhsFile;
+		rhsFile.clear();
+		alpha->setAll(0.0);
+		if (initFunc == "smooth")
+		{
+			myHESolver->initGridWithSmoothHeat(*alpha, bound_right, bound_right/DIV_SIGMA, DISTRI_FACTOR);
+		}
+		else
+		{
+			writeHelp();
+		}
+		rhsFile << "SG_HeatEquation_InnerRHS_" << dim << "d_" << level << "l.vec";
+		myHESolver->storeInnerRHS(*alpha, rhsFile.str(), dt);
 
-	// print inner solution
-	std::stringstream solFile;
-	solFile.clear();
-	alpha->setAll(0.0);
-	if (initFunc == "smooth")
-	{
-		myHESolver->initGridWithSmoothHeat(*alpha, bound_right, bound_right/DIV_SIGMA, DISTRI_FACTOR);
-	}
-	else
-	{
-		writeHelp();
-	}
-	solFile << "SG_HeatEquation_InnerSolution_" << dim << "d_" << level << "l.vec";
-	myHESolver->storeInnerSolution(*alpha, timesteps, dt, cg_its, cg_eps, solFile.str());
-	std::cout << std::endl << std::endl;
+		// print inner solution
+		std::stringstream solFile;
+		solFile.clear();
+		alpha->setAll(0.0);
+		if (initFunc == "smooth")
+		{
+			myHESolver->initGridWithSmoothHeat(*alpha, bound_right, bound_right/DIV_SIGMA, DISTRI_FACTOR);
+		}
+		else
+		{
+			writeHelp();
+		}
+		solFile << "SG_HeatEquation_InnerSolution_" << dim << "d_" << level << "l.vec";
+		myHESolver->storeInnerSolution(*alpha, timesteps, dt, cg_its, cg_eps, solFile.str());
+		std::cout << std::endl << std::endl;
 #endif
+		// Iteration cleanup
+		myHESolver->deleteGrid();
+		delete alpha;
+	}
 
 	delete myHESolver;
 	delete myBoundingBox;
-	delete alpha;
 }
 
 void testPoissonEquation(size_t dim, size_t start_level, size_t end_level, double bound_left, double bound_right,
@@ -313,7 +390,7 @@ void testPoissonEquation(size_t dim, size_t start_level, size_t end_level, doubl
 	DataMatrix EvalPoints(1, dim);
 	std::string tFileEvalCuboid = "EvalPointsPoisson";
 	std::string tFileEvalCuboidValues = "EvalValuesPoisson";
-	size_t evalPoints = 21;
+	size_t evalPoints = NUMEVALPOINTS;
 	std::vector<DataVector> results;
 
 	// set the bounding box
@@ -503,14 +580,15 @@ int main(int argc, char *argv[])
 
 	if (option == "HeatEquation")
 	{
-		if (argc != 13)
+		if (argc != 14)
 		{
 			writeHelp();
 			return 0;
 		}
 
 		size_t dim;
-		size_t level;
+		size_t start_level;
+		size_t end_level;
 		double bound_left;
 		double bound_right;
 		double a;
@@ -522,18 +600,19 @@ int main(int argc, char *argv[])
 		size_t cg_its;
 
 		dim = atoi(argv[2]);
-		level = atoi(argv[3]);
-		bound_left = atof(argv[4]);
-		bound_right = atof(argv[5]);
-		a = atof(argv[6]);
-		initFunc.assign(argv[7]);
-		T = atof(argv[8]);
-		dt = atof(argv[9]);
-		ODESolver.assign(argv[10]);
-		cg_eps = atof(argv[11]);
-		cg_its = atoi(argv[12]);
+		start_level = atoi(argv[3]);
+		end_level = atoi(argv[4]);
+		bound_left = atof(argv[5]);
+		bound_right = atof(argv[6]);
+		a = atof(argv[7]);
+		initFunc.assign(argv[8]);
+		T = atof(argv[9]);
+		dt = atof(argv[10]);
+		ODESolver.assign(argv[11]);
+		cg_eps = atof(argv[12]);
+		cg_its = atoi(argv[13]);
 
-		testHeatEquation(dim, level, bound_left, bound_right, a, initFunc, T, dt, ODESolver, cg_eps, cg_its);
+		testHeatEquation(dim, start_level, end_level, bound_left, bound_right, a, initFunc, T, dt, ODESolver, cg_eps, cg_its);
 	}
 	else if (option == "PoissonEquation")
 	{
