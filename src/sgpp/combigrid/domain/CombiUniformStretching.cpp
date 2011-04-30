1d691322b246fd3884079f9ddfd1ad9323b3a82b
Valeriy Khakhutskyy <khakhutv@in.tum.de>
2011-04-30 09:18:41 +0000

    r1225:1379 from trunk merged into the branch
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/branches/refactoring@1405 4eea3252-f0fb-4393-894d-40516dce545b

---
+++ src/sgpp/combigrid/domain/CombiUniformStretching.cpp
@@ -0,0 +1,23 @@
+/*
+ * CombiUniformStretching.cpp
+ *
+ *  Created on: Apr 4, 2011
+ *      Author: benk
+ */
+
+#include "CombiUniformStretching.hpp"
+
+using namespace combigrid;
+
+void UniformStretching::get1DStretching(
+		int level , double min, double max,
+		std::vector<double>& stretching) const{
+
+	int nrPoints = combigrid::powerOfTwo[level]+1;
+	stretching.resize( nrPoints , 0.0 );
+
+	// set each point just uniformly
+	for (int i = 0 ; i < nrPoints ; i++){
+		stretching[i] = min + ((double)i)*(max-min)/(nrPoints-1);
+	}
+}
