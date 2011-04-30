f783a56de50f30af1f2db8947cff8a8f13ee32ec
Valeriy Khakhutskyy <khakhutv@in.tum.de>
2011-04-30 08:57:17 +0000

    r1225:1379 from trunk merged into the branch
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/branches/refactoring@1389 4eea3252-f0fb-4393-894d-40516dce545b

--- src/sgpp/combigrid/combischeme/CombiSchemeBasis.hpp
+++ src/sgpp/combigrid/combischeme/CombiSchemeBasis.hpp
@@ -20,8 +20,15 @@ namespace combigrid {
 
 	public:
 
-		/** Ctor with no argument */
-		CombiSchemeBasis(int dim) { dim_ = dim; levels_vector_.resize(0); cofficients_.resize(0); }
+		/** Ctor */
+		CombiSchemeBasis(int dim, int level) {
+			dim_ = dim; levels_vector_.resize(0); levels_.resize(dim_,level); cofficients_.resize(0);
+		}
+
+		/** Ctor */
+		CombiSchemeBasis(int dim, const std::vector<int>& levels) {
+			dim_ = dim; levels_vector_.resize(0); levels_ = levels; cofficients_.resize(0);
+		}
 
 		/** return the dimension of the combi scheme */
 		inline int getDim() const { return dim_; }
@@ -31,6 +38,9 @@ namespace combigrid {
 
 		/** returns the level vector for one subspace */
 		inline const std::vector<int>& getLevel(int i) const { return levels_vector_[i]; }
+
+		/** the levels of the full grid which we extrapolate with the combi scheme*/
+		inline const std::vector<int>& getMaxLevel() const { return levels_; }
 
 		/** returns the coefficient for one subspace */
 		inline double getCoef(int i) const { return cofficients_[i]; }
@@ -45,6 +55,9 @@ namespace combigrid {
 
 		/** the level vector for each space */
 		std::vector< std::vector<int> > levels_vector_;
+
+		/** the levels of the full grid which we*/
+		std::vector<int> levels_;
 
 		/** the coefficients for the spaces */
 		std::vector< double > cofficients_;
