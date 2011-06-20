8e9b924d0756e17ef47d0cf6f5c07c0549f1b041
Kilian Roehner <roehner@in.tum.de>
2011-06-20 10:46:00 +0000

    some fixes on the poly hierarchisation
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@1540 4eea3252-f0fb-4393-894d-40516dce545b

--- src/sgpp/basis/poly/algorithm_sweep/HierarchisationPoly.cpp
+++ src/sgpp/basis/poly/algorithm_sweep/HierarchisationPoly.cpp
@@ -12,6 +12,9 @@
 
 #include "basis/poly/algorithm_sweep/HierarchisationPoly.hpp"
 
+// #include <iostream>
+
+
 namespace sg
 {
 
@@ -28,13 +31,13 @@ HierarchisationPoly::~HierarchisationPoly()
 
 void HierarchisationPoly::operator()(DataVector& source, DataVector& result, grid_iterator& index, size_t dim)
 {
-	// double &koeffs;
-	DataVector koeffs(index.getGridDepth(dim));
+	DataVector koeffs(index.getGridDepth(dim)+1);
 	koeffs.setAll(0.0);
-	rec(source, result, index, dim, koeffs);
+	rec(source, result, index, dim, koeffs, 1, 1);
+	// std::cout << "fertig\n";
 }
 
-void HierarchisationPoly::rec(DataVector& source, DataVector& result, grid_iterator& index, size_t dim, DataVector& koeffs)
+void HierarchisationPoly::rec(DataVector& source, DataVector& result, grid_iterator& index, size_t dim, DataVector& koeffs, level_type cur_lev, index_type cur_ind)
 {
 	// current position on the grid
 	size_t seq = index.seq();
@@ -42,21 +45,22 @@ void HierarchisationPoly::rec(DataVector& source, DataVector& result, grid_itera
 	// value in the middle, needed for recursive call and calculation of the hierarchical surplus
 	double fm = source[seq];
 	
-	GridStorage::index_type::level_type cur_lev;
-	GridStorage::index_type::index_type cur_ind;
-	
+	GridStorage::index_type::level_type cur_level;
+	GridStorage::index_type::index_type cur_index;
+
+	// std::cout << cur_level << "\t" << cur_index << "\n";
+
 	// get current level and index from grid
-	index.get(seq, cur_lev, cur_ind);
-	
+	index.get(seq, cur_level, cur_index);
+	// std::cout << cur_level << "\t" << cur_index << "\n";
 	// calculate the current absolute position
-	double abs = cur_ind*(pow(2.0, -1*cur_lev));
-	
+	double abs = cur_ind/(pow(2.0, cur_lev));
+	// std::cout << "lev: "<< cur_lev << ", ind: " << cur_ind << ", abs: " << abs << "\n";
 	// calculate the weighted "so far" sum of the basis funktions at this point
 	double val = this->base.evalHierToTop(cur_lev, cur_ind, abs, koeffs);
-	
 	// hierarchisation
 	result[seq] = fm - val;
-	
+	// std::cout << "fm: " << fm << ", val: " << val << ", result: " << result[seq] << "\n";
 	// recursive calls for the right and left side of the current node
 	if(index.hint() == false)
 	{
@@ -66,14 +70,14 @@ void HierarchisationPoly::rec(DataVector& source, DataVector& result, grid_itera
 		index.left_child(dim);
 		if(!storage->end(index.seq()))
 		{
-			rec(source, result, index, dim, koeffs);
+			rec(source, result, index, dim, koeffs, cur_lev+1, 2*cur_ind-1);
 		}
 
 		// descend right
 		index.step_right(dim);
 		if(!storage->end(index.seq()))
 		{
-			rec(source, result, index, dim, koeffs);
+			rec(source, result, index, dim, koeffs, cur_lev+1, 2*cur_ind+1);
 		}
 
 		// ascend
