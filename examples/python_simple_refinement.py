94b23c22a61cb007d84ff6bd6f60e90d603bca97
Dirk Pflueger <dirk.pflueger@ipvs.uni-stuttgart.de>
2011-07-07 12:45:13 +0000

    Update documentation: added refinement example
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@1607 4eea3252-f0fb-4393-894d-40516dce545b

---
+++ examples/python_simple_refinement.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+# import modules
+import sys
+# append trunk/bin to search path for modules
+sys.path.append('../lib/pysgpp')
+from pysgpp import *
+
+# create a two-dimensional piecewise bi-linear grid
+dim = 2
+grid = Grid.createLinearGrid(dim)
+gridStorage = grid.getStorage()
+print "dimensionality:         %d" % (dim)
+
+# create regular grid, level 3
+level = 3
+gridGen = grid.createGridGenerator()
+gridGen.regular(level)
+print "Start: number of grid points:  %d" % (gridStorage.size())
+
+# definition of function to interpolate - nonsymmetric(!)
+f = lambda x0, x1: 16.0 * (x0-1)*x0 * (x1-1)*x1*x1
+# create coefficient vector
+alpha = DataVector(gridStorage.size())
+
+# now refine adaptively 5 times
+for refnum in range(5):
+    # set function values in alpha
+    for i in xrange(gridStorage.size()):
+        gp = gridStorage.get(i)
+        alpha[i] = f(gp.abs(0), gp.abs(1))
+
+    # hierarchize
+    createOperationHierarchisation(grid).doHierarchisation(alpha)
+
+    # refine a single grid point each time
+    gridGen.refine(SurplusRefinementFunctor(alpha, 1))
+    print "Refinement step %d, new grid size: %d" % (refnum+1, gridStorage.size())
+
+    # extend alpha vector (new entries uninitialized)
+    alpha.resize(gridStorage.size())
