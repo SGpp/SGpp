c7d7f33b67df4ad422a91c39177ffc46669b304b
Dirk Pflueger <dirk.pflueger@ipvs.uni-stuttgart.de>
2011-07-28 06:48:13 +0000

    Added missing src/sgpp/operation/common/OperationQuadrature.hpp
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@1667 4eea3252-f0fb-4393-894d-40516dce545b

---
+++ src/sgpp/operation/common/OperationQuadrature.hpp
@@ -0,0 +1,46 @@
+/******************************************************************************
+* Copyright (C) 2009 Technische Universitaet Muenchen                         *
+* This file is part of the SG++ project. For conditions of distribution and   *
+* use, please see the copyright notice at http://www5.in.tum.de/SGpp          *
+******************************************************************************/
+// @author Dirk Pflueger (pflueged@in.tum.de)
+
+#ifndef OPERATIONQUADRATURE_HPP
+#define OPERATIONQUADRATURE_HPP
+
+#include "data/DataVector.hpp"
+
+namespace sg
+{
+namespace base
+{
+
+/**
+ * This class provides the quadrature of a sparse grid function
+ */
+class OperationQuadrature
+{
+public:
+	/**
+	 * Constructor
+	 */
+	OperationQuadrature() {}
+
+	/**
+	 * Destructor
+	 */
+	virtual ~OperationQuadrature() {}
+
+	/**
+	 * Integrate the sparse grid function
+	 *
+	 * @param node_values the function's values in the nodal basis
+	 */
+	virtual double doQuadrature(DataVector& alpha) = 0;
+
+};
+
+}
+}
+
+#endif /* OPERATIONQUADRATURE_HPP */
