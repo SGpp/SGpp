7e69fe6abb80a93e7f4cbfb5d3bed09fd5bad53a
benk <benk>
2011-07-29 08:08:10 +0000

    - separate header for solvers created
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@1676 4eea3252-f0fb-4393-894d-40516dce545b

---
+++ src/sgpp/combigrid_solvers.hpp
@@ -0,0 +1,15 @@
+/*
+ * combigrid_solvers.hpp
+ *
+ *  Created on: Jul 29, 2011
+ *      Author: benk
+ */
+
+#ifndef COMBIGRID_SOLVERS_HPP_
+#define COMBIGRID_SOLVERS_HPP_
+
+// ------ The regularization solvers --------
+#include "combigrid/multigridFG/utils/RunPoisson.hpp"
+#include "combigrid/multigridFG/utils/RunTikhonov.hpp"
+
+#endif /* COMBIGRID_SOLVERS_HPP_ */
