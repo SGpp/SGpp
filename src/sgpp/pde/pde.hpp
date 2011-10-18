03bc5cbfb2bf0e2f887e8c2ed33e3b7e7f725f27
Dirk Pflueger <dirk.pflueger@ipvs.uni-stuttgart.de>
2011-10-18 21:14:17 +0000

    Added missing files
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@1823 4eea3252-f0fb-4393-894d-40516dce545b

---
+++ src/sgpp/pde/pde.hpp
@@ -0,0 +1,26 @@
+/******************************************************************************
+* Copyright (C) 2009 Technische Universitaet Muenchen                         *
+* This file is part of the SG++ project. For conditions of distribution and   *
+* use, please see the copyright notice at http://www5.in.tum.de/SGpp          *
+******************************************************************************/
+// @author Dirk Pflueger (pflueged@in.tum.de
+
+#ifndef PDE_HPP
+#define PDE_HPP
+
+
+#include "algorithm/pde/HeatEquationParabolicPDESolverSystem.hpp"
+#include "algorithm/pde/PoissonEquationEllipticPDESolverSystemDirichlet.hpp"
+#include "application/pde/HeatEquationSolver.hpp"
+#include "application/pde/HeatEquationSolverWithStretching.hpp"
+#include "application/pde/LaserHeatEquationSolver2D.hpp"
+#include "application/pde/PoissonEquationSolver.hpp"
+#include "basis/linear/noboundary/operation/pde/OperationLaplaceLinear.hpp"
+#include "basis/linear/boundary/operation/pde/OperationLaplaceLinearBoundary.hpp"
+#include "basis/linearstretched/noboundary/operation/pde/OperationLaplaceLinearStretched.hpp"
+#include "basis/linearstretched/boundary/operation/pde/OperationLaplaceLinearStretchedBoundary.hpp"
+#include "basis/modlinear/operation/pde/OperationLaplaceModLinear.hpp"
+
+//#include "pde/operation/PdeOpFactory.hpp"
+
+#endif /* PDE_HPP */
