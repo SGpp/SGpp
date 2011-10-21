bddbb289c905051eb691031eb5ec5ac9896a2e00
Alexander Heinecke <heinecke@in.tum.de>
2011-10-21 20:47:19 +0000

    updated auto tuning parameters
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@1850 4eea3252-f0fb-4393-894d-40516dce545b

--- src/sgpp/basis/linear/noboundary/operation/datadriven/OperationMultipleEvalIterativeSPHybridX86SimdOCLLinear.cpp
+++ src/sgpp/basis/linear/noboundary/operation/datadriven/OperationMultipleEvalIterativeSPHybridX86SimdOCLLinear.cpp
@@ -30,8 +30,8 @@ OperationMultipleEvalIterativeSPHybridX86SimdOCLLinear::OperationMultipleEvalIte
 	myTimer = new sg::base::SGppStopwatch();
 	myOCLKernels = new OCLKernels();
 
-	_tuningMult = new sg::base::TwoPartitionAutoTuning(dataset->getNrows(), 128, 10, 0.75, 3);
-	_tuningMultTrans = new sg::base::TwoPartitionAutoTuning(storage->size(), 128, 10, 0.85, 3);
+	_tuningMult = new sg::base::TwoPartitionAutoTuning(dataset->getNrows(), 128, 10, 0.75, 15);
+	_tuningMultTrans = new sg::base::TwoPartitionAutoTuning(storage->size(), 128, 10, 0.75, 15);
 }
 
 OperationMultipleEvalIterativeSPHybridX86SimdOCLLinear::~OperationMultipleEvalIterativeSPHybridX86SimdOCLLinear()
