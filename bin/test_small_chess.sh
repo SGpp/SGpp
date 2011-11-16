4a5a032a2fbda8ebc535a0c1bfb97c4fc1f316db
Alexander Heinecke <heinecke@in.tum.de>
2011-11-16 20:03:52 +0000

    added new test script for small chess dataset
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@1917 4eea3252-f0fb-4393-894d-40516dce545b

---
+++ bin/test_small_chess.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# run specific variables, please adjust
+export OMP_NUM_THREADS=8
+# prefix for result files
+RESPRE=SNB
+VECTYPE="AVX"
+PRECISION="SP DP"
+
+###### DO NOT MODIFY BELOW THIS LINE #######
+export KMP_AFFINITY=compact,granularity=thread,verbose
+. /opt/intel/bin/compilervars.sh intel64
+for PREC in $PRECISION
+do
+	#  mod linear
+	../bin/ClassifyBenchmark ../input/CB5d_train_unit.labeled ../input/CB5d_test_unit.labeled 0 ${PREC} modlinear 4 0.000000316228 200 0.0001 0 0.0 100 200 0.0001 2>&1 | tee ../log/${RESPRE}_cod-rna_${VEC}_modlinear_${PREC}_Level_4.log
+	../bin/ClassifyBenchmark ../input/CB5d_train_unit.labeled ../input/CB5d_test_unit.labeled 0 ${PREC} modlinear 5 0.00000001 200 0.0001 0 0.0 100 200 0.0001 2>&1 | tee ../log/${RESPRE}_cod-rna_${VEC}_modlinear_${PREC}_Level_5.log
+	../bin/ClassifyBenchmark ../input/CB5d_train_unit.labeled ../input/CB5d_test_unit.labeled 0 ${PREC} modlinear 6 0.000000316228 200 0.0001 0 0.0 100 200 0.0001 2>&1 | tee ../log/${RESPRE}_cod-rna_${VEC}_modlinear_${PREC}_Level_6.log
+	../bin/ClassifyBenchmark ../input/CB5d_train_unit.labeled ../input/CB5d_test_unit.labeled 0 ${PREC} modlinear 7 0.000001 200 0.0001 0 0.0 100 200 0.0001 2>&1 | tee ../log/${RESPRE}_cod-rna_${VEC}_modlinear_${PREC}_Level_7.log
+	../bin/ClassifyBenchmark ../input/CB5d_train_unit.labeled ../input/CB5d_test_unit.labeled 0 ${PREC} modlinear 8 0.000001 200 0.0001 0 0.0 100 200 0.0001 2>&1 | tee ../log/${RESPRE}_cod-rna_${VEC}_modlinear_${PREC}_Level_8.log
+done
