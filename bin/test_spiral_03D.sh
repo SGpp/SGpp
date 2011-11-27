866abafa7d4fe57d3657cd4956bf434892b1363b
Alexander Heinecke <heinecke@in.tum.de>
2011-11-27 15:19:26 +0000

    updated dataset testscripts
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@1976 4eea3252-f0fb-4393-894d-40516dce545b

--- bin/test_spiral_03D.sh
+++ bin/test_spiral_03D.sh
@@ -12,12 +12,12 @@ export KMP_AFFINITY=compact,granularity=thread,verbose
 for PREC in $PRECISION
 do
 	#  mod linear
-	../bin/ClassifyBenchmark ../input/spiral_03D_train_unit.arff ../input/spiral_03D_test_unit.arff 0 ${PREC} modlinear 4 0.000001 250 0.00000001 0 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_chess_small_modlinear_${PREC}_Level_4.log
-	../bin/ClassifyBenchmark ../input/spiral_03D_train_unit.arff ../input/spiral_03D_test_unit.arff 0 ${PREC} modlinear 5 0.000001 250 0.00000001 0 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_chess_small_modlinear_${PREC}_Level_5.log
-	../bin/ClassifyBenchmark ../input/spiral_03D_train_unit.arff ../input/spiral_03D_test_unit.arff 0 ${PREC} modlinear 6 0.000001 250 0.00000001 0 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_chess_small_modlinear_${PREC}_Level_6.log
-	../bin/ClassifyBenchmark ../input/spiral_03D_train_unit.arff ../input/spiral_03D_test_unit.arff 0 ${PREC} modlinear 7 0.000001 250 0.00000001 0 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_chess_small_modlinear_${PREC}_Level_7.log
-	../bin/ClassifyBenchmark ../input/spiral_03D_train_unit.arff ../input/spiral_03D_test_unit.arff 0 ${PREC} modlinear 8 0.000001 250 0.00000001 0 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_chess_small_modlinear_${PREC}_Level_8.log
-	../bin/ClassifyBenchmark ../input/spiral_03D_train_unit.arff ../input/spiral_03D_test_unit.arff 0 ${PREC} modlinear 9 0.000001 250 0.00000001 0 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_chess_small_modlinear_${PREC}_Level_9.log
-	../bin/ClassifyBenchmark ../input/spiral_03D_train_unit.arff ../input/spiral_03D_test_unit.arff 0 ${PREC} modlinear 10 0.000001 250 0.00000001 0 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_chess_small_modlinear_${PREC}_Level_10.log
-	../bin/ClassifyBenchmark ../input/spiral_03D_train_unit.arff ../input/spiral_03D_test_unit.arff 0 ${PREC} modlinear 4 0.000001 250 0.00000001 7 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_chess_small_modlinear_${PREC}_adaptive.log
+	../bin/ClassifyBenchmark ../input/spiral_03D_train_unit.arff ../input/spiral_03D_test_unit.arff 0 ${PREC} modlinear 4 0.000001 250 0.00000001 0 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_spiral_03D_modlinear_${PREC}_Level_4.log
+	../bin/ClassifyBenchmark ../input/spiral_03D_train_unit.arff ../input/spiral_03D_test_unit.arff 0 ${PREC} modlinear 5 0.000001 250 0.00000001 0 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_spiral_03D_modlinear_${PREC}_Level_5.log
+	../bin/ClassifyBenchmark ../input/spiral_03D_train_unit.arff ../input/spiral_03D_test_unit.arff 0 ${PREC} modlinear 6 0.000001 250 0.00000001 0 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_spiral_03D_modlinear_${PREC}_Level_6.log
+	../bin/ClassifyBenchmark ../input/spiral_03D_train_unit.arff ../input/spiral_03D_test_unit.arff 0 ${PREC} modlinear 7 0.000001 250 0.00000001 0 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_spiral_03D_modlinear_${PREC}_Level_7.log
+	../bin/ClassifyBenchmark ../input/spiral_03D_train_unit.arff ../input/spiral_03D_test_unit.arff 0 ${PREC} modlinear 8 0.000001 250 0.00000001 0 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_spiral_03D_modlinear_${PREC}_Level_8.log
+	../bin/ClassifyBenchmark ../input/spiral_03D_train_unit.arff ../input/spiral_03D_test_unit.arff 0 ${PREC} modlinear 9 0.000001 250 0.00000001 0 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_spiral_03D_modlinear_${PREC}_Level_9.log
+	../bin/ClassifyBenchmark ../input/spiral_03D_train_unit.arff ../input/spiral_03D_test_unit.arff 0 ${PREC} modlinear 10 0.000001 250 0.00000001 0 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_spiral_03D_modlinear_${PREC}_Level_10.log
+	../bin/ClassifyBenchmark ../input/spiral_03D_train_unit.arff ../input/spiral_03D_test_unit.arff 0 ${PREC} modlinear 4 0.000001 250 0.00000001 7 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_spiral_03D_modlinear_${PREC}_adaptive.log
 done
