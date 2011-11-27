a1c96cf67ce39ed8d8bc7771dce535e21a45e97e
Alexander Heinecke <heinecke@in.tum.de>
2011-11-27 15:13:28 +0000

    updated classification test scripts
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@1975 4eea3252-f0fb-4393-894d-40516dce545b

--- bin/test_small_chess.sh
+++ bin/test_small_chess.sh
@@ -4,7 +4,7 @@ export OMP_NUM_THREADS=24
 # prefix for result files
 RESPRE=WSM-EP
 VECTYPE="SSE"
-PRECISION="DP"
+PRECISION="SP DP"
 
 ###### DO NOT MODIFY BELOW THIS LINE #######
 export KMP_AFFINITY=compact,granularity=thread,verbose
@@ -12,12 +12,12 @@ export KMP_AFFINITY=compact,granularity=thread,verbose
 for PREC in $PRECISION
 do
 	#  mod linear
-	../bin/ClassifyBenchmark ../input/CB5d_train_unit.labeled ../input/CB5d_test_unit.labeled 0 ${PREC} modlinear 4 0.000000316228 250 0.00000001 0 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_chess_small_modlinear_${PREC}_Level_4.log
-	../bin/ClassifyBenchmark ../input/CB5d_train_unit.labeled ../input/CB5d_test_unit.labeled 0 ${PREC} modlinear 5 0.00000001 250 0.00000001 0 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_chess_small_modlinear_${PREC}_Level_5.log
-	../bin/ClassifyBenchmark ../input/CB5d_train_unit.labeled ../input/CB5d_test_unit.labeled 0 ${PREC} modlinear 6 0.000000316228 250 0.00000001 0 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_chess_small_modlinear_${PREC}_Level_6.log
-	../bin/ClassifyBenchmark ../input/CB5d_train_unit.labeled ../input/CB5d_test_unit.labeled 0 ${PREC} modlinear 7 0.000001 250 0.00000001 0 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_chess_small_modlinear_${PREC}_Level_7.log
-	../bin/ClassifyBenchmark ../input/CB5d_train_unit.labeled ../input/CB5d_test_unit.labeled 0 ${PREC} modlinear 8 0.000001 250 0.00000001 0 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_chess_small_modlinear_${PREC}_Level_8.log
-	../bin/ClassifyBenchmark ../input/CB5d_train_unit.labeled ../input/CB5d_test_unit.labeled 0 ${PREC} modlinear 9 0.000001 250 0.00000001 0 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_chess_small_modlinear_${PREC}_Level_9.log
-	../bin/ClassifyBenchmark ../input/CB5d_train_unit.labeled ../input/CB5d_test_unit.labeled 0 ${PREC} modlinear 10 0.000001 250 0.00000001 0 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_chess_small_modlinear_${PREC}_Level_10.log
-	../bin/ClassifyBenchmark ../input/CB5d_train_unit.labeled ../input/CB5d_test_unit.labeled 0 ${PREC} modlinear 4 0.000001 250 0.00000001 7 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_chess_small_modlinear_${PREC}_adaptive.log
+	../bin/ClassifyBenchmark ../input/chess_05D_small_3fields_tr.dat.arff ../input/chess_05D_small_3fields_te.dat.arff 0 ${PREC} modlinear 4 0.000000316228 250 0.00000001 0 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_chess_small_modlinear_${PREC}_Level_4.log
+	../bin/ClassifyBenchmark ../input/chess_05D_small_3fields_tr.dat.arff ../input/chess_05D_small_3fields_te.dat.arff 0 ${PREC} modlinear 5 0.00000001 250 0.00000001 0 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_chess_small_modlinear_${PREC}_Level_5.log
+	../bin/ClassifyBenchmark ../input/chess_05D_small_3fields_tr.dat.arff ../input/chess_05D_small_3fields_te.dat.arff 0 ${PREC} modlinear 6 0.000000316228 250 0.00000001 0 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_chess_small_modlinear_${PREC}_Level_6.log
+	../bin/ClassifyBenchmark ../input/chess_05D_small_3fields_tr.dat.arff ../input/chess_05D_small_3fields_te.dat.arff 0 ${PREC} modlinear 7 0.000001 250 0.00000001 0 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_chess_small_modlinear_${PREC}_Level_7.log
+	../bin/ClassifyBenchmark ../input/chess_05D_small_3fields_tr.dat.arff ../input/chess_05D_small_3fields_te.dat.arff 0 ${PREC} modlinear 8 0.000001 250 0.00000001 0 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_chess_small_modlinear_${PREC}_Level_8.log
+	../bin/ClassifyBenchmark ../input/chess_05D_small_3fields_tr.dat.arff ../input/chess_05D_small_3fields_te.dat.arff 0 ${PREC} modlinear 9 0.000001 250 0.00000001 0 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_chess_small_modlinear_${PREC}_Level_9.log
+	../bin/ClassifyBenchmark ../input/chess_05D_small_3fields_tr.dat.arff ../input/chess_05D_small_3fields_te.dat.arff 0 ${PREC} modlinear 10 0.000001 250 0.00000001 0 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_chess_small_modlinear_${PREC}_Level_10.log
+	../bin/ClassifyBenchmark ../input/chess_05D_small_3fields_tr.dat.arff ../input/chess_05D_small_3fields_te.dat.arff 0 ${PREC} modlinear 4 0.000001 250 0.00000001 7 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_chess_small_modlinear_${PREC}_adaptive.log
 done
--- bin/test_small_chess_boundary.sh
+++ bin/test_small_chess_boundary.sh
@@ -4,7 +4,7 @@ export OMP_NUM_THREADS=24
 # prefix for result files
 RESPRE=WSM-EP
 VECTYPE="SSE"
-PRECISION="DP"
+PRECISION="SP DP"
 
 ###### DO NOT MODIFY BELOW THIS LINE #######
 export KMP_AFFINITY=compact,granularity=thread,verbose
@@ -12,11 +12,11 @@ export KMP_AFFINITY=compact,granularity=thread,verbose
 for PREC in $PRECISION
 do
 	#  mod linear
-	../bin/ClassifyBenchmark ../input/CB5d_train_unit.labeled ../input/CB5d_test_unit.labeled 0 ${PREC} linearboundary 4 0.000000316228 250 0.00000001 0 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_chess_small_linearboundary_${PREC}_Level_4.log
-	../bin/ClassifyBenchmark ../input/CB5d_train_unit.labeled ../input/CB5d_test_unit.labeled 0 ${PREC} linearboundary 5 0.00000001 250 0.00000001 0 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_chess_small_linearboundary_${PREC}_Level_5.log
-	../bin/ClassifyBenchmark ../input/CB5d_train_unit.labeled ../input/CB5d_test_unit.labeled 0 ${PREC} linearboundary 6 0.000000316228 250 0.00000001 0 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_chess_small_linearboundary_${PREC}_Level_6.log
-	../bin/ClassifyBenchmark ../input/CB5d_train_unit.labeled ../input/CB5d_test_unit.labeled 0 ${PREC} linearboundary 7 0.000001 250 0.00000001 0 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_chess_small_linearboundary_${PREC}_Level_7.log
-	../bin/ClassifyBenchmark ../input/CB5d_train_unit.labeled ../input/CB5d_test_unit.labeled 0 ${PREC} linearboundary 8 0.000001 250 0.00000001 0 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_chess_small_linearboundary_${PREC}_Level_8.log
-	../bin/ClassifyBenchmark ../input/CB5d_train_unit.labeled ../input/CB5d_test_unit.labeled 0 ${PREC} linearboundary 9 0.000001 250 0.00000001 0 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_chess_small_linearboundary_${PREC}_Level_9.log
-	../bin/ClassifyBenchmark ../input/CB5d_train_unit.labeled ../input/CB5d_test_unit.labeled 0 ${PREC} linearboundary 4 0.000001 250 0.00000001 7 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_chess_small_linearboundary_${PREC}_adaptive.log
+	../bin/ClassifyBenchmark ../input/chess_05D_small_3fields_tr.dat.arff ../input/chess_05D_small_3fields_te.dat.arff 0 ${PREC} linearboundary 4 0.000000316228 250 0.00000001 0 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_chess_small_linearboundary_${PREC}_Level_4.log
+	../bin/ClassifyBenchmark ../input/chess_05D_small_3fields_tr.dat.arff ../input/chess_05D_small_3fields_te.dat.arff 0 ${PREC} linearboundary 5 0.00000001 250 0.00000001 0 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_chess_small_linearboundary_${PREC}_Level_5.log
+	../bin/ClassifyBenchmark ../input/chess_05D_small_3fields_tr.dat.arff ../input/chess_05D_small_3fields_te.dat.arff 0 ${PREC} linearboundary 6 0.000000316228 250 0.00000001 0 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_chess_small_linearboundary_${PREC}_Level_6.log
+	../bin/ClassifyBenchmark ../input/chess_05D_small_3fields_tr.dat.arff ../input/chess_05D_small_3fields_te.dat.arff 0 ${PREC} linearboundary 7 0.000001 250 0.00000001 0 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_chess_small_linearboundary_${PREC}_Level_7.log
+	../bin/ClassifyBenchmark ../input/chess_05D_small_3fields_tr.dat.arff ../input/chess_05D_small_3fields_te.dat.arff 0 ${PREC} linearboundary 8 0.000001 250 0.00000001 0 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_chess_small_linearboundary_${PREC}_Level_8.log
+	../bin/ClassifyBenchmark .../input/chess_05D_small_3fields_tr.dat.arff ../input/chess_05D_small_3fields_te.dat.arff 0 ${PREC} linearboundary 9 0.000001 250 0.00000001 0 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_chess_small_linearboundary_${PREC}_Level_9.log
+	../bin/ClassifyBenchmark ../input/chess_05D_small_3fields_tr.dat.arff ../input/chess_05D_small_3fields_te.dat.arff 0 ${PREC} linearboundary 4 0.000001 250 0.00000001 7 0.0 100 20 0.000001 2>&1 | tee ../log/${RESPRE}_chess_small_linearboundary_${PREC}_adaptive.log
 done
