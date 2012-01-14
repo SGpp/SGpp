6ce340080376f9abf38162cb3addcab5daf51c39
Alexander Heinecke <heinecke@in.tum.de>
2012-01-14 22:22:39 +0000

    added new settings for small checkerboard 5d dataset
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@2047 4eea3252-f0fb-4393-894d-40516dce545b

--- bin/test_small_chess_linearboundary.sh
+++ bin/test_small_chess_linearboundary.sh
@@ -3,7 +3,7 @@
 export OMP_NUM_THREADS=24
 # prefix for result files
 RESPRE=WSM-EP
-VECTYPE="X86SIMD"
+VECTYPE="X86SIMD HYBRID_X86SIMD_OCL OCL"
 PRECISION="SP DP"
 
 ###### DO NOT MODIFY BELOW THIS LINE #######
@@ -11,12 +11,12 @@ export KMP_AFFINITY=compact,granularity=thread,verbose
 . /opt/intel/bin/compilervars.sh intel64
 for PREC in $PRECISION
 do
-	#  mod linear
-	../bin/ClassifyBenchmark ../input/chess_05D_small_3fields_tr.dat.arff ../input/chess_05D_small_3fields_te.dat.arff 0 ${PREC} linearboundary 4 0.000000316228 250 0.00000001 0 0.0 100 20 0.000001 ${VEC} 2>&1 | tee ../log/${RESPRE}_ChkBrd_05D_small_linearboundary_${PREC}_Level_4.log
-	../bin/ClassifyBenchmark ../input/chess_05D_small_3fields_tr.dat.arff ../input/chess_05D_small_3fields_te.dat.arff 0 ${PREC} linearboundary 5 0.00000001 250 0.00000001 0 0.0 100 20 0.000001 ${VEC} 2>&1 | tee ../log/${RESPRE}_ChkBrd_05D_small_linearboundary_${PREC}_Level_5.log
-	../bin/ClassifyBenchmark ../input/chess_05D_small_3fields_tr.dat.arff ../input/chess_05D_small_3fields_te.dat.arff 0 ${PREC} linearboundary 6 0.000000316228 250 0.00000001 0 0.0 100 20 0.000001 ${VEC} 2>&1 | tee ../log/${RESPRE}_ChkBrd_05D_small_linearboundary_${PREC}_Level_6.log
-	../bin/ClassifyBenchmark ../input/chess_05D_small_3fields_tr.dat.arff ../input/chess_05D_small_3fields_te.dat.arff 0 ${PREC} linearboundary 7 0.000001 250 0.00000001 0 0.0 100 20 0.000001 ${VEC} 2>&1 | tee ../log/${RESPRE}_ChkBrd_05D_small_linearboundary_${PREC}_Level_7.log
-	../bin/ClassifyBenchmark ../input/chess_05D_small_3fields_tr.dat.arff ../input/chess_05D_small_3fields_te.dat.arff 0 ${PREC} linearboundary 8 0.000001 250 0.00000001 0 0.0 100 20 0.000001 ${VEC} 2>&1 | tee ../log/${RESPRE}_ChkBrd_05D_small_linearboundary_${PREC}_Level_8.log
-	../bin/ClassifyBenchmark .../input/chess_05D_small_3fields_tr.dat.arff ../input/chess_05D_small_3fields_te.dat.arff 0 ${PREC} linearboundary 9 0.000001 250 0.00000001 0 0.0 100 20 0.000001 ${VEC} 2>&1 | tee ../log/${RESPRE}_ChkBrd_05D_small_linearboundary_${PREC}_Level_9.log
-	../bin/ClassifyBenchmark ../input/chess_05D_small_3fields_tr.dat.arff ../input/chess_05D_small_3fields_te.dat.arff 0 ${PREC} linearboundary 4 0.000001 250 0.00000001 7 0.0 100 20 0.000001 ${VEC} 2>&1 | tee ../log/${RESPRE}_ChkBrd_05D_small_linearboundary_${PREC}_adaptive.log
+	# linear boundary
+	../bin/ClassifyBenchmark ../input/chess_05D_small_3fields_tr.dat.arff ../input/chess_05D_small_3fields_te.dat.arff 0 ${PREC} linearboundary 4 0.000000316228 250 0.0001 0 0.0 100 20 0.0001 ${VEC} 2>&1 | tee ../log/${RESPRE}_ChkBrd_05D_small_linearboundary_${PREC}_Level_4.log
+	../bin/ClassifyBenchmark ../input/chess_05D_small_3fields_tr.dat.arff ../input/chess_05D_small_3fields_te.dat.arff 0 ${PREC} linearboundary 5 0.00000001 250 0.0001 0 0.0 100 20 0.0001 ${VEC} 2>&1 | tee ../log/${RESPRE}_ChkBrd_05D_small_linearboundary_${PREC}_Level_5.log
+	../bin/ClassifyBenchmark ../input/chess_05D_small_3fields_tr.dat.arff ../input/chess_05D_small_3fields_te.dat.arff 0 ${PREC} linearboundary 6 0.000000316228 250 0.0001 0 0.0 100 20 0.0001 ${VEC} 2>&1 | tee ../log/${RESPRE}_ChkBrd_05D_small_linearboundary_${PREC}_Level_6.log
+	../bin/ClassifyBenchmark ../input/chess_05D_small_3fields_tr.dat.arff ../input/chess_05D_small_3fields_te.dat.arff 0 ${PREC} linearboundary 7 0.000001 250 0.0001 0 0.0 100 20 0.0001 ${VEC} 2>&1 | tee ../log/${RESPRE}_ChkBrd_05D_small_linearboundary_${PREC}_Level_7.log
+	../bin/ClassifyBenchmark ../input/chess_05D_small_3fields_tr.dat.arff ../input/chess_05D_small_3fields_te.dat.arff 0 ${PREC} linearboundary 8 0.000001 250 0.0001 0 0.0 100 20 0.0001 ${VEC} 2>&1 | tee ../log/${RESPRE}_ChkBrd_05D_small_linearboundary_${PREC}_Level_8.log
+	../bin/ClassifyBenchmark .../input/chess_05D_small_3fields_tr.dat.arff ../input/chess_05D_small_3fields_te.dat.arff 0 ${PREC} linearboundary 9 0.000001 250 0.0001 0 0.0 100 20 0.0001 ${VEC} 2>&1 | tee ../log/${RESPRE}_ChkBrd_05D_small_linearboundary_${PREC}_Level_9.log
+	../bin/ClassifyBenchmark ../input/chess_05D_small_3fields_tr.dat.arff ../input/chess_05D_small_3fields_te.dat.arff 0 ${PREC} linearboundary 5 0.000001 200 0.0001 14 0.0 475 20 0.0001 ${VEC} 2>&1 | tee ../log/${RESPRE}_ChkBrd_05D_small_linearboundary_${PREC}_adaptive.log
 done
