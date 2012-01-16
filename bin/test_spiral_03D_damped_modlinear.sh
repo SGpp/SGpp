87bf32c3be197006433b798d2901c8574d1f592b
Alexander Heinecke <heinecke@in.tum.de>
2012-01-16 18:20:11 +0000

    updated scripts
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@2055 4eea3252-f0fb-4393-894d-40516dce545b

--- bin/test_spiral_03D_damped_modlinear.sh
+++ bin/test_spiral_03D_damped_modlinear.sh
@@ -12,11 +12,11 @@ export KMP_AFFINITY=compact,granularity=thread,verbose
 for PREC in $PRECISION
 do
 	#  mod linear
-	../bin/ClassifyBenchmark ../input/spiral_03D_damped_train_unit.arff ../input/spiral_03D_damped_test_unit.arff 0 ${PREC} modlinear 4 0.000001 175 0.0001 0 0.0 100 20 0.0001 ${VEC} 2>&1 | tee ../log/${RESPRE}_spiral_03D_damped_modlinear_${PREC}_Level_4.log
-	../bin/ClassifyBenchmark ../input/spiral_03D_damped_train_unit.arff ../input/spiral_03D_damped_test_unit.arff 0 ${PREC} modlinear 5 0.000001 175 0.0001 0 0.0 100 20 0.0001 ${VEC} 2>&1 | tee ../log/${RESPRE}_spiral_03D_damped_modlinear_${PREC}_Level_5.log
-	../bin/ClassifyBenchmark ../input/spiral_03D_damped_train_unit.arff ../input/spiral_03D_damped_test_unit.arff 0 ${PREC} modlinear 6 0.000001 175 0.0001 0 0.0 100 20 0.0001 ${VEC} 2>&1 | tee ../log/${RESPRE}_spiral_03D_damped_modlinear_${PREC}_Level_6.log
-	../bin/ClassifyBenchmark ../input/spiral_03D_damped_train_unit.arff ../input/spiral_03D_damped_test_unit.arff 0 ${PREC} modlinear 7 0.000001 175 0.0001 0 0.0 100 20 0.0001 ${VEC} 2>&1 | tee ../log/${RESPRE}_spiral_03D_damped_modlinear_${PREC}_Level_7.log
-	../bin/ClassifyBenchmark ../input/spiral_03D_damped_train_unit.arff ../input/spiral_03D_damped_test_unit.arff 0 ${PREC} modlinear 8 0.000001 175 0.0001 0 0.0 100 20 0.0001 ${VEC} 2>&1 | tee ../log/${RESPRE}_spiral_03D_damped_modlinear_${PREC}_Level_8.log
-	../bin/ClassifyBenchmark ../input/spiral_03D_damped_train_unit.arff ../input/spiral_03D_damped_test_unit.arff 0 ${PREC} modlinear 9 0.000001 175 0.0001 0 0.0 100 20 0.0001 ${VEC} 2>&1 | tee ../log/${RESPRE}_spiral_03D_damped_modlinear_${PREC}_Level_9.log
-	../bin/ClassifyBenchmark ../input/spiral_03D_damped_train_unit.arff ../input/spiral_03D_damped_test_unit.arff 0 ${PREC} modlinear 5 0.000001 125 0.0001 6 0.0 100 20 0.0001 ${VEC} 2>&1 | tee ../log/${RESPRE}_spiral_03D_damped_modlinear_${PREC}_adaptive.log
+	../bin/ClassifyBenchmark ../input/spiral_03D_damped_train_unit.arff ../input/spiral_03D_damped_test_unit.arff 0 ${PREC} modlinear 4 0.000001 175 0.0001 0 0.0 100 20 0.0001 ${VEC} 2>&1 | tee ../log/${RESPRE}_spiral_03D_damped_modlinear_${PREC}_${VEC}_Level_4.log
+	../bin/ClassifyBenchmark ../input/spiral_03D_damped_train_unit.arff ../input/spiral_03D_damped_test_unit.arff 0 ${PREC} modlinear 5 0.000001 175 0.0001 0 0.0 100 20 0.0001 ${VEC} 2>&1 | tee ../log/${RESPRE}_spiral_03D_damped_modlinear_${PREC}_${VEC}_Level_5.log
+	../bin/ClassifyBenchmark ../input/spiral_03D_damped_train_unit.arff ../input/spiral_03D_damped_test_unit.arff 0 ${PREC} modlinear 6 0.000001 175 0.0001 0 0.0 100 20 0.0001 ${VEC} 2>&1 | tee ../log/${RESPRE}_spiral_03D_damped_modlinear_${PREC}_${VEC}_Level_6.log
+	../bin/ClassifyBenchmark ../input/spiral_03D_damped_train_unit.arff ../input/spiral_03D_damped_test_unit.arff 0 ${PREC} modlinear 7 0.000001 175 0.0001 0 0.0 100 20 0.0001 ${VEC} 2>&1 | tee ../log/${RESPRE}_spiral_03D_damped_modlinear_${PREC}_${VEC}_Level_7.log
+	../bin/ClassifyBenchmark ../input/spiral_03D_damped_train_unit.arff ../input/spiral_03D_damped_test_unit.arff 0 ${PREC} modlinear 8 0.000001 175 0.0001 0 0.0 100 20 0.0001 ${VEC} 2>&1 | tee ../log/${RESPRE}_spiral_03D_damped_modlinear_${PREC}_${VEC}_Level_8.log
+	../bin/ClassifyBenchmark ../input/spiral_03D_damped_train_unit.arff ../input/spiral_03D_damped_test_unit.arff 0 ${PREC} modlinear 9 0.000001 175 0.0001 0 0.0 100 20 0.0001 ${VEC} 2>&1 | tee ../log/${RESPRE}_spiral_03D_damped_modlinear_${PREC}_${VEC}_Level_9.log
+	../bin/ClassifyBenchmark ../input/spiral_03D_damped_train_unit.arff ../input/spiral_03D_damped_test_unit.arff 0 ${PREC} modlinear 5 0.000001 125 0.0001 6 0.0 100 20 0.0001 ${VEC} 2>&1 | tee ../log/${RESPRE}_spiral_03D_damped_modlinear_${PREC}_${VEC}_adaptive.log
 done
