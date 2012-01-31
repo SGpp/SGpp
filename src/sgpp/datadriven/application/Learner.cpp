db63d9f2cc9b097fd33f2b030b055594e87067cb
Alexander Heinecke <heinecke@in.tum.de>
2012-01-31 12:30:31 +0000

    updated Learner
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@2085 4eea3252-f0fb-4393-894d-40516dce545b

--- src/sgpp/datadriven/application/Learner.cpp
+++ src/sgpp/datadriven/application/Learner.cpp
@@ -13,7 +13,7 @@ namespace sg
 namespace datadriven
 {
 
-Learner::Learner(bool verbose) : alpha_(NULL), grid_(NULL), verbose_(verbose)
+Learner::Learner(bool is Regression, bool verbose) : alpha_(NULL), grid_(NULL), verbose_(verbose), isRegression_(isRegression)
 {
 }
 
@@ -53,11 +53,19 @@ void Learner::createInitialGrid(sg::base::RegularGridConfiguration& GridConfig)
 	delete myGenerator;
 }
 
+void Learner::postProcessing()
+{
+}
+
 void Learner::trainGrid(sg::base::DataMatrix& trainDataset, sg::base::DataVector& classes, sg::base::DataMatrix& testDataset,
 		sg::solver::SLESolverConfiguration& SolverConfigRefine, sg::solver::SLESolverConfiguration& SolverConfigFinal,
-		sg::base::AdpativityConfiguration& AdaptConfig, sg::base::OperationMatrix& SLESystem)
+		sg::base::AdpativityConfiguration& AdaptConfig,sg::datadriven::BaseDMSystemMatrix& SLESystem,
+		bool testAccDuringAdapt)
 {
-    // Generate CG to solve System
+    double execTime = 0.0;
+    double oldAcc = 0.0;
+
+	// Generate CG to solve System
 	sg::solver::SLESolver* myCG;
 
 	if (SolverConfigRefine.type_ != sg::solver::CG)
@@ -73,14 +81,16 @@ void Learner::trainGrid(sg::base::DataMatrix& trainDataset, sg::base::DataVector
 		// @TODO: Error
 	}
 
-    std::cout << "Starting Learning...." << std::endl;
+	if (verbose_)
+		std::cout << "Starting Learning...." << std::endl;
 
     // execute adaptsteps
     sg::base::SGppStopwatch* myStopwatch = new sg::base::SGppStopwatch();
 
-    for (size_t i = 0; i < AdaptConfig.noRefinements_+1; i++)
+    for (size_t i = 0; i < AdaptConfig.numRefinements_+1; i++)
     {
-    	std::cout << std::endl << "Doing refinement: " << i << std::endl;
+    	if (verbose_)
+    		std::cout << std::endl << "Doing refinement: " << i << std::endl;
 
     	myStopwatch->start();
 
@@ -93,7 +103,9 @@ void Learner::trainGrid(sg::base::DataMatrix& trainDataset, sg::base::DataVector
 
     		SLESystem.rebuildLevelAndIndex();
 
-    		std::cout << "New Grid Size: " << grid_->getSize() << std::endl;
+    		if (verbose_)
+    			std::cout << "New Grid Size: " << grid_->getSize() << std::endl;
+
     		alpha_->resizeZero(grid_->getSize());
     	}
     	else
@@ -104,7 +116,7 @@ void Learner::trainGrid(sg::base::DataMatrix& trainDataset, sg::base::DataVector
     	sg::base::DataVector b(alpha_->getSize());
     	SLESystem.generateb(classes, b);
 
-    	if (i == AdaptConfig.noRefinements_)
+    	if (i == AdaptConfig.numRefinements_)
     	{
     		myCG->setMaxIterations(SolverConfigFinal.maxIterations_);
     		myCG->setEpsilon(SolverConfigFinal.eps_);
@@ -113,206 +125,68 @@ void Learner::trainGrid(sg::base::DataMatrix& trainDataset, sg::base::DataVector
 
         execTime += myStopwatch->stop();
 
-    	std::cout << "Needed Iterations: " << myCG->getNumberIterations() << std::endl;
-    	std::cout << "Final residuum: " << myCG->getResiduum() << std::endl;
+        if (verbose_)
+        {
+        	std::cout << "Needed Iterations: " << myCG->getNumberIterations() << std::endl;
+        	std::cout << "Final residuum: " << myCG->getResiduum() << std::endl;
+        }
 
-    	// Calc flops and mem bandwidth
-    	//nGridsize = grid_->getSize();
+        postProcessing();
 
-    	//calcGFlopsAndGBytes(gridtype, myGrid, nInstancesNo, nGridsize, nDim, myCG->getNumberIterations(), sizeof(double), GFlops, GBytes);
+        if(testAccDuringAdapt || i == AdaptConfig.numRefinements_)
+        {
+ 			double acc = getAccuracy(trainDataset, classes);
 
-    	//std::cout << std::endl;
-        //std::cout << "Current GFlop/s: " << GFlops/execTime << std::endl;
-        //std::cout << "Current GByte/s: " << GBytes/execTime << std::endl;
-        //std::cout << std::endl;
+ 			if (verbose_)
+ 			{
+ 				if (isRegression_)
+ 					std::cout << "MSE (train): " << acc << std::endl;
+ 				else
+ 					std::cout << "Acc (train): " << acc << std::endl;
+ 			}
 
-//#ifndef TEST_LAST_ONLY
-//		// Do tests on test data
-//    	if (isRegression)
-//    	{
-//    		sg::datadriven::OperationTest* myTest = sg::op_factory::createOperationTest(*myGrid);
-//			acc = myTest->testMSE(alpha, data, classes);
-//			std::cout << "MSE (train): " << acc << std::endl;
-//			accTest = myTest->testMSE(alpha, testData, testclasses);
-//			std::cout << "MSE (test): " << accTest << std::endl << std::endl;
-//			delete myTest;
-//
-//			if (((i > 0) && (oldAcc <= accTest)) || accTest == 0.0)
-//			{
-//				std::cout << "The grid is becoming worse --> stop learning" << std::endl;
-//				break;
-//			}
-//
-//			oldAcc = accTest;
-//    	}
-//    	else
-//    	{
-//    		sg::datadriven::OperationTest* myTest = sg::op_factory::createOperationTest(*myGrid);
-//    		sg::base::DataVector charNumbers(4);
-//    		acc = myTest->testWithCharacteristicNumber(alpha, data, classes, charNumbers);
-//    		acc /= static_cast<double>(classes.getSize());
-//    		std::cout << "train accuracy: " << acc << std::endl;
-//    		std::cout << "train sensitivity: " << charNumbers[0]/(charNumbers[0] + charNumbers[3]) << std::endl;
-//    		std::cout << "train specificity: " << charNumbers[1]/(charNumbers[1] + charNumbers[2]) << std::endl;
-//    		std::cout << "train precision: " << charNumbers[0]/(charNumbers[0] + charNumbers[2]) << std::endl << std::endl;
-//    		std::cout << "train true positives: " << charNumbers[0] << std::endl;
-//    		std::cout << "train true negatives: " << charNumbers[1] << std::endl;
-//    		std::cout << "train false positives: " << charNumbers[2] << std::endl;
-//    		std::cout << "train false negatives: " << charNumbers[3] << std::endl << std::endl;
-//    		accTest = myTest->testWithCharacteristicNumber(alpha, testData, testclasses, charNumbers);
-//    		accTest /= static_cast<double>(testclasses.getSize());
-//    		std::cout << "test accuracy: " << accTest << std::endl;
-//    		std::cout << "test sensitivity: " << charNumbers[0]/(charNumbers[0] + charNumbers[3]) << std::endl;
-//    		std::cout << "test specificity: " << charNumbers[1]/(charNumbers[1] + charNumbers[2]) << std::endl;
-//    		std::cout << "test precision: " << charNumbers[0]/(charNumbers[0] + charNumbers[2]) << std::endl << std::endl;
-//    		std::cout << "test true positives: " << charNumbers[0] << std::endl;
-//    		std::cout << "test true negatives: " << charNumbers[1] << std::endl;
-//    		std::cout << "test false positives: " << charNumbers[2] << std::endl;
-//    		std::cout << "test false negatives: " << charNumbers[3] << std::endl << std::endl;
-//#ifdef STORE_ROC_CURVE
-//    		std::cout << "calculating ROC curves ..." << std::endl;
-//    		sg::base::DataVector thresholds(ROC_POINTS+1);
-//    		double gap = 2.0/((double)ROC_POINTS);
-//    		for (size_t t = 0; t < ROC_POINTS+1; t++)
-//    		{
-//    			thresholds.set(t, 1.0-(t*gap));
-//    		}
-//    		sg::base::DataMatrix ROC_train(ROC_POINTS+1, 2);
-//    		sg::base::DataMatrix ROC_test(ROC_POINTS+1, 2);
-//    		myTest->calculateROCcurve(alpha, data, classes, thresholds, ROC_train);
-//    		myTest->calculateROCcurve(alpha, testData, testclasses, thresholds, ROC_test);
-//    		std::cout << "calculating ROC curves done!" << std::endl << std::endl;
-//    		std::stringstream filetrain;
-//    		filetrain << tfileTrain << "_SP" << "_level_" << start_level << "_refines_" << refine_count << "_lambda_" << lambda << ".roc";
-//    		std::stringstream filetest;
-//    		filetest << tfileTest << "_SP" << "_level_" << start_level << "_refines_" << refine_count << "_lambda_" << lambda << ".roc";
-//        	storeROCcurve(ROC_train, filetrain.str());
-//        	storeROCcurve(ROC_test, filetest.str());
-//#endif
-//    		delete myTest;
-//
-//			if (((i > 0) && (oldAcc >= accTest)) || accTest == 1.0)
-//			{
-//				std::cout << "The grid is becoming worse --> stop learning" << std::endl;
-//				break;
-//			}
-//
-//			oldAcc = accTest;
-//    	}
-//#endif
+ 			if (isRegression_)
+ 			{
+				if ((i > 0) && (oldAcc <= acc))
+				{
+					if (verbose_)
+						std::cout << "The grid is becoming worse --> stop learning" << std::endl;
+
+					break;
+				}
+ 			}
+ 			else
+ 			{
+				if ((i > 0) && (oldAcc >= acc))
+				{
+					if (verbose_)
+						std::cout << "The grid is becoming worse --> stop learning" << std::endl;
+
+					break;
+				}
+ 			}
+
+			oldAcc = acc;
+        }
     }
 
+    if (verbose_)
+    	std::cout << "Finished Learning!" << std::endl;
+
     delete myStopwatch;
-
-    std::cout << "Finished Learning!" << std::endl;
-
-//#ifdef TEST_LAST_ONLY
-//    std::cout << std::endl << std::endl;
-//    if (isRegression)
-//	{
-//		sg::datadriven::OperationTest* myTest = sg::op_factory::createOperationTest(*myGrid);
-//		acc = myTest->testMSE(alpha, data, classes);
-//		std::cout << "MSE (train): " << acc << std::endl;
-//		accTest = myTest->testMSE(alpha, testData, testclasses);
-//		std::cout << "MSE (test): " << accTest << std::endl << std::endl;
-//		delete myTest;
-//	}
-//	else
-//	{
-//		sg::datadriven::OperationTest* myTest = sg::op_factory::createOperationTest(*myGrid);
-//		sg::base::DataVector charNumbers(4);
-//		acc = myTest->testWithCharacteristicNumber(alpha, data, classes, charNumbers);
-//		acc /= static_cast<double>(classes.getSize());
-//		std::cout << "train accuracy: " << acc << std::endl;
-//		std::cout << "train sensitivity: " << charNumbers[0]/(charNumbers[0] + charNumbers[3]) << std::endl;
-//		std::cout << "train specificity: " << charNumbers[1]/(charNumbers[1] + charNumbers[2]) << std::endl;
-//		std::cout << "train precision: " << charNumbers[0]/(charNumbers[0] + charNumbers[2]) << std::endl << std::endl;
-//		std::cout << "train true positives: " << charNumbers[0] << std::endl;
-//		std::cout << "train true negatives: " << charNumbers[1] << std::endl;
-//		std::cout << "train false positives: " << charNumbers[2] << std::endl;
-//		std::cout << "train false negatives: " << charNumbers[3] << std::endl << std::endl;
-//		accTest = myTest->testWithCharacteristicNumber(alpha, testData, testclasses, charNumbers);
-//		accTest /= static_cast<double>(testclasses.getSize());
-//		std::cout << "test accuracy: " << accTest << std::endl;
-//		std::cout << "test sensitivity: " << charNumbers[0]/(charNumbers[0] + charNumbers[3]) << std::endl;
-//		std::cout << "test specificity: " << charNumbers[1]/(charNumbers[1] + charNumbers[2]) << std::endl;
-//		std::cout << "test precision: " << charNumbers[0]/(charNumbers[0] + charNumbers[2]) << std::endl << std::endl;
-//		std::cout << "test true positives: " << charNumbers[0] << std::endl;
-//		std::cout << "test true negatives: " << charNumbers[1] << std::endl;
-//		std::cout << "test false positives: " << charNumbers[2] << std::endl;
-//		std::cout << "test false negatives: " << charNumbers[3] << std::endl << std::endl;
-//#ifdef STORE_ROC_CURVE
-//		std::cout << "calculating ROC curves ..." << std::endl;
-//		sg::base::DataVector thresholds(ROC_POINTS+1);
-//		double gap = 2.0/((double)ROC_POINTS);
-//		for (size_t t = 0; t < ROC_POINTS+1; t++)
-//		{
-//			thresholds.set(t, 1.0-(t*gap));
-//		}
-//		sg::base::DataMatrix ROC_train(ROC_POINTS+1, 2);
-//		sg::base::DataMatrix ROC_test(ROC_POINTS+1, 2);
-//		myTest->calculateROCcurve(alpha, data, classes, thresholds, ROC_train);
-//		myTest->calculateROCcurve(alpha, testData, testclasses, thresholds, ROC_test);
-//		std::cout << "calculating ROC curves done!" << std::endl << std::endl;
-//		std::stringstream filetrain;
-//		filetrain << tfileTrain << "_SP" << "_level_" << start_level << "_refines_" << refine_count << "_lambda_" << lambda << ".roc";
-//		std::stringstream filetest;
-//		filetest << tfileTest << "_SP" << "_level_" << start_level << "_refines_" << refine_count << "_lambda_" << lambda << ".roc";
-//    	storeROCcurve(ROC_train, filetrain.str());
-//    	storeROCcurve(ROC_test, filetest.str());
-//#endif
-//    	delete myTest;
-//	}
-//#endif
-//
-//#ifdef GNUPLOT
-//	if (nDim <= 2)
-//	{
-//		sg::base::GridPrinter* myPrinter = new sg::base::GridPrinter(*myGrid);
-//		myPrinter->printGrid(alpha, "ClassifyBenchmark.gnuplot", GRDIRESOLUTION);
-//		delete myPrinter;
-//	}
-//#endif
-//
-//    std::cout << std::endl;
-//    std::cout << "===============================================================" << std::endl;
-//    printSettings(dataFile, testFile, isRegression, start_level,
-//			lambda, cg_max, cg_eps, refine_count, refine_thresh, refine_points, gridtype, vectorization, myGrid->getSize(), acc, accTest, execTime);
-//#ifdef ITERATIVE
-//    std::cout << "Needed time: " << execTime << " seconds (Double Precision)" << std::endl;
-//    std::cout << std::endl << "Timing Details:" << std::endl;
-//    double computeMult, completeMult, computeMultTrans, completeMultTrans;
-//    mySystem->getTimers(completeMult, computeMult, completeMultTrans, computeMultTrans);
-//    std::cout << "         mult (complete): " << completeMult << " seconds" << std::endl;
-//    std::cout << "         mult (compute) : " << computeMult << " seconds" << std::endl;
-//    std::cout << "  mult trans. (complete): " << completeMultTrans << " seconds" << std::endl;
-//    std::cout << "  mult trans. (compute) : " << computeMultTrans << " seconds" << std::endl;
-//    std::cout << std::endl << std::endl;
-//    std::cout << "GFlop/s: " << GFlops/execTime << std::endl;
-//    std::cout << "GByte/s: " << GBytes/execTime << std::endl;
-//#else
-//    std::cout << "Needed time: " << execTime << " seconds (Double Precision, recursive)" << std::endl;
-//#endif
-//    std::cout << "===============================================================" << std::endl;
-//    std::cout << std::endl;
-//
-//#ifndef ITERATIVE
-//    delete myC;
-//#endif
-//    delete myCG;
-//    delete mySystem;
-//    delete myGrid;
+    delete myCG;
 }
 
 void Learner::trainGrid(sg::base::DataMatrix& testDataset, sg::base::DataVector& classes,
-		sg::solver::SLESolverConfiguration& SolverConfig, sg::base::OperationMatrix& SLESystem)
+		sg::solver::SLESolverConfiguration& SolverConfig, sg::datadriven::BaseDMSystemMatrix& SLESystem)
 {
 
 }
 
 void Learner::train(sg::base::DataMatrix& testDataset, sg::base::DataVector& classes,
 		sg::base::RegularGridConfiguration& GridConfig, sg::base::AdpativityConfiguration& AdaptConfig,
-		sg::solver::SLESolverConfiguration& SolverConfigRefine, sg::solver::SLESolverConfiguration& SolverConfigFinal)
+		sg::solver::SLESolverConfiguration& SolverConfigRefine, sg::solver::SLESolverConfiguration& SolverConfigFinal,
+		bool testAccDuringAdapt)
 {
 
 }
@@ -323,22 +197,17 @@ void Learner::train(sg::base::DataMatrix& testDataset, sg::base::RegularGridConf
 
 }
 
-void Learner::test(sg::base::DataMatrix& testDataset, sg::base::DataVector& classes, bool isRegression)
+void sg::base::DataVector Learner::test(sg::base::DataMatrix& testDataset)
 {
+	sg::base::DataVector classesComputed(testDataset.getNroes());
 
+	return classesComputed;
 }
 
 void Learner::store(std::string tGridFilename, std::string tAlphaFilename)
 {
 
 }
-
-sg::base::DataVector Learner::getAlpha()
-{
-	sg::base::DataVector a(1);
-	return a;
-}
-
 
 }
 
--- src/sgpp/datadriven/application/Learner.hpp
+++ src/sgpp/datadriven/application/Learner.hpp
@@ -11,13 +11,24 @@
 #include "base/grid/Grid.hpp"
 #include "base/datatypes/DataVector.hpp"
 #include "base/datatypes/DataMatrix.hpp"
+
 #include "solver/SLESolver.hpp"
+
+#include "datadriven/algorithm/BaseDMSystemMatrix.hpp"
 
 namespace sg
 {
 
 namespace datadriven
 {
+
+struct ClassificatorQuality
+{
+    double truePositive;
+    double trueNegative;
+    double falsePositive;
+    double falseNegative;
+};
 
 class Learner
 {
@@ -28,32 +39,128 @@ protected:
 
 	bool verbose_;
 
+	bool isRegression_;
+
+	virtual void postProcessing();
+
 	void createInitialGrid(sg::base::RegularGridConfiguration& GridConfig);
 
-	void trainGrid(sg::base::DataMatrix& testDataset, sg::base::DataVector& classes, sg::base::DataMatrix& testDataset,
+	virtual void trainGrid(sg::base::DataMatrix& testDataset, sg::base::DataVector& classes, sg::base::DataMatrix& testDataset,
 			sg::solver::SLESolverConfiguration& SolverConfigRefine, sg::solver::SLESolverConfiguration& SolverConfigFinal,
-			sg::base::AdpativityConfiguration& AdaptConfig, sg::base::OperationMatrix& SLESystem);
+			sg::base::AdpativityConfiguration& AdaptConfig, sg::datadriven::BaseDMSystemMatrix& SLESystem,
+			bool testAccDuringAdapt);
 
-	void trainGrid(sg::base::DataMatrix& testDataset, sg::base::DataVector& classes,
-			sg::solver::SLESolverConfiguration& SolverConfig, sg::base::OperationMatrix& SLESystem);
+	virtual void trainGrid(sg::base::DataMatrix& testDataset, sg::base::DataVector& classes,
+			sg::solver::SLESolverConfiguration& SolverConfig, sg::datadriven::BaseDMSystemMatrix& SLESystem);
 
 public:
-	Learner(bool verbose);
+	Learner(bool isRegression, bool verbose);
+
+	Learner(std::string tGridFilename, std::string tAlphaFilename, bool isRegression, bool verbose);
 
 	virtual ~Learner();
 
 	virtual void train(sg::base::DataMatrix& testDataset, sg::base::DataVector& classes,
 			sg::base::RegularGridConfiguration& GridConfig, sg::base::AdpativityConfiguration& AdaptConfig,
-			sg::solver::SLESolverConfiguration& SolverConfigRefine, sg::solver::SLESolverConfiguration& SolverConfigFinal);
+			sg::solver::SLESolverConfiguration& SolverConfigRefine, sg::solver::SLESolverConfiguration& SolverConfigFinal,
+			bool testAccDuringAdapt) = 0;
 
 	virtual void train(sg::base::DataMatrix& testDataset, sg::base::RegularGridConfiguration& GridConfig,
-			sg::solver::SLESolverConfiguration& SolverConfig);
+			sg::solver::SLESolverConfiguration& SolverConfig) = 0;
 
-	virtual void test(sg::base::DataMatrix& testDataset, sg::base::DataVector& classes, bool isRegression);
+	/**
+	 * executes a Regression test for a given dataset and returns the result
+	 *
+	 * @param testDataset dataset that is evaluated with the current learner
+	 *
+	 * @return regression values of testDataset
+	 */
+	virtual sg::base::DataVector test(sg::base::DataMatrix& testDataset);
 
+	/**
+	 * compute the accuracy for given testDataset. test is automatically called
+	 * in order to determine the regression values of the current learner
+	 *
+	 * In case if classification (isRegression == false) this routine returns the learner's accuracy
+	 * In case of regressions (isRegression == true) this routine returns the learner's MSE
+	 *
+	 * @param testDataset dataset to be tested
+	 * @param classesReference reference labels of the test dataset
+	 * @param threshold threshold used for classification, ignored when performing regressions
+	 *
+	 * @return accuracy, percent or MSE, depending on the execution mode
+	 */
+	virtual double getAccuracy(sg::base::DataMatrix& testDataset, sg::base::DataVector& classesReference, double threshold = 0.0)
+
+	/**
+	 * compute the accuracy for given testDataset.
+	 *
+	 * In case if classification (isRegression == false) this routine returns the learner's accuracy
+	 * In case of regressions (isRegression == true) this routine returns the learner's MSE
+	 *
+	 * @param testDataset dataset to be tested
+	 * @param classesComputed regression results of the test dataset
+	 * @param classesReference reference labels of the test dataset
+	 * @param threshold threshold used for classification, ignored when performing regressions
+	 *
+	 * @return accuracy, percent or MSE, depending on the execution mode
+	 */
+	virtual double getAccuracy(sg::base::DataMatrix& testDataset, sg::base::DataVector& classesComputed, sg::base::DataVector& classesReference, double threshold = 0.0)
+
+	/**
+	 * compute the quality for given testDataset, classification ONLY!
+	 * test is automatically called
+	 * in order to determine the regression values of the current learner
+	 *
+	 * @param testDataset dataset to be tested
+	 * @param classesReference reference labels of the test dataset
+	 * @param threshold threshold used for classification, ignored when performing regressions
+	 *
+	 * @return quality structure containing tp, tn, fp, fn counts
+	 */
+	virtual ClassificatorQuality getCassificatorQuality(sg::base::DataMatrix& testDataset, sg::base::DataVector& classesReference, double threshold = 0.0)
+
+	/**
+	 * compute the quality for given testDataset, classification ONLY!
+	 *
+	 * @param testDataset dataset to be tested
+	 * @param classesComputed regression results of the test dataset
+	 * @param classesReference reference labels of the test dataset
+	 * @param threshold threshold used for classification, ignored when performing regressions
+	 *
+	 * @return quality structure containing tp, tn, fp, fn counts
+	 */
+	virtual ClassificatorQuality getCassificatorQuality(sg::base::DataMatrix& testDataset, sg::base::DataVector& classesComputed, sg::base::DataVector& classesReference, double threshold = 0.0)
+
+	/**
+	 * store the grid and its current coefficients into files for
+	 * further usage.
+	 *
+	 * @param tGridFilename filename of grid file
+	 * @param tAlphaFilename filename of coefficient file
+	 */
 	void store(std::string tGridFilename, std::string tAlphaFilename);
 
-	sg::base::DataVector getAlpha();
+	/**
+	 * determines the current mode
+	 *
+	 * @return returns whether the current mode is regression or not
+	 */
+	bool getIsRegression();
+
+	/**
+	 * determines the current verbose mode of learner
+	 *
+	 * @return returns whether the current learner has verbose output
+	 */
+	bool getVerbose();
+
+	/**
+	 * sets the current verbose mode of learner
+	 *
+	 * @param verbose the current learner's verbose output
+	 */
+	bool setVerbose(bool verbose);
 };
 
 }
