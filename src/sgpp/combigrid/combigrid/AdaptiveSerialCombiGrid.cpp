272df0b73d256f105b3e553ea025243a92279386
Christoph Kowitz <kowitz@in.tum.de>
2012-02-21 10:03:30 +0000

    removed a bug for adaptive combigrids added stubs for the change of combi coefficients
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@2135 4eea3252-f0fb-4393-894d-40516dce545b

--- src/sgpp/combigrid/combigrid/AdaptiveSerialCombiGrid.cpp
+++ src/sgpp/combigrid/combigrid/AdaptiveSerialCombiGrid.cpp
@@ -7,19 +7,42 @@
 
 #include "combigrid/combigrid/AdaptiveSerialCombiGrid.hpp"
 
-
-void combigrid::AdaptiveSerialCombiGrid::addToCombiScheme(
+std::vector<int> combigrid::AdaptiveSerialCombiGrid::addToCombiScheme(
 		std::vector<int> level) {
 	CombigridLevelVector current(combischeme_->getLevels(),
 			combischeme_->getCoef());
+	int currentSize = combischeme_->getNrSapces();
+//	current.printLevelVec();
+//	std::cout<<"-----------"<<std::endl;
 	current = current.getChanges(level);
-	std::vector<int> changes = combischeme_->updateScheme(
-			current.getLevelVec(), current.getCoef());
+//	current.printLevelVec();
+	std::vector<int> changes = combischeme_->updateScheme(current.getLevelVec(),
+			current.getCoef());
 	std::vector<int> newCoef(changes.size());
+
+	std::vector<std::vector<int> > newLevels = current.getLevelVec();
 	for (unsigned int i = 0; i < changes.size(); ++i) {
 		newCoef[i] = combischeme_->getCoef(changes[i]);
 	}
-	(*combikernel_).updateCombiScheme(/*combischeme_*/newCoef, level, changes);
+	(*combikernel_).updateCombiScheme(/*combischeme_*/newCoef, newLevels,
+			changes);
+//	for (int i = 0; i < newLevels.size(); i++) {
+//		for (int j = 0; j < newLevels[i].size(); j++)
+//			std::cout << newLevels[i][j] << '\t';
+//		std::cout << std::endl;
+
+//	std::cout << "changes: ";
+//	for (int i = 0; i < changes.size(); i++)
+//		std::cout << changes[i] << '\t';
+//	std::cout << std::endl;
+	std::vector<int> newGridsIndices;
+	for (int i = 0; i < changes.size(); i++) {
+		if (changes[i] >= currentSize)
+			newGridsIndices.push_back(changes[i]);
+	}
+	return newGridsIndices;
+
+
 //	for ( int i = 0; i < combischeme_->getNrSapces(); i++) {
 //		for ( int j = 0; j < combischeme_->getDim(); j++) {
 //			std::cout << combischeme_->getLevel(i)[j] << "\t";
---
+++ src/sgpp/combigrid/combigrid/AdaptiveSerialCombigridVariableCoefficients.cpp
@@ -0,0 +1,16 @@
+/*
+ * AdaptiveSerialCombigridVariableCoefficients.cpp
+ *
+ *  Created on: 21.02.2012
+ *      Author: ckow
+ */
+
+#include "AdaptiveSerialCombigridVariableCoefficients.hpp"
+
+namespace combigrid {
+
+void AdaptiveSerialCombigridVariableCoefficients::changeCoefficient(){
+	//TODO method stub
+}
+
+} /* namespace combigrid */
