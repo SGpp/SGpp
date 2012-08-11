afc129c1386c053215fe8146a885e903045f0940
maurus <maurus>
2012-08-11 07:10:25 +0000

    Added up/down scheme files for boundary grids, SqrtXPhiPhi and XdPhidPhi.
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/branches/heston-branch@2346 4eea3252-f0fb-4393-894d-40516dce545b

--- src/sgpp/finance/basis/linear/boundary/algorithm_sweep/XdPhidPhiDownBBLinearBoundary.hpp
+++ src/sgpp/finance/basis/linear/boundary/algorithm_sweep/XdPhidPhiDownBBLinearBoundary.hpp
@@ -3,15 +3,15 @@
 * This file is part of the SG++ project. For conditions of distribution and   *
 * use, please see the copyright notice at http://www5.in.tum.de/SGpp          *
 ******************************************************************************/
-// @author Chao qi (qic@in.tum.de) Stefanie Schraufstetter (schraufs@in.tum.de)
+// @author Sam Maurus (MA thesis)
 
-#ifndef XPHIPHIUPBBLINEARBOUNDARY_HPP
-#define XPHIPHIUPBBLINEARBOUNDARY_HPP
+#ifndef XDPHIDPHIDOWNBBLINEARBOUNDARY_HPP
+#define XDPHIDPHIDOWNBBLINEARBOUNDARY_HPP
 
 #include "base/grid/GridStorage.hpp"
 #include "base/datatypes/DataVector.hpp"
 
-#include "finance/basis/linear/noboundary/algorithm_sweep/XPhiPhiUpBBLinear.hpp"
+#include "finance/basis/linear/noboundary/algorithm_sweep/XdPhidPhiDownBBLinear.hpp"
 
 namespace sg
 {
@@ -21,11 +21,11 @@ namespace finance
 
 
 /**
- * Implementation of sweep operator (): 1D Up for
- * Bilinearform \f$\int_{x} x \phi(x) \phi(x) dx\f$
+ * Implementation of sweep operator (): 1D Down for
+ * Bilinearform \f$\int_{x} x \frac{\partial \phi(x)}{x} \frac{\partial \phi(x)}{x} dx\f$
  * on linear boundary grids
  */
-class XPhiPhiUpBBLinearBoundary : public XPhiPhiUpBBLinear
+class XdPhidPhiDownBBLinearBoundary : public XdPhidPhiDownBBLinear
 {
 public:
 	/**
@@ -33,23 +33,25 @@ public:
 	 *
 	 * @param storage the grid's sg::base::GridStorage object
 	 */
-	XPhiPhiUpBBLinearBoundary(sg::base::GridStorage* storage);
+	XdPhidPhiDownBBLinearBoundary(sg::base::GridStorage* storage);
 
 	/**
 	 * Destructor
 	 */
-	virtual ~XPhiPhiUpBBLinearBoundary();
+	virtual ~XdPhidPhiDownBBLinearBoundary();
 
 	/**
-	 * This operations performs the calculation of up in the direction of dimension <i>dim</i>
+	 * This operations performs the calculation of down in the direction of dimension <i>dim</i>
 	 *
 	 * For level zero it's assumed, that both ansatz-functions do exist: 0,0 and 0,1
 	 * If one is missing this code might produce some bad errors (segmentation fault, wrong calculation
 	 * result)
 	 * So please assure that both functions do exist!
 	 *
+	 * On level zero the getfixDirechletBoundaries of the storage object evaluated
+	 *
 	 * @param source sg::base::DataVector that contains the gridpoint's coefficients (values from the vector of the laplace operation)
-	 * @param result sg::base::DataVector that contains the result of the up operation
+	 * @param result sg::base::DataVector that contains the result of the down operation
 	 * @param index a iterator object of the grid
 	 * @param dim current fixed dimension of the 'execution direction'
 	 */
@@ -61,4 +63,4 @@ public:
 } // namespace sg
 }
 
-#endif /* XPHIPHIUPBBLINEARBOUNDARY_HPP */
+#endif /* XDPHIDPHIDOWNBBLINEARBOUNDARY_HPP */
