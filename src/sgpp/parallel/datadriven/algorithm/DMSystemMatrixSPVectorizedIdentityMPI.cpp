d3870267b9e3f96218e7b6c745950a57e210ce90
Roman Karlstetter <karlstet@in.tum.de>
2012-08-11 09:07:24 +0000

    include changes from trunk to *MPI versions of DMSystemMatrixVectorizedIdentity
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/branches/MPIDataMining@2349 4eea3252-f0fb-4393-894d-40516dce545b

--- src/sgpp/parallel/datadriven/algorithm/DMSystemMatrixSPVectorizedIdentityMPI.cpp
+++ src/sgpp/parallel/datadriven/algorithm/DMSystemMatrixSPVectorizedIdentityMPI.cpp
@@ -9,6 +9,7 @@
 #include "parallel/tools/MPI/SGppMPITools.hpp"
 #include "base/exception/operation_exception.hpp"
 
+#include "parallel/datadriven/tools/DMVectorizationPaddingAssistant.hpp"
 #include "parallel/datadriven/algorithm/DMSystemMatrixSPVectorizedIdentityMPI.hpp"
 #include "parallel/operation/ParallelOpFactory.hpp"
 #include "parallel/tools/PartitioningTool.hpp"
@@ -19,60 +20,17 @@ namespace parallel
 {
 
 DMSystemMatrixSPVectorizedIdentityMPI::DMSystemMatrixSPVectorizedIdentityMPI(sg::base::Grid& SparseGrid, sg::base::DataMatrixSP& trainData, float lambda, VectorizationType vecMode)
-	:  DMSystemMatrixBaseSP(trainData, lambda), vecMode_(vecMode), vecWidth_(0), numTrainingInstances_(0), numPatchedTrainingInstances_(0), m_grid(SparseGrid)
+	:  DMSystemMatrixBaseSP(trainData, lambda), vecMode_(vecMode), numTrainingInstances_(0), numPatchedTrainingInstances_(0), m_grid(SparseGrid)
 {
-	// handle unsupported vector extensions and set vector width
-	// @TODO (heinecke) refactor: better way to set vector width
-	if (this->vecMode_ == X86SIMD)
-	{
-		this->vecWidth_ = 48;
-	}
-	else if (this->vecMode_ == OpenCL)
-	{
-		this->vecWidth_ = 128;
-	}
-	else if (this->vecMode_ == Hybrid_X86SIMD_OpenCL)
-	{
-		this->vecWidth_ = 128;
-	}
-	else if (this->vecMode_ == ArBB)
-	{
-		this->vecWidth_ = 16;
-	}
-	else if (this->vecMode_ == MIC)
-	{
-		this->vecWidth_ = 192;
-	}
-	else if (this->vecMode_ == Hybrid_X86SIMD_MIC)
-	{
-		this->vecWidth_ = 192;
-	}
-	else
+	// handle unsupported vector extensions
+	if (this->vecMode_ != X86SIMD && this->vecMode_ != MIC && this->vecMode_ != Hybrid_X86SIMD_MIC && this->vecMode_ != OpenCL && this->vecMode_ != ArBB && this->vecMode_ != Hybrid_X86SIMD_OpenCL)
 	{
 		throw new sg::base::operation_exception("DMSystemMatrixSPVectorizedIdentity : un-supported vector extension!");
 	}
 
-	// create the operations needed in ApplyMatrix
 	this->dataset_ = new sg::base::DataMatrixSP(trainData);
-
 	this->numTrainingInstances_ = this->dataset_->getNrows();
-
-	// Assure that data has a even number of instances -> padding might be needed
-	size_t remainder = this->dataset_->getNrows() % this->vecWidth_;
-	size_t loopCount = this->vecWidth_ - remainder;
-
-	if (loopCount != this->vecWidth_)
-	{
-		sg::base::DataVectorSP lastRow(this->dataset_->getNcols());
-		for (size_t i = 0; i < loopCount; i++)
-		{
-			this->dataset_->getRow(this->dataset_->getNrows()-1, lastRow);
-			this->dataset_->resize(this->dataset_->getNrows()+1);
-			this->dataset_->setRow(this->dataset_->getNrows()-1, lastRow);
-		}
-	}
-
-	this->numPatchedTrainingInstances_ = this->dataset_->getNrows();
+	this->numPatchedTrainingInstances_ = sg::parallel::DMVectorizationPaddingAssistant::padDataset(*(this->dataset_), vecMode_);
 
 	if (this->vecMode_ != OpenCL && this->vecMode_ != ArBB && this->vecMode_ != Hybrid_X86SIMD_OpenCL)
 	{
@@ -90,7 +48,8 @@ DMSystemMatrixSPVectorizedIdentityMPI::DMSystemMatrixSPVectorizedIdentityMPI(sg:
 
 	// calculate distribution
 	calcDistribution(m_grid.getStorage()->size(), _mpi_grid_sizes, _mpi_grid_offsets, 1);
-	calcDistribution(this->dataset_->getNcols(), _mpi_data_sizes, _mpi_data_offsets, this->vecWidth_);
+	calcDistribution(this->numPatchedTrainingInstances_, _mpi_data_sizes, _mpi_data_offsets,
+				 sg::parallel::DMVectorizationPaddingAssistant::getVecWidthSP(this->vecMode_));
 
 	debugMPI(sg::parallel::myGlobalMPIComm, "storage: " << _mpi_grid_offsets[mpi_rank] << " -- " << _mpi_grid_offsets[mpi_rank] + _mpi_grid_sizes[mpi_rank] - 1 << "size: " <<  _mpi_grid_sizes[mpi_rank]);
 	debugMPI(sg::parallel::myGlobalMPIComm, "data:" << _mpi_data_offsets[mpi_rank]  << " -- " <<_mpi_data_offsets[mpi_rank] + _mpi_data_sizes[mpi_rank] - 1 << "size: " <<  _mpi_data_sizes[mpi_rank]);
