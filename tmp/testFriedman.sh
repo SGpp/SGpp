f8c3f72a736f986a3e124dfc0c6a8ede19f901ba
Alexander Heinecke <heinecke@in.tum.de>
2013-04-04 12:22:12 +0000

    removed files not longer needed
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@2743 4eea3252-f0fb-4393-894d-40516dce545b

--- tmp/testFriedman.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-./testFriedmanOne_10000.sh
-./testFriedmanOne_90000.sh
-./testFriedmanTwo_10000.sh
-./testFriedmanTwo_90000.sh
-./testFriedmanThree_10000.sh
-./testFriedmanThree_90000.sh
