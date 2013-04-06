5ed56debd49f259704ab4b3469fd21871dbb4b31
Alexander Heinecke <heinecke@in.tum.de>
2013-04-06 23:02:15 +0000

    added first bunch of files for experimental module
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@2822 4eea3252-f0fb-4393-894d-40516dce545b

---
+++ src/sgpp/experimental/operation/ExperimentalOpFactory.hpp
@@ -0,0 +1,48 @@
+/* *****************************************************************************
+* Copyright (C) 2013 Technische Universitaet Muenchen                          *
+* This file is part of the SG++ project. For conditions of distribution and    *
+* use, please see the copyright notice at http://www5.in.tum.de/SGpp           *
+***************************************************************************** */
+// @author Alexander Heinecke (alexander.heinecke@mytum.de)
+
+#ifndef EXPERIMENTAL_OP_FACTORY_HPP
+#define EXPERIMENTAL_OP_FACTORY_HPP
+
+#include "base/grid/Grid.hpp"
+#include "base/operation/OperationMatrix.hpp"
+
+/*
+ * This file contains factory methods for operations.
+ */
+namespace sg
+{
+
+namespace op_factory
+{
+    /**
+     * Factory method, returning an OperationLaplace (OperationMatrix) for the grid at hand.
+     * Note: object has to be freed after use.
+     *
+     * This Laplacian is implemented by a multi-dimensional sweep operation
+     *
+     * @param grid Grid which is to be used
+     * @return Pointer to the new OperationMatrix object for the Grid grid
+     */
+    base::OperationMatrix* createOperationLaplaceEnhanced(base::Grid& grid);
+
+    /**
+     * Factory method, returning an OperationLaplace (OperationMatrix) for the grid at hand.
+     * Note: object has to be freed after use.
+     *
+     * This Laplacian is implemented by a multi-dimensional sweep operation
+     *
+     * @param grid Grid which is to be used
+     * @param coef Coefficient vector for OperationLaplace
+     * @return Pointer to the new OperationMatrix object for the Grid grid
+     */
+    base::OperationMatrix* createOperationLaplaceEnhanced(base::Grid& grid, sg::base::DataVector& coef);
+}
+
+}
+
+#endif /* EXPERIMENTAL_OP_FACTORY_HPP */
