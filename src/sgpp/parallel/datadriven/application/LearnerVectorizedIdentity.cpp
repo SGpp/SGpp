b735a2b37857a4be5621f9e075c847f7fd165e2a
Alexander Heinecke <heinecke@in.tum.de>
2013-04-26 05:43:27 +0000

    updated transpose condition
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/branches/OCLDataTransposed@2938 4eea3252-f0fb-4393-894d-40516dce545b

--- src/sgpp/parallel/datadriven/application/LearnerVectorizedIdentity.cpp
+++ src/sgpp/parallel/datadriven/application/LearnerVectorizedIdentity.cpp
@@ -78,9 +78,9 @@ namespace sg {
 
       classesComputed.setAll(0.0);
 
-      //if (this->vecType_ != OpenCL && this->vecType_ != ArBB && this->vecType_ != Hybrid_X86SIMD_OpenCL) {
-      tmpDataSet.transpose();
-      //}
+      if (this->vecType_ != ArBB) {
+        tmpDataSet.transpose();
+      }
 
       sg::parallel::OperationMultipleEvalVectorized* MultEval = sg::op_factory::createOperationMultipleEvalVectorized(*grid_, vecType_, &tmpDataSet);
       MultEval->multVectorized(*alpha_, classesComputed);
