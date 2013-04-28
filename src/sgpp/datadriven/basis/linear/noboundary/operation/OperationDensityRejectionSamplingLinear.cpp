8111e0a7a209b4f6f8a674fd3b128b1367f3feba
Benjamin Peherstorfer <pehersto@in.tum.de>
2013-04-28 12:08:21 +0000

    fixed bugs in sampling
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@2954 4eea3252-f0fb-4393-894d-40516dce545b

--- src/sgpp/datadriven/basis/linear/noboundary/operation/OperationDensityRejectionSamplingLinear.cpp
+++ src/sgpp/datadriven/basis/linear/noboundary/operation/OperationDensityRejectionSamplingLinear.cpp
@@ -25,11 +25,16 @@ namespace sg {
       base::DataMatrix* tmp = new base::DataMatrix(SEARCH_MAX, num_dims);
       base::DataVector* tmpEval = new base::DataVector(SEARCH_MAX);
 
-      for (size_t i = 0; i < SEARCH_MAX; i++) {
-        for (size_t j = 0; j < num_dims; j++)
-          tmp->set(i, j, (double)rand() / RAND_MAX);
+      #pragma omp parallel 
+      {
+	unsigned int seedp = time(NULL)*(omp_get_thread_num()+1);
+        #pragma omp for
+        for (size_t i = 0; i < SEARCH_MAX; i++) {
+          for (size_t j = 0; j < num_dims; j++)
+            tmp->set(i, j, (double)rand_r(&seedp) / RAND_MAX);
+        }
       }
-
+      
       base::OperationMultipleEval* opMultEval = op_factory::createOperationMultipleEval(*grid, tmp);
       opMultEval->mult(*alpha, *tmpEval);
       maxValue = tmpEval->max();
@@ -38,38 +43,37 @@ namespace sg {
       delete tmpEval;
       tmpEval = NULL;
 
-      double fhat = 0.0;
-      size_t j;
-      base::DataVector* p;
-      base::OperationEval* opEval;
+      
+      #pragma omp parallel 
+      {
+	unsigned int seedp = time(NULL)*(omp_get_thread_num()+1);
+	base::DataVector p(num_dims);
+	double fhat = 0.0;
+	base::OperationEval* opEval = op_factory::createOperationEval(*grid);
 
-      #pragma omp parallel for private(p, j, opEval) schedule(dynamic)
-
-      for (size_t i = 0; i < num_samples; i++) { //for every sample
-
-        p = new base::DataVector(num_dims);
-        opEval = op_factory::createOperationEval(*grid);
-
-        //find the appropriate sample within a # of trial
-        for (j = 0; j < trial_max; j++) {
-
-          // pick a random data point "p"
-          for (size_t d = 0; d < num_dims; d++)
-            p->set(d, (double)rand() / RAND_MAX);
-
-          // evaluate at this point "p"
-          fhat = opEval->eval(*alpha, *p);
-
-          if (((double)rand() / RAND_MAX * maxValue < fhat) && (fhat > maxValue * 0.050)) {
-            samples->setRow(i, *p);
-            break;
-          }
-        }
-
-        if (j == trial_max)
-          throw base::operation_exception("Error: maximum # of trials reached. Operation aborted!");
-
-        delete p;
+        #pragma omp for schedule(dynamic)
+	for (size_t i = 0; i < num_samples; i++) { //for every sample
+	  //find the appropriate sample within a # of trials
+	  size_t j = 0;
+	  for (; j < trial_max; j++) {
+	    
+	    // pick a random data point "p"
+	    #pragma omp critical
+	    {
+	    for (size_t d = 0; d < num_dims; d++)
+	      p[d] = static_cast<double>(rand_r(&seedp))/RAND_MAX;
+	    
+	    // evaluate at this point "p"
+	    fhat = opEval->eval(*alpha, p);
+	    }
+	    if ((static_cast<double>(rand_r(&seedp))/RAND_MAX * maxValue < fhat) && (fhat > maxValue * 0.01)) {
+	      samples->setRow(i, p);
+	      break;
+	    }
+	  }
+	  if (j == trial_max)
+	    throw base::operation_exception("Error: maximum # of trials reached. Operation aborted!");
+	}
         delete opEval;
       }
 
