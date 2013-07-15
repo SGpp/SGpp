de7ef9faee4a248c01722ac6c9859fc4b9e1e9e5
Alexander Heinecke <heinecke@in.tum.de>
2013-07-15 12:59:01 +0000

    updated test parameters
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@3240 4eea3252-f0fb-4393-894d-40516dce545b

--- bin/test_datasets.sh
+++ bin/test_datasets.sh
@@ -2,15 +2,13 @@
 # run specific variables, please adjust
 export OMP_NUM_THREADS=16
 # prefix for result files
-RESPRE=K20m
-VECTYPE="OCL"
+RESPRE=SNB-EP
+VECTYPE="X86SIMD"
 PRECISION="SP DP"
 
 ###### DO NOT MODIFY BELOW THIS LINE #######
 export KMP_AFFINITY=proclist=[0-15],granularity=thread,verbose
-#export GOMP_CPU_AFFINITY="0-7"
-#. /opt/intel/bin/compilervars.sh intel64
-#. /opt/intel/arbb/1.0.0.026/tools/arbbvars.sh intel64
+#export GOMP_CPU_AFFINITY="0-15"
 for VEC in $VECTYPE
 do
 	for PREC in $PRECISION
@@ -20,9 +18,9 @@ do
 		# test Checkerboard, mod linear
 		../bin/ClassifyBenchmark ../input/chess_05D_3fields_tr.dat.arff ../input/chess_05D_3fields_te.dat.arff 0 ${PREC} modlinear 5 0.0000001 250 0.0001 8 0.0 100 250 0.0001 ${VEC} 2>&1 | tee ../log/${RESPRE}_ChkBrd_05D_${VEC}_modlinear_${PREC}.log
 		# test DR5, linear boundary
-		../bin/ClassifyBenchmark ../input/DR5_nowarnings_less05_train.arff ../input/DR5_nowarnings_less05_test.arff 1 ${PREC} linearboundary 3 0.000001 150 0.0001 6 0.0 100 150 0.0001 ${VEC} 2>&1 | tee ../log/${RESPRE}_DR5_${VEC}_linearboundary_${PREC}.log
+		../bin/ClassifyBenchmark ../input/DR5_nowarnings_less05_train.arff ../input/DR5_nowarnings_less05_test.arff 1 ${PREC} linearboundary 6 0.00001 200 0.0001 7 0.0 200 150 0.0001 ${VEC} 2>&1 | tee ../log/${RESPRE}_DR5_${VEC}_linearboundary_${PREC}.log
 		# test DR5, mod linear
-		../bin/ClassifyBenchmark ../input/DR5_nowarnings_less05_train.arff ../input/DR5_nowarnings_less05_test.arff 1 ${PREC} modlinear 5 0.00001 150 0.0001 7 0.0 150 150 0.0001 ${VEC} 2>&1 | tee ../log/${RESPRE}_DR5_${VEC}_modlinear_${PREC}.log
+		../bin/ClassifyBenchmark ../input/DR5_nowarnings_less05_train.arff ../input/DR5_nowarnings_less05_test.arff 1 ${PREC} modlinear 6 0.00001 200 0.0001 7 0.0 200 150 0.0001 ${VEC} 2>&1 | tee ../log/${RESPRE}_DR5_${VEC}_modlinear_${PREC}.log
 		# test friedman1, linear boundary
 		../bin/ClassifyBenchmark ../input/friedman1_90000_train.arff ../input/friedman1_10000_test.arff 1 ${PREC} linearboundary 0 0.0000001 200 0.0001 2 0.0 50 200 0.0001 ${VEC} 2>&1 | tee ../log/${RESPRE}_friedman1_${VEC}_linearboundary_${PREC}.log
 		# test friedman1, mod linear
