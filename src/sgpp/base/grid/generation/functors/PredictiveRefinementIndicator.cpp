ced7f23af398551e596b4533db0910b4f7dddaf4
Michael Lettrich <lettrich@in.tum.de>
2013-12-18 07:08:57 +0000

    - bugfixes in the predictive refinement indicator, estimating contributions of future grid points.
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/branches/AnovaGsG@3464 4eea3252-f0fb-4393-894d-40516dce545b

--- src/sgpp/base/grid/generation/functors/PredictiveRefinementIndicator.cpp
+++ src/sgpp/base/grid/generation/functors/PredictiveRefinementIndicator.cpp
@@ -20,6 +20,7 @@ PredictiveRefinementIndicator::PredictiveRefinementIndicator(Grid* grid, DataMat
 {
 	//find out what type of grid is used;
 	gridType = determineGridType(grid);
+	//std::cout << "gridType = " << grid->getType() << " corresponding to " << gridType << "\n";
 
 	//set global Variables accordingly
 	this->dataSet = dataSet;
@@ -28,36 +29,43 @@ PredictiveRefinementIndicator::PredictiveRefinementIndicator(Grid* grid, DataMat
 	this->threshold = threshold;
 }
 
-
 double PredictiveRefinementIndicator::operator ()(AbstractRefinement::index_type* gridPoint)
 {
+	//calculate the floor and ceiling of the support on dimension of the grid point.
+	DataVector floorMask(dataSet->getNcols());
+	DataVector ceilingMask(dataSet->getNcols());
+	buildGPSupportMask(gridPoint,&floorMask,&ceilingMask);
+
+	//level and index vectors for grid point needed below
+	AbstractRefinement::level_t level = 0;
+	AbstractRefinement::index_t index = 0;
+	//value of the data set in the dimension DIM (needed below)
+	double valueInDim;
+
 	double errorIndicator = 0;
+	//DEBUG
+	size_t counter = 0;
 
-		for (size_t dim = 0; dim < gridPoint->dim(); ++dim) {
+	//go through the whole dataset.
+	for(size_t row = 0; row < dataSet->getNrows(); ++row)
+	{
+		//if on the support of the grid point in all dim
+		if(isOnSupport(&floorMask,&ceilingMask,row))
+		{
+			//calculate error Indicator
+			for(size_t dim = 0; dim < gridPoint->dim(); ++dim)
+			{
+				++counter;
+				level = gridPoint->getLevel(dim);
+				index = gridPoint->getIndex(dim);
 
-			AbstractRefinement::level_t level = gridPoint->getLevel(dim);
-			AbstractRefinement::index_t index = gridPoint->getIndex(dim);
-
-			double supportFloor = (index-1)/(1<<level);
-			double supportCeiling= index/(1<<level);
-
-			//walk through dataSet and select points on support
-			for (size_t row = 0; row < dataSet->getNrows(); ++row) {
-
-				//get the values in all dimensions of the current DataPoint
-				double valueInDim = dataSet->get(row,dim);
-
-				if (valueInDim>=supportFloor &&
-						valueInDim<supportCeiling ) {
-					//is on support
-
-					//@TODO:
-				errorIndicator += ((RefinementFunctor::value_type) basisFunctionEvalHelper(level,index,valueInDim)*errorVector->get(dim));
-				}
+				valueInDim = dataSet->get(row,dim);
+				errorIndicator += ((RefinementFunctor::value_type) basisFunctionEvalHelper(level,index,valueInDim)*errorVector->get(row));
 			}
-
 		}
-		return errorIndicator;
+	}
+	std::cout << gridPoint->toString() << " with error estimate " << errorIndicator << ",caused by " << counter << "contribs - in average: " << (errorIndicator/static_cast<double>(counter)) << "\n";
+	return errorIndicator;
 
 }
 
@@ -101,10 +109,11 @@ double PredictiveRefinementIndicator::getRefinementThreshold()
 double PredictiveRefinementIndicator::start() {
 	return 0.0;
 }
+
 size_t PredictiveRefinementIndicator::determineGridType(Grid* grid) {
 
 	//define a map where to add the supported grid Types
-	typedef std::map<const char*,size_t> GridTypes;
+	typedef std::map<std::string,size_t> GridTypes;
 	GridTypes gridTypes;
 	// add grid Types and map an integer to them
 	gridTypes.insert(std::make_pair("linear",1));
@@ -126,15 +135,48 @@ size_t PredictiveRefinementIndicator::determineGridType(Grid* grid) {
 
 	//find the integer representation of the grid type and return it.
 	//zero otherwise.
-	GridTypes::iterator typeIter = gridTypes.find(grid->getType());
+	GridTypes::iterator typeIter = gridTypes.find(std::string(grid->getType()));
 
 	if (typeIter != gridTypes.end()) {
 		return typeIter->second;
 	}else {
 		return 0;
 	}
-
 }
+
+bool PredictiveRefinementIndicator::isOnSupport(
+		DataVector* floorMask, DataVector* ceilingMask, size_t entry)
+{
+	for(size_t col = 0; col < dataSet->getNcols(); ++col)
+	{
+		double valueInDim = dataSet->get(entry,col);
+		if(valueInDim < floorMask->get(col) || valueInDim >= ceilingMask->get(col) )
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+void PredictiveRefinementIndicator::buildGPSupportMask(
+		AbstractRefinement::index_type* gridPoint, DataVector* floorMask, DataVector* ceilingMask)
+{
+
+	AbstractRefinement::level_t level;
+	AbstractRefinement::index_t index;
+
+	for(size_t dim=0; dim<gridPoint->dim();++dim)
+	{
+		level = gridPoint->getLevel(dim);
+		index = gridPoint->getIndex(dim);
+
+		floorMask->set(dim,(index-1.0)/(1<<level));
+		ceilingMask->set(dim,index*1.0/(1<<level));
+	}
+}
+
+
+
 
 } /* namespace base */
 } /* namespace sg */
