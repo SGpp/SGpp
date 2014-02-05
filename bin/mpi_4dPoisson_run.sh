41bb7d5e826726187abfc36bc3b01df5a9968605
Alexander Heinecke <heinecke@in.tum.de>
2014-02-05 16:47:23 +0000

    removed outpated MPI scripts
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@3501 4eea3252-f0fb-4393-894d-40516dce545b

--- bin/mpi_4dPoisson_run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-mpiexec -ppn 2 -genv OMP_NUM_THREADS 4 -genv I_MPI_PIN_DOMAIN omp:platform -n 4 ./HESolverMPI PoissonEquation 4 10 0.0 3.0 smooth 0.00001 400
--- bin/mpi_start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-mpdboot -r ssh -f mpi.hosts -n 2 -v
