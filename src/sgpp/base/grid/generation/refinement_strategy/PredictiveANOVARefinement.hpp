f0f93c63c53482576dd983659f7853b42489ab86
Michael Lettrich <lettrich@in.tum.de>
2014-02-24 01:02:01 +0000

    astyle formatting
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/branches/AnovaGsG@3522 4eea3252-f0fb-4393-894d-40516dce545b

--- src/sgpp/base/grid/generation/refinement_strategy/PredictiveANOVARefinement.hpp
+++ src/sgpp/base/grid/generation/refinement_strategy/PredictiveANOVARefinement.hpp
@@ -12,8 +12,10 @@
 #include "base/grid/generation/refinement_strategy/PredictiveRefinement.hpp";
 #include "base/grid/generation/refinement_strategy/ANOVARefinement.hpp";
 
-namespace sg {
-namespace base {
+namespace sg
+{
+namespace base
+{
 
 /**
  * Dimension-adaptive refinement as
@@ -24,67 +26,70 @@ namespace base {
  * corresponding level is not 1. This method works with ModLinear basis functions
  * and the PredictiveRefinementIndicator
  */
-class PredictiveANOVARefinement: public PredictiveRefinement, public ANOVARefinement {
+class PredictiveANOVARefinement: public PredictiveRefinement, public ANOVARefinement
+{
 
 public:
 
-	/**
-	 * Constructor
-	 *
-	 * @param refinement object implementing the core functionality (e.g.
-	 * refinement with or without boundaries).
-	 */
-	PredictiveANOVARefinement(AbstractRefinement* refinement):PredictiveRefinement(refinement),ANOVARefinement(refinement), RefinementDecorator(refinement){};
+    /**
+     * Constructor
+     *
+     * @param refinement object implementing the core functionality (e.g.
+     * refinement with or without boundaries).
+     */
+    PredictiveANOVARefinement(AbstractRefinement* refinement):PredictiveRefinement(refinement),ANOVARefinement(refinement), RefinementDecorator(refinement)
+    {}
+    ;
 
 
-	/**
-	 * Refines a grid according to a RefinementFunctor provided.
-	 * Refines up to RefinementFunctor::getRefinementsNum() grid points if
-	 * possible, and if their refinement value is larger than RefinementFunctor::start()
-	 * and their absolute value is larger or equal than RefinementFunctor::getRefinementThreshold()
-	 *
-	 * @param storage hashmap that stores the grid points
-	 * @param functor a PredictiveRefinementIndicator specifying the refinement criteria
-	 */
-	void free_refine(GridStorage* storage, RefinementFunctor* functor)
-	{
-		ANOVARefinement::free_refine(storage,functor);
-	};
+    /**
+     * Refines a grid according to a RefinementFunctor provided.
+     * Refines up to RefinementFunctor::getRefinementsNum() grid points if
+     * possible, and if their refinement value is larger than RefinementFunctor::start()
+     * and their absolute value is larger or equal than RefinementFunctor::getRefinementThreshold()
+     *
+     * @param storage hashmap that stores the grid points
+     * @param functor a PredictiveRefinementIndicator specifying the refinement criteria
+     */
+    void free_refine(GridStorage* storage, RefinementFunctor* functor)
+    {
+        ANOVARefinement::free_refine(storage,functor);
+    };
 protected:
 
 
-	/**
-	 * Refines a grid according to a RefinementFunctor provided.
-	 * Refines up to RefinementFunctor::getRefinementsNum() grid points if
-	 * possible, and if their refinement value is larger than RefinementFunctor::start()
-	 * and their absolute value is larger or equal than RefinementFunctor::getRefinementThreshold()
-	 *
-	 * @param storage hashmap that stores the grid points
-	 * @param functor a PredictiveRefinementIndicator specifying the refinement criteria
-	 */
-	virtual void refineGridpointsCollection(GridStorage* storage,
-			RefinementFunctor* functor, size_t refinements_num, size_t* max_indices,
-			RefinementFunctor::value_type* max_values)
-	{
-		ANOVARefinement::refineGridpointsCollection(storage,functor,refinements_num,max_indices,max_values);
-	};
+    /**
+     * Refines a grid according to a RefinementFunctor provided.
+     * Refines up to RefinementFunctor::getRefinementsNum() grid points if
+     * possible, and if their refinement value is larger than RefinementFunctor::start()
+     * and their absolute value is larger or equal than RefinementFunctor::getRefinementThreshold()
+     *
+     * @param storage hashmap that stores the grid points
+     * @param functor a PredictiveRefinementIndicator specifying the refinement criteria
+     */
+    virtual void refineGridpointsCollection(GridStorage* storage,
+                                            RefinementFunctor* functor, size_t refinements_num, size_t* max_indices,
+                                            RefinementFunctor::value_type* max_values)
+    {
+        ANOVARefinement::refineGridpointsCollection(storage,functor,refinements_num,max_indices,max_values);
+    };
 
-	/**
-	 * Refines a grid according to a RefinementFunctor provided.
-	 * Refines up to RefinementFunctor::getRefinementsNum() grid points if
-	 * possible, and if their refinement value is larger than RefinementFunctor::start()
-	 * and their absolute value is larger or equal than RefinementFunctor::getRefinementThreshold()
-	 *
-	 * @param storage hashmap that stores the grid points
-	 * @param functor a PredictiveRefinementIndicator specifying the refinement criteria
-	 */
-	virtual void collectRefinablePoints(
-			GridStorage* storage, RefinementFunctor* functor,
-			size_t refinements_num, size_t* max_indices,
-			RefinementFunctor::value_type* max_values)
-	{
-		PredictiveRefinement::collectRefinablePoints(storage,functor,refinements_num,max_indices,max_values);
-	};
+    /**
+     * Refines a grid according to a RefinementFunctor provided.
+     * Refines up to RefinementFunctor::getRefinementsNum() grid points if
+     * possible, and if their refinement value is larger than RefinementFunctor::start()
+     * and their absolute value is larger or equal than RefinementFunctor::getRefinementThreshold()
+     *
+     * @param storage hashmap that stores the grid points
+     * @param functor a PredictiveRefinementIndicator specifying the refinement criteria
+     */
+    virtual void collectRefinablePoints(
+        GridStorage* storage, RefinementFunctor* functor,
+        size_t refinements_num, size_t* max_indices,
+        RefinementFunctor::value_type* max_values)
+    {
+        PredictiveRefinement::collectRefinablePoints(storage,functor,refinements_num,max_indices,max_values);
+    };
 };
 
 } /* namespace base */
