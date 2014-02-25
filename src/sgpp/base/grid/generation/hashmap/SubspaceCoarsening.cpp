0dce98249de162696f51b27320ea3e02c59e8558
Michael Lettrich <lettrich@in.tum.de>
2014-02-25 10:52:20 +0000

    reversed deletion of HashRefinement.cpp
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/branches/AnovaGsG@3525 4eea3252-f0fb-4393-894d-40516dce545b

--- src/sgpp/base/grid/generation/hashmap/SubspaceCoarsening.cpp
+++ src/sgpp/base/grid/generation/hashmap/SubspaceCoarsening.cpp
@@ -8,184 +8,170 @@
 #include <vector>
 #include <list>
 
-namespace sg
-{
-namespace base
-{
+namespace sg {
+namespace base {
 
 
-void SubspaceCoarsening::free_coarsen(GridStorage* storage,CoarseningFunctor* functor, DataVector* alpha)
-{
-    SubspaceCoarseningErrorStorage errorStorage;
-    SubspaceCoarseningErrorContainer* minContribSubspaces = new SubspaceCoarseningErrorContainer[functor->getRemovementsNum()];
+	void SubspaceCoarsening::free_coarsen(GridStorage* storage,CoarseningFunctor* functor, DataVector* alpha)
+	{
+		SubspaceCoarseningErrorStorage errorStorage;
+		SubspaceCoarseningErrorContainer* minContribSubspaces = new SubspaceCoarseningErrorContainer[functor->getRemovementsNum()];
 
-    collectSubspacesToCoarsen(storage,functor,&errorStorage);
+		collectSubspacesToCoarsen(storage,functor,&errorStorage);
 
-    pickLowestContributionSubspaces(&errorStorage,functor->getRemovementsNum(),minContribSubspaces);
-
-
-    //DEBUG:
-    //		std::cout << "picked ";
-    //		for(size_t i=0; i<functor->getRemovementsNum(); ++i)
-    //		{
-    //			index_type* index = storage->get(minContribSubspaces[i].gridPoints[0]);
-    //			std::cout << index->toString() << ", is coarsenable " <<
-    //					minContribSubspaces[i].isCoarsenable << " has " << minContribSubspaces[i].gridPoints.size() << " points, " <<
-    //					"contributing " << minContribSubspaces[i].error << std::endl;
-    //		}
+		pickLowestContributionSubspaces(&errorStorage,functor->getRemovementsNum(),minContribSubspaces);
 
 
-    //delete gridPoints
-    std::list<size_t> pointsToDelete;
+		//DEBUG:
+//		std::cout << "picked ";
+//		for(size_t i=0; i<functor->getRemovementsNum(); ++i)
+//		{
+//			index_type* index = storage->get(minContribSubspaces[i].gridPoints[0]);
+//			std::cout << index->toString() << ", is coarsenable " <<
+//					minContribSubspaces[i].isCoarsenable << " has " << minContribSubspaces[i].gridPoints.size() << " points, " <<
+//					"contributing " << minContribSubspaces[i].error << std::endl;
+//		}
 
-    // iterate over minContribSubspaces. add all points to be deleted to a list.
-    for (size_t i = 0; i < functor->getRemovementsNum(); ++i)
-    {
 
-        //move all points of subspace to list
-        for(std::vector<size_t>::iterator pointsIter = minContribSubspaces[i].gridPoints.begin();
-                pointsIter != minContribSubspaces[i].gridPoints.end(); pointsIter++
-           )
-        {
-            pointsToDelete.push_back(*pointsIter);
-        }
-    }
-    //delete gridPoints
-    std::vector<size_t> remainingIndex = storage->deletePoints(pointsToDelete);
+		//delete gridPoints
+		std::list<size_t> pointsToDelete;
 
-    //delete all surpluses belonging to points deleted by coarsening from alpha vector;
-    alpha->restructure(remainingIndex);
+		// iterate over minContribSubspaces. add all points to be deleted to a list.
+		for (size_t i = 0; i < functor->getRemovementsNum(); ++i) {
 
-    //clean up
-    delete[] minContribSubspaces;
-}
+			//move all points of subspace to list
+			for(std::vector<size_t>::iterator pointsIter = minContribSubspaces[i].gridPoints.begin();
+					pointsIter != minContribSubspaces[i].gridPoints.end(); pointsIter++
+					)
+			{
+				pointsToDelete.push_back(*pointsIter);
+			}
+		}
+		//delete gridPoints
+		std::vector<size_t> remainingIndex = storage->deletePoints(pointsToDelete);
 
-void SubspaceCoarsening::collectSubspacesToCoarsen (GridStorage* gridStorage, CoarseningFunctor* functor, SubspaceCoarseningErrorStorage* errorStorage)
-{
+		//delete all surpluses belonging to points deleted by coarsening from alpha vector;
+		alpha->restructure(remainingIndex);
 
-    SubspaceCoarseningErrorStorage::iterator errorIter = errorStorage->begin();
+		//clean up
+		delete[] minContribSubspaces;
+	}
 
-    for (GridStorage::grid_map_iterator gridIter = gridStorage->begin();
-            gridIter != gridStorage->end();
-            ++gridIter)
-    {
-        //we do only coarsen inner points.
-        if (!gridIter->first->isInnerPoint())
-        {
-            continue;
-        }
+	void SubspaceCoarsening::collectSubspacesToCoarsen (GridStorage* gridStorage, CoarseningFunctor* functor, SubspaceCoarseningErrorStorage* errorStorage)
+	{
 
-        //try to find subspace of point in storage
-        index_type* gridPoint = gridIter->first;
-        errorIter = errorStorage->find(gridPoint);
+		SubspaceCoarseningErrorStorage::iterator errorIter = errorStorage->begin();
 
-        if (errorIter == errorStorage->end())
-        {
-            // not found. add
+		for (GridStorage::grid_map_iterator gridIter = gridStorage->begin();
+				gridIter != gridStorage->end();
+				++gridIter)
+		{
+			//we do only coarsen inner points.
+			if (!gridIter->first->isInnerPoint()) {
+				continue;
+			}
 
-            //create a new error container and store all coarsening information.
-            SubspaceCoarseningErrorContainer errorContainer;
-            errorContainer.isCoarsenable = gridIter->first->isLeaf();
-            errorContainer.error = (*functor)(gridStorage, gridIter->second);
-            errorContainer.gridPoints.push_back(gridIter->second);
+			//try to find subspace of point in storage
+			index_type* gridPoint = gridIter->first;
+			errorIter = errorStorage->find(gridPoint);
 
-            //add to error Container.
-            errorStorage->insert(std::make_pair(gridIter->first,errorContainer));
-        }
-        else
-        {
+			if (errorIter == errorStorage->end()) {
+				// not found. add
 
-            // found - test if the grid point is a leaf (else it can not be coarsened)
-            // or the subspace itself is already impossible to coarsen.
-            if(errorIter->second.isCoarsenable && gridPoint->isLeaf())
-            {
+				//create a new error container and store all coarsening information.
+				SubspaceCoarseningErrorContainer errorContainer;
+				errorContainer.isCoarsenable = gridIter->first->isLeaf();
+				errorContainer.error = (*functor)(gridStorage, gridIter->second);
+				errorContainer.gridPoints.push_back(gridIter->second);
 
-                // add to existing error.
-                errorIter->second.error = (*functor)(gridStorage, gridIter->second);
-                errorIter->second.gridPoints.push_back(gridIter->second);
+				//add to error Container.
+				errorStorage->insert(std::make_pair(gridIter->first,errorContainer));
+			}else{
 
-            }
-            else
-            {
+				// found - test if the grid point is a leaf (else it can not be coarsened)
+				// or the subspace itself is already impossible to coarsen.
+				if(errorIter->second.isCoarsenable && gridPoint->isLeaf()){
 
-                //subspace can not be coarsened
-                errorIter->second.isCoarsenable = false;
-            }
-        }
-    }
+					// add to existing error.
+					errorIter->second.error = (*functor)(gridStorage, gridIter->second);
+					errorIter->second.gridPoints.push_back(gridIter->second);
 
-    // DEBUG: print subspaces to coarsen
-    //		std::cout << "Subspaces:" << std::endl;
-    //		for (SubspaceCoarseningErrorStorage::iterator it = errorStorage->begin();
-    //				it != errorStorage->end(); ++it) {
-    //			std::cout << ((index_type) it->first).toString() << ", is coarsenable " <<
-    //					it->second.isCoarsenable << " has " << it->second.gridPoints.size() << " points, " <<
-    //					"contributing " << it->second.error << std::endl;
-    //		}
-}
+				}else{
 
-void SubspaceCoarsening::cleanUpErrorStorage(SubspaceCoarseningErrorStorage* errorStorage)
-{
-    SubspaceCoarseningErrorStorage::iterator storageIter;
+					//subspace can not be coarsened
+					errorIter->second.isCoarsenable = false;
+				}
+			}
+		}
 
-    for(storageIter = errorStorage->begin(); storageIter != errorStorage->end(); ++storageIter)
-    {
-        //@TODO:check if index is not moved by removing!
-        // subspace can not be coarsened. clean it up.
-        if(!storageIter->second.isCoarsenable)
-        {
-            errorStorage->erase(storageIter);
-        }
-    }
+		// DEBUG: print subspaces to coarsen
+//		std::cout << "Subspaces:" << std::endl;
+//		for (SubspaceCoarseningErrorStorage::iterator it = errorStorage->begin();
+//				it != errorStorage->end(); ++it) {
+//			std::cout << ((index_type) it->first).toString() << ", is coarsenable " <<
+//					it->second.isCoarsenable << " has " << it->second.gridPoints.size() << " points, " <<
+//					"contributing " << it->second.error << std::endl;
+//		}
+	}
 
-}
+	void SubspaceCoarsening::cleanUpErrorStorage(SubspaceCoarseningErrorStorage* errorStorage)
+	{
+		SubspaceCoarseningErrorStorage::iterator storageIter;
 
-void SubspaceCoarsening::pickLowestContributionSubspaces(SubspaceCoarseningErrorStorage* errorStorage,
-        size_t removementsNum,
-        SubspaceCoarseningErrorContainer* minContribSubspaces )
-{
-    //initialize
-    size_t maxIndex = 0;
-    //create and init helper array
-    for (size_t i = 0; i < removementsNum; ++i)
-    {
-        minContribSubspaces[i] = SubspaceCoarseningErrorContainer();
-        minContribSubspaces[i].error = INFINITY;
-    }
+		for(storageIter = errorStorage->begin(); storageIter != errorStorage->end(); ++storageIter)
+		{
+			//@TODO:check if index is not moved by removing!
+			// subspace can not be coarsened. clean it up.
+			if(!storageIter->second.isCoarsenable)
+			{
+				errorStorage->erase(storageIter);
+			}
+		}
 
-    //iterate over all subspaces
-    SubspaceCoarseningErrorStorage::iterator storageIter;
-    for(storageIter = errorStorage->begin(); storageIter != errorStorage->end(); ++storageIter)
-    {
-        //if subspace can be coarsened
-        //and if its contribution is smaller then one of the others, insert it
-        if (storageIter->second.isCoarsenable &&
-                storageIter->second.error < minContribSubspaces[maxIndex].error)
-        {
+	}
 
-            //override the entry with the highest contribution to the grid
-            minContribSubspaces[maxIndex] = storageIter->second;
+	void SubspaceCoarsening::pickLowestContributionSubspaces(SubspaceCoarseningErrorStorage* errorStorage,
+										 size_t removementsNum,
+										 SubspaceCoarseningErrorContainer* minContribSubspaces )
+	{
+		//initialize
+		size_t maxIndex = 0;
+		//create and init helper array
+		for (size_t i = 0; i < removementsNum; ++i) {
+			minContribSubspaces[i] = SubspaceCoarseningErrorContainer();
+			minContribSubspaces[i].error = INFINITY;
+		}
 
-            //find a new maximum
-            maxIndex = getMaxErrorElem(minContribSubspaces,removementsNum);
-        }
-    }
-}
+		//iterate over all subspaces
+		SubspaceCoarseningErrorStorage::iterator storageIter;
+		for(storageIter = errorStorage->begin(); storageIter != errorStorage->end(); ++storageIter)
+		{
+			//if subspace can be coarsened
+			//and if its contribution is smaller then one of the others, insert it
+			if (storageIter->second.isCoarsenable &&
+					storageIter->second.error < minContribSubspaces[maxIndex].error) {
 
-size_t SubspaceCoarsening::getMaxErrorElem(SubspaceCoarseningErrorContainer* minContribSubspaces, size_t removementsNum)
-{
-    size_t maxIndex = 0;
+				//override the entry with the highest contribution to the grid
+				minContribSubspaces[maxIndex] = storageIter->second;
 
-    for (size_t i = 1; i < removementsNum; i++)
-    {
-        if (minContribSubspaces[i].error > minContribSubspaces[maxIndex].error)
-        {
-            maxIndex = i;
-        }
-    }
+				//find a new maximum
+				maxIndex = getMaxErrorElem(minContribSubspaces,removementsNum);
+			}
+		}
+	}
 
-    return maxIndex;
-}
+	size_t SubspaceCoarsening::getMaxErrorElem(SubspaceCoarseningErrorContainer* minContribSubspaces, size_t removementsNum)
+	{
+		size_t maxIndex = 0;
+
+		for (size_t i = 1; i < removementsNum; i++) {
+			if (minContribSubspaces[i].error > minContribSubspaces[maxIndex].error) {
+				maxIndex = i;
+			}
+		}
+
+		return maxIndex;
+	}
 
 
 } /* namespace base */
--- src/sgpp/base/grid/generation/refinement_strategy/RefinementDecorator.cpp
+++ src/sgpp/base/grid/generation/refinement_strategy/RefinementDecorator.cpp
@@ -9,74 +9,65 @@
 #include "base/grid/generation/hashmap/AbstractRefinement.hpp"
 
 
-namespace sg
-{
-namespace base
-{
+namespace sg {
+  namespace base {
 
-void RefinementDecorator::free_refine(GridStorage* storage, RefinementFunctor* functor)
-{
-    this->decorated_refinement_->free_refine(storage, functor);
-}
-
-size_t RefinementDecorator::getNumberOfRefinablePoints(GridStorage* storage)
-{
-    return this->decorated_refinement_->getNumberOfRefinablePoints(storage);
-}
-
-void RefinementDecorator::refineGridpoint1D(GridStorage* storage, index_type& index, size_t d)
-{
-    this->decorated_refinement_->refineGridpoint1D(storage, index, d);
-}
-
-void RefinementDecorator::refineGridpoint(GridStorage* storage, size_t refine_index)
-{
-    this->decorated_refinement_->refineGridpoint(storage, refine_index);
-}
-
-void RefinementDecorator::createGridpoint(GridStorage* storage, index_type& index)
-{
-    this->decorated_refinement_->createGridpoint(storage, index);
-}
-
-void RefinementDecorator::collectRefinablePoints(GridStorage* storage, RefinementFunctor* functor, size_t refinements_num, size_t* max_indices, RefinementFunctor::value_type* max_values)
-{
-    this->decorated_refinement_->collectRefinablePoints(storage, functor, refinements_num, max_indices, max_values);
-}
-
-void RefinementDecorator::refineGridpointsCollection(GridStorage* storage, RefinementFunctor* functor, size_t refinements_num, size_t* max_indices, RefinementFunctor::value_type* max_values)
-{
-    this->decorated_refinement_->refineGridpointsCollection(storage, functor, refinements_num, max_indices, max_values);
-}
-
-bool RefinementDecorator::checkAdmissibility(GridStorage* storage, index_type& index)
-{
-    index_type gridPoint = index;
-    bool isAdmissible = true;
-    size_t dim = 0;
-    level_t parentLevel = 0;
-    index_t parentIndex = 0;
-
-    //go through all dimensions and check if all parents are availabe
-    while(dim < gridPoint.dim() && isAdmissible)
-    {
-        //get the parent index
-        index.getParentLevelAndIndex(&parentLevel,&parentIndex,dim);
-        gridPoint.set(dim,parentLevel,parentIndex);
-
-        if(parentLevel!=0)
-        {
-            //if we can not find the parent in the grid, the child is not admissible;
-            isAdmissible = (storage->find(&gridPoint) != storage->end());
-        }
-
-        gridPoint = index;
-        ++dim;
+    void RefinementDecorator::free_refine(GridStorage* storage, RefinementFunctor* functor) {
+      this->decorated_refinement_->free_refine(storage, functor);
     }
 
-    return isAdmissible;
+    size_t RefinementDecorator::getNumberOfRefinablePoints(GridStorage* storage) {
+      return this->decorated_refinement_->getNumberOfRefinablePoints(storage);
+    }
 
-}
+    void RefinementDecorator::refineGridpoint1D(GridStorage* storage, index_type& index, size_t d) {
+      this->decorated_refinement_->refineGridpoint1D(storage, index, d);
+    }
 
-}
+    void RefinementDecorator::refineGridpoint(GridStorage* storage, size_t refine_index) {
+      this->decorated_refinement_->refineGridpoint(storage, refine_index);
+    }
+
+    void RefinementDecorator::createGridpoint(GridStorage* storage, index_type& index) {
+      this->decorated_refinement_->createGridpoint(storage, index);
+    }
+
+    void RefinementDecorator::collectRefinablePoints(GridStorage* storage, RefinementFunctor* functor, size_t refinements_num, size_t* max_indices, RefinementFunctor::value_type* max_values) {
+      this->decorated_refinement_->collectRefinablePoints(storage, functor, refinements_num, max_indices, max_values);
+    }
+
+    void RefinementDecorator::refineGridpointsCollection(GridStorage* storage, RefinementFunctor* functor, size_t refinements_num, size_t* max_indices, RefinementFunctor::value_type* max_values) {
+      this->decorated_refinement_->refineGridpointsCollection(storage, functor, refinements_num, max_indices, max_values);
+    }
+
+    bool RefinementDecorator::checkAdmissibility(GridStorage* storage, index_type& index)
+    {
+    	index_type gridPoint = index;
+    	bool isAdmissible = true;
+    	size_t dim = 0;
+    	level_t parentLevel = 0;
+    	index_t parentIndex = 0;
+
+    	//go through all dimensions and check if all parents are availabe
+    	while(dim < gridPoint.dim() && isAdmissible)
+    	{
+    		//get the parent index
+    		index.getParentLevelAndIndex(&parentLevel,&parentIndex,dim);
+    		gridPoint.set(dim,parentLevel,parentIndex);
+
+    		if(parentLevel!=0)
+    		{
+    			//if we can not find the parent in the grid, the child is not admissible;
+    			isAdmissible = (storage->find(&gridPoint) != storage->end());
+    		}
+
+    		gridPoint = index;
+    		++dim;
+    	}
+
+    	return isAdmissible;
+
+    }
+
+  }
 }
