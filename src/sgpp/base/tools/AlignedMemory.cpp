b05d316d1a0699d483e7565f4ff1f9f385a478a8
David Pfander <pfandedd@ipvs.uni-stuttgart.de>
2014-03-05 15:19:56 +0000

    Memory alignment is now done through 'posix_memalign()' instead of (obsolete) 'memalign()'. 'posix_memalign()' is also available on OS X which allow correct alignment on that plattform as well. There now also is a 'OPT' variable in scons to switch between a debug and a performance build.
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@3532 4eea3252-f0fb-4393-894d-40516dce545b

--- src/sgpp/base/tools/AlignedMemory.cpp
+++ src/sgpp/base/tools/AlignedMemory.cpp
@@ -9,22 +9,59 @@
 
 void* operator new (size_t size) throw (std::bad_alloc) {
   void* p;
-  p = aligned_malloc(size, SGPPMEMALIGNMENT);
 
+  //Workaround for apples non-standard implementation of posix_memalign().
+  //If a size of 0 is requested, posix_memalign() should return a pointer which
+  //free() can be called successfully with. Unfortunately this is not possible 
+  //on OS X.
+  #ifdef __APPLE__
+  if (size == 0) {
+    size = 1;
+  }
+  #endif
+  
+  //p = aligned_malloc(size, SGPPMEMALIGNMENT);
+  aligned_malloc(p, size, SGPPMEMALIGNMENT);
+
+  #ifdef POSIX_MEMALIGN
+  if (success != 0) {
+    throw std::bad_alloc();
+  }
+  #else
   if (p == 0) {
     throw std::bad_alloc();
   }
+  #endif
 
   return p;
 }
 
 void* operator new[] (size_t size) throw (std::bad_alloc) {
   void* p;
-  p = aligned_malloc(size, SGPPMEMALIGNMENT);
 
+  //Workaround for apples non-standard implementation of posix_memalign().
+  //If a size of 0 is requested, posix_memalign() should return a pointer which
+  //free() can be called successfully with. Unfortunately this is not possible 
+  //on OS X.
+  //long unsigned int oldSize = size;
+  #ifdef __APPLE__
+  if (size == 0) {
+    size = 64;
+  }
+  #endif
+
+  //p = aligned_malloc(size, SGPPMEMALIGNMENT);
+  aligned_malloc(p, size, SGPPMEMALIGNMENT);
+
+  #ifdef POSIX_MEMALIGN
+  if (success != 0) {
+    throw std::bad_alloc();
+  }
+  #else
   if (p == 0) {
     throw std::bad_alloc();
   }
+  #endif
 
   return p;
 }
