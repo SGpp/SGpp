6aa22467a699c77ab3a54306b40dc8f52c787635
Petar Tzenov <tzenov@in.tum.de>
2014-06-06 11:26:36 +0000

    Added the branch at the wrong place in the wrong time (PeTz)
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@3602 4eea3252-f0fb-4393-894d-40516dce545b

--- branches/PeTz/tests/CPP_Apps/BSSolver/2d/test_BSSolver_2d.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cd cart
-./test_BSSolver_2d_cart.sh
-cd ..
-cd log
-./test_BSSolver_2d_log.sh
-./test_BSSolver_2d_PAT.sh
-cd ..
--- branches/PeTz/tests/CPP_Apps/BSSolverWithStretching/3d/test_BSSolverWithStretching_3d.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd cart
-./test_BSSolverWithStretching_3d_cart.sh
-#cd ..
-#cd log
-#./test_BSSolver_3d_log.sh
-#cd ..
