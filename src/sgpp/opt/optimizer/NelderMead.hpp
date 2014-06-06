94870f948efe6eb6881cb7816009fa7c978500cb
Julian Valentin <julian.valentin@ipvs.uni-stuttgart.de>
2014-06-06 15:26:23 +0000

    Rename Nelder-Mead parameters and modify the algorithm slightly (nearer to the original formulation of Nelder and Mead).
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/branches/sgopt@3607 4eea3252-f0fb-4393-894d-40516dce545b

--- src/sgpp/opt/optimizer/NelderMead.hpp
+++ src/sgpp/opt/optimizer/NelderMead.hpp
@@ -14,18 +14,18 @@ class NelderMead : public Optimizer
 {
 public:
     static const double DEFAULT_ALPHA;
+    static const double DEFAULT_BETA;
     static const double DEFAULT_GAMMA;
-    static const double DEFAULT_RHO;
-    static const double DEFAULT_SIGMA;
+    static const double DEFAULT_DELTA;
     static const size_t DEFAULT_MAX_IT_COUNT = 1000;
     static const double STARTING_SIMPLEX_EDGE_LENGTH;
     
     NelderMead(function::Objective &f,
                size_t max_it_count = DEFAULT_MAX_IT_COUNT,
                double alpha = DEFAULT_ALPHA,
+               double beta = DEFAULT_BETA,
                double gamma = DEFAULT_GAMMA,
-               double rho = DEFAULT_RHO,
-               double sigma = DEFAULT_SIGMA);
+               double delta = DEFAULT_DELTA);
     
     double optimize(std::vector<double> &xopt);
     
@@ -34,23 +34,23 @@ public:
     double getAlpha() const;
     void setAlpha(double alpha);
     
+    double getBeta() const;
+    void setBeta(double beta);
+    
     double getGamma() const;
     void setGamma(double gamma);
     
-    double getRho() const;
-    void setRho(double rho);
-    
-    double getSigma() const;
-    void setSigma(double sigma);
+    double getDelta() const;
+    void setDelta(double delta);
     
     //size_t getMaxFcnEvalCount() const;
     //void setMaxFcnEvalCount(size_t max_fcn_eval_count);
     
 protected:
     double alpha;
+    double beta;
     double gamma;
-    double rho;
-    double sigma;
+    double delta;
     //size_t max_fcn_eval_count;
 };
 
