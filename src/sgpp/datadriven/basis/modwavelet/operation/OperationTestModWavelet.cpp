18d7ba7fedcaf5f25a0243886d98529acb31faed
Julian Valentin <julian.valentin@ipvs.uni-stuttgart.de>
2014-10-17 11:07:56 +0000

    Correct include for modified basis functions.
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/branches/sgopt@3788 4eea3252-f0fb-4393-894d-40516dce545b

--- src/sgpp/datadriven/basis/modwavelet/operation/OperationTestModWavelet.cpp
+++ src/sgpp/datadriven/basis/modwavelet/operation/OperationTestModWavelet.cpp
@@ -15,22 +15,22 @@ namespace sg {
   namespace datadriven {
 
     double OperationTestModWavelet::test(sg::base::DataVector& alpha, sg::base::DataMatrix& data, sg::base::DataVector& classes) {
-      base::ModifiedWaveletBasis<unsigned int, unsigned int> base;
+      base::ModWaveletBasis<unsigned int, unsigned int> base;
       return test_dataset(this->storage, base, alpha, data, classes);
     }
 
     double OperationTestModWavelet::testMSE(sg::base::DataVector& alpha, sg::base::DataMatrix& data, sg::base::DataVector& refValues) {
-      base::ModifiedWaveletBasis<unsigned int, unsigned int> base;
+      base::ModWaveletBasis<unsigned int, unsigned int> base;
       return test_dataset_mse(this->storage, base, alpha, data, refValues);
     }
 
     double OperationTestModWavelet::testWithCharacteristicNumber(sg::base::DataVector& alpha, sg::base::DataMatrix& data, sg::base::DataVector& classes, sg::base::DataVector& charaNumbers) {
-      base::ModifiedWaveletBasis<unsigned int, unsigned int> base;
+      base::ModWaveletBasis<unsigned int, unsigned int> base;
       return test_datasetWithCharacteristicNumber(this->storage, base, alpha, data, classes, charaNumbers, 0.0);
     }
 
     void OperationTestModWavelet::calculateROCcurve(sg::base::DataVector& alpha, sg::base::DataMatrix& data, sg::base::DataVector& classes, sg::base::DataVector& thresholds, sg::base::DataMatrix& ROC_curve) {
-      base::ModifiedWaveletBasis<unsigned int, unsigned int> base;
+      base::ModWaveletBasis<unsigned int, unsigned int> base;
       test_calculateROCcurve(this->storage, base, alpha, data, classes, thresholds, ROC_curve);
     }
 
