6f459cf74af0077f99785c629272d6dc8f61b6ca
Alexander Heinecke <heinecke@in.tum.de>
2014-12-18 18:19:23 +0000

    fixed some OpenMP standard issues, now compiling with ICC 15
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@3869 4eea3252-f0fb-4393-894d-40516dce545b

--- src/sgpp/misc/pde/algorithm/UpDownOneOpDimEnhanced.cpp
+++ src/sgpp/misc/pde/algorithm/UpDownOneOpDimEnhanced.cpp
@@ -58,6 +58,9 @@ namespace sg {
     }
 
     void UpDownOneOpDimEnhanced::updown(sg::base::DataMatrix& alpha, sg::base::DataMatrix& result, size_t dim) {
+      size_t curNumAlgoDims = this->numAlgoDims_;
+      size_t curMaxParallelDims = this->maxParallelDims_;
+
       //Unidirectional scheme
       if (dim > 0) {
         // Reordering ups and downs
@@ -65,13 +68,13 @@ namespace sg {
         sg::base::DataMatrix result_temp(alpha.getNrows(), this->numAlgoDims_);
         sg::base::DataMatrix temp_two(alpha.getNrows(), this->numAlgoDims_);
 
-        #pragma omp task if(this->numAlgoDims_ - dim <= this->maxParallelDims_) shared(alpha, temp, result)
+        #pragma omp task if(curNumAlgoDims - dim <= curMaxParallelDims) shared(alpha, temp, result)
         {
           up(alpha, temp, dim);
           updown(temp, result, dim - 1);
         }
 
-        #pragma omp task if(this->numAlgoDims_ - dim <= this->maxParallelDims_) shared(alpha, temp_two, result_temp)
+        #pragma omp task if(curNumAlgoDims - dim <= curMaxParallelDims) shared(alpha, temp_two, result_temp)
         {
           updown(alpha, temp_two, dim - 1);
           down(temp_two, result_temp, dim);
@@ -84,10 +87,10 @@ namespace sg {
         // Terminates dimension recursion
         sg::base::DataMatrix temp(alpha.getNrows(), this->numAlgoDims_);
 
-        #pragma omp task if(this->numAlgoDims_ - dim <= this->maxParallelDims_) shared(alpha, result)
+        #pragma omp task if(curNumAlgoDims - dim <= curMaxParallelDims) shared(alpha, result)
         up(alpha, result, dim);
 
-        #pragma omp task if(this->numAlgoDims_ - dim <= this->maxParallelDims_) shared(alpha, temp)
+        #pragma omp task if(curNumAlgoDims - dim <= curMaxParallelDims) shared(alpha, temp)
         down(alpha, temp, dim);
 
         #pragma omp taskwait
