e1945f2bc06d20e23eda66e748380dc90fc25d04
Valeriy Khakhutskyy <khakhutv@in.tum.de>
2015-01-25 23:01:28 +0000

    Reconstructed files accidatially deleted in the previous commit
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@3973 4eea3252-f0fb-4393-894d-40516dce545b

---
+++ examples/python_simple.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+# import modules
+import sys
+# append trunk/bin to search path for modules
+sys.path.append('../lib/pysgpp')
+from pysgpp import DataVector, Grid, createOperationHierarchisation, createOperationEval
+
+# create a two-dimensional piecewise bi-linear grid
+dim = 2
+grid = Grid.createLinearGrid(dim)
+gridStorage = grid.getStorage()
+print "dimensionality:         %d" % (gridStorage.dim())
+
+# create regular grid, level 3
+level = 3
+gridGen = grid.createGridGenerator()
+gridGen.regular(level)
+print "number of grid points:  %d" % (gridStorage.size())
+
+# create coefficient vector
+alpha = DataVector(gridStorage.size())
+alpha.setAll(0.0)
+print "length of alpha-vector: %d" % (len(alpha))
+
+# set function values in alpha
+f = lambda x0, x1: 16.0 * (x0-1)*x0 * (x1-1)*x1
+for i in xrange(gridStorage.size()):
+    gp = gridStorage.get(i)
+    alpha[i] = f(gp.abs(0), gp.abs(1))
+print alpha
+
+# hierarchize
+createOperationHierarchisation(grid).doHierarchisation(alpha)
+print alpha
+
+# evaluate
+p = DataVector(dim)
+p[0] = 0.52
+p[1] = 0.73
+opEval = createOperationEval(grid)
+print "u(0.52, 0.73) =", opEval.eval(alpha, p)
