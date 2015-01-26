da3b310ecb5082d05a9d6b2294d77a2c58399e11
Dirk Pflueger <dirk.pflueger@ipvs.uni-stuttgart.de>
2015-01-26 19:59:04 +0000

    CodingDays: corrected copyright notice for .py files, part 2
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/branches/SGppCodingDays@4008 4eea3252-f0fb-4393-894d-40516dce545b

--- datasets/chessboard/create_scripts.py
+++ datasets/chessboard/create_scripts.py
@@ -7,25 +7,7 @@
 # -*- coding: latin-1 -*
 
 #############################################################################
-# This file is part of pysgpp, a program package making use of spatially    #
-# adaptive sparse grids to solve numerical problems                         #
-#                                                                           #
-# Copyright (C) 2009 Alexander Heinecke (Alexander.Heinecke@mytum.de)       #
-#                                                                           #
-# pysgpp is free software; you can redistribute it and/or modify            #
-# it under the terms of the GNU General Public License as published by      #
-# the Free Software Foundation; either version 3 of the License, or         #
-# (at your option) any later version.                                       #
-#                                                                           #
-# pysgpp is distributed in the hope that it will be useful,                 #
-# but WITHOUT ANY WARRANTY; without even the implied warranty of            #
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
-# GNU Lesser General Public License for more details.                       #
-#                                                                           #
-# You should have received a copy of the GNU General Public License         #
-# along with pysgpp; if not, write to the Free Software                     #
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA #
-# or see <http://www.gnu.org/licenses/>.                                    #
+                                    #
 #############################################################################
 
 #!/usr/bin/python
