00707b6c419f88a89a3e404f75bd0e07f8d652e9
Dirk Pflueger <dirk.pflueger@ipvs.uni-stuttgart.de>
2015-01-26 19:59:04 +0000

    CodingDays: corrected copyright notice for .py files, part 2
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/branches/SGppCodingDays@4008 4eea3252-f0fb-4393-894d-40516dce545b

--- bin/utils/converter.py
+++ bin/utils/converter.py
@@ -1,25 +1,5 @@
-# Copyright (C) 2008-today The SG++ project
-# This file is part of the SG++ project. For conditions of distribution and
-# use, please see the copyright notice provided with SG++ or at 
-# sgpp.sparsegrids.org
 
-# This file is part of pysgpp, a program package making use of spatially adaptive sparse grids to solve numerical problems.
-#
-# Copyright (C) 2007-2010 Dirk Pflueger (pflueged@in.tum.de)
-#               2007 Joerg Blank (blankj@in.tum.de), Richard Roettger (roettger@in.tum.de), 
-# 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Lesser General Public License for more details.
-# 
-# You should have received a copy of the GNU Lesser General Public License
-# along with pyclass. If not, see <http://www.gnu.org/licenses/>.
+
 
 ## @package converter
 # @ingroup bin
