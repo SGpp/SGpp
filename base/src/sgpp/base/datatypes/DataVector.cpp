f63bab40e2352c82d32cb5796c123c59a7af834e
Julian Valentin <julian.valentin@ipvs.uni-stuttgart.de>
2015-03-16 13:07:48 +0000

    * base: Fix bugs in DataVector::insert.
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@4224 4eea3252-f0fb-4393-894d-40516dce545b

--- base/src/sgpp/base/datatypes/DataVector.cpp
+++ base/src/sgpp/base/datatypes/DataVector.cpp
@@ -177,7 +177,8 @@ namespace SGPP {
       }
 
       append();
-      std::memmove(data + index + 1, data + index, size - index);
+      std::memmove(data + index + 1, data + index,
+                   (size - 1 - index) * sizeof(float_t));
       data[index] = value;
     }
 
