917b3311797766056d1d5e279a2127bace576e01
David Pfander <pfandedd@ipvs.uni-stuttgart.de>
2015-03-26 12:42:56 +0000

    platforms can be better controlled
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/branches/SGppAutoTune@4259 4eea3252-f0fb-4393-894d-40516dce545b

--- datadriven/examples/sampler.cpp
+++ datadriven/examples/sampler.cpp
@@ -6,12 +6,12 @@
 int main(int argc, char **argv) {
 
 //	int maxLevel = 9;
-	int maxLevel = 7;
+	int maxLevel = 14;
 
 //	std::string fileName = "debugging.arff";
-	std::string fileName = "friedman_4d_2000.arff";
+	//std::string fileName = "friedman_4d_2000.arff";
 	//std::string fileName = "friedman2_90000.arff";
-	//std::string fileName = "bigger.arff";
+	std::string fileName = "bigger.arff";
 
 	//sg::base::RegularGridConfiguration gridConfig;
 	sg::solver::SLESolverConfiguration SLESolverConfigRefine;
