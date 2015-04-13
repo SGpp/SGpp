a93fd4f15ae46537dd2a9504b14698549fad0d8b
David Pfander <pfandedd@ipvs.uni-stuttgart.de>
2015-04-13 11:23:33 +0000

    - dummy for bspline-evaluation
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/branches/SGppAutoTune@4311 4eea3252-f0fb-4393-894d-40516dce545b

---
+++ datadriven/src/sgpp/datadriven/operation/hash/OperationMultipleEvalStreamingBSplineOCL/StreamingBSplineOCLOperatorFactory.cpp
@@ -0,0 +1,46 @@
+/*
+ * OCLOperatorFactory.hpp
+ *
+ *  Created on: Mar 25, 2015
+ *      Author: pfandedd
+ */
+
+#include "StreamingBSplineOCLOperatorFactory.hpp"
+
+#include <sgpp/base/operation/hash/OperationMultipleEval.hpp>
+#include <sgpp/base/exception/factory_exception.hpp>
+#include <sgpp/base/opencl/OpenCLConfigurationParameters.hpp>
+#include <sgpp/globaldef.hpp>
+
+namespace SGPP {
+namespace datadriven {
+
+base::OperationMultipleEval *createStreamingBSplineOCLConfigured(base::Grid& grid, base::DataMatrix& dataset) {
+
+  std::map<std::string, std::string> defaultParameter;
+  defaultParameter["KERNEL_USE_LOCAL_MEMORY"] = "true";
+  defaultParameter["KERNEL_DATA_BLOCKING_SIZE"] = "1";
+  defaultParameter["LINEAR_LOAD_BALANCING_VERBOSE"] = "false";
+  defaultParameter["KERNEL_TRANS_DATA_BLOCK_SIZE"] = "1";
+
+  base::OpenCLConfigurationParameters parameters("StreamingBSplineOCL.cfg", defaultParameter);
+
+  std::cout << "are optimizations on: " << parameters.getAsBoolean("ENABLE_OPTIMIZATIONS") << std::endl;
+  std::cout << "is local memory on: " << parameters.getAsBoolean("KERNEL_USE_LOCAL_MEMORY") << std::endl;
+  std::cout << "local size: " << parameters.getAsUnsigned("LOCAL_SIZE") << std::endl;
+  std::cout << "internal precision: " << parameters["INTERNAL_PRECISION"] << std::endl;
+  std::cout << "platform is: " << parameters["PLATFORM"] << std::endl;
+  std::cout << "device type is: " << parameters["DEVICE_TYPE"] << std::endl;
+
+  if (parameters["INTERNAL_PRECISION"] == "float") {
+    return new datadriven::OperationMultiEvalStreamingBSplineOCL<float>(grid, dataset, parameters);
+  } else if (parameters["INTERNAL_PRECISION"] == "double") {
+    return new datadriven::OperationMultiEvalStreamingBSplineOCL<double>(grid, dataset, parameters);
+  } else {
+    throw base::factory_exception(
+        "Error creating operation\"OperationMultiEvalStreamingModOCL\": invalid value for parameter \"INTERNAL_PRECISION\"");
+  }
+}
+
+}
+}
