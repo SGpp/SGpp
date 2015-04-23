429738e176707dbbe4d5aac0430e4bc97a89a945
David Pfander <pfandedd@ipvs.uni-stuttgart.de>
2015-04-23 08:02:30 +0000

    - dummy for multi platform support
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/branches/SGppAutoTune@4335 4eea3252-f0fb-4393-894d-40516dce545b

--- base/src/sgpp/base/opencl/OCLManagerMultiPlatform.hpp
+++ base/src/sgpp/base/opencl/OCLManagerMultiPlatform.hpp
@@ -5,63 +5,87 @@
  *      Author: pfandedd
  */
 
+/*
 #pragma once
 
 //define required for clCreateCommandQueue on platforms that don't support OCL2.0 yet
 #define CL_USE_DEPRECATED_OPENCL_2_0_APIS
 #include <CL/cl.h>
 
+#include <map>
+#include <vector>
+
 #include <sgpp/base/opencl/OpenCLConfigurationParameters.hpp>
 
 namespace SGPP {
-  namespace base {
+namespace base {
 
-    class OCLManagerMultiPlatform {
-      public:
-        base::OpenCLConfigurationParameters parameters;
-        size_t deviceType;
-        cl_uint num_platforms;
-        size_t* platformDeviceCount; //devices on the individual platforms
-        cl_platform_id* platform_ids;
-        cl_device_id* device_ids; // device ids over all platforms
-        cl_uint num_devices; //devices over all platforms
-        cl_command_queue* command_queue;
-        cl_context context;
-        bool verbose;
+class OCLPlatformWrapper {
+public:
+	cl_platform_id platformId;
+	char platformName[128];
+	cl_context context;
+	cl_device_id *deviceIds;
+	size_t deviceCount;
+	cl_command_queue *commandQeues;
 
-      public:
-        OCLManagerMultiPlatform(base::OpenCLConfigurationParameters parameters);
+	OCLPlatformWrapper(cl_platform_id platformId) :
+			platformId(platformId) {
 
-        /**
-         * @brief buildKernel builds the program that is represented by @a program_src and creates @a num_devices kernel objects
-         * that are stored into the array @a kernel (must be already allocated with at least @a num_devices )
-         *
-         * @param program_src the source of the program to compile
-         * @param kernel_name name of the kernel function (in program_src) to create the kernel for
-         * @param context OpenCL context
-         * @param num_devices number of OpenCL devices
-         * @param device_ids array with device ids, necessary for displaying build info
-         * @param kernel already allocated array: the resulting kernels are put into this array, one for each device (=> at least num_devices entries)
-         * @return
-         */
-        void buildKernel(const std::string& program_src, const char* kernel_name, cl_context context, size_t num_devices,
-                         cl_device_id* device_ids, cl_kernel* kernel);
+	}
+};
 
-        void setPlatformCount();
+class OCLManagerMultiPlatform {
+public:
+	base::OpenCLConfigurationParameters parameters;
+	cl_uint deviceType;
+	std::vector<OCLPlatformWrapper> platforms;
+//        cl_uint platformCount;
+//
+//        std::map<cl_platform_id, size_t> platformDeviceCount; //devices on the individual platforms
+//        cl_platform_id *platformIds;
+//
+//        std::map<cl_platform_id, cl_device_id *> platformDeviceIds; // device ids over all platforms
+	cl_uint overallDeviceCount; //devices over all platforms
 
-        void setPlatformIDs();
+//        //platforms -> (deviceId -> command_queue)
+//        std::map<cl_uint, std::map<cl_uint, cl_command_queue> > commandQueues;
+//        std::vector<cl_context> platformContext;
+	bool verbose;
 
-        void printPlatformsInfo();
+public:
+	OCLManagerMultiPlatform(base::OpenCLConfigurationParameters parameters);
 
-        void selectPlatform();
+	/ **
+	 * @brief buildKernel builds the program that is represented by @a program_src and creates @a num_devices kernel objects
+	 * that are stored into the array @a kernel (must be already allocated with at least @a num_devices )
+	 *
+	 * @param program_src the source of the program to compile
+	 * @param kernel_name name of the kernel function (in program_src) to create the kernel for
+	 * @param context OpenCL context
+	 * @param num_devices number of OpenCL devices
+	 * @param device_ids array with device ids, necessary for displaying build info
+	 * @param kernel already allocated array: the resulting kernels are put into this array, one for each device (=> at least num_devices entries)
+	 * @return
+	 * /
+	void buildKernel(const std::string &program_src, const char* kernel_name,
+			cl_context context, size_t num_devices, cl_device_id* device_ids,
+			cl_kernel* kernel);
 
-        void setTotalDeviceCount();
+	void setPlatformIDs();
 
-        void setDeviceType();
+	void printPlatformsInfo();
 
-        void setupDeviceIDs();
+	void selectPlatform();
 
-    };
+	void setTotalDeviceCount();
 
-  }
+	void setDeviceType();
+
+	void setupDeviceIDs();
+
+};
+
 }
+}
+*/
