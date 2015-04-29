319bc4046c94b3ee8dda022d5660e23ec9b4eb90
David Pfander <pfandedd@ipvs.uni-stuttgart.de>
2015-04-29 08:48:41 +0000

    - dummy kernel for subspace adaptive treatment (currently a copy of the streaming ocl kernel) - fixed some warnings in the operator factory
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/branches/SGppAutoTune@4341 4eea3252-f0fb-4393-894d-40516dce545b

---
+++ datadriven/src/sgpp/datadriven/operation/hash/OperationMultipleEvalAdaptiveOCL/AdaptiveOCLOperatorFactory.cpp
@@ -0,0 +1,45 @@
+/*
+ * OCLOperatorFactory.hpp
+ *
+ *  Created on: Mar 25, 2015
+ *      Author: pfandedd
+ */
+
+#include <sgpp/base/operation/hash/OperationMultipleEval.hpp>
+#include <sgpp/base/exception/factory_exception.hpp>
+#include <sgpp/base/opencl/OpenCLConfigurationParameters.hpp>
+#include <sgpp/globaldef.hpp>
+#include "OperationMultiEvalAdaptiveOCL.hpp"
+
+namespace SGPP {
+namespace datadriven {
+
+base::OperationMultipleEval *createAdaptiveOCLConfigured(base::Grid& grid, base::DataMatrix& dataset) {
+
+  std::map<std::string, std::string> defaultParameter;
+  defaultParameter["KERNEL_USE_LOCAL_MEMORY"] = "true";
+  defaultParameter["KERNEL_MAX_DIM_UNROLL"] = "10";
+  defaultParameter["LINEAR_LOAD_BALANCING_VERBOSE"] = "false";
+
+  base::OpenCLConfigurationParameters parameters("AdaptiveOCL.cfg", defaultParameter);
+
+//  std::cout << "are optimizations on: " << parameters.getAsBoolean("STREAMING_OCL_ENABLE_OPTIMIZATIONS") << std::endl;
+//  std::cout << "is local memory on: " << parameters.getAsBoolean("STREAMING_OCL_USE_LOCAL_MEMORY") << std::endl;
+//  std::cout << "local size: " << parameters.getAsUnsigned("STREAMING_OCL_LOCAL_SIZE") << std::endl;
+//  std::cout << "max dim unroll: " << parameters.getAsUnsigned("STREAMING_OCL_MAX_DIM_UNROLL") << std::endl;
+//  std::cout << "internal precision: " << parameters["STREAMING_OCL_INTERNAL_PRECISION"] << std::endl;
+//  std::cout << "platform is: " << parameters["STREAMING_OCL_PLATFORM"] << std::endl;
+//  std::cout << "device type is: " << parameters["STREAMING_OCL_DEVICE_TYPE"] << std::endl;
+
+  if (parameters["INTERNAL_PRECISION"] == "float") {
+    return new datadriven::OperationMultiEvalAdaptiveOCL<float>(grid, dataset, parameters);
+  } else if (parameters["INTERNAL_PRECISION"] == "double") {
+    return new datadriven::OperationMultiEvalAdaptiveOCL<double>(grid, dataset, parameters);
+  } else {
+    throw base::factory_exception(
+        "Error creating operation\"OperationMultiEvalAdaptiveOCL\": invalid value for parameter \"INTERNAL_PRECISION\"");
+  }
+}
+
+}
+}
---
+++ datadriven/src/sgpp/datadriven/operation/hash/OperationMultipleEvalAdaptiveOCL/AdaptiveOCLOperatorFactory.hpp
@@ -0,0 +1,20 @@
+/*
+ * OCLOperatorFactory.hpp
+ *
+ *  Created on: Mar 25, 2015
+ *      Author: pfandedd
+ */
+
+#pragma once
+
+#include <sgpp/globaldef.hpp>
+
+#include <sgpp/base/operation/hash/OperationMultipleEval.hpp>
+
+namespace SGPP {
+namespace datadriven {
+
+base::OperationMultipleEval *createAdaptiveOCLConfigured(base::Grid& grid, base::DataMatrix& dataset);
+
+}
+}
