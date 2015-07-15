b8583eb830644460266140e4c42b31959c6735eb
Julian Valentin <julian.valentin@ipvs.uni-stuttgart.de>
2015-07-15 09:27:35 +0000

    trunk: Fix Doxygen warnings and reformat (datadriven).
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@4571 4eea3252-f0fb-4393-894d-40516dce545b

--- datadriven/examples/batchLearnerExample.cpp
+++ datadriven/examples/batchLearnerExample.cpp
@@ -6,7 +6,7 @@
 #include <iostream>
 #include <string>
 #include <stdio.h>
-#include <string.h> 
+#include <string.h>
 
 #include <sgpp/base/grid/Grid.hpp>
 #include <sgpp/datadriven/application/BatchLearner.hpp>
@@ -15,7 +15,7 @@
 #include <sgpp/datadriven/tools/ARFFTools.hpp>
 
 /**
- * This programm demonstrates the usage of the BatchLearner class. After the parameters are set, the method trainBatch() is called until the end of the file has been reached. 
+ * This programm demonstrates the usage of the BatchLearner class. After the parameters are set, the method trainBatch() is called until the end of the file has been reached.
  */
 
 using namespace sg::base;
@@ -23,22 +23,21 @@ using namespace sg::datadriven;
 using namespace std;
 
 
-int main (int argc, char **args)
-{
+int main (int argc, char** args) {
   cout << "parameters: bs(batch size), ts (test size), input, mode(weighting), arg(for weighting), level, pts(to refine every refinement), ref(refine every xth batch, 0=never)" << endl;
- 
+
   std::map<string, string> argsMap;
-  for (int i = 1; i < argc; i += 2)
-  {
-        argsMap[args[i]] = args[i + 1];
+
+  for (int i = 1; i < argc; i += 2) {
+    argsMap[args[i]] = args[i + 1];
   }
 
-  
-   //set variables
+
+  //set variables
   sg::base::BatchConfiguration batchConfig;
   sg::solver::SLESolverConfiguration solverConfig;
   sg::base::AdpativityConfiguration adaptConfig;
-  sg::base::RegularGridConfiguration gridConfig;  
+  sg::base::RegularGridConfiguration gridConfig;
 
   // Set Adaptivity
   adaptConfig.maxLevelType_ = false;//not used by BatchLearner
@@ -68,26 +67,25 @@ int main (int argc, char **args)
 
   //set up the grid config
   gridConfig.level_ = atoi(argsMap["level"].c_str());
-  
+
   //init the learner
   sg::datadriven::BatchLearner learner(batchConfig, gridConfig, solverConfig, adaptConfig);
 
 
-  int i =0;
-  
-  while (!learner.getIsFinished())//while there are still batches to read
-  {
-	std::cout << "called trainBatch() #" << i << std::endl;
-	long int start_l = clock();
-	learner.trainBatch();
-	long int stop_l=clock();
-	cout << "trainBatch() took: " << (stop_l-start_l)/double(CLOCKS_PER_SEC)*1000 << endl;
-	
-	//If testsize > 0, the learner will test on testsize items following the batch; in the next batch these items will be included and the batch is filled with new items until batchsize is reached. This will be done after every batch.
-	//AC = accuracy of the current batch
-	//AG = accuracy over all predictions
-	cout << "AC: " <<i <<", " << learner.getAccCurrent() << "\nAG: " << i <<", "<< learner.getAccGlobal() << endl;
-	i++;
+  int i = 0;
+
+  while (!learner.getIsFinished()) { //while there are still batches to read
+    std::cout << "called trainBatch() #" << i << std::endl;
+    long int start_l = clock();
+    learner.trainBatch();
+    long int stop_l = clock();
+    cout << "trainBatch() took: " << (stop_l - start_l) / double(CLOCKS_PER_SEC) * 1000 << endl;
+
+    //If testsize > 0, the learner will test on testsize items following the batch; in the next batch these items will be included and the batch is filled with new items until batchsize is reached. This will be done after every batch.
+    //AC = accuracy of the current batch
+    //AG = accuracy over all predictions
+    cout << "AC: " << i << ", " << learner.getAccCurrent() << "\nAG: " << i << ", " << learner.getAccGlobal() << endl;
+    i++;
   }
 
   //don't forget to close the file (only needed if the file has not been read to completion yet, otherwise the reader will close the file automatically)
