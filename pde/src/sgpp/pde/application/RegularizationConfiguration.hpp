9720f9198a24e2b22702688fe2c4aafb2086a958
Fabian Franzelin <fabian.franzelin@ipvs.uni-stuttgart.de>
2015-07-31 12:22:24 +0000

    missing file added
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@4684 4eea3252-f0fb-4393-894d-40516dce545b

---
+++ pde/src/sgpp/pde/application/RegularizationConfiguration.hpp
@@ -0,0 +1,26 @@
+ // Copyright (C) 2008-today The SG++ project
+// This file is part of the SG++ project. For conditions of distribution and
+// use, please see the copyright notice provided with SG++ or at
+// sgpp.sparsegrids.org
+
+#ifndef REGULARIZATIONCONFIGURATION_HPP_
+#define REGULARIZATIONCONFIGURATION_HPP_
+
+#include <sgpp/globaldef.hpp>
+
+namespace SGPP {
+  namespace pde {
+
+  enum RegularizationType {
+      Identity,
+      Laplace
+  };
+
+  struct RegularizationConfiguration {
+      RegularizationType regType_;
+  };
+
+  } // pde
+} // SGPP
+
+#endif /* REGULARIZATIONCONFIGURATION_HPP_ */
