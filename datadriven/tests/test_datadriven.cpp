4bdbceaa8ed666e26400114779f85f8c94659c9e
Julian Valentin <julian.valentin@ipvs.uni-stuttgart.de>
2015-08-04 13:01:04 +0000

    trunk: Next try to accelerate Jenkins testing.
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@4721 4eea3252-f0fb-4393-894d-40516dce545b

--- datadriven/tests/test_datadriven.cpp
+++ datadriven/tests/test_datadriven.cpp
@@ -1,19 +1,3 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE DatadrivenTestModule
 #include <boost/test/unit_test.hpp>
-#include <omp.h>
-
-struct GlobalFixture {
-  const int MAX_NUM_THREADS = 2;
-
-  GlobalFixture() {
-    // limit number of OpenMP threads, because if chosen too high,
-    // then testing will be very slow on machines with a high CPU load
-    // (e.g., bad for Jenkins)
-    if (omp_get_max_threads() > MAX_NUM_THREADS) {
-      omp_set_num_threads(MAX_NUM_THREADS);
-    }
-  }
-};
-
-BOOST_GLOBAL_FIXTURE(GlobalFixture)
