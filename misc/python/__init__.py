a21fc7966413b40ea2be3d445e525ff887896b2a
Fabian Franzelin <fabian.franzelin@ipvs.uni-stuttgart.de>
2015-08-19 13:36:13 +0000

    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@4800 4eea3252-f0fb-4393-894d-40516dce545b

---
+++ misc/python/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (C) 2008-today The SG++ Project
+# This file is part of the SG++ project. For conditions of distribution and
+# use, please see the copyright notice provided with SG++ or at 
+# sgpp.sparsegrids.org
+
+__all__ = ['python']
