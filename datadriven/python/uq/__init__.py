452efeac3c67829bc8d772e505a77483d84d4d9a
Fabian Franzelin <fabian.franzelin@ipvs.uni-stuttgart.de>
2015-08-20 14:24:54 +0000

    updated module list
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@4830 4eea3252-f0fb-4393-894d-40516dce545b

--- datadriven/python/uq/__init__.py
+++ datadriven/python/uq/__init__.py
@@ -5,8 +5,20 @@ UQ package for SGpp
 
 __version__ = "1.0"
 
-__all__ = ["dists", "sampler", "parameters", "transformation",
-           "uq_plot", "uq_setting", "jsonLib", "tools",
+__all__ = ["analysis",
+           "dists",
+           "estimators",
+           "learner",
+           "operations",
+           "parameters",
+           "plot",
+           "quadrature",
+           "refinement",
+           "sampler",
+           "transformation",
+           "uq_setting",
+           "jsonLib",
+           "tools",
            "toolsKbhitCountdown"]
 
 __author__ = "Fabian Franzelin, fabian.franzelin@ipvs.uni-stuttgart.de"
