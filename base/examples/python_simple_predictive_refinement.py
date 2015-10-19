5462cb4b48d58b7d218b752142c93ab9cfe6bd28
Markus Baur <st107919@stud.uni-stuttgart.de>
2015-10-19 15:04:14 +0000

    Refactor of old examples and adding ANOVA c++ example
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@5106 4eea3252-f0fb-4393-894d-40516dce545b

--- base/examples/python_simple_predictive_refinement.py
+++ base/examples/python_simple_predictive_refinement.py
@@ -27,7 +27,7 @@ print "Start: number of grid points:  %d" % (HashGridStorage.size())
 
 # definition of function to interpolate - nonsymmetric(!)
 #f = lambda x0, x1: 16.0 * (x0-1)*x0 * (x1-1)*x1-x1
-f = lambda x0, x1: math.sin(x0*math.pi)
+f = lambda x0, x1: math.sin(x0*math.pi) *math.sin(x1*math.pi)
 # create coefficient vectors
 alpha = DataVector(HashGridStorage.size())
 
@@ -39,14 +39,15 @@ cols = 100
 dataSet = DataMatrix(rows*cols,dim)
 vals = DataVector(rows*cols)
 
+# Create a "List" of points where the error should be calculated. 
+# This represents a regular 2d grid with a step size of 1 / rows and 1 / cols.
 for i in xrange(rows):
     for j in xrange(cols):
-            #xcoord
-            dataSet.set(i*cols+j,0,i*1.0/rows)
-            #ycoord
-            dataSet.set(i*cols+j,1,j*1.0/cols)
-            vals[i*cols+j] = f(i*1.0/rows,j*1.0/cols)
-            
+        #xcoord
+        dataSet.set(i*cols+j,0,i*1.0/rows)
+        #ycoord
+        dataSet.set(i*cols+j,1,j*1.0/cols)
+        vals[i*cols+j] = f(i*1.0/rows,j*1.0/cols) 
 
 def calculateError(dataSet,f,grid,alpha,error):
     print "calculating error"
@@ -69,8 +70,8 @@ for i in xrange(HashGridStorage.size()):
         xCoordsOld.append(gridPointCoordinates[0])
         yCoordsOld.append(gridPointCoordinates[1])
  
-# now refine adaptively 10 times
-for refnum in range(10):
+# now refine adaptively 20 times
+for refnum in range(20):
     # set function values in alpha
     for i in xrange(HashGridStorage.size()):
         gp = HashGridStorage.get(i)
@@ -85,7 +86,6 @@ for refnum in range(10):
   
     xCoordinates = []
     yCoordinates = []
-    #zCoordinates = []
   
     #print all points
     
@@ -116,7 +116,7 @@ for refnum in range(10):
     refinement = HashRefinement()
     decorator = PredictiveRefinement(refinement)
     # refine a single grid point each time
-    print "ErrorVector = %s" % errorVector.sum()
+    print "Error over all = %s" % errorVector.sum()
     indicator = PredictiveRefinementIndicator(grid,dataSet,errorVector,1)
     decorator.free_refine(HashGridStorage,indicator)
     
