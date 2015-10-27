494f5620c17b0a73e42f5ca1814dda1609d437ec
David Pfander <pfandedd@ipvs.uni-stuttgart.de>
2015-10-27 16:10:13 +0000

    - beautified examples
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/branches/SGppAutoTune@5152 4eea3252-f0fb-4393-894d-40516dce545b

--- datadriven/examplesMIC/samplerMod.cpp
+++ datadriven/examplesMIC/samplerMod.cpp
@@ -6,8 +6,7 @@
 
 int main(int argc, char** argv) {
 
-    int maxLevel = 10;
-    //  int maxLevel = 2;
+    int maxLevel = 7;
 
     //  std::string fileName = "debugging.arff";
    std::string fileName = "friedman_4d.arff";
@@ -15,39 +14,34 @@ int main(int argc, char** argv) {
 //  std::string fileName = "chess_train.arff";
 //    std::string fileName = "bigger.arff";
 
-//sg::base::RegularGridConfiguration gridConfig;
     sg::base::RegularGridConfiguration gridConfig;
     sg::solver::SLESolverConfiguration SLESolverConfigRefine;
     sg::solver::SLESolverConfiguration SLESolverConfigFinal;
     sg::base::AdpativityConfiguration adaptConfig;
 
-    // setup grid
+	// setup grid
     gridConfig.dim_ = 0; //dim is inferred from the data
     gridConfig.level_ = maxLevel;
     gridConfig.type_ = sg::base::ModLinear;
 
-    // Set Adaptivity
+	// setup adaptivity
     adaptConfig.maxLevelType_ = false;
     adaptConfig.noPoints_ = 80;
     adaptConfig.numRefinements_ = 0;
     adaptConfig.percent_ = 200.0;
     adaptConfig.threshold_ = 0.0;
 
-    // Set solver during refinement
+	// setup solver during refinement
     SLESolverConfigRefine.eps_ = 0;
     SLESolverConfigRefine.maxIterations_ = 0;
     SLESolverConfigRefine.threshold_ = -1.0;
     SLESolverConfigRefine.type_ = sg::solver::CG;
 
-    // Set solver for final step
+	// setup solver for final step
     SLESolverConfigFinal.eps_ = 0;
     SLESolverConfigFinal.maxIterations_ = 10;
     SLESolverConfigFinal.threshold_ = -1.0;
     SLESolverConfigFinal.type_ = sg::solver::CG;
-
-    std::string metaInformation = "refine: " + std::to_string((unsigned long long) adaptConfig.numRefinements_)
-            + " points: " + std::to_string((unsigned long long) adaptConfig.noPoints_) + " iterations: "
-            + std::to_string((unsigned long long) SLESolverConfigRefine.maxIterations_);
 
     double lambda = 0.000001;
 
@@ -55,24 +49,22 @@ int main(int argc, char** argv) {
     SGPP::datadriven::MetaLearner learner(gridConfig, SLESolverConfigRefine, SLESolverConfigFinal, adaptConfig, lambda,
             verbose);
 
-    //learner.learn(kernelType, fileName);
-    //learner.learnReference(fileName);
+	// configuration for intrisics-based streaming implementation
+	SGPP::datadriven::OperationMultipleEvalConfiguration configuration(
+	SGPP::datadriven::OperationMultipleEvalType::STREAMING,
+	SGPP::datadriven::OperationMultipleEvalSubType::DEFAULT);
 
-    //buggy are:
-    //subspace simple - 0
-    //subspacelinear combined - 60
-    //streaming default - 1600 (13 without avx)
-    //streaming ocl - 13
+//	// configuration for subspace-based evaluation (with subspace-skipping)
+//	SGPP::datadriven::OperationMultipleEvalConfiguration configuration(
+//	SGPP::datadriven::OperationMultipleEvalType::SUBSPACELINEAR,
+//	SGPP::datadriven::OperationMultipleEvalSubType::COMBINED);
 
-    SGPP::datadriven::OperationMultipleEvalConfiguration configuration(
-            SGPP::datadriven::OperationMultipleEvalType::STREAMING,
-            SGPP::datadriven::OperationMultipleEvalSubType::DEFAULT);
+	// only execute learning (no comparisons or tests, for performance measurements)
     learner.learn(configuration, fileName);
-    //learner.learnReference(fileName);
 
-    //learner.learnAndTest(fileName, testFileName, isBinaryClassificationProblem);
-    //learner.learnAndCompare(configuration, fileName, 8, 1.0);
-    //learner.writeStatisticsFile("statistics.csv", "test");
+    // execute learning with the specified configuration and use the implementation from base as comparison
+    // result grids are compared by sampling the domain (again with a grid) and comparing the evaluated values
+//    learner.learnAndCompare(configuration, fileName, 8);
 
     return EXIT_SUCCESS;
 }
