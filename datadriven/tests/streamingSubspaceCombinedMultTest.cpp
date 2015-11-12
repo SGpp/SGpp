e07f048e5732610f8178d50c5cfaf7c51e1e6ea8
David Pfander <pfandedd@ipvs.uni-stuttgart.de>
2015-11-12 15:30:54 +0000

    - added missing files
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/branches/SGppAutoTune@5232 4eea3252-f0fb-4393-894d-40516dce545b

---
+++ datadriven/tests/streamingSubspaceCombinedMultTest.cpp
@@ -0,0 +1,52 @@
+/*
+ * multiEvalPerformance.cpp
+ *
+ *  Created on: Mar 12, 2015
+ *      Author: pfandedd
+ */
+#ifdef __AVX__
+
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+
+#include <random>
+#include <fstream>
+#include <iostream>
+
+#include <zlib.h>
+
+
+#include "test_datadrivenCommon.hpp"
+
+#include <sgpp/globaldef.hpp>
+
+#include <sgpp/base/operation/hash/OperationMultipleEval.hpp>
+#include <sgpp/datadriven/DatadrivenOpFactory.hpp>
+#include <sgpp/base/operation/BaseOpFactory.hpp>
+#include <sgpp/datadriven/tools/ARFFTools.hpp>
+#include <sgpp/base/grid/generation/functors/SurplusRefinementFunctor.hpp>
+#include <sgpp/base/tools/ConfigurationParameters.hpp>
+
+BOOST_AUTO_TEST_SUITE(TestStreamingSubspaceCombinedMult)
+
+BOOST_AUTO_TEST_CASE(Simple) {
+
+    std::vector<std::tuple<std::string, double> > fileNamesError = { std::tuple<std::string, double>(
+            "datadriven/tests/data/friedman_4d.arff.gz", 1E-24), std::tuple<std::string, double>("datadriven/tests/data/friedman_10d.arff.gz", 1E-21) };
+
+    uint32_t level = 5;
+
+    SGPP::datadriven::OperationMultipleEvalConfiguration configuration(
+    SGPP::datadriven::OperationMultipleEvalType::SUBSPACELINEAR,
+    SGPP::datadriven::OperationMultipleEvalSubType::COMBINED);
+
+    for (std::tuple<std::string, double> fileNameError : fileNamesError) {
+        double mse = compareToReference(SGPP::base::Linear, std::get<0>(fileNameError), level, configuration);
+//        BOOST_TEST_MESSAGE(std::get<0>(fileNameError));
+        BOOST_CHECK(mse < std::get<1>(fileNameError));
+    }
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+#endif
