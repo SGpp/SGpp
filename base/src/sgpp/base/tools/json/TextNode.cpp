6a5b52ee266b97e00fde6563a917cad73c43db3b
Markus Baur <st107919@stud.uni-stuttgart.de>
2015-12-01 00:37:32 +0000

    and some more missing files
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@5290 4eea3252-f0fb-4393-894d-40516dce545b

---
+++ base/src/sgpp/base/tools/json/TextNode.cpp
@@ -0,0 +1,51 @@
+/*
+ * JSONStringNode.cpp
+ *
+ *  Created on: Nov 7, 2015
+ *      Author: pfandedd
+ */
+
+#include "TextNode.hpp"
+
+#include <fstream>
+
+#include "json_exception.hpp"
+
+namespace json {
+
+TextNode::TextNode() :
+    value() {
+}
+
+void TextNode::parse(std::vector<Token> &stream) {
+//create new text node
+  if (stream[0].type == TokenType::STRING) {
+    this->value = stream[0].value;
+    stream.erase(stream.begin());
+  } else {
+    throw json_exception("expected string value");
+  }
+}
+
+void TextNode::serialize(std::ofstream &outFile, size_t indentWidth) {
+  outFile << "\"" << this->value << "\"";
+}
+
+std::string &TextNode::get() {
+  return this->value;
+}
+
+void TextNode::set(const std::string &value) {
+  this->value = value;
+}
+
+size_t TextNode::size() {
+  return 1;
+}
+
+Node *TextNode::clone() {
+  TextNode *newNode = new TextNode(*this);
+  return newNode;
+}
+
+}
