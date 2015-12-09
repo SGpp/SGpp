8dd2c6291e4be61084dffba443bfb88d219b92d0
Julian Valentin <julian.valentin@ipvs.uni-stuttgart.de>
2015-12-09 10:26:31 +0000

    trunk: Try to remove some warnings on MinGW and MSVC++. There are some warnings left, most of which are bugs in the Boost headers. [base, datadriven, optimization, solver]
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@5329 4eea3252-f0fb-4393-894d-40516dce545b

--- base/src/sgpp/globaldef.hpp
+++ base/src/sgpp/globaldef.hpp
@@ -39,30 +39,43 @@
  * and they're missing, e.g., on MinGW-w64.
  */
 // e
+#undef  M_E
 #define M_E         2.7182818284590452354
 // log_10(2)
+#undef  M_LOG2E
 #define M_LOG2E     1.4426950408889634074
 // log_10(e)
+#undef  M_LOG10E
 #define M_LOG10E    0.43429448190325182765
 // ln(2)
+#undef  M_LN2
 #define M_LN2       0.69314718055994530942
 // ln(10)
+#undef  M_LN10
 #define M_LN10      2.30258509299404568402
 // pi
+#undef  M_PI
 #define M_PI        3.14159265358979323846
 // pi/2
+#undef  M_PI_2
 #define M_PI_2      1.57079632679489661923
 // pi/4
+#undef  M_PI_4
 #define M_PI_4      0.78539816339744830962
 // 1/pi
+#undef  M_1_PI
 #define M_1_PI      0.31830988618379067154
 // 2/pi
+#undef  M_2_PI
 #define M_2_PI      0.63661977236758134308
 // 2/sqrt(pi)
+#undef  M_2_SQRTPI
 #define M_2_SQRTPI  1.12837916709551257390
 // sqrt(2)
+#undef  M_SQRT2
 #define M_SQRT2     1.41421356237309504880
 // 1/sqrt(2)
+#undef  M_SQRT1_2
 #define M_SQRT1_2   0.70710678118654752440 
 
 namespace SGPP {
