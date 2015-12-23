b2dfc3be719176fc6465e0c35247fd33d599ea6e
David Pfander <pfandedd@ipvs.uni-stuttgart.de>
2015-12-23 18:27:22 +0000

    - fix for doxygen
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@5384 4eea3252-f0fb-4393-894d-40516dce545b

--- base/src/sgpp/base/opencl/OCLManagerMultiPlatform.cpp
+++ base/src/sgpp/base/opencl/OCLManagerMultiPlatform.cpp
@@ -75,18 +75,6 @@ OCLManagerMultiPlatform::~OCLManagerMultiPlatform() {
 
 }
 
-/**
- * @brief buildKernel builds the program that is represented by @a program_src and creates @a num_devices kernel objects
- * that are stored into the array @a kernel (must be already allocated with at least @a num_devices )
- *
- * @param program_src the source of the program to compile
- * @param kernel_name name of the kernel function (in program_src) to create the kernel for
- * @param context OpenCL context
- * @param num_devices number of OpenCL devices
- * @param device_ids array with device ids, necessary for displaying build info
- * @param kernel already allocated array: the resulting kernels are put into this array, one for each device (=> at least num_devices entries)
- * @return
- */
 void OCLManagerMultiPlatform::buildKernel(const std::string &program_src, const char* kernel_name,
         std::map<cl_platform_id, std::vector<cl_kernel> > &kernels) {
     cl_int err;
