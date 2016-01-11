b707c2bf10327a02f543223cfff878df330b268c
David Pfander <pfandedd@ipvs.uni-stuttgart.de>
2016-01-11 16:58:33 +0000

    - python script to generate the chess dataset
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@5395 4eea3252-f0fb-4393-894d-40516dce545b

---
+++ datasets/chessboard/chess.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+import random
+
+fileName = "chess_3d.arff"
+numElements = 10000
+dim = 3
+fields = dim
+seed = 135
+
+random.seed(seed)
+
+# compute intervalsize
+intervalsize = 1.0/fields
+print "intervalsize = %f"%(intervalsize)
+
+dat = ""
+for n in range(numElements):
+    point = ""
+    b = 1.0
+    for i in range(dim):
+        r = random.random()
+        rang = r/intervalsize
+        irang = int(rang+1.0)
+        
+        # calcualte class
+        if (irang % 2) == 0:
+            b *= 1.0
+        else:
+            b *= (-1.0)
+             
+        point = point + str(r) + " "
+        
+    point = point + str(b) + "\n"
+    dat = dat + point
+
+outFile = open(fileName, "w")
+
+header = "@RELATION \"" + fileName + "\"\n"
+header += "\n"
+for d in range(dim):
+    header += "@ATTRIBUTE x" + str(d) + " NUMERIC\n"
+header += "@ATTRIBUTE class NUMERIC"
+header += "\n"
+header += "@DATA\n"
+
+outFile.write(header + dat)
