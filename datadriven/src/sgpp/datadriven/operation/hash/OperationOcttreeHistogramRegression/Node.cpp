d9822f12e0b44b033e8f7e7a09aadaaf72ca77a3
David Pfander <pfandedd@ipvs.uni-stuttgart.de>
2016-01-11 16:26:43 +0000

    - added missing files
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@5394 4eea3252-f0fb-4393-894d-40516dce545b

---
+++ datadriven/src/sgpp/datadriven/operation/hash/OperationOcttreeHistogramRegression/Node.cpp
@@ -0,0 +1,20 @@
+/*
+ * Node.cpp
+ *
+ *  Created on: Jan 8, 2016
+ *      Author: pfandedd
+ */
+
+#include "Node.hpp"
+
+namespace SGPP {
+namespace datadriven {
+namespace HistogramTree {
+
+uint64_t Node::integratedNodes;
+
+uint64_t Node::hierarchizeMaxLevel;
+
+}
+}
+}
