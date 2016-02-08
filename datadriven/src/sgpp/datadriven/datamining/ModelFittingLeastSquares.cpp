d412700b6307308d30772fe60fd331f02252c3cc
David Pfander <pfandedd@ipvs.uni-stuttgart.de>
2016-02-08 19:20:23 +0000

    - commit to permit compilation
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@5552 4eea3252-f0fb-4393-894d-40516dce545b

---
+++ datadriven/src/sgpp/datadriven/datamining/ModelFittingLeastSquares.cpp
@@ -0,0 +1,58 @@
+// Copyright (C) 2008-today The SG++ project
+// This file is part of the SG++ project. For conditions of distribution and
+// use, please see the copyright notice provided with SG++ or at
+// sgpp.sparsegrids.org
+
+#include "ModelFittingLeastSquares.hpp"
+
+#include <sgpp/datadriven/algorithm/SystemMatrixLeastSquaresIdentity.hpp>
+#include <sgpp/datadriven/tools/LearnerVectorizedPerformanceCalculator.hpp>
+#include <sgpp/datadriven/DatadrivenOpFactory.hpp>
+
+#include <sgpp/base/exception/factory_exception.hpp>
+#include <sgpp/base/exception/application_exception.hpp>
+
+//TODO: use the system matrix with flexible regularization
+
+namespace SGPP {
+namespace datadriven {
+
+ModelFittingLeastSquares::ModelFittingLeastSquares(
+		DataMiningConfigurationLeastSquares &configuration) :
+		datadriven::ModelFittingBase(configuration), configuration(
+				configuration) {
+}
+
+ModelFittingLeastSquares::~ModelFittingLeastSquares() {
+}
+
+datadriven::DMSystemMatrixBase*
+ModelFittingLeastSquares::createDMSystem(base::DataMatrix& trainDataset,
+		float_t lambda) {
+	if (this->grid) {
+		//TODO: improve
+		throw base::application_exception("grid is null");
+	}
+
+	datadriven::SystemMatrixLeastSquaresIdentity* systemMatrix =
+			new datadriven::SystemMatrixLeastSquaresIdentity(*(this->grid),
+					trainDataset, lambda);
+	systemMatrix->setImplementation(this->implementationConfiguration);
+	return systemMatrix;
+}
+
+base::DataVector ModelFittingLeastSquares::predict(
+		base::DataMatrix& testDataset) {
+	base::DataVector classesComputed(testDataset.getNrows());
+
+	base::OperationMultipleEval* MultEval =
+			op_factory::createOperationMultipleEval(*(this->grid), testDataset,
+					this->implementationConfiguration);
+	MultEval->mult(*alpha, classesComputed);
+	delete MultEval;
+
+	return classesComputed;
+}
+
+}
+}
