64bef38a345ccfba4ea2494549dea34317a2db54
Christoph Kowitz <kowitz@in.tum.de>
2016-02-08 16:45:40 +0000

    cleared combigrid.hpp
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@5533 4eea3252-f0fb-4393-894d-40516dce545b

--- combigrid/src/combigrid.hpp
+++ combigrid/src/combigrid.hpp
@@ -1,39 +1,6 @@
-// Copyright (C) 2008-today The SG++ project
-// This file is part of the SG++ project. For conditions of distribution and
-// use, please see the copyright notice provided with SG++ or at
-// sgpp.sparsegrids.org
-
-#ifndef COMBIGRID_HPP_
+#ifdef COMBIGRID_HPP_
 #define COMBIGRID_HPP_
 
-// --------- Core SGpp includes -----------
-#include <sgpp/base/datatypes/DataVector.hpp>
-#include <sgpp/base/grid/Grid.hpp>
-#include <sgpp/base/grid/GridStorage.hpp>
-
-// ----------- Combi Grid Includes ----------
-#include <sgpp/combigrid/combigrid/AbstractCombiGrid.hpp>
-#include <sgpp/combigrid/combigrid/SerialCombiGrid.hpp>
-#include <sgpp/combigrid/combigrid/AdaptiveSerialCombiGrid.hpp>
-#include <sgpp/combigrid/combigrid/AdaptiveSerialCombiGridVariableCoefficients.hpp>
-#include <sgpp/combigrid/fullgrid/CombiFullGrid.hpp>
-
-#include <sgpp/combigrid/combischeme/CombiSchemeBasis.hpp>
-#include <sgpp/combigrid/combischeme/CombiS_CT.hpp>
-
-#include <sgpp/combigrid/combischeme/CombiTS_CT.hpp>
-#include <sgpp/combigrid/combischeme/CombiArbitraryScheme.hpp>
-
-#include <sgpp/combigrid/domain/AbstractStretchingMaker.hpp>
-#include <sgpp/combigrid/domain/CombiGridDomain.hpp>
-#include <sgpp/combigrid/domain/CombiDomain1D.hpp>
-#include <sgpp/combigrid/domain/CombiTanStretching.hpp>
-#include <sgpp/combigrid/domain/CombiAtanSpecialStretching.hpp>
-#include <sgpp/combigrid/domain/CombiUniformStretching.hpp>
-
-#include <sgpp/combigrid/converter/CombiSGppConverter.hpp>
-
-#include <sgpp/combigrid/operation/hash/OperationMatrixLTwoExplicitFullGrid.hpp>
 
 
-#endif /* COMBIGRID_HPP_ */
+#endif
