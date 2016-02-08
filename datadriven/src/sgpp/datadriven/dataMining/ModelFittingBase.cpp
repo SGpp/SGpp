db454141627e12467a1c6a745f00ccccd5dd6f0e
Fabian Franzelin <fabian.franzelin@ipvs.uni-stuttgart.de>
2016-02-08 17:10:45 +0000

    Base class for learner
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@5536 4eea3252-f0fb-4393-894d-40516dce545b

---
+++ datadriven/src/sgpp/datadriven/dataMining/ModelFittingBase.cpp
@@ -0,0 +1,41 @@
+/*
+ * ModelFittingBase.cpp
+ *
+ *  Created on: Feb 8, 2016
+ *      Author: franzefn
+ */
+
+#include <sgpp/datadriven/dataMining/ModelFittingBase.hpp>
+
+using namespace SGPP::base;
+
+namespace SGPP {
+namespace datadriven {
+
+ModelFittingBase::ModelFittingBase() : grid(nullptr), alpha(0) {
+}
+
+ModelFittingBase::~ModelFittingBase() {
+}
+
+void ModelFittingBase::evaluate(DataMatrix& samples, DataVector& result) {
+  uint32_t numSamples = samples.getNrows();
+  uint32_t numDims = samples.getNcols();
+
+  DataVector sample(numDims);
+  for (size_t i = 0; i < numSamples; i++) {
+    samples.getRow(i, sample);
+    result[i] = evaluate(sample);
+  }
+}
+
+std::shared_ptr<SGPP::base::Grid> ModelFittingBase::getGrid() {
+  return grid;
+}
+
+std::shared_ptr<SGPP::base::DataVector> ModelFittingBase::getSurpluses() {
+  return alpha;
+}
+
+} /* namespace datadriven */
+} /* namespace SGPP */
---
+++ datadriven/src/sgpp/datadriven/dataMining/ModelFittingBase.hpp
@@ -0,0 +1,52 @@
+// Copyright (C) 2008-today The SG++ project
+// This file is part of the SG++ project. For conditions of distribution and
+// use, please see the copyright notice provided with SG++ or at
+// sgpp.sparsegrids.org
+
+
+#pragma once
+
+#include <sgpp/globaldef.hpp>
+#include <sgpp/base/grid/Grid.hpp>
+#include <sgpp/base/datatypes/DataVector.hpp>
+#include <sgpp/base/datatypes/DataMatrix.hpp>
+
+
+namespace SGPP {
+  namespace datadriven {
+
+    class ModelFittingBase {
+      public:
+        ModelFittingBase();
+        virtual ~ModelFittingBase();
+
+        /**
+         *
+         */
+        virtual void fit() = 0;
+
+        /**
+         *
+         * @param sample
+         * @return
+         */
+        virtual SGPP::float_t evaluate(SGPP::base::DataVector& sample) = 0;
+
+        /**
+         *
+         * @param samples
+         * @return
+         */
+        virtual void evaluate(SGPP::base::DataMatrix& samples, SGPP::base::DataVector& result);
+
+        virtual std::shared_ptr<SGPP::base::Grid> getGrid();
+        virtual std::shared_ptr<SGPP::base::DataVector> getSurpluses();
+
+      protected:
+        std::shared_ptr<SGPP::base::Grid> grid;
+        std::shared_ptr<SGPP::base::DataVector> alpha;
+    };
+
+  } /* namespace datadriven */
+} /* namespace SGPP */
+
