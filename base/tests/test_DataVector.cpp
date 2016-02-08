99b8c9e97986adf5dcf7a12afb3d720611bdf802
Friedrich Menhorn <menhorn@in.tum.de>
2016-02-08 17:18:58 +0000

    added some tests to test_DataVector
    git-svn-id: https://ipvs.informatik.uni-stuttgart.de/SGpp/repos/trunk@5539 4eea3252-f0fb-4393-894d-40516dce545b

--- base/tests/test_DataVector.cpp
+++ base/tests/test_DataVector.cpp
@@ -66,14 +66,129 @@ BOOST_AUTO_TEST_CASE(testMinMax) {
 }
 
 BOOST_AUTO_TEST_CASE(testOps) {
+  DataVector d = d_rand;
+  DataVector d2 = DataVector(N);
+  SGPP::float_t scalar = 0.213;
+
+  for (int i = 0; i < N; ++i) {
+	d2[i] = static_cast<SGPP::float_t>(i);
+  }
+
+  //add
+  d = DataVector(d_rand);
+  d.add(d2);
+  for (int i = 0; i < N; ++i){
+	  BOOST_CHECK_EQUAL(d[i], d_rand[i]+static_cast<SGPP::float_t>(i));
+  }
+
+  //axpy
+  d = DataVector(d_rand);
+  d.axpy(scalar, d2);
+  for (int i = 0; i < N; ++i){
+	  BOOST_CHECK_EQUAL(d[i],d_rand[i] + scalar * static_cast<SGPP::float_t>(i));
+  }
+
+  //dotProduct
+  d = DataVector(d_rand);
+  SGPP::float_t dotProdResultActual = d.dotProduct(d2);
+  SGPP::float_t dotProdResultExact = 0;
+  for (int i = 0; i < N; ++i){
+	  dotProdResultExact += d_rand[i]*static_cast<SGPP::float_t>(i);
+  }
+  BOOST_CHECK_EQUAL(dotProdResultActual, dotProdResultExact);
+
+  //L2Norm
+  d = DataVector(d_rand);
+  SGPP::float_t lTwoNormSquaredActual = d.l2Norm()*d.l2Norm();
+  SGPP::float_t lTwoNormSquaredExact = 0;
+  for (int i = 0; i < N; ++i){
+	  lTwoNormSquaredExact += d_rand[i]*d_rand[i];
+  }
+  BOOST_CHECK_EQUAL(lTwoNormSquaredActual, lTwoNormSquaredExact);
+
+  //max
+  d = DataVector(d_rand);
+  SGPP::float_t maxActual = d.max();
+  BOOST_CHECK_EQUAL(max, maxActual);
+
+  //maxNorm
+  d = DataVector(d_rand);
+  SGPP::float_t maxNormActual = d.maxNorm();
+  SGPP::float_t maxNormExpected = 0.0;
+  for (int i = 1; i < N; ++i){
+	  maxNormExpected = maxNormExpected < fabs(d_rand[i]) ? fabs(d_rand[i]) : maxNormExpected;
+  }
+  BOOST_CHECK_EQUAL(maxNormActual, maxNormExpected);
+
+  //min
+  d = DataVector(d_rand);
+  SGPP::float_t minActual = d.min();
+  BOOST_CHECK_EQUAL(minActual, min);
+
+  //minmax
+  d.minmax(&minActual, &maxActual);
+  BOOST_CHECK_EQUAL(minActual, min);
+  BOOST_CHECK_EQUAL(maxActual, max);
+
+  //normalize
+  d = DataVector(d_rand);
+  d.normalize();
+  SGPP::float_t border = 0.0;
+  float_t delta = (d_rand.max() - d_rand.min()) / (1 - 2 * border);
+  for (int i = 0; i < N; i++){
+	  BOOST_CHECK_EQUAL(d[i], (d_rand[i] - d_rand.min())/delta + border);
+  }
+
+  //normalize with border
+  d = DataVector(d_rand);
+  border = 3.64;
+  d.normalize(border);
+  float_t delta = (d_rand.max() - d_rand.min()) / (1 - 2 * border);
+  for (int i = 0; i < N; i++){
+	  BOOST_CHECK_EQUAL(d[i], (d_rand[i] - d_rand.min())/delta + border);
+  }
+
+  //RMSNorm
+  d = DataVector(d_rand);
+  SGPP::float_t rmsNormSquaredActual = d.RMSNorm()*d.RMSNorm();
+  SGPP::float_t rmsNormSquaredExpected = 0;
+  for (int i = 0; i < N; ++i){
+	  rmsNormSquaredExpected += d_rand[i]*d_rand[i];
+  }
+  rmsNormSquaredExpected *= 1/N;
+  BOOST_CHECK_EQUAL(rmsNormSquaredActual, rmsNormSquaredExpected);
+
+  //sub
+  d = DataVector(d_rand);
+  d.sub(d2);
+  for (int i = 0; i < N; ++i){
+	  BOOST_CHECK_EQUAL(d[i], d_rand[i]-static_cast<SGPP::float_t>(i));
+  }
+
+  //mult scalar
+  d = DataVector(d_rand);
+  d.mult(scalar);
+  for (int i = 0; i < N; ++i){
+	  BOOST_CHECK_EQUAL(d[i], d_rand[i]*scalar);
+  }
+
   //sum
   BOOST_CHECK_CLOSE(d_rand.sum(), sum, 1e-12);
+
   //square
-  DataVector d = d_rand;
+  d = DataVector(d_rand);
   d.sqr();
 
   for (int i = 0; i < N; ++i) {
-    BOOST_CHECK_EQUAL(d_rand[i] * d_rand[i], d[i]);
+    BOOST_CHECK_EQUAL(d[i], d_rand[i] * d_rand[i]);
+  }
+
+  //sqrt
+  d = DataVector(d_rand);
+  d.sqr();
+  d.sqrt();
+  for (int i = 0; i < N; ++i){
+	    BOOST_CHECK_EQUAL(d[i], d_rand[i]);
   }
 
   //abs
@@ -81,21 +196,16 @@ BOOST_AUTO_TEST_CASE(testOps) {
   d.abs();
 
   for (int i = 0; i < N; ++i) {
-    BOOST_CHECK_EQUAL(fabs(d_rand[i]), d[i]);
+    BOOST_CHECK_EQUAL(d[i], fabs(d_rand[i]));
   }
 
   //componentwise mult
   d = DataVector(d_rand);
-  DataVector d2 = DataVector(N);
-
-  for (int i = 0; i < N; ++i) {
-    d2[i] = static_cast<SGPP::float_t>(i);
-  }
 
   d.componentwise_mult(d2);
 
   for (int i = 0; i < N; ++i) {
-    BOOST_CHECK_EQUAL(d_rand[i] * static_cast<SGPP::float_t>(i), d[i]);
+    BOOST_CHECK_EQUAL(d[i], d_rand[i] * static_cast<SGPP::float_t>(i));
   }
 
   // componentwise div
