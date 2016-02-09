f58f6f3def6a672dee1f5dfdc9fc48f07452ff6a
Malte Brunn <brunnme@ipvs.uni-stuttgart.de>
2016-02-09 11:27:22 +0100

    Grid Generator Unit Test

---
+++ base/tests/test_GridGenerator.cpp
@@ -0,0 +1,18 @@
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+
+#include <sgpp/base/grid/generation/BoundaryGridGenerator.hpp>
+#include <sgpp/base/grid/generation/GeneralizedBoundaryGridGenerator.hpp>
+#include <sgpp/base/grid/generation/GridGenerator.hpp>
+#include <sgpp/base/grid/generation/L0BoundaryGridGenerator.hpp>
+#include <sgpp/base/grid/generation/ParabolicGridGenerator.hpp>
+#include <sgpp/base/grid/generation/PrewaveletGridGenerator.hpp>
+#include <sgpp/base/grid/generation/SquareRootGridGenerator.hpp>
+#include <sgpp/base/grid/generation/StandardGridGenerator.hpp>
+#include <sgpp/base/grid/generation/StrechedBoundaryGridGenerator.hpp>
+
+using namespace SGPP::base;
+
+BOOST_AUTO_TEST_CASE(testSerialize) {
+  
+}
