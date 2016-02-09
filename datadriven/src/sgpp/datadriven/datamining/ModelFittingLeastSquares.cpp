0e1f51d15416cd1eea278e272abf0f0ee086f7da
pfandedd <pfandedd@ipvs.uni-stuttgart.de>
2016-02-09 16:17:09 +0100

    - partial refactoring of least squares learner

--- datadriven/src/sgpp/datadriven/datamining/ModelFittingLeastSquares.cpp
+++ datadriven/src/sgpp/datadriven/datamining/ModelFittingLeastSquares.cpp
@@ -19,228 +19,93 @@ namespace datadriven {
 
 ModelFittingLeastSquares::ModelFittingLeastSquares(
 SGPP::datadriven::DataMiningConfigurationLeastSquares config) :
-		datadriven::ModelFittingBase(), configuration(config) {
+        datadriven::ModelFittingBase(), configuration(config) {
 }
 
 ModelFittingLeastSquares::~ModelFittingLeastSquares() {
 }
 
 datadriven::DMSystemMatrixBase*
-ModelFittingLeastSquares::createDMSystem(base::DataMatrix& trainDataset,
-		float_t lambda) {
-	if (this->grid) {
-		//TODO: improve
-		throw base::application_exception("grid is null");
-	}
-
-	datadriven::SystemMatrixLeastSquaresIdentity* systemMatrix =
-			new datadriven::SystemMatrixLeastSquaresIdentity(*(this->grid),
-					trainDataset, lambda);
-	systemMatrix->setImplementation(this->implementationConfiguration);
-	return systemMatrix;
+ModelFittingLeastSquares::createSystemMatrix(base::DataMatrix& trainDataset, float_t lambda) {
+  datadriven::SystemMatrixLeastSquaresIdentity* systemMatrix =
+          new datadriven::SystemMatrixLeastSquaresIdentity(*(this->grid), trainDataset, lambda);
+  systemMatrix->setImplementation(this->implementationConfiguration);
+  return systemMatrix;
 }
 
 void ModelFittingLeastSquares::fit(datadriven::Dataset& dataset) {
-	/*
-	 LearnerTiming result;
 
-	 if (trainDataset.getNrows() != classes.getSize()) {
-	 throw base::application_exception(
-	 "LearnerBase::train: length of classes vector does not match "
-	 "to dataset!");
-	 }
+//  LearnerTiming result;
+//  result.timeComplete_ = 0.0;
+//  result.timeMultComplete_ = 0.0;
+//  result.timeMultCompute_ = 0.0;
+//  result.timeMultTransComplete_ = 0.0;
+//  result.timeMultTransCompute_ = 0.0;
+//  result.timeRegularization_ = 0.0;
+//  result.GFlop_ = 0.0;
+//  result.GByte_ = 0.0;
 
-	 result.timeComplete_ = 0.0;
-	 result.timeMultComplete_ = 0.0;
-	 result.timeMultCompute_ = 0.0;
-	 result.timeMultTransComplete_ = 0.0;
-	 result.timeMultTransCompute_ = 0.0;
-	 result.timeRegularization_ = 0.0;
-	 result.GFlop_ = 0.0;
-	 result.GByte_ = 0.0;
+  float_t oldAcc = 0.0;
 
-	 execTime_ = 0.0;
-	 GFlop_ = 0.0;
-	 GByte_ = 0.0;
+  alpha.reset();
+  grid.reset();
+  systemMatrix.reset();
 
-	 float_t oldAcc = 0.0;
+  InitializeGrid(GridConfig);
 
-	 // Construct Grid
-	 if (alpha != NULL)
-	 delete alpha;
+  // create DMSystem
+  systemMatrix = std::shared_ptr<datadriven::DMSystemMatrixBase>(
+          createSystemMatrix(dataset.getTrainingData(), configuration.lambda));
 
-	 if (grid != NULL)
-	 delete grid;
+  if (configuration.solverRefineConfig.type_ == SGPP::solver::SLESolverType::CG) {
+    solver = std::make_shared<solver::ConjugateGradients>(
+            configuration.solverRefineConfig.maxIterations_, configuration.solverRefineConfig.eps_);
+  } else if (configuration.solverRefineConfig.type_ == SGPP::solver::SLESolverType::BiCGSTAB) {
+    solver = std::make_shared<solver::BiCGStab>(configuration.solverRefineConfig.maxIterations_,
+            configuration.solverRefineConfig.eps_);
+  } else {
+    throw base::application_exception("LearnerBase::train: An unsupported SLE solver type was "
+            "chosen!");
+  }
 
-	 if (isTrained_ == true)
-	 isTrained_ = false;
+  SGPP::base::DataVector b(alpha->getSize());
+  systemMatrix->generateb(dataset.getClasses(), b);
 
-	 InitializeGrid(GridConfig);
+  if (configuration.adaptivityConfig.numRefinements_ == 0) {
+    solver->setMaxIterations(configuration.solverFinalConfig.maxIterations_);
+    solver->setEpsilon(configuration.solverFinalConfig.eps_);
+  }
 
-	 // check if grid was created
-	 if (grid == NULL)
-	 return result;
+  solver->solve(*systemMatrix, *alpha, b, true, false, 0.0);
 
-	 // create DMSystem
-	 SGPP::datadriven::DMSystemMatrixBase* DMSystem = createDMSystem(
-	 trainDataset, lambdaRegularization);
+//  float_t tmp1, tmp2, tmp3, tmp4;
+//  systemMatrix->getTimers(tmp1, tmp2, tmp3, tmp4);
+//  result.timeComplete_ = execTime_;
+//  result.timeMultComplete_ = tmp1;
+//  result.timeMultCompute_ = tmp2;
+//  result.timeMultTransComplete_ = tmp3;
+//  result.timeMultTransCompute_ = tmp4;
+//  result.timeRegularization_ = 0.0;
+//  result.GFlop_ = GFlop_;
+//  result.GByte_ = GByte_;
+}
 
-	 // check if System was created
-	 if (DMSystem == NULL)
-	 return result;
-
-	 SGPP::solver::SLESolver* myCG;
-
-	 if (SolverConfigRefine.type_ == SGPP::solver::SLESolverType::CG) {
-	 myCG = new SGPP::solver::ConjugateGradients(
-	 SolverConfigRefine.maxIterations_, SolverConfigRefine.eps_);
-	 } else if (SolverConfigRefine.type_
-	 == SGPP::solver::SLESolverType::BiCGSTAB) {
-	 myCG = new SGPP::solver::BiCGStab(SolverConfigRefine.maxIterations_,
-	 SolverConfigRefine.eps_);
-	 } else {
-	 throw base::application_exception(
-	 "LearnerBase::train: An unsupported SLE solver type was "
-	 "chosen!");
-	 }
-
-	 // Pre-Procession
-	 preProcessing();
-
-	 if (isVerbose_)
-	 std::cout << "Starting Learning...." << std::endl;
-
-	 // execute adaptsteps
-	 SGPP::base::SGppStopwatch* myStopwatch = new SGPP::base::SGppStopwatch();
-	 SGPP::base::SGppStopwatch* myStopwatch2 = new SGPP::base::SGppStopwatch();
-
-	 for (size_t i = 0; i < AdaptConfig.numRefinements_ + 1; i++) {
-	 if (isVerbose_)
-	 std::cout << std::endl << "Doing refinement: " << i << std::endl;
-
-	 this->currentRefinementStep = i;
-
-	 myStopwatch->start();
-
-	 // Do Refinements
-	 if (i > 0) {
-	 myStopwatch2->start();
-
-	 // disable refinement here!
-	 SGPP::base::SurplusRefinementFunctor* myRefineFunc = new
-	 SGPP::base::SurplusRefinementFunctor(alpha_, AdaptConfig.noPoints_,
-	 AdaptConfig.threshold_);
-	 grid_->createGridGenerator()->refine(myRefineFunc);
-	 delete myRefineFunc;
-
-	 // tell the SLE manager that the grid changed (for interal data structures)
-	 DMSystem->prepareGrid();
-
-	 alpha_->resizeZero(grid_->getSize());
-	 float_t refineTime = myStopwatch2->stop();
-
-	 if (isVerbose_)
-	 std::cout << "New Grid Size: " << grid_->getSize()
-	 << " (Refinement took " << refineTime << " secs)"
-	 << std::endl;
-	 } else {
-	 if (isVerbose_)
-	 std::cout << "Grid Size: " << grid_->getSize() << std::endl;
-	 }
-
-	 SGPP::base::DataVector b(alpha_->getSize());
-	 DMSystem->generateb(classes, b);
-
-	 if (i == AdaptConfig.numRefinements_) {
-	 myCG->setMaxIterations(SolverConfigFinal.maxIterations_);
-	 myCG->setEpsilon(SolverConfigFinal.eps_);
-	 }
-
-	 myCG->solve(*DMSystem, *alpha_, b, true, false, 0.0);
-
-	 float_t stopTime = myStopwatch->stop();
-	 this->execTime_ += stopTime;
-	 this->stepExecTime_ = stopTime;
-
-	 if (isVerbose_) {
-	 std::cout << std::endl;
-	 std::cout << "Needed Iterations: " << myCG->getNumberIterations()
-	 << std::endl;
-	 std::cout << "Final residuum: " << myCG->getResiduum() << std::endl;
-	 }
-
-	 // use post-processing to determine Flops and time
-	 if (i < AdaptConfig.numRefinements_) {
-	 postProcessing(trainDataset, SolverConfigRefine.type_,
-	 myCG->getNumberIterations());
-	 } else {
-	 postProcessing(trainDataset, SolverConfigFinal.type_,
-	 myCG->getNumberIterations());
-	 }
-
-	 float_t tmp1, tmp2, tmp3, tmp4;
-	 DMSystem->getTimers(tmp1, tmp2, tmp3, tmp4);
-	 result.timeComplete_ = execTime_;
-	 result.timeMultComplete_ = tmp1;
-	 result.timeMultCompute_ = tmp2;
-	 result.timeMultTransComplete_ = tmp3;
-	 result.timeMultTransCompute_ = tmp4;
-	 result.timeRegularization_ = 0.0;
-	 result.GFlop_ = GFlop_;
-	 result.GByte_ = GByte_;
-
-	 if (testAccDuringAdapt) {
-	 float_t acc = getAccuracy(trainDataset, classes);
-
-	 if (isVerbose_) {
-	 if (isRegression_) {
-	 if (isVerbose_)
-	 std::cout << "MSE (train): " << acc << std::endl;
-	 } else {
-	 if (isVerbose_)
-	 std::cout << "Acc (train): " << acc << std::endl;
-	 }
-	 }
-
-	 if (isRegression_) {
-	 if ((i > 0) && (oldAcc <= acc)) {
-	 if (isVerbose_)
-	 std::cout
-	 << "The grid is becoming worse --> stop learning"
-	 << std::endl;
-
-	 break;
-	 }
-	 } else {
-	 if ((i > 0) && (oldAcc >= acc)) {
-	 if (isVerbose_)
-	 std::cout
-	 << "The grid is becoming worse --> stop learning"
-	 << std::endl;
-
-	 break;
-	 }
-	 }
-
-	 oldAcc = acc;
-	 }
-	 }
-
-	 if (isVerbose_) {
-	 std::cout << "Finished Training!" << std::endl << std::endl;
-	 std::cout << "Training took: " << execTime_ << " seconds" << std::endl
-	 << std::endl;
-	 }
-
-	 isTrained_ = true;
-
-	 delete myStopwatch;
-	 delete myStopwatch2;
-	 delete myCG;
-	 delete DMSystem;
-
-	 return result;
-	 */
+void ModelFittingLeastSquares::refine() {
+//  // disable refinement here!
+//  auto refinementFunctor = std::make_shared<base::SurplusRefinementFunctor>(alpha,
+//          configuration.adaptivityConfig.noPoints_, configuration.adaptivityConfig.threshold_);
+//  std::unique_ptr<base::GridGenerator>(grid->createGridGenerator())->refine(
+//          refinementFunctor.get());
+//
+//  // tell the SLE manager that the grid changed (for interal data structures)
+//  systemMatrix->prepareGrid();
+//
+//  alpha->resizeZero(grid->getSize());
+//
+//  if (i == configuration.adaptivityConfig.numRefinements_) {
+//    myCG->setMaxIterations(configuration.solverFinalConfig.maxIterations_);
+//    myCG->setEpsilon(configuration.solverFinalConfig.eps_);
+//  }
 }
 
 }
