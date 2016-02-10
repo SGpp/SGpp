8532da673f971a7b6f51f8e06d8cdce06befb660
pfandedd <pfandedd@ipvs.uni-stuttgart.de>
2016-02-10 17:02:07 +0100

    - added ".clang-format" with google style preconfigured - some reformatting of new learner components

--- datadriven/src/sgpp/datadriven/datamining/MSE.cpp
+++ datadriven/src/sgpp/datadriven/datamining/MSE.cpp
@@ -1,32 +1,28 @@
-/*
- * MSE.cpp
- *
- *  Created on: Feb 8, 2016
- *      Author: perun
- */
+// Copyright (C) 2008-today The SG++ project
+// This file is part of the SG++ project. For conditions of distribution and
+// use, please see the copyright notice provided with SG++ or at
+// sgpp.sparsegrids.org
 
 #include "MSE.hpp"
 
 #include <sgpp/globaldef.hpp>
 
-
 namespace SGPP {
 namespace datadriven {
 
 MSE::MSE() {
-	// TODO Auto-generated constructor stub
-
+  // TODO(valeriy) Auto-generated constructor stub
 }
 
 MSE::~MSE() {
-	// TODO Auto-generated destructor stub
+  // TODO(valeriy) Auto-generated destructor stub
 }
 
-double MSE::operator()(DataVector& predictedValues, DataVector& trueValues){
-	DataVector tmp(predictedValues);
-	tmp.sub(trueValues);
-	double error =  tmp.l2Norm();
-	return error*error/static_cast<double>(tmp.getSize());
+double MSE::operator()(DataVector& predictedValues, DataVector& trueValues) {
+  DataVector tmp(predictedValues);
+  tmp.sub(trueValues);
+  double error = tmp.l2Norm();
+  return error * error / static_cast<double>(tmp.getSize());
 }
 
 } /* namespace datadriven */
