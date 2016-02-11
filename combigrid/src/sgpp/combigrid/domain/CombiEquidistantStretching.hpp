fa489dfabd1b5a1f6b5f7d92f2998389b0d5901d
Steffen Seckler <seckler@in.tum.de>
2016-02-11 13:00:36 +0100

    doxytest

--- combigrid/src/sgpp/combigrid/domain/CombiEquidistantStretching.hpp
+++ combigrid/src/sgpp/combigrid/domain/CombiEquidistantStretching.hpp
@@ -26,12 +26,12 @@ class CombiEquidistantStretching : public AbstractStretchingMaker {
 
   /**
    *
-   * @param level - integer specifying the current grid level; the
+   * @param level integer specifying the current grid level, the
    * corresponding nr of points is 2**level + 1
-   * @param min - the left boundary of the interval
-   * @param max - the right boundary of the interval
-   * @param stretching - the output vector of pre-computed grid points
-   * @param jacobian - the evaluated jacobian at all points of the stretching,
+   * @param min the left boundary of the interval
+   * @param max the right boundary of the interval
+   * @param stretching the output vector of pre-computed grid points
+   * @param jacobian the evaluated jacobian at all points of the stretching,
    * taking into consideration size of the interval and underlying tranformations
    */
   void get1DStretching(int level, double min, double max,
