08eb690f497795012c1a28f551123ce26fc60c61
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2016-02-17 19:47:05 +0100

    Added depth-first algorithm for searching clusters

--- datadriven/src/sgpp/datadriven/operation/hash/OperationCreateGraphOCL/OperationCreateGraphOCL.hpp
+++ datadriven/src/sgpp/datadriven/operation/hash/OperationCreateGraphOCL/OperationCreateGraphOCL.hpp
@@ -20,11 +20,66 @@ namespace StreamingOCLMultiPlatform {
 class OperationCreateGraphOCL
 {
 protected:
+	static int find_neighbors(int index, std::vector<int> &nodes, int cluster, int k, std::vector<int> &clusterList,
+							  bool overwrite=false) {
+		int currIndex;
+		clusterList[index]=cluster;
+		bool overwrite_enabled = overwrite;
+		for (int i = index*k; i < (index+1) *k; i++) {
+			if(nodes[i]==-2)
+				continue;
+			currIndex = nodes[i];
+			if (nodes[currIndex*k]!=-1) {
+				if(clusterList[currIndex]==0 && !overwrite_enabled) {
+					clusterList[currIndex] = cluster;
+					int ret_cluster = OperationCreateGraphOCL::find_neighbors(currIndex, nodes, cluster, k, clusterList);
+					if(ret_cluster != cluster) {
+						cluster = ret_cluster;
+						clusterList[index]=cluster;
+						overwrite_enabled = true;
+						i = index*k;
+						continue;
+					}
+				}
+				else if(!overwrite_enabled && clusterList[currIndex]!=cluster) {
+					cluster = clusterList[currIndex];
+					clusterList[index]=cluster;
+					overwrite_enabled = true;
+					i = index*k-1;
+					continue;
+				}
+				else{
+					if(clusterList[currIndex]!=cluster) {
+						OperationCreateGraphOCL::find_neighbors(currIndex, nodes, cluster, k, clusterList,true);
+						clusterList[currIndex] = cluster;
+					}
+				}
+			}
+		}
+		return cluster;
+	}
 public:
 	OperationCreateGraphOCL()  {
 	}
 
 	virtual void create_graph(base::DataVector& data, std::vector<int> &resultVector) {}
+	static std::vector<int> find_clusters(std::vector<int> &graph, size_t k) {
+		std::vector<int> clusters(graph.size()/k);
+		int clustercount = 0;
+		for (size_t node = 0; node < clusters.size(); node++)
+			clusters[node]=0;
+		for (int i = 0; i < clusters.size(); i++) {
+			if(clusters[i] == 0 && graph[i*k]!=-1) {
+				clustercount++;
+				if(OperationCreateGraphOCL::find_neighbors(i, graph, clustercount, k, clusters) != clustercount)
+					clustercount--;
+			}
+		}
+		std::cout<<"Found "<<clustercount<<" clusters!"<<std::endl;
+		return clusters;
+
+	}
+
 };
 
 }
