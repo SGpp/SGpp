edb496a4e8028175d200c9df14256d6f0d803773
pfandedd <pfandedd@ipvs.uni-stuttgart.de>
2016-02-17 18:39:50 +0100

    - datadriven(OCL): - now actually compiling - datadriven(OCL): - examples improved

--- datadriven/src/sgpp/datadriven/operation/hash/OperationMultipleEvalStreamingModOCLFastMultiPlattform/OperatorFactory.cpp
+++ datadriven/src/sgpp/datadriven/operation/hash/OperationMultipleEvalStreamingModOCLFastMultiPlattform/OperatorFactory.cpp
@@ -9,7 +9,7 @@
 #include "sgpp/base/operation/hash/OperationMultipleEval.hpp"
 #include "sgpp/base/exception/factory_exception.hpp"
 #include "sgpp/globaldef.hpp"
-#include "StreamingModOCLFastMultiPlatformOperatorFactory.hpp"
+#include "OperatorFactory.hpp"
 #include "sgpp/base/opencl/OCLOperationConfiguration.hpp"
 #include "OperationMultiEvalStreamingModOCLFastMultiPlatform.hpp"
 
