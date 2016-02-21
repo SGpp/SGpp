c9221511969c933c28a38d617e7924255b7e79e3
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2016-02-21 04:42:42 +0100

    Modified mult source builder to enable vectorization

--- datadriven/src/sgpp/datadriven/operation/hash/OperationDensityOCLMultiPlatform/KernelSourceBuilderMult.hpp
+++ datadriven/src/sgpp/datadriven/operation/hash/OperationDensityOCLMultiPlatform/KernelSourceBuilderMult.hpp
@@ -103,39 +103,38 @@ public:
             device(device), kernelConfiguration(kernelConfiguration), dims(dims) {
     }
 
-    std::string generateSource() {
+    std::string generateSource(size_t dimensions, size_t gridsize) {
         std::stringstream sourceStream;
 
         if (this->floatType().compare("double") == 0) {
             sourceStream << "#pragma OPENCL EXTENSION cl_khr_fp64 : enable" << std::endl << std::endl;
 			}
 
-		sourceStream << "void kernel multdensity(global const int *starting_points,global const "<<this->floatType()<<" *alpha, global "<<this->floatType()<<" *result, private const int dimensionsize, private const int gridsize,const "<<this->floatType()<<" lambda)" << std::endl;
+		sourceStream << "void kernel multdensity(global const int *starting_points,global const "<<this->floatType()<<" *alpha, global "<<this->floatType()<<" *result,const "<<this->floatType()<<" lambda)" << std::endl;
 		sourceStream << "{" << std::endl;
 		sourceStream << ""<<this->floatType()<<" gesamtint=0.0;" << std::endl;
-		sourceStream << "for(private int i=0;i<gridsize;i++) {" << std::endl;
+		sourceStream << "for(private int i=0;i< " << gridsize << ";i++) {" << std::endl;
 		sourceStream << ""<<this->floatType()<<" zellenintegral=1.0;" << std::endl;
-		sourceStream << "for(private int dim=0;dim<dimensionsize;dim++) {" << std::endl;
-		sourceStream << "int index=starting_points[get_global_id(0)*dimensionsize*2+2*dim];" << std::endl;
-		sourceStream << "int level=starting_points[get_global_id(0)*dimensionsize*2+2*dim+1];" << std::endl;
-		sourceStream << "int index2=starting_points[i*dimensionsize*2+2*dim];" << std::endl;
-		sourceStream << "int level2=starting_points[i*dimensionsize*2+2*dim+1];" << std::endl;
-		sourceStream << "if(starting_points[get_global_id(0)*dimensionsize*2+2*dim+1]>starting_points[i*dimensionsize*2+2*dim+1]) {" << std::endl;
-		sourceStream << "index=starting_points[i*dimensionsize*2+2*dim];" << std::endl;
-		sourceStream << "level=starting_points[i*dimensionsize*2+2*dim+1];" << std::endl;
-		sourceStream << "index2=starting_points[get_global_id(0)*dimensionsize*2+2*dim];" << std::endl;
-		sourceStream << "level2=starting_points[get_global_id(0)*dimensionsize*2+2*dim+1];" << std::endl;
+		sourceStream << "for(private int dim=0;dim< " << dimensions << ";dim++) {" << std::endl;
+		sourceStream << "int index=starting_points[get_global_id(0)* " << dimensions << "*2+2*dim];" << std::endl;
+		sourceStream << "int level=starting_points[get_global_id(0)* " << dimensions << "*2+2*dim+1];" << std::endl;
+		sourceStream << "int index2=starting_points[i* " << dimensions << "*2+2*dim];" << std::endl;
+		sourceStream << "int level2=starting_points[i* " << dimensions << "*2+2*dim+1];" << std::endl;
+		sourceStream << "if(starting_points[get_global_id(0)* " << dimensions << "*2+2*dim+1]>starting_points[i* " << dimensions << "*2+2*dim+1]) {" << std::endl;
+		sourceStream << "index=starting_points[i* " << dimensions << "*2+2*dim];" << std::endl;
+		sourceStream << "level=starting_points[i* " << dimensions << "*2+2*dim+1];" << std::endl;
+		sourceStream << "index2=starting_points[get_global_id(0)* " << dimensions << "*2+2*dim];" << std::endl;
+		sourceStream << "level2=starting_points[get_global_id(0)* " << dimensions << "*2+2*dim+1];" << std::endl;
 		sourceStream << "}" << std::endl;
-		sourceStream << ""<<this->floatType()<<" h=1.0;" << std::endl;
-		sourceStream << "for(unsigned int z=1;z<=level2;z++)" << std::endl;
-		sourceStream << "h*=0.5;" << std::endl;
+		sourceStream << "int teiler = (1 << level2);" << std::endl;
+		sourceStream << ""<<this->floatType()<<" h=1.0 / teiler;" << std::endl;
+		//sourceStream << "for(unsigned int z=1;z<=level2;z++)" << std::endl;
+		//sourceStream << "h*=0.5;" << std::endl;
 		sourceStream << ""<<this->floatType()<<" grenze1=h*(index2-1);" << std::endl;
 		sourceStream << ""<<this->floatType()<<" grenze2=h*(index2+1);" << std::endl;
-		sourceStream << ""<<this->floatType()<<" uright=1;" << std::endl;
-		sourceStream << "for(int z=1;z<=level;z++)" << std::endl;
-		sourceStream << "uright*=2;" << std::endl;
-		sourceStream << ""<<this->floatType()<<" uleft=uright*grenze1-index;" << std::endl;
-		sourceStream << "uright=uright*grenze2-index;" << std::endl;
+		sourceStream << "int u= (1 << level);" << std::endl;
+		sourceStream << ""<<this->floatType()<<" uright=u*grenze2-index;" << std::endl;
+		sourceStream << ""<<this->floatType()<<" uleft=u*grenze1-index;" << std::endl;
 		sourceStream << "if(uleft<0)" << std::endl;
 		sourceStream << "uleft*=-1;" << std::endl;
 		sourceStream << "uleft=1-uleft;" << std::endl;
@@ -146,9 +145,9 @@ public:
 		sourceStream << "uright=1-uright;" << std::endl;
 		sourceStream << "if(uright<0)" << std::endl;
 		sourceStream << "uright=0;" << std::endl;
-		sourceStream << "if(starting_points[i*dimensionsize*2+2*dim+1]==starting_points[get_global_id(0)*dimensionsize*2+2*dim+1]) {" << std::endl;
+		sourceStream << "if(starting_points[i* " << dimensions << "*2+2*dim+1]==starting_points[get_global_id(0)* " << dimensions << "*2+2*dim+1]) {" << std::endl;
 		sourceStream << "zellenintegral*=2.0/3.0*h;" << std::endl;
-		sourceStream << "if(starting_points[i*dimensionsize*2+2*dim]!=starting_points[get_global_id(0)*dimensionsize*2+2*dim])" << std::endl;
+		sourceStream << "if(starting_points[i* " << dimensions << "*2+2*dim]!=starting_points[get_global_id(0)* " << dimensions << "*2+2*dim])" << std::endl;
 		sourceStream << "zellenintegral=0.0;" << std::endl;
 		sourceStream << "}" << std::endl;
 		sourceStream << "else" << std::endl;
