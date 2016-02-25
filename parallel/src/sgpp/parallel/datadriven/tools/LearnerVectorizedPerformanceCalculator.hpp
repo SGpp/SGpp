41cd52c1a2d49daeee62b8a96994e2945256cea9
seckler <seckler@in.tum.de>
2016-02-25 10:39:50 +0100

    reformatting for parallel/datadriven completed, except for ArBB

--- parallel/src/sgpp/parallel/datadriven/tools/LearnerVectorizedPerformanceCalculator.hpp
+++ parallel/src/sgpp/parallel/datadriven/tools/LearnerVectorizedPerformanceCalculator.hpp
@@ -12,7 +12,6 @@
 
 #include <sgpp/globaldef.hpp>
 
-
 namespace SGPP {
 
 namespace parallel {
@@ -47,13 +46,11 @@ class LearnerVectorizedPerformanceCalculator {
    *
    * @return a LearnerVectorizedPerformance struct containing the results
    */
-  static LearnerVectorizedPerformance getGFlopAndGByte(SGPP::base::Grid& Grid,
-      size_t numInstances,
-      SGPP::solver::SLESolverType solver, size_t numIterations, size_t sizeDatatype);
+  static LearnerVectorizedPerformance getGFlopAndGByte(SGPP::base::Grid& Grid, size_t numInstances,
+                                                       SGPP::solver::SLESolverType solver,
+                                                       size_t numIterations, size_t sizeDatatype);
 };
-
-}
-
-}
+}  // namespace parallel
+}  // namespace SGPP
 
 #endif /* LEARNERVECTORIZEDPERFORMANCECALCULATOR_HPP */
