ec25d08e439eaeac1f090f7cb9d9c42721d52b94
Julian Valentin <julian.valentin@ipvs.uni-stuttgart.de>
2016-02-28 11:59:16 +0100

    Replace remaining `sg::` with `sgpp::`. [base, datadriven, parallel]

--- datadriven/examplesMIC/sampler.cpp
+++ datadriven/examplesMIC/sampler.cpp
@@ -17,10 +17,10 @@ int main(int argc, char** argv) {
   //  std::string fileName = "friedman2_90000.arff";
   //  std::string fileName = "bigger.arff";
 
-  sg::base::RegularGridConfiguration gridConfig;
-  sg::solver::SLESolverConfiguration SLESolverConfigRefine;
-  sg::solver::SLESolverConfiguration SLESolverConfigFinal;
-  sg::base::AdpativityConfiguration adaptConfig;
+  sgpp::base::RegularGridConfiguration gridConfig;
+  sgpp::solver::SLESolverConfiguration SLESolverConfigRefine;
+  sgpp::solver::SLESolverConfiguration SLESolverConfigFinal;
+  sgpp::base::AdpativityConfiguration adaptConfig;
 
   // setup grid
   gridConfig.dim_ = 0;  // dim is inferred from the data
