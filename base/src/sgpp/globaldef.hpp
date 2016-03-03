07a7f41bb81b4390535dabffb2ecd82815b04deb
Julian Valentin <julian.valentin@ipvs.uni-stuttgart.de>
2016-03-03 08:52:32 +0100

    Test commit 4 for Jenkins (will fail).

--- base/src/sgpp/globaldef.hpp
+++ base/src/sgpp/globaldef.hpp
@@ -3,7 +3,7 @@
 // use, please see the copyright notice provided with SG++ or at
 // sgpp.sparsegrids.org
 
-#error "Test error 3."
+#error "Test error 4."
 
 #ifndef SGPP_GLOBALDEF_HPP_
 #define SGPP_GLOBALDEF_HPP_
