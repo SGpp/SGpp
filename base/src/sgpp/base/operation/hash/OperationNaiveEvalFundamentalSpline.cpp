b216ed58e53ef95dc860dc52f5dac7da05067bca
Julian Valentin <julian.valentin@ipvs.uni-stuttgart.de>
2016-03-04 14:52:08 +0100

    base/OperationNaiveEval: Implement eval* methods for vector-valued functions. Also, remove std::vector overloads which were inconsistent.

--- base/src/sgpp/base/operation/hash/OperationNaiveEvalFundamentalSpline.cpp
+++ base/src/sgpp/base/operation/hash/OperationNaiveEvalFundamentalSpline.cpp
@@ -36,5 +36,36 @@ double OperationNaiveEvalFundamentalSpline::eval(const DataVector& alpha,
   return result;
 }
 
+void OperationNaiveEvalFundamentalSpline::eval(const DataMatrix& alpha,
+                                               const DataVector& point,
+                                               DataVector& value) {
+  const size_t n = storage.getSize();
+  const size_t d = storage.getDimension();
+  const size_t m = alpha.getNcols();
+
+  value.resize(m);
+  value.setAll(0.0);
+
+  for (size_t i = 0; i < n; i++) {
+    const GridIndex& gp = *storage[i];
+    double curValue = 1.0;
+
+    for (size_t t = 0; t < d; t++) {
+      const double val1d = base.eval(gp.getLevel(t), gp.getIndex(t), point[t]);
+
+      if (val1d == 0.0) {
+        curValue = 0.0;
+        break;
+      }
+
+      curValue *= val1d;
+    }
+
+    for (size_t j = 0; j < m; j++) {
+      value[j] += alpha(i, j) * curValue;
+    }
+  }
+}
+
 }  // namespace base
 }  // namespace sgpp
