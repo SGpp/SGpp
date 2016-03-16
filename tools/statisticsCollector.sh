91b33a3cfcd9eab02869f99ab93b2bacccf18ebf
David Pfander <david.pfander@ipvs.uni-stuttgart.de>
2016-03-16 18:37:15 +0100

    datadriven(OCL): updated statistics collector script

--- tools/statisticsCollector.sh
+++ tools/statisticsCollector.sh
@@ -1,13 +1,15 @@
-mkdir -p statistics
-scp pfandedd@vgpu2:/scratch/pfandedd/git/SGppAutoTune/statistics/* statistics/
-scp pfandedd@neon:~/git/SGppAutoTuneNeon/statistics/* statistics/
-scp pfandedd@kepler:~/git/SGppAutoTuneKepler/statistics/* statistics/
-scp pfandedd@pcsgs07:~/git/SGppAutoTune/statistics/* statistics/
-scp winter@itojvkowjmf4u8jk.myfritz.net:~/git/SGppAutoTune/statistics/* statistics/
+#!/bin/bash
 
-mkdir -p logs
-scp pfandedd@vgpu2:/scratch/pfandedd/git/SGppAutoTune/vgpu2.log logs/
-scp pfandedd@neon:~/git/SGppAutoTuneNeon/neon.log logs/
-scp pfandedd@kepler:~/git/SGppAutoTuneKepler/kepler.log logs/
-scp pfandedd@pcsgs07:~/git/SGppAutoTune/tahiti.log logs/
-scp winter@itojvkowjmf4u8jk.myfritz.net:~/git/SGppAutoTune/hawaii.log logs/
+mkdir -p collectedStatistics
+scp pfandedd@vgpu2:/scratch/pfandedd/git/SGppAutoTune/statistics/* collectedStatistics/
+scp pfandedd@neon:~/git/SGppAutoTuneNeon/statistics/* collectedStatistics/
+scp pfandedd@kepler:~/git/SGppAutoTuneKepler/statistics/* collectedStatistics/
+scp pfandedd@pcsgs07:~/git/SGppAutoTune/statistics/* collectedStatistics/
+scp winter@itojvkowjmf4u8jk.myfritz.net:~/SGpp/statistics/* collectedStatistics/
+
+mkdir -p collectedLogs
+scp pfandedd@vgpu2:/scratch/pfandedd/git/SGppAutoTune/vgpu2.log collectedLogs/
+scp pfandedd@neon:~/git/SGppAutoTuneNeon/neon.log collectedLogs/
+scp pfandedd@kepler:~/git/SGppAutoTuneKepler/kepler.log collectedLogs/
+scp pfandedd@pcsgs07:~/git/SGppAutoTune/tahiti.log collectedLogs/
+scp winter@itojvkowjmf4u8jk.myfritz.net:~/SGpp/hawaii.log collectedLogs/
