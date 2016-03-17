cddd1168db6bff780b25187da1b38d30846eb8a2
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2016-03-17 09:56:47 +0100

    Added base class for kNN graph related MPI operations

--- datadriven/src/sgpp/datadriven/operation/hash/OperationMPI/OperationCreateGraphMPI.hpp
+++ datadriven/src/sgpp/datadriven/operation/hash/OperationMPI/OperationCreateGraphMPI.hpp
@@ -14,92 +14,28 @@
 #include <sgpp/datadriven/operation/hash/OperationCreateGraphOCL/OperationCreateGraphOCL.hpp>
 
 #include <vector>
+#include <string>
 
 namespace sgpp {
 namespace datadriven {
 namespace clusteringmpi {
-class OperationCreateGraphSlave : public MPISlaveOperation {
+
+/// Base class for kNN graph operations with MPI
+class OperationGraphMethodMPI : public MPIOperation {
  public:
-  bool verbose;
-  OperationCreateGraphSlave()
-      : MPISlaveOperation() {
-    verbose = true;
+  virtual ~OperationGraphMethodMPI(void) {
   }
-  virtual ~OperationCreateGraphSlave() {}
-  virtual void slave_code(void) {
-    MPI_Status stat;
-    int k;
-    int dimensions;
-    // Receive dataset
-    int dataset_size = 0;
-    MPI_Probe(0, 1, MPI_COMM_WORLD, &stat);
-    MPI_Get_count(&stat, MPI_DOUBLE, &dataset_size);
-    double *dataset = new double[dataset_size];
-    MPI_Recv(dataset, dataset_size, MPI_DOUBLE, stat.MPI_SOURCE, stat.MPI_TAG,
-             MPI_COMM_WORLD, &stat);
-    // Receive clustering parameters
-    MPI_Probe(0, 1, MPI_COMM_WORLD, &stat);
-    MPI_Recv(&dimensions, 1, MPI_INT, stat.MPI_SOURCE, stat.MPI_TAG,
-             MPI_COMM_WORLD, &stat);
-    MPI_Probe(0, 1, MPI_COMM_WORLD, &stat);
-    MPI_Recv(&k, 1, MPI_INT, stat.MPI_SOURCE, stat.MPI_TAG,
-             MPI_COMM_WORLD, &stat);
-    if (verbose) {
-      std::cout << "Node " << MPIEnviroment::get_node_rank() << ": Received dataset (size "
-                << dataset_size / dimensions << " datapoints) and graph parameters" << std::endl;
-    }
-    // Create opencl operation
-    sgpp::datadriven::DensityOCLMultiPlatform::OperationCreateGraphOCL* op =
-        sgpp::datadriven::createNearestNeighborGraphConfigured(dataset, dataset_size,
-                                                               k, dimensions, "MyOCLConf.cfg");
 
-    if (verbose) {
-      std::cout << "Node " << MPIEnviroment::get_node_rank()
-                << ": Created opencl graph operation"<< std::endl;
-    }
-    int datainfo[2];
-    do {
-      MPI_Probe(0, 1, MPI_COMM_WORLD, &stat);
-      MPI_Recv(datainfo, 2, MPI_INT, 0, stat.MPI_TAG, MPI_COMM_WORLD, &stat);
-      // Check for exit
-      if (datainfo[0] == -2 && datainfo[1] == -2) {
-        std::cerr << "Node" << MPIEnviroment::get_node_rank()
-                  << " received exit signal" << std::endl;
-        break;
-      } else {
-        if (verbose) {
-          std::cout << "Node " << MPIEnviroment::get_node_rank()
-                    << ": Received work package" << std::endl;
-        }
-        // Create partial graph
-        std::vector<int> partial_graph(datainfo[1] * k);
-        op->create_graph(partial_graph, datainfo[0], datainfo[1]);
-        // Send results back
-        MPI_Send(partial_graph.data(), datainfo[1] * k, MPI_INT, 0, 1, MPI_COMM_WORLD);
-      }
-    }while(true);
-    delete op;
-    delete [] dataset;
-  }
-};
-
-class OperationCreateGraphMPI : public MPIOperation {
- private:
+ protected:
   sgpp::base::DataMatrix &data;
   size_t datasize;
   size_t dimensions;
   size_t k;
 
- public:
-  OperationCreateGraphMPI(sgpp::base::DataMatrix& data, size_t dimensions, size_t k)
-      : MPIOperation(typeid(OperationCreateGraphSlave).name()), data(data),
+  OperationGraphMethodMPI(sgpp::base::DataMatrix& data, size_t dimensions, size_t k,
+                          std::string slave_class_id)
+      : MPIOperation(slave_class_id), data(data),
         datasize(data.getSize()), dimensions(dimensions), k(k) {
-  }
-  std::vector<int> create_graph(void) {
-    this->start_slave_code();
-    int *graph = new int[datasize / dimensions * k];
-    std::vector<int> returngraph(graph, graph + (datasize / dimensions *k));
-
     // Sending dataset to slaves
     for (int dest = 1; dest < MPIEnviroment::get_node_count(); dest++)
       MPI_Send(data.getPointer(), static_cast<int>(data.getSize()), MPI_DOUBLE,
@@ -108,6 +44,60 @@ class OperationCreateGraphMPI : public MPIOperation {
       MPI_Send(&dimensions, 1, MPI_INT, dest, 1, MPI_COMM_WORLD);
     for (int dest = 1; dest < MPIEnviroment::get_node_count(); dest++)
       MPI_Send(&k, 1, MPI_INT, dest, 1, MPI_COMM_WORLD);
+  }
+
+ protected:
+  /// Slave base class for kNN graph operations
+  class OperationGraphMethodSlave : public MPISlaveOperation {
+   protected:
+    bool verbose;
+    int k;
+    int dimensions;
+    double *dataset;
+    int dataset_size;
+
+   public:
+    OperationGraphMethodSlave(void) : verbose(true) {
+      MPI_Status stat;
+      // Receive dataset
+      MPI_Probe(0, 1, MPI_COMM_WORLD, &stat);
+      MPI_Get_count(&stat, MPI_DOUBLE, &dataset_size);
+      dataset = new double[dataset_size];
+      MPI_Recv(dataset, dataset_size, MPI_DOUBLE, stat.MPI_SOURCE, stat.MPI_TAG,
+               MPI_COMM_WORLD, &stat);
+      // Receive clustering parameters
+      MPI_Probe(0, 1, MPI_COMM_WORLD, &stat);
+      MPI_Recv(&dimensions, 1, MPI_INT, stat.MPI_SOURCE, stat.MPI_TAG,
+               MPI_COMM_WORLD, &stat);
+      MPI_Probe(0, 1, MPI_COMM_WORLD, &stat);
+      MPI_Recv(&k, 1, MPI_INT, stat.MPI_SOURCE, stat.MPI_TAG,
+               MPI_COMM_WORLD, &stat);
+      if (verbose) {
+        std::cout << "Node " << MPIEnviroment::get_node_rank() << ": Received dataset (size "
+                  << dataset_size / dimensions << " datapoints) and graph parameters" << std::endl;
+      }
+    }
+    virtual ~OperationGraphMethodSlave(void) {
+      delete [] dataset;
+    }
+    virtual void slave_code(void) = 0;
+  };
+};
+
+
+class OperationCreateGraphMPI : public OperationGraphMethodMPI {
+ public:
+  static MPISlaveOperation* create_slave(void) {
+    return new OperationCreateGraphSlave();
+  }
+  OperationCreateGraphMPI(sgpp::base::DataMatrix& data, size_t dimensions, size_t k)
+      : OperationGraphMethodMPI(data, dimensions, k,
+                                "OperationCreateGraphSlave") {
+  }
+  std::vector<int> create_graph(void) {
+    this->start_slave_code();
+    int *graph = new int[datasize / dimensions * k];
+    std::vector<int> returngraph(graph, graph + (datasize / dimensions *k));
 
     // Create packages and let the slaves solve them
     int *partial_result = new int[2000 * k];
@@ -129,6 +119,48 @@ class OperationCreateGraphMPI : public MPIOperation {
     return returngraph;
   }
   virtual ~OperationCreateGraphMPI() {}
+ protected:
+  class OperationCreateGraphSlave : public OperationGraphMethodSlave {
+   public:
+    OperationCreateGraphSlave()
+        : OperationGraphMethodSlave() {
+    }
+    virtual ~OperationCreateGraphSlave() {}
+    virtual void slave_code(void) {
+      MPI_Status stat;
+      // Create opencl operation
+      sgpp::datadriven::DensityOCLMultiPlatform::OperationCreateGraphOCL* op =
+          sgpp::datadriven::createNearestNeighborGraphConfigured(dataset, dataset_size,
+                                                                 k, dimensions, "MyOCLConf.cfg");
+      if (verbose) {
+        std::cout << "Node " << MPIEnviroment::get_node_rank()
+                  << ": Created opencl graph operation"<< std::endl;
+      }
+      int datainfo[2];
+      do {
+        MPI_Probe(0, 1, MPI_COMM_WORLD, &stat);
+        MPI_Recv(datainfo, 2, MPI_INT, 0, stat.MPI_TAG, MPI_COMM_WORLD, &stat);
+        // Check for exit
+        if (datainfo[0] == -2 && datainfo[1] == -2) {
+          std::cerr << "Node" << MPIEnviroment::get_node_rank()
+                    << " received exit signal" << std::endl;
+          break;
+        } else {
+          if (verbose) {
+            std::cout << "Node " << MPIEnviroment::get_node_rank()
+                      << ": Received work package" << std::endl;
+          }
+          // Create partial graph
+          std::vector<int> partial_graph(datainfo[1] * k);
+          op->create_graph(partial_graph, datainfo[0], datainfo[1]);
+          // Send results back
+          MPI_Send(partial_graph.data(), datainfo[1] * k, MPI_INT, 0, 1, MPI_COMM_WORLD);
+        }
+      }while(true);
+      delete op;
+      delete [] dataset;
+    }
+  };
 };
 
 }  // namespace clusteringmpi
