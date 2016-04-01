0ca11dc5d831acf40658f0d8036b61ee5f9b47f6
Michael Lettrich <m.lettrich@tum.de>
2016-04-01 12:44:19 +0200

    Implemented Gzip file handling prototype for data mining using decorator pattern.

---
+++ datadriven/src/sgpp/datadriven/datamining/dataSource/FileSampleDecorator.hpp
@@ -0,0 +1,31 @@
+/* Copyright (C) 2008-today The SG++ project
+ * This file is part of the SG++ project. For conditions of distribution and
+ * use, please see the copyright notice provided with SG++ or at
+ * sgpp.sparsegrids.org
+ *
+ * FileSampleDecorator.hpp
+ *
+ *  Created on: 01.04.2016
+ *      Author: Michael Lettrich
+ */
+
+#pragma once
+
+#include <sgpp/datadriven/datamining/dataSource/FileSampleProvider.hpp>
+#include <memory>
+
+namespace sgpp {
+namespace datadriven {
+
+class FileSampleDecorator : public FileSampleProvider {
+ public:
+  explicit FileSampleDecorator(std::unique_ptr<FileSampleProvider> fileSampleProvider)
+      : fileSampleProvider(std::move(fileSampleProvider)){};
+  virtual ~FileSampleDecorator(){};
+
+ protected:
+  std::unique_ptr<FileSampleProvider> fileSampleProvider;
+};
+
+} /* namespace datadriven */
+} /* namespace sgpp */
