46d4f53ca044d88ca12b88e81c006060ecb37ba0
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2016-04-04 17:33:22 +0200

    Replaced all ull suffixes with ul to avoid ambiguous function calls

--- datadriven/src/sgpp/datadriven/operation/hash/OperationMultipleEvalStreamingModOCLFastMultiPlattform/Configuration.hpp
+++ datadriven/src/sgpp/datadriven/operation/hash/OperationMultipleEvalStreamingModOCLFastMultiPlattform/Configuration.hpp
@@ -56,11 +56,11 @@ class Configuration {
         }
 
         if (kernelNode.contains("LOCAL_SIZE") == false) {
-          kernelNode.addIDAttr("LOCAL_SIZE", 128ull);
+          kernelNode.addIDAttr("LOCAL_SIZE", 128ul);
         }
 
         if (kernelNode.contains("KERNEL_SCHEDULE_SIZE") == false) {
-          kernelNode.addIDAttr("KERNEL_SCHEDULE_SIZE", 102400ull);
+          kernelNode.addIDAttr("KERNEL_SCHEDULE_SIZE", 102400ul);
         }
 
         if (kernelNode.contains("INTERNAL_PRECISION") == false) {
@@ -76,23 +76,23 @@ class Configuration {
         }
 
         if (kernelNode.contains("KERNEL_MAX_DIM_UNROLL") == false) {
-          kernelNode.addIDAttr("KERNEL_MAX_DIM_UNROLL", 1ull);
+          kernelNode.addIDAttr("KERNEL_MAX_DIM_UNROLL", 1ul);
         }
 
         if (kernelNode.contains("KERNEL_DATA_BLOCK_SIZE") == false) {
-          kernelNode.addIDAttr("KERNEL_DATA_BLOCK_SIZE", 1ull);
+          kernelNode.addIDAttr("KERNEL_DATA_BLOCK_SIZE", 1ul);
         }
 
         if (kernelNode.contains("KERNEL_TRANS_DATA_BLOCK_SIZE") == false) {
-          kernelNode.addIDAttr("KERNEL_TRANS_DATA_BLOCK_SIZE", 1ull);
+          kernelNode.addIDAttr("KERNEL_TRANS_DATA_BLOCK_SIZE", 1ul);
         }
 
         if (kernelNode.contains("KERNEL_TRANS_GRID_BLOCK_SIZE") == false) {
-          kernelNode.addIDAttr("KERNEL_TRANS_GRID_BLOCK_SIZE", 1ull);
+          kernelNode.addIDAttr("KERNEL_TRANS_GRID_BLOCK_SIZE", 1ul);
         }
 
         if (kernelNode.contains("KERNEL_PREFETCH_SIZE") == false) {
-          kernelNode.addIDAttr("KERNEL_PREFETCH_SIZE", 64ull);
+          kernelNode.addIDAttr("KERNEL_PREFETCH_SIZE", 64ul);
         }
       }
     }
