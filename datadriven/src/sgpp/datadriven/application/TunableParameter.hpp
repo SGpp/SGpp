e39e1bbab2fb664352112ba68e40ff5fdd6e7c0d
pfandedd <pfandedd@ipvs.uni-stuttgart.de>
2016-04-05 15:20:34 +0200

    datadriven(OCL): - new scenarios added for friedman 10d 				 - added datasets

--- datadriven/src/sgpp/datadriven/application/TunableParameter.hpp
+++ datadriven/src/sgpp/datadriven/application/TunableParameter.hpp
@@ -13,21 +13,14 @@
 namespace sgpp {
 namespace datadriven {
 
-// enum class ParameterType {
-//    ID, TEXT
-//};
-
 class TunableParameter {
  private:
   std::string name;
   std::vector<std::string> values;
-  //    ParameterType type;
+
  public:
-  //    TunableParameter(std::string name, std::vector<std::string> values, ParameterType type) :
   TunableParameter(std::string name, std::vector<std::string> values)
-      :  //            name(name), values(values), type(type) {
-        name(name),
-        values(values) {}
+      : name(name), values(values) {}
 
   std::string &getName() { return this->name; }
 
