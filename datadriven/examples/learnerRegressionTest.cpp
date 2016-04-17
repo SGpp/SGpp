93c652352e9ff0454328d51c1402001c6abc9796
Lukas Krenz <lukas@krenz.land>
2016-04-17 22:52:09 +0200

    Finish example implementation.
    Refactor example and RegressionLearner.
    Allow solver selection in RegressionLearner.

--- datadriven/examples/learnerRegressionTest.cpp
+++ datadriven/examples/learnerRegressionTest.cpp
@@ -13,20 +13,12 @@
 #include <string>
 #include <exception>
 
-int main(int argc, char** argv) {
-  const auto filename = std::string("../tests/data/friedman_4d_2000.arff");
-  auto dataset = sgpp::datadriven::ARFFTools::readARFF(filename);
-  std::cout << "Read file " << filename << " ." << std::endl;
-
-  auto data = dataset.getData();
-  auto classes = dataset.getTargets();
-
+sgpp::datadriven::RegressionLearner getLearner(sgpp::datadriven::Dataset& data) {
   auto gridConfig = sgpp::base::RegularGridConfiguration();
-  gridConfig.dim_ = dataset.getDimension();
+  gridConfig.dim_ = data.getDimension();
   gridConfig.level_ = 5;
   gridConfig.type_ = sgpp::base::GridType::Linear;
 
-  // TODO(ARGH): Validate!
   auto adaptivityConfig = sgpp::base::AdpativityConfiguration();
   adaptivityConfig.noPoints_ = 0;
   adaptivityConfig.numRefinements_ = 0;
@@ -36,21 +28,38 @@ int main(int argc, char** argv) {
   regularizationConfig.regType_ = regularizationType;
 
   auto solverConfig = sgpp::solver::SLESolverConfiguration();
-  solverConfig.maxIterations_ = 1000;
-  solverConfig.eps_ = 1e-5;
+  solverConfig.type_ = sgpp::solver::SLESolverType::CG;
+  solverConfig.maxIterations_ = 500;
+  solverConfig.eps_ = 1e-6;
 
   std::cout << "Initializing the learner." << std::endl;
-  auto learner = sgpp::datadriven::RegressionLearner(gridConfig, adaptivityConfig, solverConfig,
-                                                     regularizationConfig);
+  return sgpp::datadriven::RegressionLearner(gridConfig, adaptivityConfig, solverConfig,
+                                             regularizationConfig);
+}
+
+int main(int argc, char** argv) {
+  const auto filenameTrain = std::string("../tests/data/friedman3_10k_train.arff");
+  const auto filenameValidation = std::string("../tests/data/friedman3_10k_validation.arff");
+
+  auto dataTrain = sgpp::datadriven::ARFFTools::readARFF(filenameTrain);
+  std::cout << "Read file " << filenameTrain << "." << std::endl;
+  auto xTrain = dataTrain.getData();
+  auto yTrain = dataTrain.getTargets();
+
+  auto learner = getLearner(dataTrain);
 
   std::cout << "Training the learner." << std::endl;
-  try {
-    learner.train(data, classes);
-  } catch (const std::exception& e) {
-    std::cout << "std exception " << e.what() << std::endl;
-  } catch (...) {
-    std::cout << "unknown exception" << std::endl;
-  }
-
+  learner.train(xTrain, yTrain);
   std::cout << "Finished training." << std::endl;
+
+  const double MSETrain = learner.getMSE(xTrain, yTrain);
+  std::cout << "Training MSE = " << MSETrain << std::endl;
+
+  auto dataValidation = sgpp::datadriven::ARFFTools::readARFF(filenameValidation);
+  std::cout << "Read file " << filenameTrain << "." << std::endl;
+  auto xValidation = dataValidation.getData();
+  auto yValidation = dataValidation.getTargets();
+
+  const double MSEValidation = learner.getMSE(xValidation, yValidation);
+  std::cout << "Validation MSE = " << MSEValidation << std::endl;
 }
