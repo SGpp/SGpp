969877a793b3d0a6387f0771464b734498bb3257
Fabian Franzelin <fabian.franzelin@ipvs.uni-stuttgart.de>
2016-05-17 15:48:31 +0200

    minor changes

--- quadrature/tests/test_quadrature.cpp
+++ quadrature/tests/test_quadrature.cpp
@@ -16,8 +16,8 @@ namespace unit_test {
 namespace ut_detail {
 
 std::string normalize_test_case_name(const_string name) {
-    return ((name[0] == '&') ? std::string(name.begin() + 1, name.size() - 1) :
-                               std::string(name.begin(), name.size()));
+  return ((name[0] == '&') ? std::string(name.begin() + 1, name.size() - 1)
+                           : std::string(name.begin(), name.size()));
 }
 
 }  // namespace ut_detail
@@ -115,9 +115,11 @@ void testOperationQuadratureMCAdvanced(Grid& grid, DataVector& alpha,
 
     case sgpp::quadrature::SamplerTypes::Sobol:
       opQuad->useQuasiMonteCarloWithSobolSequences();
+      break;
 
     case sgpp::quadrature::SamplerTypes::ScrambledSobol:
       opQuad->useQuasiMonteCarloWithScrambledSobolSequences();
+      break;
 
     default:
       std::cout << "test_quadrature::testOperationQuadratureMCAdvanced : sampler type not available"
@@ -161,4 +163,8 @@ BOOST_AUTO_TEST_CASE(testOperationMCAdvanced) {
                                     dim, numSamples, blockSize, analyticResult, 1e-3, seed);
   testOperationQuadratureMCAdvanced(*grid, alpha, sgpp::quadrature::SamplerTypes::Halton, dim,
                                     numSamples, blockSize, analyticResult, 1e-3, seed);
+  testOperationQuadratureMCAdvanced(*grid, alpha, sgpp::quadrature::SamplerTypes::Sobol, dim,
+                                    numSamples, blockSize, analyticResult, 1e-3, seed);
+  testOperationQuadratureMCAdvanced(*grid, alpha, sgpp::quadrature::SamplerTypes::ScrambledSobol,
+                                    dim, numSamples, blockSize, analyticResult, 1e-3, seed);
 }
