SGpp/SGpp

commit c13988cf666ebbe5c823abde7501a00bd5ffd696

Consider BoundingBox in OperationNaiveEval*

base/src/sgpp/base/operation/hash/OperationNaiveEvalLinearClenshawCurtis.cpp
@@ -43,6 +43,9 @@ void OperationNaiveEvalLinearClenshawCurtis::eval(const DataMatrix& alpha,
   const size_t d = storage.getDimension();
   const size_t m = alpha.getNcols();
 
+  pointInUnitCube = point;
+  storage.getBoundingBox()->transformPointToUnitCube(pointInUnitCube);
+
   value.resize(m);
   value.setAll(0.0);
 
@@ -51,7 +54,7 @@ void OperationNaiveEvalLinearClenshawCurtis::eval(const DataMatrix& alpha,
     double curValue = 1.0;
 
     for (size_t t = 0; t < d; t++) {
-      const double val1d = base.eval(gp.getLevel(t), gp.getIndex(t), point[t]);
+      const double val1d = base.eval(gp.getLevel(t), gp.getIndex(t), pointInUnitCube[t]);
 
       if (val1d == 0.0) {
         curValue = 0.0;

base/src/sgpp/base/operation/hash/OperationNaiveEvalModBsplineClenshawCurtis.cpp
@@ -44,6 +44,9 @@ void OperationNaiveEvalModBsplineClenshawCurtis::eval(const DataMatrix& alpha,
   const size_t d = storage.getDimension();
   const size_t m = alpha.getNcols();
 
+  pointInUnitCube = point;
+  storage.getBoundingBox()->transformPointToUnitCube(pointInUnitCube);
+
   value.resize(m);
   value.setAll(0.0);
 
@@ -52,7 +55,7 @@ void OperationNaiveEvalModBsplineClenshawCurtis::eval(const DataMatrix& alpha,
     double curValue = 1.0;
 
     for (size_t t = 0; t < d; t++) {
-      const double val1d = base.eval(gp.getLevel(t), gp.getIndex(t), point[t]);
+      const double val1d = base.eval(gp.getLevel(t), gp.getIndex(t), pointInUnitCube[t]);
 
       if (val1d == 0.0) {
         curValue = 0.0;

base/src/sgpp/base/operation/hash/OperationNaiveEvalModPoly.cpp
@@ -40,6 +40,9 @@ void OperationNaiveEvalModPoly::eval(const DataMatrix& alpha, const DataVector& 
   const size_t d = storage.getDimension();
   const size_t m = alpha.getNcols();
 
+  pointInUnitCube = point;
+  storage.getBoundingBox()->transformPointToUnitCube(pointInUnitCube);
+
   value.resize(m);
   value.setAll(0.0);
 
@@ -48,7 +51,7 @@ void OperationNaiveEvalModPoly::eval(const DataMatrix& alpha, const DataVector& 
     double curValue = 1.0;
 
     for (size_t t = 0; t < d; t++) {
-      const double val1d = base.eval(gp.getLevel(t), gp.getIndex(t), point[t]);
+      const double val1d = base.eval(gp.getLevel(t), gp.getIndex(t), pointInUnitCube[t]);
 
       if (val1d == 0.0) {
         curValue = 0.0;

base/src/sgpp/base/operation/hash/OperationNaiveEvalPoly.cpp
@@ -43,6 +43,9 @@ void OperationNaiveEvalPoly::eval(const DataMatrix& alpha,
   const size_t d = storage.getDimension();
   const size_t m = alpha.getNcols();
 
+  pointInUnitCube = point;
+  storage.getBoundingBox()->transformPointToUnitCube(pointInUnitCube);
+
   value.resize(m);
   value.setAll(0.0);
 
@@ -51,7 +54,7 @@ void OperationNaiveEvalPoly::eval(const DataMatrix& alpha,
     double curValue = 1.0;
 
     for (size_t t = 0; t < d; t++) {
-      const double val1d = base.eval(gp.getLevel(t), gp.getIndex(t), point[t]);
+      const double val1d = base.eval(gp.getLevel(t), gp.getIndex(t), pointInUnitCube[t]);
 
       if (val1d == 0.0) {
         curValue = 0.0;

base/src/sgpp/base/operation/hash/OperationNaiveEvalPolyBoundary.cpp
@@ -43,6 +43,9 @@ void OperationNaiveEvalPolyBoundary::eval(const DataMatrix& alpha,
   const size_t d = storage.getDimension();
   const size_t m = alpha.getNcols();
 
+  pointInUnitCube = point;
+  storage.getBoundingBox()->transformPointToUnitCube(pointInUnitCube);
+
   value.resize(m);
   value.setAll(0.0);
 
@@ -51,7 +54,7 @@ void OperationNaiveEvalPolyBoundary::eval(const DataMatrix& alpha,
     double curValue = 1.0;
 
     for (size_t t = 0; t < d; t++) {
-      const double val1d = base.eval(gp.getLevel(t), gp.getIndex(t), point[t]);
+      const double val1d = base.eval(gp.getLevel(t), gp.getIndex(t), pointInUnitCube[t]);
 
       if (val1d == 0.0) {
         curValue = 0.0;

commit 01ed95424344c29297df5a49ebfc9e46ac79d725

Fix OperationNaiveEval bugs introduced in c13988c

base/src/sgpp/base/operation/hash/OperationNaiveEvalLinearClenshawCurtis.cpp
@@ -15,22 +15,25 @@ double OperationNaiveEvalLinearClenshawCurtis::eval(
   const size_t d = storage.getDimension();
   double result = 0.0;
 
+  pointInUnitCube = point;
+  storage.getBoundingBox()->transformPointToUnitCube(pointInUnitCube);
+
   for (size_t i = 0; i < n; i++) {
     const GridPoint& gp = storage[i];
-    double curVal = 1.0;
+    double curValue = 1.0;
 
     for (size_t t = 0; t < d; t++) {
-      const double val1d = base.eval(gp.getLevel(t), gp.getIndex(t), point[t]);
+      const double val1d = base.eval(gp.getLevel(t), gp.getIndex(t), pointInUnitCube[t]);
 
       if (val1d == 0.0) {
-        curVal = 0.0;
+        curValue = 0.0;
         break;
       }
 
-      curVal *= val1d;
+      curValue *= val1d;
     }
 
-    result += alpha[i] * curVal;
+    result += alpha[i] * curValue;
   }
 
   return result;

base/src/sgpp/base/operation/hash/OperationNaiveEvalModBsplineClenshawCurtis.cpp
@@ -15,13 +15,15 @@ double OperationNaiveEvalModBsplineClenshawCurtis::eval(
   const size_t d = storage.getDimension();
   double result = 0.0;
 
+  pointInUnitCube = point;
+  storage.getBoundingBox()->transformPointToUnitCube(pointInUnitCube);
+
   for (size_t i = 0; i < n; i++) {
     const GridPoint& gp = storage[i];
     double curValue = 1.0;
 
     for (size_t t = 0; t < d; t++) {
-      const double val1d = base.eval(gp.getLevel(t), gp.getIndex(t),
-                                      point[t]);
+      const double val1d = base.eval(gp.getLevel(t), gp.getIndex(t), pointInUnitCube[t]);
 
       if (val1d == 0.0) {
         curValue = 0.0;

base/src/sgpp/base/operation/hash/OperationNaiveEvalModPoly.cpp
@@ -10,15 +10,18 @@ namespace base {
 
 double OperationNaiveEvalModPoly::eval(const DataVector& alpha, const DataVector& point) {
   const size_t n = storage.getSize();
-  const size_t dim = storage.getDimension();
+  const size_t d = storage.getDimension();
   double result = 0.0;
+
+  pointInUnitCube = point;
+  storage.getBoundingBox()->transformPointToUnitCube(pointInUnitCube);
 
   for (size_t i = 0; i < n; i++) {
     const GridPoint& gp = storage[i];
     double curValue = 1.0;
 
-    for (size_t idim = 0; idim < dim; idim++) {
-      const double val1d = base.evalSave(gp.getLevel(idim), gp.getIndex(idim), point[idim]);
+    for (size_t t = 0; t < d; t++) {
+      const double val1d = base.evalSave(gp.getLevel(t), gp.getIndex(t), pointInUnitCube[t]);
 
       if (val1d == 0.0) {
         curValue = 0.0;
@@ -51,7 +54,7 @@ void OperationNaiveEvalModPoly::eval(const DataMatrix& alpha, const DataVector& 
     double curValue = 1.0;
 
     for (size_t t = 0; t < d; t++) {
-      const double val1d = base.eval(gp.getLevel(t), gp.getIndex(t), pointInUnitCube[t]);
+      const double val1d = base.evalSave(gp.getLevel(t), gp.getIndex(t), pointInUnitCube[t]);
 
       if (val1d == 0.0) {
         curValue = 0.0;

base/src/sgpp/base/operation/hash/OperationNaiveEvalPoly.cpp
@@ -11,16 +11,18 @@ namespace base {
 double OperationNaiveEvalPoly::eval(const DataVector& alpha,
                                      const DataVector& point) {
   const size_t n = storage.getSize();
-  const size_t dim = storage.getDimension();
+  const size_t d = storage.getDimension();
   double result = 0.0;
+
+  pointInUnitCube = point;
+  storage.getBoundingBox()->transformPointToUnitCube(pointInUnitCube);
 
   for (size_t i = 0; i < n; i++) {
     const GridPoint& gp = storage[i];
     double curValue = 1.0;
 
-    for (size_t idim = 0; idim < dim; idim++) {
-      const double val1d = base.evalSave(gp.getLevel(idim), gp.getIndex(idim),
-                                          point[idim]);
+    for (size_t t = 0; t < d; t++) {
+      const double val1d = base.evalSave(gp.getLevel(t), gp.getIndex(t), pointInUnitCube[t]);
 
       if (val1d == 0.0) {
         curValue = 0.0;
@@ -54,7 +56,7 @@ void OperationNaiveEvalPoly::eval(const DataMatrix& alpha,
     double curValue = 1.0;
 
     for (size_t t = 0; t < d; t++) {
-      const double val1d = base.eval(gp.getLevel(t), gp.getIndex(t), pointInUnitCube[t]);
+      const double val1d = base.evalSave(gp.getLevel(t), gp.getIndex(t), pointInUnitCube[t]);
 
       if (val1d == 0.0) {
         curValue = 0.0;

base/src/sgpp/base/operation/hash/OperationNaiveEvalPolyBoundary.cpp
@@ -11,16 +11,18 @@ namespace base {
 double OperationNaiveEvalPolyBoundary::eval(const DataVector& alpha,
     const DataVector& point) {
   const size_t n = storage.getSize();
-  const size_t dim = storage.getDimension();
+  const size_t d = storage.getDimension();
   double result = 0.0;
+
+  pointInUnitCube = point;
+  storage.getBoundingBox()->transformPointToUnitCube(pointInUnitCube);
 
   for (size_t i = 0; i < n; i++) {
     const GridPoint& gp = storage[i];
     double curValue = 1.0;
 
-    for (size_t idim = 0; idim < dim; idim++) {
-      const double val1d = base.evalSave(gp.getLevel(idim),
-                                          gp.getIndex(idim), point[idim]);
+    for (size_t t = 0; t < d; t++) {
+      const double val1d = base.evalSave(gp.getLevel(t), gp.getIndex(t), pointInUnitCube[t]);
 
       if (val1d == 0.0) {
         curValue = 0.0;
@@ -54,7 +56,7 @@ void OperationNaiveEvalPolyBoundary::eval(const DataMatrix& alpha,
     double curValue = 1.0;
 
     for (size_t t = 0; t < d; t++) {
-      const double val1d = base.eval(gp.getLevel(t), gp.getIndex(t), pointInUnitCube[t]);
+      const double val1d = base.evalSave(gp.getLevel(t), gp.getIndex(t), pointInUnitCube[t]);
 
       if (val1d == 0.0) {
         curValue = 0.0;