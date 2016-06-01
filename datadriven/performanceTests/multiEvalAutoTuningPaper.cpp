85f750681abb738937b57bf4de4a5d1b7e001c8d
pfandedd <pfandedd@pcsgs09.informatik.uni-stuttgart.de>
2016-06-01 18:29:21 +0200

    datadriven(OCL): - error bounds for new chess scenario adjusted

--- datadriven/performanceTests/multiEvalAutoTuningPaper.cpp
+++ datadriven/performanceTests/multiEvalAutoTuningPaper.cpp
@@ -30,6 +30,190 @@ std::string scenarioBaseDir = "datadriven/performanceTests/scenarios/";
 
 BOOST_AUTO_TEST_SUITE(AutoTuningPaper)
 
+BOOST_AUTO_TEST_CASE(Chess_4d_Linear_Float) {
+  // internal precision is specified by the scenario, the parameter configuration is overwritten
+  std::string scenarioFileName = "chess_4d_500000_Linear_float.scenario";
+  std::string parameterConfigurationFileName = "platformFloat.cfg";
+  std::string kernelName = "StreamingOCLMultiPlatform";
+  bool collectStatistics = true;
+
+  size_t dotPosition = scenarioFileName.find('.');
+  std::string scenarioFileNamePrefix = scenarioFileName.substr(0, dotPosition);
+  scenarioFileNamePrefix = scenarioFileNamePrefix.append("_" + kernelName);
+  std::string outputFileName = scenarioFileNamePrefix + "_tuned.cfg";
+
+  sgpp::datadriven::LearnerScenario scenario(scenarioBaseDir + scenarioFileName);
+  sgpp::base::OCLOperationConfiguration parameter(parameterConfigurationFileName);
+  sgpp::datadriven::StaticParameterTuner staticParameterTuner(parameter, true);
+
+  std::string statisticsFolderName = "statistics";
+
+  if (collectStatistics) {
+    staticParameterTuner.enableStatistics(statisticsFolderName, scenarioFileNamePrefix);
+    try {
+      if (boost::filesystem::create_directory(statisticsFolderName)) {
+        BOOST_TEST_MESSAGE("created output directory: " << statisticsFolderName);
+      }
+    } catch (boost::filesystem::filesystem_error &e) {
+      BOOST_FAIL("could not create statistics output folder: " << statisticsFolderName << ": "
+                                                               << e.what());
+    }
+  }
+
+  staticParameterTuner.addParameter("KERNEL_USE_LOCAL_MEMORY", {"true", "false"});
+  staticParameterTuner.addParameter("KERNEL_DATA_BLOCK_SIZE", {"1", "2", "4"});
+  staticParameterTuner.addParameter("KERNEL_TRANS_GRID_BLOCK_SIZE", {"1", "2", "4"});
+  staticParameterTuner.addParameter("KERNEL_STORE_DATA", {"array"});
+  staticParameterTuner.addParameter("KERNEL_MAX_DIM_UNROLL", {"10", "1"});
+  staticParameterTuner.addParameter("LOCAL_SIZE", {"128", "256"});
+  staticParameterTuner.addParameter("VERBOSE", {"true"});
+  staticParameterTuner.addParameter("OPTIMIZATION_FLAGS",
+                                    {"-cl-strict-aliasing -cl-fast-relaxed-math"});
+
+  sgpp::base::OCLOperationConfiguration bestParameters =
+      staticParameterTuner.tuneEverything(scenario, kernelName);
+
+  bestParameters.serialize(outputFileName);
+}
+
+BOOST_AUTO_TEST_CASE(Chess_4d_Linear_Double) {
+  // internal precision is specified by the scenario, the parameter configuration is overwritten
+  std::string scenarioFileName = "chess_4d_500000_Linear_double.scenario";
+  std::string parameterConfigurationFileName = "platformDouble.cfg";
+  std::string kernelName = "StreamingOCLMultiPlatform";
+  bool collectStatistics = true;
+
+  size_t dotPosition = scenarioFileName.find('.');
+  std::string scenarioFileNamePrefix = scenarioFileName.substr(0, dotPosition);
+  scenarioFileNamePrefix = scenarioFileNamePrefix.append("_" + kernelName);
+  std::string outputFileName = scenarioFileNamePrefix + "_tuned.cfg";
+
+  sgpp::datadriven::LearnerScenario scenario(scenarioBaseDir + scenarioFileName);
+  sgpp::base::OCLOperationConfiguration parameter(parameterConfigurationFileName);
+  sgpp::datadriven::StaticParameterTuner staticParameterTuner(parameter, true);
+
+  std::string statisticsFolderName = "statistics";
+
+  if (collectStatistics) {
+    staticParameterTuner.enableStatistics(statisticsFolderName, scenarioFileNamePrefix);
+    try {
+      if (boost::filesystem::create_directory(statisticsFolderName)) {
+        BOOST_TEST_MESSAGE("created output directory: " << statisticsFolderName);
+      }
+    } catch (boost::filesystem::filesystem_error &e) {
+      BOOST_FAIL("could not create statistics output folder: " << statisticsFolderName << ": "
+                                                               << e.what());
+    }
+  }
+
+  staticParameterTuner.addParameter("KERNEL_USE_LOCAL_MEMORY", {"true", "false"});
+  staticParameterTuner.addParameter("KERNEL_DATA_BLOCK_SIZE", {"1", "2", "4"});
+  staticParameterTuner.addParameter("KERNEL_TRANS_GRID_BLOCK_SIZE", {"1", "2", "4"});
+  staticParameterTuner.addParameter("KERNEL_STORE_DATA", {"array"});
+  staticParameterTuner.addParameter("KERNEL_MAX_DIM_UNROLL", {"10", "1"});
+  staticParameterTuner.addParameter("LOCAL_SIZE", {"128", "256"});
+  staticParameterTuner.addParameter("VERBOSE", {"true"});
+  staticParameterTuner.addParameter("OPTIMIZATION_FLAGS",
+                                    {"-cl-strict-aliasing -cl-fast-relaxed-math"});
+
+  sgpp::base::OCLOperationConfiguration bestParameters =
+      staticParameterTuner.tuneEverything(scenario, kernelName);
+
+  bestParameters.serialize(outputFileName);
+}
+
+BOOST_AUTO_TEST_CASE(Chess_4d_ModLinear_Float) {
+  // internal precision is specified by the scenario, the parameter configuration is overwritten
+  std::string scenarioFileName = "chess_4d_500000_ModLinear_float.scenario";
+  std::string parameterConfigurationFileName = "platformFloat.cfg";
+  std::string kernelName = "StreamingModOCLMaskMultiPlatform";
+  bool collectStatistics = true;
+
+  size_t dotPosition = scenarioFileName.find('.');
+  std::string scenarioFileNamePrefix = scenarioFileName.substr(0, dotPosition);
+  scenarioFileNamePrefix = scenarioFileNamePrefix.append("_" + kernelName);
+  std::string outputFileName = scenarioFileNamePrefix + "_tuned.cfg";
+
+  sgpp::datadriven::LearnerScenario scenario(scenarioBaseDir + scenarioFileName);
+  sgpp::base::OCLOperationConfiguration parameter(parameterConfigurationFileName);
+  sgpp::datadriven::StaticParameterTuner staticParameterTuner(parameter, true);
+
+  std::string statisticsFolderName = "statistics";
+
+  if (collectStatistics) {
+    staticParameterTuner.enableStatistics(statisticsFolderName, scenarioFileNamePrefix);
+    try {
+      if (boost::filesystem::create_directory(statisticsFolderName)) {
+        BOOST_TEST_MESSAGE("created output directory: " << statisticsFolderName);
+      }
+    } catch (boost::filesystem::filesystem_error &e) {
+      BOOST_FAIL("could not create statistics output folder: " << statisticsFolderName << ": "
+                                                               << e.what());
+    }
+  }
+
+  staticParameterTuner.addParameter("KERNEL_USE_LOCAL_MEMORY", {"false", "true"});
+  staticParameterTuner.addParameter("KERNEL_DATA_BLOCK_SIZE", {"1", "2", "4"});
+  staticParameterTuner.addParameter("KERNEL_TRANS_GRID_BLOCK_SIZE", {"1", "2", "4"});
+  staticParameterTuner.addParameter("KERNEL_STORE_DATA", {"array"});
+  staticParameterTuner.addParameter("KERNEL_MAX_DIM_UNROLL", {"10", "1"});
+  staticParameterTuner.addParameter("LOCAL_SIZE", {"128", "256"});
+  staticParameterTuner.addParameter("VERBOSE", {"true"});
+  staticParameterTuner.addParameter("OPTIMIZATION_FLAGS",
+                                    {"-cl-strict-aliasing -cl-fast-relaxed-math"});
+
+  sgpp::base::OCLOperationConfiguration bestParameters =
+      staticParameterTuner.tuneEverything(scenario, kernelName);
+
+  bestParameters.serialize(outputFileName);
+}
+
+BOOST_AUTO_TEST_CASE(Chess_4d_ModLinear_Double) {
+  // internal precision is specified by the scenario, the parameter configuration is overwritten
+  std::string scenarioFileName = "chess_4d_500000_ModLinear_double.scenario";
+  std::string parameterConfigurationFileName = "platformDouble.cfg";
+  std::string kernelName = "StreamingModOCLMaskMultiPlatform";
+  bool collectStatistics = true;
+
+  size_t dotPosition = scenarioFileName.find('.');
+  std::string scenarioFileNamePrefix = scenarioFileName.substr(0, dotPosition);
+  scenarioFileNamePrefix = scenarioFileNamePrefix.append("_" + kernelName);
+  std::string outputFileName = scenarioFileNamePrefix + "_tuned.cfg";
+
+  sgpp::datadriven::LearnerScenario scenario(scenarioBaseDir + scenarioFileName);
+  sgpp::base::OCLOperationConfiguration parameter(parameterConfigurationFileName);
+  sgpp::datadriven::StaticParameterTuner staticParameterTuner(parameter, true);
+
+  std::string statisticsFolderName = "statistics";
+
+  if (collectStatistics) {
+    staticParameterTuner.enableStatistics(statisticsFolderName, scenarioFileNamePrefix);
+    try {
+      if (boost::filesystem::create_directory(statisticsFolderName)) {
+        BOOST_TEST_MESSAGE("created output directory: " << statisticsFolderName);
+      }
+    } catch (boost::filesystem::filesystem_error &e) {
+      BOOST_FAIL("could not create statistics output folder: " << statisticsFolderName << ": "
+                                                               << e.what());
+    }
+  }
+
+  staticParameterTuner.addParameter("KERNEL_USE_LOCAL_MEMORY", {"false", "true"});
+  staticParameterTuner.addParameter("KERNEL_DATA_BLOCK_SIZE", {"1", "2", "4"});
+  staticParameterTuner.addParameter("KERNEL_TRANS_GRID_BLOCK_SIZE", {"1", "2", "4"});
+  staticParameterTuner.addParameter("KERNEL_STORE_DATA", {"array"});
+  staticParameterTuner.addParameter("KERNEL_MAX_DIM_UNROLL", {"10", "1"});
+  staticParameterTuner.addParameter("LOCAL_SIZE", {"128", "256"});
+  staticParameterTuner.addParameter("VERBOSE", {"true"});
+  staticParameterTuner.addParameter("OPTIMIZATION_FLAGS",
+                                    {"-cl-strict-aliasing -cl-fast-relaxed-math"});
+
+  sgpp::base::OCLOperationConfiguration bestParameters =
+      staticParameterTuner.tuneEverything(scenario, kernelName);
+
+  bestParameters.serialize(outputFileName);
+}
+
 BOOST_AUTO_TEST_CASE(Friedman2_4d_Linear_Float) {
   // internal precision is specified by the scenario, the parameter configuration is overwritten
   std::string scenarioFileName = "friedman2_4d_500000_Linear_float.scenario";
