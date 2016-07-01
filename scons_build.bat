53e36941744d753fb5e4f471da093bbfedc826bc
Peter Schober <schober@finance.uni-frankfurt.de>
2016-07-01 10:13:47 +0200

    added build scripts for win and unix

---
+++ scons_build.bat
@@ -0,0 +1,13 @@
+scons -j 4 ^
+  SG_ALL=0 ^
+  SG_BASE=1 ^
+  SG_OPTIMIZATION=1 ^
+  USE_EIGEN=1 ^
+  COMPILER=gnu ^
+  BOOST_INCLUDE_PATH=C:\boost_1_59_0 ^
+  BOOST_LIBRARY_PATH=C:\boost_1_59_0\stage\lib ^
+  PYDOC=0 ^
+  ARCH=sse3 ^
+  BUILD_STATICLIB=yes ^
+  CPPPATH=E:\MATLAB\SharedSourceCode\Development\Mex\
+pause
