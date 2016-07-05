6d10faed857f36deeff1af0e477ed8569f404977
Dirk Pflüger <dirk.pflueger@ipvs.uni-stuttgart.de>
2016-07-05 16:05:32 +0200

    Tidy up branch

--- parallel/examples/NativeCppHeatEquationSolverWithStretching.sh
+++
@@ -1,3 +0,0 @@
-./NativeCppHeatEquationSolverWithStretching HeatEquation 3 5 0.0 3.0 1.0 smooth 1.0 0.01 ImEul 0.00001 400 discrete stretch3D_discrete.dat 
-./NativeCppHeatEquationSolverWithStretching HeatEquation 3 5 1.0 3.0 1.0 smooth 1.0 0.01 ImEul 0.00001 400 analytic stretch3D_analytic.dat
-
--- parallel/examplesMPI/NativeCppHeatEquationSolverMPI.sh
+++
@@ -1,3 +0,0 @@
-./NativeCppHeatEquationSolverMPI HeatEquation 3 2 5 0.0 3.0 1.0 smooth 1.0 0.1 ImEul 0.00001 400
-./NativeCppHeatEquationSolverMPI PoissonEquation 3 2 5 0.0 3.0 smooth 0.00001 400
-./NativeCppHeatEquationSolverMPI PoissonEquationAdapt 3 2 maxLevel 5 5 1e-7 0.0 3.0 smooth 0.00001 400
