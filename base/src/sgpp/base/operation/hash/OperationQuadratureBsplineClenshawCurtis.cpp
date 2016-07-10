a46a2d783fdc6fa9797179dbf85e327cd802fed7
schroeji <schroeji@studi.informatik.uni-stuttgart.de>
2016-07-10 10:59:00 +0200

    Commit before merge

---
+++ base/src/sgpp/base/operation/hash/OperationQuadratureBsplineClenshawCurtis.cpp
@@ -0,0 +1,33 @@
+// Copyright (C) 2008-today The SG++ project
+// This file is part of the SG++ project. For conditions of distribution and
+// use, please see the copyright notice provided with SG++ or at
+// sgpp.sparsegrids.org
+
+#include <sgpp/base/operation/hash/OperationQuadratureBsplineClenshawCurtis.hpp>
+
+#include <sgpp/globaldef.hpp>
+#include <iostream>
+
+namespace sgpp {
+namespace base {
+
+double OperationQuadratureBsplineClenshawCurtis::doQuadrature(DataVector& alpha) {
+  double res = 0;
+  double tmpres = 0;
+  GridIndex* gp;
+
+  for (size_t i = 0; i < alpha.getSize(); i++) {
+    gp = storage.get(i);
+    tmpres = 1.;
+
+    for (size_t d = 0; d < storage.getDimension(); d++) {
+      tmpres *= base.getIntegral(gp->getLevel(d), gp->getIndex(d));
+    }
+    res += alpha[i] * tmpres;
+  }
+
+  return res;
+}
+
+}  // namespace base
+}  // namespace sgpp
