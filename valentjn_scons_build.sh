2203075da897c903730b7228dd32f8bbf4d066a4
Julian Valentin <julian.valentin@ipvs.uni-stuttgart.de>
2016-07-25 13:03:02 +0200

    Compile with MATLAB solver

--- valentjn_scons_build.sh
+++ valentjn_scons_build.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-scons -j $(nproc) VERBOSE=1 OPT=1 SG_ALL=0 SG_BASE=1 SG_OPTIMIZATION=1 USE_EIGEN=1 BUILD_STATICLIB=1
+scons -j $(nproc) VERBOSE=1 OPT=1 SG_ALL=0 SG_BASE=1 SG_OPTIMIZATION=1 \
+    USE_EIGEN=1 USE_MATLAB=1 BUILD_STATICLIB=1 \
+    CPPPATH=/home/valentjn/local/share/matlab/R2016a/extern/include \
+    LIBPATH=/home/valentjn/local/share/matlab/R2016a/bin/glnxa64 \
+    RPATH=/home/valentjn/local/share/matlab/R2016a/bin/glnxa64
