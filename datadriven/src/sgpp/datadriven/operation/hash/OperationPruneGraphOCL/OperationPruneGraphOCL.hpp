88478c1cfc58f52b14de365d723c3238db0039ba
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2016-08-02 21:33:25 +0200

    Moved loading of pruning parameters to prune operation

--- datadriven/src/sgpp/datadriven/operation/hash/OperationPruneGraphOCL/OperationPruneGraphOCL.hpp
+++ datadriven/src/sgpp/datadriven/operation/hash/OperationPruneGraphOCL/OperationPruneGraphOCL.hpp
@@ -11,8 +11,8 @@
 #include <sgpp/base/exception/operation_exception.hpp>
 #include <sgpp/base/opencl/OCLOperationConfiguration.hpp>
 #include <sgpp/base/opencl/OCLManager.hpp>
-
 #include <vector>
+#include "KernelPruneGraph.hpp"
 
 namespace sgpp {
 namespace datadriven {
@@ -25,6 +25,23 @@ class OperationPruneGraphOCL {
 
   virtual void prune_graph(std::vector<int> &graph, size_t startid = 0, size_t chunksize = 0) = 0;
   virtual ~OperationPruneGraphOCL(void) {}
+  static void load_default_parameters(base::OCLOperationConfiguration *parameters) {
+    if (parameters->contains("INTERNAL_PRECISION") == false) {
+      std::cout << "Warning! No internal precision setting detected."
+                << " Using double precision from now on!" << std::endl;
+      parameters->addIDAttr("INTERNAL_PRECISION", "double");
+    }
+    if ((*parameters)["INTERNAL_PRECISION"].get().compare("float") == 0) {
+      DensityOCLMultiPlatform::KernelPruneGraph<float>::augmentDefaultParameters(*parameters);
+    } else if ((*parameters)["INTERNAL_PRECISION"].get().compare("double") == 0) {
+      DensityOCLMultiPlatform::KernelPruneGraph<double>::augmentDefaultParameters(*parameters);
+    } else {
+      std::stringstream errorString;
+      errorString << "Error creating operation\"CreateGraphOCL\": "
+                  << " invalid value for parameter \"INTERNAL_PRECISION\"";
+      throw base::operation_exception(errorString.str().c_str());
+    }
+  }
 };
 
 }  // namespace DensityOCLMultiPlatform
