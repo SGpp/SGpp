92fb52c52afafe4c240bf548fce469c9cab5db0c
Fabian Franzelin <fabian.franzelin@ipvs.uni-stuttgart.de>
2016-08-02 15:37:17 +0200

    some warnings removed

--- combigrid/src/sgpp/combigrid/algebraic/ArrayVector.hpp
+++ combigrid/src/sgpp/combigrid/algebraic/ArrayVector.hpp
@@ -1,9 +1,7 @@
-/*
- * ArrayVector.hpp
- *
- *  Created on: 11.12.2015
- *      Author: david
- */
+// Copyright (C) 2008-today The SG++ project
+// This file is part of the SG++ project. For conditions of distribution and
+// use, please see the copyright notice provided with SG++ or at
+// sgpp.sparsegrids.org
 
 #ifndef COMBIGRID_SRC_SGPP_COMBIGRID_ALGEBRAIC_ARRAYVECTOR_HPP_
 #define COMBIGRID_SRC_SGPP_COMBIGRID_ALGEBRAIC_ARRAYVECTOR_HPP_
@@ -14,138 +12,116 @@
 #include "ScalarVector.hpp"
 #include <cmath>
 
-namespace sgpp{
+namespace sgpp {
 namespace combigrid {
 
-template<typename Scalar, typename V> class ArrayVector {
-	std::vector<V> values;
+template <typename Scalar, typename V>
+class ArrayVector {
+  std::vector<V> values;
 
-	void ensureMinimumSize(size_t minSize) {
-		if (values.size() == 0) {
-			values.push_back(V::zero());
-		}
-		while (values.size() < minSize) {
-			values.push_back(values.back());
-		}
-	}
+  void ensureMinimumSize(size_t minSize) {
+    if (values.size() == 0) {
+      values.push_back(V::zero());
+    }
+    while (values.size() < minSize) {
+      values.push_back(values.back());
+    }
+  }
 
-public:
-	ArrayVector(std::vector<V> const &values) :
-			values(values) {
+ public:
+  ArrayVector(std::vector<V> const &values) : values(values) {}
 
-	}
+  ArrayVector(V value) : values(1, value) {}
 
-	ArrayVector(V value) :
-			values(1, value) {
+  ArrayVector() : values() {}
 
-	}
+  ArrayVector(ArrayVector<Scalar, V> const &other) : values(other.values) {}
 
-	ArrayVector() : values() {
+  ArrayVector<Scalar, V> &operator=(ArrayVector<Scalar, V> const &other) {
+    values = other.values;
+    return *this;
+  }
 
-	}
+  std::vector<V> const &getValues() const { return values; }
 
-	ArrayVector(ArrayVector<Scalar, V> const &other) :
-			values(other.values) {
+  V get(size_t i) const { return (*this)[i]; }
 
-	}
+  V &at(size_t i) {
+    ensureMinimumSize(i + 1);
+    return values[i];
+  }
 
-	ArrayVector<Scalar, V> &operator=(ArrayVector<Scalar, V> const &other) {
-		values = other.values;
-		return *this;
-	}
+  V &operator[](size_t i) {
+    ensureMinimumSize(i + 1);
+    return values[i];
+  }
 
-	std::vector<V> const &getValues() const {
-		return values;
-	}
+  /**
+   * This operator is unsafe because it does no range checking
+   */
+  V const &operator[](size_t i) const { return values[i]; }
 
-	V get(size_t i) const {
-		return (*this)[i];
-	}
+  size_t size() const { return values.size(); }
 
-	V &at(size_t i) {
-		ensureMinimumSize(i + 1);
-		return values[i];
-	}
+  void add(ArrayVector<Scalar, V> const &other) {
+    ensureMinimumSize(other.size());
 
-	V &operator[](size_t i) {
-		ensureMinimumSize(i + 1);
-		return values[i];
-	}
+    size_t otherSize = other.size();
+    for (size_t i = 0; i < otherSize; ++i) {
+      values[i].add(other.values[i]);
+    }
 
-	/**
-	 * This operator is unsafe because it does no range checking
-	 */
-	V const &operator[](size_t i) const {
-		return values[i];
-	}
+    for (size_t i = otherSize; i < size(); ++i) {
+      values[i].add(other.values.back());
+    }
+  }
 
-	size_t size() const {
-		return values.size();
-	}
+  void sub(ArrayVector<Scalar, V> const &other) {
+    ensureMinimumSize(other.size());
 
-	void add(ArrayVector<Scalar, V> const &other) {
-		ensureMinimumSize(other.size());
+    size_t otherSize = other.size();
+    for (size_t i = 0; i < otherSize; ++i) {
+      values[i].sub(other.values[i]);
+    }
 
-		size_t otherSize = other.size();
-		for (size_t i = 0; i < otherSize; ++i) {
-			values[i].add(other.values[i]);
-		}
+    for (size_t i = otherSize; i < size(); ++i) {
+      values[i].sub(other.values.back());
+    }
+  }
 
-		for (size_t i = otherSize; i < size(); ++i) {
-			values[i].add(other.values.back());
-		}
-	}
+  void componentwiseMult(ArrayVector<Scalar, V> const &other) {
+    ensureMinimumSize(other.size());
 
-	void sub(ArrayVector<Scalar, V> const &other) {
-		ensureMinimumSize(other.size());
+    size_t otherSize = other.size();
+    for (size_t i = 0; i < otherSize; ++i) {
+      values[i].componentwiseMult(other.values[i]);
+    }
 
-		size_t otherSize = other.size();
-		for (size_t i = 0; i < otherSize; ++i) {
-			values[i].sub(other.values[i]);
-		}
+    for (size_t i = otherSize; i < size(); ++i) {
+      values[i].componentwiseMult(other.values.back());
+    }
+  }
 
-		for (size_t i = otherSize; i < size(); ++i) {
-			values[i].sub(other.values.back());
-		}
-	}
+  void scalarMult(Scalar const &factor) {
+    for (size_t i = 0; i < values.size(); ++i) {
+      values[i].scalarMult(factor);
+    }
+  }
 
-	void componentwiseMult(ArrayVector<Scalar, V> const &other) {
-		ensureMinimumSize(other.size());
+  Scalar norm() const {
+    Scalar result = Scalar();
 
-		size_t otherSize = other.size();
-		for (size_t i = 0; i < otherSize; ++i) {
-			values[i].componentwiseMult(other.values[i]);
-		}
+    for (auto &val : values) {
+      Scalar n = val.norm();
+      result += n * n;
+    }
 
-		for (size_t i = otherSize; i < size(); ++i) {
-			values[i].componentwiseMult(other.values.back());
-		}
-	}
+    return sqrt(result);
+  }
 
-	void scalarMult(Scalar const &factor) {
-		for (size_t i = 0; i < values.size(); ++i) {
-			values[i].scalarMult(factor);
-		}
-	}
+  static ArrayVector<Scalar, V> zero() { return ArrayVector<Scalar, V>(V::zero()); }
 
-	Scalar norm() const {
-		Scalar result = Scalar();
-
-		for (auto &val : values) {
-			Scalar n = val.norm();
-			result += n * n;
-		}
-
-		return sqrt(result);
-	}
-
-	static ArrayVector<Scalar, V> zero() {
-		return ArrayVector<Scalar, V>(V::zero());
-	}
-
-	static ArrayVector<Scalar, V> one() {
-		return ArrayVector<Scalar, V>(V::one());
-	}
+  static ArrayVector<Scalar, V> one() { return ArrayVector<Scalar, V>(V::one()); }
 };
 
 typedef ArrayVector<double, FloatScalarVector> FloatArrayVector;
--- combigrid/src/sgpp/combigrid/operation/onedim/QuadratureEvaluator.cpp
+++ combigrid/src/sgpp/combigrid/operation/onedim/QuadratureEvaluator.cpp
@@ -1,251 +1,241 @@
-/*
- * QuadratureEvaluator.cpp
- *
- *  Created on: Feb 23, 2016
- *      Author: liedtkjn
- */
+// Copyright (C) 2008-today The SG++ project
+// This file is part of the SG++ project. For conditions of distribution and
+// use, please see the copyright notice provided with SG++ or at
+// sgpp.sparsegrids.org
 
-#include "QuadratureEvaluator.hpp"
-#include "BarycentricInterpolationEvaluator.hpp"
+#include <sgpp/combigrid/operation/onedim/QuadratureEvaluator.hpp>
+#include <sgpp/combigrid/operation/onedim/BarycentricInterpolationEvaluator.hpp>
 
 #include <iomanip>
+#include <vector>
 
-namespace sgpp{
+namespace sgpp {
 namespace combigrid {
 
 /**
  * Struct for a LagrangePolynom, used to eval it
  */
 struct LagrangePolynom {
+  std::vector<double> points;
+  size_t point;
 
-	std::vector<double> points;
-	size_t point;
-
-	double evaluate(double x) {
-		double result = 1.0;
-		for (size_t i = 0; i < points.size(); ++i) {
-			if (i != point) {
-				// TODO: precalculate denominator?
-				result *= (x - points[i]) / (points[point] - points[i]);
-			}
-		}
-		return result;
-	}
+  double evaluate(double x) {
+    double result = 1.0;
+    for (size_t i = 0; i < points.size(); ++i) {
+      if (i != point) {
+        // TODO(holzmudd): precalculate denominator?
+        result *= (x - points[i]) / (points[point] - points[i]);
+      }
+    }
+    return result;
+  }
 };
 
 /*
  *	Implementation of Gauss-Legendre quadrature
  */
 class GaussLegendreQuadrature {
-public:
+ public:
+  explicit GaussLegendreQuadrature(size_t eDEGREE)
+      : s_LegendrePolynomial(eDEGREE), eDEGREE(eDEGREE) {}
 
-	GaussLegendreQuadrature(size_t eDEGREE) :
-			s_LegendrePolynomial(eDEGREE), eDEGREE(eDEGREE) {
+  /*! Compute the integral of a functor
+   *
+   *   @param a    lower limit of integration
+   *   @param b    upper limit of integration
+   *   @param f    the polynomial to integrate
+   */
+  double integrate(double a, double b, LagrangePolynom f) {
+    double p = (b - a) / 2;
+    double q = (b + a) / 2;
+    const LegendrePolynomial& legpoly = s_LegendrePolynomial;
 
-	}
+    double sum = 0;
+    for (size_t i = 1; i <= eDEGREE; ++i) {  // TODO(holzmudd): warum hier ab 1?
+      sum += legpoly.weight(i) * f.evaluate(p * legpoly.root(i) + q);
+    }
 
-	/*! Compute the integral of a functor
-	 *
-	 *   @param a    lower limit of integration
-	 *   @param b    upper limit of integration
-	 *   @param f    the polynomial to integrate
-	 */
-	double integrate(double a, double b, LagrangePolynom f) {
-		double p = (b - a) / 2;
-		double q = (b + a) / 2;
-		const LegendrePolynomial& legpoly = s_LegendrePolynomial;
+    return p * sum;
+  }
 
-		double sum = 0;
-		for (size_t i = 1; i <= eDEGREE; ++i) { // TODO: warum hier ab 1?
-			sum += legpoly.weight(i) * f.evaluate(p * legpoly.root(i) + q);
-		}
+ private:
+  /*
+   * Implementation of the Legendre polynomials that form
+   * the basis of this quadrature
+   */
+  class LegendrePolynomial {
+    size_t eDEGREE;
 
-		return p * sum;
-	}
+   public:
+    explicit LegendrePolynomial(size_t eDEGREE) : eDEGREE(eDEGREE) {
+      _r = std::vector<double>(
+          eDEGREE + 1,
+          0.0);  // TODO(holzmudd): die beiden waren (eDEGREE, 0.0), wie rum ist es richtig?
+      _w = std::vector<double>(eDEGREE + 1, 0.0);
+      // Solve roots and weights
+      for (size_t i = 0; i <= eDEGREE;
+           ++i) {  // TODO(holzmudd): war i <= eDEGREE, wie rum ist es richtig?
+        double dr = 1;
 
-private:
-	/*
-	 * Implementation of the Legendre polynomials that form
-	 * the basis of this quadrature
-	 */
-	class LegendrePolynomial {
-		size_t eDEGREE;
+        // Find zero
+        Evaluation eval(
+            cos(M_PI * (static_cast<double>(i) - 0.25) / (static_cast<double>(eDEGREE) + 0.5)),
+            eDEGREE);
+        do {
+          dr = eval.v() / eval.d();
+          eval.evaluate(eval.x() - dr);
+        } while (fabs(dr) > 2e-16);
 
-	public:
-		LegendrePolynomial(size_t eDEGREE) :
-				eDEGREE(eDEGREE) {
-			_r = std::vector<double>(eDEGREE + 1, 0.0); // TODO: die beiden waren (eDEGREE, 0.0), wie rum ist es richtig?
-			_w = std::vector<double>(eDEGREE + 1, 0.0);
-			// Solve roots and weights
-			for (size_t i = 0; i <= eDEGREE; ++i) { // TODO: war i <= eDEGREE, wie rum ist es richtig?
-				double dr = 1;
+        this->_r[i] = eval.x();
+        this->_w[i] = 2 / ((1 - eval.x() * eval.x()) * eval.d() * eval.d());
+      }
+    }
 
-				// Find zero
-				Evaluation eval(cos(
-				M_PI * (static_cast<double>(i) - 0.25) / (static_cast<double>(eDEGREE) + 0.5)), eDEGREE);
-				do {
-					dr = eval.v() / eval.d();
-					eval.evaluate(eval.x() - dr);
-				} while (fabs(dr) > 2e-16);
+    double root(size_t i) const { return this->_r[i]; }
+    double weight(size_t i) const { return this->_w[i]; }
 
-				this->_r[i] = eval.x();
-				this->_w[i] = 2 / ((1 - eval.x() * eval.x()) * eval.d() * eval.d());
-			}
-		}
+   private:
+    std::vector<double> _r;
+    std::vector<double> _w;
 
-		double root(size_t i) const {
-			return this->_r[i];
-		}
-		double weight(size_t i) const {
-			return this->_w[i];
-		}
-	private:
-		std::vector<double> _r;
-		std::vector<double> _w;
+    /*
+     * Evaluate the value *and* derivative of the
+     * Legendre polynomial
+     */
+    class Evaluation {
+      size_t eDEGREE;
 
-		/*
-		 * Evaluate the value *and* derivative of the
-		 * Legendre polynomial
-		 */
-		class Evaluation {
-			size_t eDEGREE;
-		public:
-			explicit Evaluation(double x, size_t eDEGREE) :
-					eDEGREE(eDEGREE), _x(x), _v(1), _d(0) {
-				this->evaluate(x);
-			}
+     public:
+      explicit Evaluation(double x, size_t eDEGREE) : eDEGREE(eDEGREE), _x(x), _v(1), _d(0) {
+        this->evaluate(x);
+      }
 
-			void evaluate(double x) {
-				this->_x = x;
+      void evaluate(double x) {
+        this->_x = x;
 
-				double vsub1 = x;
-				double vsub2 = 1;
-				double f = 1 / (x * x - 1);
+        double vsub1 = x;
+        double vsub2 = 1;
+        double f = 1 / (x * x - 1);
 
-				for (size_t i = 2; i <= eDEGREE; ++i) { // TODO: wirklich hier ab 2?
-					this->_v = ((2 * static_cast<double>(i) - 1) * x * vsub1 - (static_cast<double>(i) - 1) * vsub2)
-							/ static_cast<double>(i);
-					this->_d = static_cast<double>(i) * f * (x * this->_v - vsub1);
+        for (size_t i = 2; i <= eDEGREE; ++i) {  // TODO(holzmudd): wirklich hier ab 2?
+          this->_v = ((2 * static_cast<double>(i) - 1) * x * vsub1 -
+                      (static_cast<double>(i) - 1) * vsub2) /
+                     static_cast<double>(i);
+          this->_d = static_cast<double>(i) * f * (x * this->_v - vsub1);
 
-					vsub2 = vsub1;
-					vsub1 = this->_v;
-				}
-			}
+          vsub2 = vsub1;
+          vsub1 = this->_v;
+        }
+      }
 
-			double v() const {
-				return this->_v;
-			}
-			double d() const {
-				return this->_d;
-			}
-			double x() const {
-				return this->_x;
-			}
+      double v() const { return this->_v; }
+      double d() const { return this->_d; }
+      double x() const { return this->_x; }
 
-		private:
-			double _x;double _v;double _d;
-		};
-	};
+     private:
+      double _x;
+      double _v;
+      double _d;
+    };
+  };
 
-	/*
-	 * Pre-compute the weights and abscissae of the Legendre polynomials
-	 */
-	LegendrePolynomial s_LegendrePolynomial;
-	size_t eDEGREE;
+  /*
+   * Pre-compute the weights and abscissae of the Legendre polynomials
+   */
+  LegendrePolynomial s_LegendrePolynomial;
+  size_t eDEGREE;
 };
 
 double gauss(LagrangePolynom polynom) {
-	size_t size = polynom.points.size() + 1;
-	GaussLegendreQuadrature quad(size); // TODO: kann man optimieren, indem man das nicht immer neu erstellt
-	return quad.integrate(0.0, 1.0, polynom);
+  size_t size = polynom.points.size() + 1;
+  GaussLegendreQuadrature quad(
+      size);  // TODO(holzmudd): kann man optimieren, indem man das nicht immer neu erstellt
+  return quad.integrate(0.0, 1.0, polynom);
 }
 
 /**
  * Integrates the polynom
  */
 double integrate(LagrangePolynom polynom) {
-	//return romberg2(polynom);
-	return gauss(polynom);
+  // return romberg2(polynom);
+  return gauss(polynom);
 }
 
 /**
  * Calculates the weight for the specific point
  */
 double getWeight(std::vector<double>& points, size_t point) {
-	LagrangePolynom p;
-	p.points = points;
-	p.point = point;
+  LagrangePolynom p;
+  p.points = points;
+  p.point = point;
 
-	return integrate(p);
+  return integrate(p);
 }
 
 /**
  * This Function calculates the weights of the given points, each weight is calculated individually
  * @param points The vector with the points, they dont need to have a specific order
- * @param weights The weights will be added to the back of this vector in the order of the points in the vector with the points,
- * it is recommended to clear the weight vector before calling this function to ensure that the weights are at the same position
+ * @param weights The weights will be added to the back of this vector in the order of the points in
+ * the vector with the points,
+ * it is recommended to clear the weight vector before calling this function to ensure that the
+ * weights are at the same position
  * as their points
  */
 void calculateWeights(std::vector<double>& points, std::vector<FloatScalarVector>& weights) {
-	// calc weight for each point
-	for (size_t i = 0; i < points.size(); ++i) {
-		weights.push_back(getWeight(points, i));
-	}
+  // calc weight for each point
+  for (size_t i = 0; i < points.size(); ++i) {
+    weights.push_back(getWeight(points, i));
+  }
 }
 
-QuadratureEvaluator::~QuadratureEvaluator() {
-}
+QuadratureEvaluator::~QuadratureEvaluator() {}
 
-bool QuadratureEvaluator::needsOrderedPoints() {
-	return false;
-}
+bool QuadratureEvaluator::needsOrderedPoints() { return false; }
 
-bool QuadratureEvaluator::needsParameter() {
-	return false;
-}
+bool QuadratureEvaluator::needsParameter() { return false; }
 
-void QuadratureEvaluator::setGridPoints(std::vector<double> const &newXValues) {
-	xValues = newXValues;
-	weights.clear();
-	calculateWeights(xValues, weights);
+void QuadratureEvaluator::setGridPoints(std::vector<double> const& newXValues) {
+  xValues = newXValues;
+  weights.clear();
+  calculateWeights(xValues, weights);
 
-	if (normalizeWeights) {
-		double sum = 0.0;
+  if (normalizeWeights) {
+    double sum = 0.0;
 
-		// multiply the weights with the weight function
-		for (size_t i = 0; i < weights.size(); ++i) {
-			weights[i].scalarMult(weight_function(xValues[i]));
-			sum += weights[i].getValue();
-		}
+    // multiply the weights with the weight function
+    for (size_t i = 0; i < weights.size(); ++i) {
+      weights[i].scalarMult(weight_function(xValues[i]));
+      sum += weights[i].getValue();
+    }
 
-		double sumInv = 1.0 / sum;
+    double sumInv = 1.0 / sum;
 
-		for (size_t i = 0; i < weights.size(); ++i) {
-			weights[i].scalarMult(sumInv);
-		}
-	} else {
-		for (size_t i = 0; i < weights.size(); ++i) {
-			weights[i].scalarMult(weight_function(xValues[i]));
-		}
-	}
+    for (size_t i = 0; i < weights.size(); ++i) {
+      weights[i].scalarMult(sumInv);
+    }
+  } else {
+    for (size_t i = 0; i < weights.size(); ++i) {
+      weights[i].scalarMult(weight_function(xValues[i]));
+    }
+  }
 }
 
 std::shared_ptr<AbstractLinearEvaluator<FloatScalarVector> > QuadratureEvaluator::cloneLinear() {
-	return std::shared_ptr<AbstractLinearEvaluator<FloatScalarVector> >(new QuadratureEvaluator(*this));
+  return std::shared_ptr<AbstractLinearEvaluator<FloatScalarVector> >(
+      new QuadratureEvaluator(*this));
 }
 
-QuadratureEvaluator::QuadratureEvaluator(sgpp::combigrid::SingleFunction weight_function, bool normalizeWeights) :
-		weight_function(weight_function), normalizeWeights(normalizeWeights) {
+QuadratureEvaluator::QuadratureEvaluator(sgpp::combigrid::SingleFunction weight_function,
+                                         bool normalizeWeights)
+    : weight_function(weight_function), normalizeWeights(normalizeWeights) {}
 
-}
+QuadratureEvaluator::QuadratureEvaluator(QuadratureEvaluator const& other)
+    : xValues(other.xValues),
+      weights(other.weights),
+      weight_function(other.weight_function),
+      normalizeWeights(other.normalizeWeights) {}
 
-QuadratureEvaluator::QuadratureEvaluator(QuadratureEvaluator const &other) :
-		xValues(other.xValues), weights(other.weights), weight_function(other.weight_function), normalizeWeights(other.normalizeWeights) {
-}
-
-void QuadratureEvaluator::setParameter(const FloatScalarVector& param) {
-	return;
-}
+void QuadratureEvaluator::setParameter(const FloatScalarVector& param) { return; }
 
 } /* namespace combigrid */
 } /* namespace sgpp*/
