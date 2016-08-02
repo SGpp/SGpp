e5f77ed17caf54f70a3597de751c9a4d77e4aaa9
David Holzmüller <david.holzmueller@online.de>
2016-08-02 17:15:56 +0200

    added AbstractLevelEvaluator to combigrid module and added constructor to LevelManager

---
+++ combigrid/src/sgpp/combigrid/operation/multidim/AbstractLevelEvaluator.hpp
@@ -0,0 +1,39 @@
+/*
+ * AbstractLevelEvaluator.hpp
+ *
+ *  Created on: 02.08.2016
+ *      Author: david
+ */
+
+#ifndef COMBIGRID_SRC_SGPP_COMBIGRID_OPERATION_MULTIDIM_ABSTRACTLEVELEVALUATOR_HPP_
+#define COMBIGRID_SRC_SGPP_COMBIGRID_OPERATION_MULTIDIM_ABSTRACTLEVELEVALUATOR_HPP_
+
+#include <memory>
+#include <mutex>
+#include <sgpp/combigrid/definitions.hpp>
+#include <sgpp/combigrid/storage/tree/TreeStorage.hpp>
+#include <sgpp/combigrid/threading/ThreadPool.hpp>
+#include <vector>
+
+namespace sgpp {
+namespace combigrid {
+
+class AbstractLevelEvaluator {
+ public:
+  virtual ~AbstractLevelEvaluator();
+
+  virtual bool addLevel(MultiIndex const &level) = 0;
+  virtual std::vector<ThreadPool::Task> getLevelTasks(MultiIndex const &level,
+                                                      ThreadPool::Task callback,
+                                                      std::mutex &lock) = 0;
+  virtual bool containsLevel(MultiIndex const &level) = 0;
+  virtual size_t maxNewPoints(MultiIndex const &level) = 0;
+  virtual double getDifferenceNorm(MultiIndex const &level) = 0;
+  virtual size_t dim() const = 0;
+  virtual void clear() = 0;
+  virtual std::shared_ptr<TreeStorage<uint8_t>> getLevelStructure() = 0;
+};
+}
+}
+
+#endif /* COMBIGRID_SRC_SGPP_COMBIGRID_OPERATION_MULTIDIM_ABSTRACTLEVELEVALUATOR_HPP_ */
--- combigrid/src/sgpp/combigrid/operation/multidim/CombigridEvaluator.hpp
+++ combigrid/src/sgpp/combigrid/operation/multidim/CombigridEvaluator.hpp
@@ -8,316 +8,325 @@
 #ifndef COMBIGRID_SRC_SGPP_COMBIGRID_OPERATION_MULTIDIM_COMBIGRIDEVALUATOR_HPP_
 #define COMBIGRID_SRC_SGPP_COMBIGRID_OPERATION_MULTIDIM_COMBIGRIDEVALUATOR_HPP_
 
-#include "../../definitions.hpp"
-#include "AbstractFullGridEvaluator.hpp"
-#include "FullGridTensorEvaluator.hpp"
-#include "../../common/BoundedSumMultiIndexIterator.hpp"
-#include "../../storage/AbstractMultiStorage.hpp"
-#include "../../storage/tree/TreeStorage.hpp"
-#include "AdaptiveRefinementStrategy.hpp"
-#include <sgpp/combigrid/serialization/TreeStorageSerializationStrategy.hpp>
-#include <memory>
-#include <queue>
-#include <unordered_set>
 #include <cmath>
 #include <limits>
+#include <memory>
+#include <queue>
+#include <sgpp/combigrid/serialization/TreeStorageSerializationStrategy.hpp>
+#include <unordered_set>
+#include "../../common/BoundedSumMultiIndexIterator.hpp"
+#include "../../definitions.hpp"
+#include "../../storage/AbstractMultiStorage.hpp"
+#include "../../storage/tree/TreeStorage.hpp"
+#include "AbstractFullGridEvaluator.hpp"
+#include "AbstractLevelEvaluator.hpp"
+#include "AdaptiveRefinementStrategy.hpp"
+#include "FullGridTensorEvaluator.hpp"
 
-#include "LevelHelpers.hpp" // TODO: remove
+#include "LevelHelpers.hpp"  // TODO: remove
 
-namespace sgpp{
+namespace sgpp {
 namespace combigrid {
 
-template<typename V> class CombigridEvaluator {
-	V sum;
-	size_t numDimensions;
+template <typename V>
+class CombigridEvaluator : public AbstractLevelEvaluator {
+  V sum;
+  size_t numDimensions;
 
-	/**
-	 * All partial differences are stored in these storages.
-	 * I. e. storages[0] contains the values for the full grids
-	 * and storages[numDimensions] contains the normal differences.
-	 * The storages in between contain partial differences.
-	 */
-	std::vector<std::shared_ptr<AbstractMultiStorage<V> > > partialDifferences;
-	std::shared_ptr<AbstractFullGridEvaluator<V> > multiEval;
+  /**
+   * All partial differences are stored in these storages.
+   * I. e. storages[0] contains the values for the full grids
+   * and storages[numDimensions] contains the normal differences.
+   * The storages in between contain partial differences.
+   */
+  std::vector<std::shared_ptr<AbstractMultiStorage<V>>> partialDifferences;
+  std::shared_ptr<AbstractFullGridEvaluator<V>> multiEval;
 
-	void initPartialDifferences() {
-		partialDifferences.clear();
-		for (size_t d = 0; d <= numDimensions; ++d) {
-			partialDifferences.push_back(std::shared_ptr<AbstractMultiStorage<V>>(new TreeStorage<V>(numDimensions)));
-		}
-	}
+  void initPartialDifferences() {
+    partialDifferences.clear();
+    for (size_t d = 0; d <= numDimensions; ++d) {
+      partialDifferences.push_back(
+          std::shared_ptr<AbstractMultiStorage<V>>(new TreeStorage<V>(numDimensions)));
+    }
+  }
 
-public:
-	CombigridEvaluator(size_t numDimensions, std::shared_ptr<AbstractFullGridEvaluator<V> > multiEval) :
-			sum(V::zero()), numDimensions(numDimensions), partialDifferences(), multiEval(multiEval), queue(), refinementStrategy(
-					AdaptiveRefinementStrategy::minStrategy()) {
-		initPartialDifferences();
-	}
+ public:
+  CombigridEvaluator(size_t numDimensions, std::shared_ptr<AbstractFullGridEvaluator<V>> multiEval)
+      : sum(V::zero()),
+        numDimensions(numDimensions),
+        partialDifferences(),
+        multiEval(multiEval),
+        queue(),
+        refinementStrategy(AdaptiveRefinementStrategy::minStrategy()) {
+    initPartialDifferences();
+  }
 
-	/**
-	 * For some reason, SWIG cannot convert the shared_ptr into the more abstract type, so we need this 'duplicate'
-	 */
-	CombigridEvaluator(size_t numDimensions, std::shared_ptr<FullGridTensorEvaluator<V> > multiEval) :
-			sum(V::zero()), numDimensions(numDimensions), partialDifferences(), multiEval(multiEval), queue(), refinementStrategy(
-					AdaptiveRefinementStrategy::minStrategy()) {
-		initPartialDifferences();
-	}
+  /**
+   * For some reason, SWIG cannot convert the shared_ptr into the more abstract type, so we need
+   * this 'duplicate'
+   */
+  CombigridEvaluator(size_t numDimensions, std::shared_ptr<FullGridTensorEvaluator<V>> multiEval)
+      : sum(V::zero()),
+        numDimensions(numDimensions),
+        partialDifferences(),
+        multiEval(multiEval),
+        queue(),
+        refinementStrategy(AdaptiveRefinementStrategy::minStrategy()) {
+    initPartialDifferences();
+  }
 
-	/**
-	 * @return Returns true if the level was already there or if it was computed and the difference was not nan or +-inf.
-	 */
-	bool addLevel(MultiIndex const &level) {
-		CGLOG("addLevel(): start");
-		if (containsLevel(level)) {
-			return true;
-		}
+  /**
+   * @return Returns true if the level was already there or if it was computed and the difference
+   * was not nan or +-inf.
+   */
+  bool addLevel(MultiIndex const &level) {
+    CGLOG("addLevel(): start");
+    if (containsLevel(level)) {
+      return true;
+    }
 
-		CGLOG("addLevel(): add previous levels");
-		// ensure that preceding indices are already computed
-		for (size_t d = 0; d < numDimensions; ++d) {
-			if (level[d] > 0) {
-				MultiIndex l = level;
-				--l[d];
-				addLevel(l); // should not affect performance because nothing is computed if the storage already contains a value
-			}
-		}
+    CGLOG("addLevel(): add previous levels");
+    // ensure that preceding indices are already computed
+    for (size_t d = 0; d < numDimensions; ++d) {
+      if (level[d] > 0) {
+        MultiIndex l = level;
+        --l[d];
+        addLevel(l);  // should not affect performance because nothing is computed if the storage
+                      // already contains a value
+      }
+    }
 
-		CGLOG("addLevel(): eval level");
-		V value = multiEval->eval(level);
+    CGLOG("addLevel(): eval level");
+    V value = multiEval->eval(level);
 
-		CGLOG("addLevel(): evaluate partial differences");
-		partialDifferences[0]->set(level, value);
+    CGLOG("addLevel(): evaluate partial differences");
+    partialDifferences[0]->set(level, value);
 
-		for (size_t d = 0; d < numDimensions; ++d) {
-			if (level[d] == 0) {
-				partialDifferences[d + 1]->set(level, value);
-			} else {
-				MultiIndex predecessor = level;
-				--predecessor[d];
-				value.sub(partialDifferences[d]->get(predecessor));
-				partialDifferences[d + 1]->set(level, value);
-			}
-		}
+    for (size_t d = 0; d < numDimensions; ++d) {
+      if (level[d] == 0) {
+        partialDifferences[d + 1]->set(level, value);
+      } else {
+        MultiIndex predecessor = level;
+        --predecessor[d];
+        value.sub(partialDifferences[d]->get(predecessor));
+        partialDifferences[d + 1]->set(level, value);
+      }
+    }
 
-		double norm = value.norm();
+    double norm = value.norm();
 
-		if (std::isnan(norm) || std::isinf(norm)) {
-			return false;
-		} else {
-			sum.add(value);
-			return true;
-		}
-	}
+    if (std::isnan(norm) || std::isinf(norm)) {
+      return false;
+    } else {
+      sum.add(value);
+      return true;
+    }
+  }
 
-	std::vector<ThreadPool::Task> getLevelTasks(MultiIndex const &level, ThreadPool::Task callback, std::mutex &lock) {
-		return multiEval->getLevelTasks(level, callback, lock);
-	}
+  std::vector<ThreadPool::Task> getLevelTasks(MultiIndex const &level, ThreadPool::Task callback,
+                                              std::mutex &lock) {
+    return multiEval->getLevelTasks(level, callback, lock);
+  }
 
-	std::shared_ptr<AbstractMultiStorage<V> > differences() const {
-		return partialDifferences[numDimensions];
-	}
+  std::shared_ptr<AbstractMultiStorage<V>> differences() const {
+    return partialDifferences[numDimensions];
+  }
 
-	bool containsLevel(MultiIndex const &level) {
-		return partialDifferences[0]->containsIndex(level);
-	}
+  bool containsLevel(MultiIndex const &level) {
+    return partialDifferences[0]->containsIndex(level);
+  }
 
-	void setRefinementStrategy(AdaptiveRefinementStrategy strategy) {
-		refinementStrategy = strategy;
-	}
+  void setRefinementStrategy(AdaptiveRefinementStrategy strategy) { refinementStrategy = strategy; }
 
-	size_t maxNewPoints(MultiIndex const &level) {
-		return multiEval->maxNewPoints(level);
-	}
+  size_t maxNewPoints(MultiIndex const &level) { return multiEval->maxNewPoints(level); }
 
-	double getDifferenceNorm(MultiIndex const &level) {
-		return partialDifferences[numDimensions]->get(level).norm();
-	}
+  double getDifferenceNorm(MultiIndex const &level) {
+    return partialDifferences[numDimensions]->get(level).norm();
+  }
 
-	/**
-	 * @param q: Maximum 1-norm of the level-multi-index, where the levels start from 0 (not from 1 as in most papers).
-	 * If you have a norm w with levels starting from 1, simply use q = w - dim().
-	 */
-	void addRegularLevels(size_t q) {
-		BoundedSumMultiIndexIterator iterator(numDimensions, q);
+  /**
+   * @param q: Maximum 1-norm of the level-multi-index, where the levels start from 0 (not from 1 as
+   * in most papers).
+   * If you have a norm w with levels starting from 1, simply use q = w - dim().
+   */
+  void addRegularLevels(size_t q) {
+    BoundedSumMultiIndexIterator iterator(numDimensions, q);
 
-		while (iterator.isValid()) {
-			addLevel(iterator.value());
-			iterator.moveToNext();
-		}
-	}
+    while (iterator.isValid()) {
+      addLevel(iterator.value());
+      iterator.moveToNext();
+    }
+  }
 
-	void addRegularLevelsByNumPoints(size_t maxNumPoints) {
-		size_t numPoints = 0;
+  void addRegularLevelsByNumPoints(size_t maxNumPoints) {
+    size_t numPoints = 0;
 
-		size_t q = 0;
+    size_t q = 0;
 
-		while(true) {
-			BoundedSumMultiIndexIterator iterator(numDimensions, q);
+    while (true) {
+      BoundedSumMultiIndexIterator iterator(numDimensions, q);
 
-			while(iterator.isValid()) {
-				MultiIndex nextLevel = iterator.value();
+      while (iterator.isValid()) {
+        MultiIndex nextLevel = iterator.value();
 
-				if(partialDifferences[0]->containsIndex(nextLevel)) {
-					iterator.moveToNext();
-					continue;
-				}
+        if (partialDifferences[0]->containsIndex(nextLevel)) {
+          iterator.moveToNext();
+          continue;
+        }
 
-				size_t maxNewPoints = multiEval->maxNewPoints(nextLevel);
-				numPoints += maxNewPoints;
+        size_t maxNewPoints = multiEval->maxNewPoints(nextLevel);
+        numPoints += maxNewPoints;
 
-				if(numPoints > maxNumPoints) {
-					return;
-				}
+        if (numPoints > maxNumPoints) {
+          return;
+        }
 
-				addLevel(nextLevel);
-				iterator.moveToNext();
-			}
+        addLevel(nextLevel);
+        iterator.moveToNext();
+      }
 
-			++q;
-		}
-	}
+      ++q;
+    }
+  }
 
-	size_t dim() const {
-		return numDimensions;
-	}
+  size_t dim() const { return numDimensions; }
 
-	V getValue() const {
-		return sum;
-	}
+  V getValue() const { return sum; }
 
-	void clear() {
-		sum = V::zero();
-		initPartialDifferences();
-	}
+  void clear() {
+    sum = V::zero();
+    initPartialDifferences();
+  }
 
-	std::shared_ptr<TreeStorage<uint8_t>> getLevelStructure() {
-		std::shared_ptr<TreeStorage<uint8_t>> storage(new TreeStorage<uint8_t>(numDimensions));
+  std::shared_ptr<TreeStorage<uint8_t>> getLevelStructure() {
+    std::shared_ptr<TreeStorage<uint8_t>> storage(new TreeStorage<uint8_t>(numDimensions));
 
-		auto it = partialDifferences[numDimensions]->getStoredDataIterator();
+    auto it = partialDifferences[numDimensions]->getStoredDataIterator();
 
-		while (it->isValid()) {
-			storage->set(it->getMultiIndex(), 1);
-			it->moveToNext();
-		}
+    while (it->isValid()) {
+      storage->set(it->getMultiIndex(), 1);
+      it->moveToNext();
+    }
 
-		return storage;
-	}
+    return storage;
+  }
 
-	std::string getSerializedLevelStructure() {
-		return TreeStorageSerializationStrategy<uint8_t>(numDimensions).serialize(getLevelStructure());
-	}
+  std::string getSerializedLevelStructure() {
+    return TreeStorageSerializationStrategy<uint8_t>(numDimensions).serialize(getLevelStructure());
+  }
 
-	void addLevelsFromStructure(std::shared_ptr<TreeStorage<uint8_t>> storage) {
-		auto it = storage->getStoredDataIterator();
+  void addLevelsFromStructure(std::shared_ptr<TreeStorage<uint8_t>> storage) {
+    auto it = storage->getStoredDataIterator();
 
-		while (it->isValid()) {
-			addLevel(it->getMultiIndex());
-			it->moveToNext();
-		}
-	}
+    while (it->isValid()) {
+      addLevel(it->getMultiIndex());
+      it->moveToNext();
+    }
+  }
 
-	void addLevelsFromSerializedStructure(std::string serializedStructure) {
-		addLevelsFromStructure(TreeStorageSerializationStrategy<uint8_t>(numDimensions).deserialize(serializedStructure));
-	}
+  void addLevelsFromSerializedStructure(std::string serializedStructure) {
+    addLevelsFromStructure(
+        TreeStorageSerializationStrategy<uint8_t>(numDimensions).deserialize(serializedStructure));
+  }
 
-	void addLevelsAdaptive(size_t maxNumPoints) {
-		CGLOG("addLevelsAdaptive(): start");
-		initAdaption();
+  void addLevelsAdaptive(size_t maxNumPoints) {
+    CGLOG("addLevelsAdaptive(): start");
+    initAdaption();
 
-		CGLOG("addLevelsAdaptive(): adaption initialized");
-		size_t currentPointBound = 0;
+    CGLOG("addLevelsAdaptive(): adaption initialized");
+    size_t currentPointBound = 0;
 
-		while (!queue.empty()) {
-			CGLOG("addLevelsAdaptive(): next queue entry");
-			QueueEntry entry = queue.top();
-			queue.pop();
+    while (!queue.empty()) {
+      CGLOG("addLevelsAdaptive(): next queue entry");
+      QueueEntry entry = queue.top();
+      queue.pop();
 
-			currentPointBound += entry.maxNewPoints;
+      currentPointBound += entry.maxNewPoints;
 
-			if (currentPointBound > maxNumPoints) {
-				break;
-			}
+      if (currentPointBound > maxNumPoints) {
+        break;
+      }
 
-			CGLOG("addLevelsAdaptive(): add next level");
-			bool validResult = addLevel(entry.level);
+      CGLOG("addLevelsAdaptive(): add next level");
+      bool validResult = addLevel(entry.level);
 
-			CGLOG("addLevelsAdaptive(): added next level");
-			if (validResult) {
-				tryAddSuccessorsToQueue(entry.level);
-			}
-		}
-	}
+      CGLOG("addLevelsAdaptive(): added next level");
+      if (validResult) {
+        tryAddSuccessorsToQueue(entry.level);
+      }
+    }
+  }
 
-private:
-	// data structures for adaptive refinement
-	typedef std::priority_queue<QueueEntry, std::vector<QueueEntry>, QueueComparator> MultiIndexQueue;
-	MultiIndexQueue queue;
-	AdaptiveRefinementStrategy refinementStrategy;
+ private:
+  // data structures for adaptive refinement
+  typedef std::priority_queue<QueueEntry, std::vector<QueueEntry>, QueueComparator> MultiIndexQueue;
+  MultiIndexQueue queue;
+  AdaptiveRefinementStrategy refinementStrategy;
 
-	void clearQueue() {
-		MultiIndexQueue other;
-		queue.swap(other); // unfortunately, there is no clear() method
-	}
+  void clearQueue() {
+    MultiIndexQueue other;
+    queue.swap(other);  // unfortunately, there is no clear() method
+  }
 
-	void initAdaption() {
-		clearQueue();
+  void initAdaption() {
+    clearQueue();
 
-		auto it = partialDifferences[numDimensions]->getStoredDataIterator();
+    auto it = partialDifferences[numDimensions]->getStoredDataIterator();
 
-		while (it->isValid()) {
-			tryAddSuccessorsToQueue(it->getMultiIndex());
-			it->moveToNext();
-		}
+    while (it->isValid()) {
+      tryAddSuccessorsToQueue(it->getMultiIndex());
+      it->moveToNext();
+    }
 
-		if (queue.empty()) {
-			// no difference has yet been computed, so add start value
-			MultiIndex startIndex(numDimensions, 0);
-			queue.push(QueueEntry(startIndex, 1.0, multiEval->maxNewPoints(startIndex)));
-		}
-	}
+    if (queue.empty()) {
+      // no difference has yet been computed, so add start value
+      MultiIndex startIndex(numDimensions, 0);
+      queue.push(QueueEntry(startIndex, 1.0, multiEval->maxNewPoints(startIndex)));
+    }
+  }
 
-	void tryAddSuccessorsToQueue(MultiIndex const &index) {
-		for (size_t d = 0; d < numDimensions; ++d) {
-			MultiIndex nextIndex = index;
-			++nextIndex[d];
+  void tryAddSuccessorsToQueue(MultiIndex const &index) {
+    for (size_t d = 0; d < numDimensions; ++d) {
+      MultiIndex nextIndex = index;
+      ++nextIndex[d];
 
-			tryAddIndexToQueue(nextIndex);
-		}
-	}
+      tryAddIndexToQueue(nextIndex);
+    }
+  }
 
-	void tryAddIndexToQueue(MultiIndex const &nextIndex) {
-		if (partialDifferences[numDimensions]->containsIndex(nextIndex)) {
-			return;
-		}
+  void tryAddIndexToQueue(MultiIndex const &nextIndex) {
+    if (partialDifferences[numDimensions]->containsIndex(nextIndex)) {
+      return;
+    }
 
-		// calculate estimated value...
-		std::vector<double> predecessorNorms;
+    // calculate estimated value...
+    std::vector<double> predecessorNorms;
 
-		bool hasAllPredecessors = true;
+    bool hasAllPredecessors = true;
 
-		for (size_t prevDim = 0; prevDim < numDimensions; ++prevDim) {
-			if (nextIndex[prevDim] > 0) {
-				MultiIndex prevIndex = nextIndex;
-				--prevIndex[prevDim];
+    for (size_t prevDim = 0; prevDim < numDimensions; ++prevDim) {
+      if (nextIndex[prevDim] > 0) {
+        MultiIndex prevIndex = nextIndex;
+        --prevIndex[prevDim];
 
-				if (partialDifferences[numDimensions]->containsIndex(prevIndex)) {
-					double norm = partialDifferences[numDimensions]->get(prevIndex).norm();
-					predecessorNorms.push_back(norm);
-				} else {
-					hasAllPredecessors = false; // add index later, not all predecessors are available
-					break;
-				}
-			}
-		}
+        if (partialDifferences[numDimensions]->containsIndex(prevIndex)) {
+          double norm = partialDifferences[numDimensions]->get(prevIndex).norm();
+          predecessorNorms.push_back(norm);
+        } else {
+          hasAllPredecessors = false;  // add index later, not all predecessors are available
+          break;
+        }
+      }
+    }
 
-		if (hasAllPredecessors) {
-			size_t maxNewPoints = multiEval->maxNewPoints(nextIndex);
-			double priority = refinementStrategy.computePriority(predecessorNorms, maxNewPoints);
+    if (hasAllPredecessors) {
+      size_t maxNewPoints = multiEval->maxNewPoints(nextIndex);
+      double priority = refinementStrategy.computePriority(predecessorNorms, maxNewPoints);
 
-			QueueEntry nextEntry(nextIndex, priority, maxNewPoints);
-			queue.push(nextEntry);
-		}
-	}
+      QueueEntry nextEntry(nextIndex, priority, maxNewPoints);
+      queue.push(nextEntry);
+    }
+  }
 };
 
 } /* namespace combigrid */
