72b9f97bb6e18a2e234632dca00b83ebdf2d4c69
David Holzmüller <david.holzmueller@online.de>
2016-08-02 17:15:56 +0200

    added AbstractLevelEvaluator to combigrid module and added constructor to LevelManager

---
+++ combigrid/src/sgpp/combigrid/operation/multidim/AbstractLevelEvaluator.cpp
@@ -0,0 +1,15 @@
+/*
+ * AbstractLevelEvaluator.cpp
+ *
+ *  Created on: 02.08.2016
+ *      Author: david
+ */
+
+#include <sgpp/combigrid/operation/multidim/AbstractLevelEvaluator.hpp>
+
+namespace sgpp {
+namespace combigrid {
+
+AbstractLevelEvaluator::~AbstractLevelEvaluator() {}
+}
+}
