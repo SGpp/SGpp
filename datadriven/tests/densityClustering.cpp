a44a63ce56badc61841aa656ec5928d9f361e59d
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2016-08-03 08:58:04 +0200

    Added unit test for opencl knn graph pruning

--- datadriven/tests/densityClustering.cpp
+++ datadriven/tests/densityClustering.cpp
@@ -352,65 +352,84 @@ BOOST_AUTO_TEST_CASE(KNNGraphOpenCL)  {
 }
 
 BOOST_AUTO_TEST_CASE(KNNPruneGraphOpenCL)  {
-  // // Load input
-  // std::vector<int> graph;
-  // std::ifstream graph_in( "/media/DatenPartition/Programmieren/ipvs/SGpp/datadriven/tests/datasets/graph_erg_dim2_depth11.txt");
-  // if (graph_in) {
-  //   double value;
-  //   while (graph_in >> value)
-  //     graph_optimal_result.push_back(value);
-  // } else {
-  //   BOOST_THROW_EXCEPTION(std::runtime_error("knn graph result file is missing!"));
-  // }
-  // graph_in.close();
+  // Load input
+  std::vector<int> graph;
+  std::ifstream graph_in( "/media/DatenPartition/Programmieren/ipvs/SGpp/datadriven/tests/datasets/graph_erg_dim2_depth11.txt");
+  if (graph_in) {
+    double value;
+    while (graph_in >> value)
+      graph.push_back(value);
+  } else {
+    BOOST_THROW_EXCEPTION(std::runtime_error("knn graph result file is missing!"));
+  }
 
-  // // Load optimal results for comparison
-  // std::vector<int> graph_optimal_result;
-  // std::ifstream graph_result( "/media/DatenPartition/Programmieren/ipvs/SGpp/datadriven/tests/datasets/graph_pruned_erg_dim2_depth11.txt");
-  // if (graph_result) {
-  //   double value;
-  //   while (graph_result >> value)
-  //     graph_optimal_result.push_back(value);
-  // } else {
-  //   BOOST_THROW_EXCEPTION(std::runtime_error("knn pruned graph result file is missing!"));
-  // }
-  // graph_result.close();
+  // Load optimal results for comparison
+  std::vector<int> graph_optimal_result;
+  std::ifstream graph_result( "/media/DatenPartition/Programmieren/ipvs/SGpp/datadriven/tests/datasets/graph_pruned_erg_dim2_depth11.txt");
+  if (graph_result) {
+    double value;
+    while (graph_result >> value)
+      graph_optimal_result.push_back(value);
+  } else {
+    BOOST_THROW_EXCEPTION(std::runtime_error("knn pruned graph result file is missing!"));
+  }
+  std::cerr << "Loaded graphs" << std::endl;
 
-  // // Create OCL configuration
-  // std::shared_ptr<sgpp::base::OCLOperationConfiguration> parameters =
-  //     getConfigurationDefaultsSingleDevice();
-  // sgpp::datadriven::DensityOCLMultiPlatform::
-  //     OperationDensityOCL::load_default_parameters(parameters.get());
+  // Create grid for test scenario
+  sgpp::base::Grid *grid = sgpp::base::Grid::createLinearGrid(2);
+  sgpp::base::GridGenerator& gridGen = grid->getGenerator();
+  gridGen.regular(11);
+  size_t gridsize = grid->getStorage().getSize();
+    std::cerr << "start " << std::endl;
 
-  // // Create OpenCL Manager
-  // std::shared_ptr<sgpp::base::OCLManagerMultiPlatform> manager;
-  // manager = std::make_shared<sgpp::base::OCLManagerMultiPlatform>(true);
+  // Load optimal results for comparison
+  sgpp::base::DataVector alpha(gridsize);
+  std::ifstream alpha_result( "/media/DatenPartition/Programmieren/ipvs/SGpp/datadriven/tests/datasets/alpha_erg_dim2_depth11.txt");
+  if (alpha_result) {
+    int counter = 0;
+    std::cerr << "start counter " << counter << std::endl;
+    double value;
+    while (alpha_result >> value) {
+      alpha[counter] = value;
+      counter++;
+    }
+    std::cerr << "End counter " << counter << std::endl;
+    BOOST_CHECK(counter == gridsize);
+  } else {
+    BOOST_THROW_EXCEPTION(std::runtime_error("knn pruned graph result file is missing!"));
+  }
+  alpha_result.close();
+  std::cerr << "Loaded alphas" << std::endl;
 
-  // // Create grid for test scenario
-  // sgpp::base::Grid *grid = sgpp::base::Grid::createLinearGrid(2);
-  // sgpp::base::GridGenerator& gridGen = grid->getGenerator();
-  // gridGen.regular(11);
-  // size_t gridsize = grid->getStorage().getSize();
+  // Create OCL configuration
+  std::shared_ptr<sgpp::base::OCLOperationConfiguration> parameters =
+      getConfigurationDefaultsSingleDevice();
+  sgpp::datadriven::DensityOCLMultiPlatform::
+      OperationPruneGraphOCL::load_default_parameters(parameters.get());
 
-  // // Load dataset for test scenario
-  // sgpp::datadriven::Dataset data =
-  //     sgpp::datadriven::ARFFTools::
-  //     readARFF("/media/DatenPartition/Programmieren/ipvs/SGpp/datadriven/tests/datasets/clustering_testdataset_dim2.arff");
-  // sgpp::base::DataMatrix& dataset = data.getData();
+  // Create OpenCL Manager
+  std::shared_ptr<sgpp::base::OCLManagerMultiPlatform> manager;
+  manager = std::make_shared<sgpp::base::OCLManagerMultiPlatform>(true);
 
-  // // Create operation
-  // sgpp::datadriven::DensityOCLMultiPlatform::OperationDensityOCL* operation_rhs =
-  //     new sgpp::datadriven::DensityOCLMultiPlatform::
-  //     OperationDensityOCLMultiPlatform<double>(*grid, 2, manager, parameters.get(), 0.001,
-  //                                              0, 0);
+  // Load dataset for test scenario
+  sgpp::datadriven::Dataset data =
+      sgpp::datadriven::ARFFTools::
+      readARFF("/media/DatenPartition/Programmieren/ipvs/SGpp/datadriven/tests/datasets/clustering_testdataset_dim2.arff");
+  sgpp::base::DataMatrix& dataset = data.getData();
 
-  // std::cout << "Test rhs kernel ..." << std::endl;
-  // sgpp::base::DataVector b(gridsize);
-  // operation_rhs->generateb(dataset, b);
-  // for (size_t i = 0; i < gridsize; ++i) {
-  //   BOOST_CHECK_CLOSE(rhs_optimal_result[i], b[i], 0.001);
-  // }
-  // delete operation_rhs;
+  // Create operation
+  sgpp::datadriven::DensityOCLMultiPlatform::OperationPruneGraphOCL* operation_prune =
+      new sgpp::datadriven::DensityOCLMultiPlatform::
+        OperationPruneGraphOCLMultiPlatform<double>(*grid, alpha, dataset, 2, manager,
+                                                    parameters.get(), 0.2, 8, 0, 0);
+
+  std::cout << "Test knn prune kernel ..." << std::endl;
+  operation_prune->prune_graph(graph);
+  for (size_t i = 0; i < gridsize; ++i) {
+    BOOST_CHECK(graph[i] == graph_optimal_result[i]);
+  }
+  delete operation_prune;
 }
+
 BOOST_AUTO_TEST_SUITE_END()
 #endif
