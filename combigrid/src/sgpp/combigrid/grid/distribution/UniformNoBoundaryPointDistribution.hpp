d6ed0a287c437b91837adf6516c1d54581883c51
Ionut-Gabriel Farcas <ionut.gabriel.farcas@gmail.com>
2016-08-03 12:32:45 +0200

    added uniformnoboundary and chebyshev point distributions

---
+++ combigrid/src/sgpp/combigrid/grid/distribution/UniformNoBoundaryPointDistribution.hpp
@@ -0,0 +1,27 @@
+// Copyright (C) 2008-today The SG++ project
+// This file is part of the SG++ project. For conditions of distribution and
+// use, please see the copyright notice provided with SG++ or at
+// sgpp.sparsegrids.org
+
+#ifndef COMBIGRID_SRC_SGPP_COMBIGRID_GRID_POINTS_DISTRIBUTION_UNIFORMNOBOUNDARYPOINTDISTRIBUTION_HPP_
+#define COMBIGRID_SRC_SGPP_COMBIGRID_GRID_POINTS_DISTRIBUTION_UNIFORMNOBOUNDARYPOINTDISTRIBUTION_HPP_
+
+#include "AbstractPointDistribution.hpp"
+
+namespace sgpp {
+namespace combigrid {
+
+/**
+ * provides uniform points, i. e. {(k + 1)/(n + 1) for k = 0, ..., n-1}
+ */
+class UniformNoBoundaryPointDistribution : public AbstractPointDistribution {
+ public:
+  virtual ~UniformNoBoundaryPointDistribution();
+
+  virtual double compute(size_t numPoints, size_t j);
+};
+
+} /* namespace combigrid */
+} /* namespace sgpp*/
+
+#endif /* COMBIGRID_SRC_SGPP_COMBIGRID_GRID_POINTS_DISTRIBUTION_UNIFORMNOBOUNDARYPOINTDISTRIBUTION_HPP_ */
