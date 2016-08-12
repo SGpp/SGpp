92af322de9c2f3961acd5d4aca0bd0d850eb0cf1
Michael Lettrich <m.lettrich@tum.de>
2016-08-12 12:26:52 +0200

    Python and Java swig wrapping for DataSource Module

--- datadriven/src/sgpp_datadriven.hpp
+++ datadriven/src/sgpp_datadriven.hpp
@@ -6,12 +6,12 @@
 #ifndef DATADRIVEN_HPP
 #define DATADRIVEN_HPP
 
-#include <sgpp/datadriven/algorithm/test_dataset.hpp>
-#include <sgpp/datadriven/algorithm/DMSystemMatrix.hpp>
-#include <sgpp/datadriven/algorithm/DMWeightMatrix.hpp>
 #include <sgpp/datadriven/algorithm/AlgorithmAdaBoostBase.hpp>
 #include <sgpp/datadriven/algorithm/AlgorithmAdaBoostIdentity.hpp>
+#include <sgpp/datadriven/algorithm/DMSystemMatrix.hpp>
+#include <sgpp/datadriven/algorithm/DMWeightMatrix.hpp>
 #include <sgpp/datadriven/algorithm/DensitySystemMatrix.hpp>
+#include <sgpp/datadriven/algorithm/test_dataset.hpp>
 
 #include <sgpp/datadriven/functors/MultiGridRefinementFunctor.hpp>
 #include <sgpp/datadriven/functors/MultiSurplusRefinementFunctor.hpp>
@@ -19,10 +19,9 @@
 #include <sgpp/datadriven/functors/classification/GridPointBasedRefinementFunctor.hpp>
 #include <sgpp/datadriven/functors/classification/ZeroCrossingRefinementFunctor.hpp>
 
-
-#include <sgpp/datadriven/application/Learner.hpp>
 #include <sgpp/datadriven/application/DensityEstimator.hpp>
 #include <sgpp/datadriven/application/GaussianKDE.hpp>
+#include <sgpp/datadriven/application/Learner.hpp>
 #include <sgpp/datadriven/application/LearnerSGDE.hpp>
 
 #include <sgpp/datadriven/operation/hash/simple/OperationRegularizationDiagonal.hpp>
@@ -32,16 +31,16 @@
 #include <sgpp/datadriven/tools/Dataset.hpp>
 
 #include <sgpp/datadriven/operation/hash/OperationMultipleEvalSubspace/AbstractOperationMultipleEvalSubspace.hpp>
-#include <sgpp/datadriven/operation/hash/OperationMultipleEvalSubspace/simple/SubspaceNodeSimple.hpp>
 #include <sgpp/datadriven/operation/hash/OperationMultipleEvalSubspace/simple/OperationMultipleEvalSubspaceSimple.hpp>
 #include <sgpp/datadriven/operation/hash/OperationMultipleEvalSubspace/simple/OperationMultipleEvalSubspaceSimpleParameters.hpp>
+#include <sgpp/datadriven/operation/hash/OperationMultipleEvalSubspace/simple/SubspaceNodeSimple.hpp>
 
-#include <sgpp/datadriven/operation/hash/simple/OperationTransformation1D.hpp>
 #include <sgpp/datadriven/operation/hash/simple/OperationRosenblattTransformation.hpp>
 #include <sgpp/datadriven/operation/hash/simple/OperationRosenblattTransformationKDE.hpp>
+#include <sgpp/datadriven/operation/hash/simple/OperationTransformation1D.hpp>
 
-#include <sgpp/datadriven/operation/hash/simple/OperationDensityMarginalize.hpp>
 #include <sgpp/datadriven/operation/hash/simple/OperationDensityMargTo1D.hpp>
+#include <sgpp/datadriven/operation/hash/simple/OperationDensityMarginalize.hpp>
 
 #include <sgpp/datadriven/tools/TypesDatadriven.hpp>
 
@@ -53,21 +52,15 @@
  * datamining
  * ************************/
 
-//// configuration:
-//#include <sgpp/datadriven/datamining/configuration/DataMiningConfigJsonParser.hpp>
-//
-//// dataSource:
-//
-//
-//// fitting:
-//#include <sgpp/datadriven/datamining/fitting/ModelFittingBase.hpp>
-//#include <sgpp/datadriven/datamining/fitting/ModelFittingDensityEstimation.hpp>
-//#include <sgpp/datadriven/datamining/fitting/ModelFittingLeastSquares.hpp>
-//
-//// scoring:
-//#include <sgpp/datadriven/datamining/scoring/Metric.hpp>
-//#include <sgpp/datadriven/datamining/scoring/MSE.hpp>
-//#include <sgpp/datadriven/datamining/scoring/Scorer.hpp>
-//#include <sgpp/datadriven/datamining/scoring/SimpleSplittingScorer.hpp>
+// datasource
+#include <sgpp/datadriven/datamining/builder/DataSourceBuilder.hpp>
+#include <sgpp/datadriven/datamining/modules/dataSource/ArffFileSampleProvider.hpp>
+#include <sgpp/datadriven/datamining/modules/dataSource/DataSource.hpp>
+#include <sgpp/datadriven/datamining/modules/dataSource/DataSourceConfig.hpp>
+#include <sgpp/datadriven/datamining/modules/dataSource/DataSourceIterator.hpp>
+#include <sgpp/datadriven/datamining/modules/dataSource/FileSampleDecorator.hpp>
+#include <sgpp/datadriven/datamining/modules/dataSource/FileSampleProvider.hpp>
+#include <sgpp/datadriven/datamining/modules/dataSource/GzipFileSampleDecorator.hpp>
+#include <sgpp/datadriven/datamining/modules/dataSource/SampleProvider.hpp>
 
 #endif /* DATADRIVEN_HPP */
