6b8cda13a5d648914bc2f4a1d4527f0f1a27207c
Peter Schober <schober@finance.uni-frankfurt.de>
2016-09-13 14:10:21 +0200

    updated windows build file to run with MATLAB solver etc

--- scons_build.bat
+++ scons_build.bat
@@ -2,12 +2,16 @@ scons -j 4 ^
   SG_ALL=0 ^
   SG_BASE=1 ^
   SG_OPTIMIZATION=1 ^
+  OPT=1 ^
   USE_EIGEN=1 ^
+  USE_MATLAB=1 ^
   COMPILER=gnu ^
   BOOST_INCLUDE_PATH=C:\boost_1_59_0 ^
   BOOST_LIBRARY_PATH=C:\boost_1_59_0\stage\lib ^
   PYDOC=0 ^
   ARCH=sse3 ^
   BUILD_STATICLIB=yes ^
-  CPPPATH=E:\MATLAB\SharedSourceCode\Development\Mex\
+  CPPPATH=E:\MATLAB\SharedSourceCode\Development\Mex\matlab\include,E:\MATLAB\SharedSourceCode\Development\Mex ^
+  LIBPATH=E:\MATLAB\SharedSourceCode\Development\Mex\matlab\bin ^
+  RPATH=E:\MATLAB\SharedSourceCode\Development\Mex\matlab\bin
 pause
