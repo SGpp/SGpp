4e284d22c02138874d1914df5aaf2f71608e48be
Lukas Krenz <lukas@krenz.land>
2016-09-22 22:06:10 +0200

    Add documentation for learners.

--- datadriven/src/sgpp/datadriven/application/ClassificationLearner.hpp
+++ datadriven/src/sgpp/datadriven/application/ClassificationLearner.hpp
@@ -23,26 +23,74 @@
 namespace sgpp {
 namespace datadriven {
 
+/**
+ * @brief The ClassificationLearner class
+ * Solves a classification problem.
+ */
 class ClassificationLearner {
  public:
+  /**
+   * @brief ClassificationLearner
+   * @param gridConfig
+   * @param adaptivityConfig
+   * @param solverConfig is the solver used during each adaptivity step
+   * @param finalSolverConfig is the solver used to build the final model
+   * @param regularizationConfig
+   * @param terms is a vector that contains all desired interaction terms.
+   * For example, if we want to include grid points that model an
+   * interaction between the first and the second predictor, we would
+   * include the vector [1,2] in terms.
+   */
   ClassificationLearner(sgpp::base::RegularGridConfiguration gridConfig,
                         sgpp::base::AdpativityConfiguration adaptivityConfig,
                         sgpp::solver::SLESolverConfiguration solverConfig,
                         sgpp::solver::SLESolverConfiguration finalSolverConfig,
                         sgpp::datadriven::RegularizationConfiguration regularizationConfig,
                         std::vector<std::vector<size_t>> terms);
-
+  /**
+   * @brief ClassificationLearner
+   * @param gridConfig
+   * @param adaptivityConfig
+   * @param solverConfig is the solver used during each adaptivity step
+   * @param finalSolverConfig is the solver used to build the final model
+   * @param regularizationConfig
+   */
   ClassificationLearner(sgpp::base::RegularGridConfiguration gridConfig,
                         sgpp::base::AdpativityConfiguration adaptivityConfig,
                         sgpp::solver::SLESolverConfiguration solverConfig,
                         sgpp::solver::SLESolverConfiguration finalSolverConfig,
                         sgpp::datadriven::RegularizationConfiguration regularizationConfig);
-
+  /**
+   * @brief train fits a sparse grid regression model.
+   * @param trainDataset is the design matrix
+   * @param classes is the (continuous) target
+   */
   void train(sgpp::base::DataMatrix& trainDataset, sgpp::base::DataVector& classes);
+  /**
+   * @brief predict
+   * @param data are observations
+   * @return the predicted target for matrix data
+   */
   sgpp::base::DataVector predict(sgpp::base::DataMatrix& data);
+  /**
+   * @brief predict
+   * @param data are observations
+   * @return the predicted target for matrix data and the certainty of the model,
+   * that the observation belongs to the predicted class.
+   */
   std::pair<sgpp::base::DataVector, sgpp::base::DataVector> predictWithCertainty(
       sgpp::base::DataMatrix& data);
+  /**
+   * @brief getGridSize
+   * @return the grid size
+   */
   size_t getGridSize() const;
+  /**
+   * @brief getAccuracy
+   * @param data is the design matrix
+   * @param y is the target
+   * @return the accuracy of the prediction of the model for the matrix data
+   */
   double getAccuracy(sgpp::base::DataMatrix& data, const sgpp::base::DataVector& y);
 
  private:
