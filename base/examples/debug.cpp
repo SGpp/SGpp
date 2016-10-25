d712f96cddb963be83fc3fc7323195ad5d9ef64d
Fabian Franzelin <fabian.franzelin@ipvs.uni-stuttgart.de>
2016-10-25 13:03:52 +0200

    style warnings removed

--- base/examples/debug.cpp
+++ base/examples/debug.cpp
@@ -1,8 +1,17 @@
-#include <iostream>
+// Copyright (C) 2008-today The SG++ project
+// This file is part of the SG++ project. For conditions of distribution and
+// use, please see the copyright notice provided with SG++ or at
+// sgpp.sparsegrids.org
 
 #include <sgpp_base.hpp>
 
-using namespace sgpp::base;
+#include <iostream>
+
+using sgpp::base::Grid;
+using sgpp::base::DataVector;
+using sgpp::base::SurplusRefinementFunctor;
+using sgpp::base::HashRefinementBoundaries;
+using sgpp::base::SubspaceRefinement;
 
 int main() {
   const size_t d = 2;
@@ -17,7 +26,7 @@ int main() {
 
   DataVector tmp(N, 0.0);
   const size_t indexToRefine = 5;
-  //const size_t indexToRefine = 35;
+  // const size_t indexToRefine = 35;
   tmp[indexToRefine] = 1.0;
   const size_t numberOfPointsToRefine = 1;
 
@@ -28,11 +37,12 @@ int main() {
   SubspaceRefinement subspaceRefinement(&hashRefinement);
 
   /*AbstractRefinement::refinement_container_type refinablePoints;
-  subspaceRefinement.collectRefinablePoints(grid->getStorage(), functor, refinablePoints);
-  std::cout << "refinablePoints.size() = " << refinablePoints.size() << "\n";
-  std::cout << "refinablePoints[0] seq = " << refinablePoints[0].first->getSeq() << "\n";
-  std::cout << "refinablePoints[0] coord = [" << refinablePoints[0].first->getIndex().getCoordsString() << "]\n";
-  std::cout << "refinablePoints[0] value = " << refinablePoints[0].second << "\n";*/
+   subspaceRefinement.collectRefinablePoints(grid->getStorage(), functor, refinablePoints);
+   std::cout << "refinablePoints.size() = " << refinablePoints.size() << "\n";
+   std::cout << "refinablePoints[0] seq = " << refinablePoints[0].first->getSeq() << "\n";
+   std::cout << "refinablePoints[0] coord = [" <<
+   refinablePoints[0].first->getIndex().getCoordsString() << "]\n";
+   std::cout << "refinablePoints[0] value = " << refinablePoints[0].second << "\n";*/
 
   subspaceRefinement.free_refine(grid->getStorage(), functor);
 }
