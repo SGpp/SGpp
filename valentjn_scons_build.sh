d60018280545ea665dea0d317ebc15ac38e4d12c
Julian Valentin <julian.valentin@ipvs.uni-stuttgart.de>
2016-10-26 14:33:39 +0200

    Also build Python interface in build.sh

--- valentjn_scons_build.sh
+++ valentjn_scons_build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-scons -j $(nproc) VERBOSE=1 OPT=1 SG_ALL=0 SG_BASE=1 SG_OPTIMIZATION=1 \
+scons -j $(nproc) VERBOSE=1 OPT=1 SG_ALL=0 SG_BASE=1 SG_OPTIMIZATION=1 SG_PYTHON=1 \
     USE_EIGEN=1 USE_MATLAB=1 BUILD_STATICLIB=1 \
     CPPPATH=/home/valentjn/local/share/matlab/R2016a/extern/include \
     LIBPATH=/home/valentjn/local/share/matlab/R2016a/bin/glnxa64 \
