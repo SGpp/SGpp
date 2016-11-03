9267350e616783e4103d23d347c04ee1b8afa947
David Holzmüller <david.holzmueller@online.de>
2016-11-03 19:16:51 +0100

    removed unnecessary mutex include

--- combigrid/src/sgpp/combigrid/storage/tree/TreeStorageContext.hpp
+++ combigrid/src/sgpp/combigrid/storage/tree/TreeStorageContext.hpp
@@ -28,6 +28,6 @@ class TreeStorageContext {
 };
 
 } /* namespace combigrid */
-} /* namespace sgpp*/
+} /* namespace sgpp */
 
 #endif /* COMBIGRID_SRC_SGPP_COMBIGRID_STORAGE_TREE_TREESTORAGECONTEXT_HPP_ */
