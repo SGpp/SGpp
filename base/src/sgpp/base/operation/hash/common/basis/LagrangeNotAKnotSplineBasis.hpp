15f325edee996b36ac0b8fffe15ccc96d8ca1fb7
Julian Valentin <julian.valentin@ipvs.uni-stuttgart.de>
2016-11-07 17:36:22 +0100

    Add Lagrange, not-a-knot, and natural bases

---
+++ base/src/sgpp/base/operation/hash/common/basis/LagrangeNotAKnotSplineBasis.hpp
@@ -0,0 +1,195 @@
+// Copyright (C) 2008-today The SG++ project
+// This file is part of the SG++ project. For conditions of distribution and
+// use, please see the copyright notice provided with SG++ or at
+// sgpp.sparsegrids.org
+
+#ifndef LAGRANGE_NOTAKNOT_SPLINE_BASE_HPP
+#define LAGRANGE_NOTAKNOT_SPLINE_BASE_HPP
+
+#include <sgpp/base/operation/hash/common/basis/LagrangeSplineBasis.hpp>
+
+#include <sgpp/globaldef.hpp>
+
+#include <cmath>
+#include <cstddef>
+
+namespace sgpp {
+namespace base {
+
+/**
+ * Lagrange not-a-knot spline basis.
+ */
+template <class LT, class IT>
+class LagrangeNotAKnotSplineBasis: public Basis<LT, IT> {
+ public:
+  /**
+   * Default constructor.
+   */
+  LagrangeNotAKnotSplineBasis() : lagrangeSplineBasis(LagrangeSplineBasis<LT, IT>()) {
+  }
+
+  /**
+   * Constructor.
+   *
+   * @param degree    Spline degree, must be odd
+   *                  (if it's even, degree - 1 is used)
+   */
+  explicit LagrangeNotAKnotSplineBasis(size_t degree) :
+        lagrangeSplineBasis(LagrangeSplineBasis<LT, IT>(degree)) {
+    if (getDegree() > 3) {
+      throw std::runtime_error("Unsupported spline degree.");
+    }
+  }
+
+  /**
+   * Destructor.
+   */
+  ~LagrangeNotAKnotSplineBasis() override {
+  }
+
+  /**
+   * @param l     level of basis function
+   * @param i     index of basis function
+   * @param x     evaluation point
+   * @return      value of basis function
+   */
+  inline double eval(LT l, IT i, double x) override {
+    const IT hInv = static_cast<IT>(1) << l;
+    double t = x * static_cast<double>(hInv) - static_cast<double>(i);
+    
+    switch (getDegree()) {
+      case 1:
+        return lagrangeSplineBasis.eval(l, i, x);
+
+      case 3:
+        if (l == 0) {
+          if (i == 0) {
+            // l = 0, i = 0
+            return 1.0 - x;
+          } else {
+            // l = 0, i = 1
+            return x;
+          }
+        } else if (l == 1) {
+          // l = 1, i = 1
+          return 1.0 - t * t;
+        } else if ((i > 3) && (i < hInv - 3)) {
+          // l >= 4, 3 < i < 2^l - 3
+          return lagrangeSplineBasis.eval(l, i, x);
+        } else {
+          if (i > hInv / 2) {
+            i = hInv - i;
+            t *= -1.0;
+          }
+
+          if (l == 2) {
+            // l == 2, i == 1
+            if ((t < -1.0) || (t > 3.0)) {
+              return 0.0;
+            } else if (t < 1.0) {
+              t += 1.0;
+              double result = 21.0/104.0;
+              result = -99.0/104.0 + result * t;
+              result = 57.0/52.0 + result * t;
+              result *= t;
+              return result;
+            } else {
+              t -= 1.0;
+              double result = -3.0/52.0;
+              result = 27.0/104.0 + result * t;
+              result = -15.0/52.0 + result * t;
+              result *= t;
+              return result;
+            }
+          } else if (i == 1) {
+            // l >= 3, i == 1
+            if ((t < -1.0) || (t > 3.0)) {
+              return 0.0;
+            } else if (t < 1.0) {
+              t += 1.0;
+              double result = 3.0/14.0;
+              result = -27.0/28.0 + result * t;
+              result = 15.0/14.0 + result * t;
+              result *= t;
+              return result;
+            } else if (t < 2.0) {
+              t -= 1.0;
+              double result = -1.0/8.0;
+              result = 9.0/28.0 + result * t;
+              result = -3.0/14.0 + result * t;
+              result *= t;
+              return result;
+            } else {
+              t -= 2.0;
+              double result = 1.0/56.0;
+              result = -3.0/56.0 + result * t;
+              result = 3.0/56.0 + result * t;
+              result = -1.0/56.0 + result * t;
+              return result;
+            }
+          } else {
+            // l >= 3, i == 3
+            if ((t < -3.0) || (t > 3.0)) {
+              return 0.0;
+            } else if (t < -1.0) {
+              t += 3.0;
+              double result = 1.0/8.0;
+              result = -3.0/16.0 + result * t;
+              result = -1.0/8.0 + result * t;
+              result *= t;
+              return result;
+            } else if (t < 0.0) {
+              t += 1.0;
+              double result = -19.0/32.0;
+              result = 9.0/16.0 + result * t;
+              result = 5.0/8.0 + result * t;
+              result *= t;
+              return result;
+            } else if (t < 1.0) {
+              double result = 21.0/32.0;
+              result = -39.0/32.0 + result * t;
+              result = -1.0/32.0 + result * t;
+              result = 19.0/32.0 + result * t;
+              return result;
+            } else if (t < 2.0) {
+              t -= 1.0;
+              double result = -7.0/24.0;
+              result = 3.0/4.0 + result * t;
+              result = -1.0/2.0 + result * t;
+              result *= t;
+              return result;
+            } else {
+              t -= 2.0;
+              double result = 1.0/24.0;
+              result = -1.0/8.0 + result * t;
+              result = 1.0/8.0 + result * t;
+              result = -1.0/24.0 + result * t;
+              return result;
+            }
+          }
+        }
+
+      default:
+        return 0.0;
+    }
+  }
+
+  /**
+   * @return      B-spline degree
+   */
+  inline size_t getDegree() const {
+    return lagrangeSplineBasis.getDegree();
+  }
+
+ protected:
+  /// B-spline basis for B-spline evaluation
+  LagrangeSplineBasis<LT, IT> lagrangeSplineBasis;
+};
+
+// default type-def (unsigned int for level and index)
+typedef LagrangeNotAKnotSplineBasis<unsigned int, unsigned int> SLagrangeNotAKnotSplineBase;
+
+}  // namespace base
+}  // namespace sgpp
+
+#endif /* LAGRANGE_NOTAKNOT_SPLINE_BASE_HPP */
