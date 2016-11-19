05796a08b1c1237881ba20ea18c0caf6e79acccb
Fabian Franzelin <fabian.franzelin@ipvs.uni-stuttgart.de>
2016-11-19 15:13:52 +0100

    using now Gauss Legendre Quad rule from base

--- base/src/sgpp/base/tools/GaussLegendreQuadRule1D.hpp
+++ base/src/sgpp/base/tools/GaussLegendreQuadRule1D.hpp
@@ -29,9 +29,10 @@ class GaussLegendreQuadRule1D : public QuadRule1D {
    * @param coordinates returns the x-coordinates in [0, 1]
    * @param weights returns the corresponding weights (scaled by 0.5)
    */
-  void getLevelPointsAndWeightsNormalized(size_t level,
-                                          base::DataVector& coordinates,
+  void getLevelPointsAndWeightsNormalized(size_t level, base::DataVector& coordinates,
                                           base::DataVector& weights);
+
+  static GaussLegendreQuadRule1D& getInstance();
 };
 
 }  // namespace base
