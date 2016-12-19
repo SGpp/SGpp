b8f442fe0b7da60370fdb7e70a95fcf70da019a7
Fabian Franzelin <fabian.franzelin@ipvs.uni-stuttgart.de>
2016-12-19 10:19:45 +0100

    working on atan test example

--- datadriven/tests/uq/analysis/atan/run_atan.py
+++ datadriven/tests/uq/analysis/atan/run_atan.py
@@ -12,18 +12,19 @@ if __name__ == '__main__':
 
     parser = ArgumentParser(description='Get a program and run it with input', version='%(prog)s 1.0')
     parser.add_argument('--surrogate', default="both", type=str, help="define which surrogate model should be used (sg, pce)")
-    parser.add_argument('--full', default=False, action='store_true', help='run the full model')
-    parser.add_argument('--reduced', default=False, action='store_true', help='run the reduced model')
     parser.add_argument('--out', default=False, action='store_true', help='write results to file')
-    parser.add_argument('--plot', default=False, action='store_true', help='plot results (1d)')
     parser.add_argument('--parallel', default=False, action='store_true', help='run in parallel')
     args = parser.parse_args()
 
-    scenarions_sg = {'uniform': {'gridType': ["polyBoundary",
+    scenarions_sg = {'uniform': {'gridType': ["linear",
+                                              "linearBoundry",
+                                              "modLinear",
+                                              "polyBoundary",
                                               "modPoly",
                                               "polyClenshawCurtisBoundary",
                                               "modPolyClenshawCurtis"],
-                                 'level': [2],
+                                 'full': [True, False],
+                                 'level': [3],
                                  'refinement': [None,
                                                 'simple',
                                                 'weighted',
@@ -31,11 +32,15 @@ if __name__ == '__main__':
                                                 'var',
                                                 'exp'],
                                  'maxGridPoints': [3000]},
-                     'normal': {'gridType': ["polyBoundary",
-                                              "modPoly",
-                                              "polyClenshawCurtisBoundary",
-                                              "modPolyClenshawCurtis"],
-                                 'level': [2],
+                     'normal': {'gridType': ["linear",
+                                             "linearBoundry",
+                                             "modLinear",
+                                             "polyBoundary",
+                                             "modPoly",
+                                             "polyClenshawCurtisBoundary",
+                                             "modPolyClenshawCurtis"],
+                                 'full': [True, False],
+                                 'level': [3],
                                  'refinement': [None,
                                                 'simple',
                                                 'weighted',
@@ -54,32 +59,41 @@ if __name__ == '__main__':
     processes = []
     if args.surrogate in ["both", "sg"]:
         for model, surrogate in scenarions_sg.items():
-            for gridType in surrogate['gridType']:
-                for refinement in surrogate['refinement']:
-                    maxGridPoints = [surrogate['maxGridPoints'][-1]]
-                    if refinement is not None:
-                        maxGridPoints = surrogate['maxGridPoints']
+            if model == "normal":
+                continue
+            for level in surrogate["level"]:
+                for gridType in surrogate['gridType']:
+                    for refinement in surrogate['refinement']:
+                        maxGridPoints = [surrogate['maxGridPoints'][-1]]
+                        fulls = [True]
+                        if refinement is not None:
+                            maxGridPoints = surrogate['maxGridPoints']
+                            fulls = surrogate['full']
+                        for maxGridPoint in maxGridPoints:
+                            for full in fulls:
+                                print "-" * 80
+                                print "scenario: (%s, %s, %i, %s, %s)" % (model, gridType, maxGridPoint, refinement,
+                                                                          "full" if full else "sparse")
+                                print "-" * 80
 
-                    for maxGridPoint in maxGridPoints:
-                        print "-" * 80
-                        print "scenario: (%s, %s, %i, %s)" % (model, gridType, maxGridPoint, refinement)
-                        print "-" * 80
-
-                        if args.parallel:
-                            myargs = (model, gridType, maxGridPoint,
-                                      1, False, refinement, args.out)
-                            processes.append(Process(target=run_atan_sg, args=myargs))
-                        else:
-                            run_atan_sg(model,
-                                        gridType,
-                                        maxGridPoint,
-                                        1,
-                                        False,
-                                        refinement,
-                                        args.out)
+                                if args.parallel:
+                                    myargs = (model, gridType, level, maxGridPoint,
+                                              1, full, refinement, args.out)
+                                    processes.append(Process(target=run_atan_sg, args=myargs))
+                                else:
+                                    run_atan_sg(model,
+                                                gridType,
+                                                level,
+                                                maxGridPoint,
+                                                1,
+                                                full,
+                                                refinement,
+                                                args.out)
 
     if args.surrogate in ["both", "pce"]:
         for model, surrogate in scenarions_pce.items():
+            if model == "normal":
+                continue
             for sampler in surrogate['sampler']:
                 for expansion in surrogate["expansion"]:
                     for max_num_samples in surrogate["max_num_samples"]:
