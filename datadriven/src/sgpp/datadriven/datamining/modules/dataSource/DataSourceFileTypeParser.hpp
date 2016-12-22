fc138b6d2a21e7ebedae66f2b2bf412fa50e4fe8
Michael Lettrich <m.lettrich@tum.de>
2016-12-22 11:11:49 +0100

    factored out DataSourceFileTypeParser

---
+++ datadriven/src/sgpp/datadriven/datamining/modules/dataSource/DataSourceFileTypeParser.hpp
@@ -0,0 +1,51 @@
+/*
+ * DataSourceFileTypeParser.hpp
+ *
+ *  Created on: 22.12.2016
+ *      Author: Michael Lettrich
+ */
+
+#pragma once
+
+#include <sgpp/datadriven/datamining/modules/dataSource/DataSourceConfig.hpp>
+
+#include <map>
+#include <string>
+
+namespace sgpp {
+namespace datadriven {
+
+/**
+ * Convenience class to convert strings to #sgpp::datadriven::DataSourceFileType and generate
+ * string representations for values of #sgpp::datadriven::DataSourceFileType.
+ */
+class DataSourceFileTypeParser {
+ public:
+  /**
+   * Convert strings to values #sgpp::datadriven::DataSourceFileType. Throws if there is no valid
+   * representation
+   * @param input case insensitive string representation of a
+   * #sgpp::datadriven::DataSourceFileType.
+   * @return the corresponding #sgpp::datadriven::DataSourceFileType.
+   */
+  static DataSourceFileType parse(const std::string& input);
+
+  /**
+   * generate string representations for values of #sgpp::datadriven::DataSourceFileType.
+   * @param type enum value.
+   * @return string representation of a #sgpp::datadriven::DataSourceFileType.
+   */
+  static const std::string& toString(DataSourceFileType type);
+
+ private:
+  typedef std::map<DataSourceFileType, std::string> FileTypeMap_t;
+
+  /**
+   * Map containing all values of  #sgpp::datadriven::DataSourceFileType and the corresponding
+   * string representation.
+   */
+  static const FileTypeMap_t fileTypeMap;
+};
+
+} /* namespace datadriven */
+} /* namespace sgpp */
