79615bbc687bc4282d3b0e40da4cc7e0c15a2d5f
Michael Lettrich <m.lettrich@tum.de>
2016-12-22 12:11:54 +0100

    fitter type parser bugfix

--- datadriven/src/sgpp/datadriven/datamining/modules/dataSource/DataSourceFileTypeParser.hpp
+++ datadriven/src/sgpp/datadriven/datamining/modules/dataSource/DataSourceFileTypeParser.hpp
@@ -1,4 +1,8 @@
-/*
+/* Copyright (C) 2008-today The SG++ project
+ * This file is part of the SG++ project. For conditions of distribution and
+ * use, please see the copyright notice provided with SG++ or at
+ * sgpp.sparsegrids.org
+ *
  * DataSourceFileTypeParser.hpp
  *
  *  Created on: 22.12.2016
