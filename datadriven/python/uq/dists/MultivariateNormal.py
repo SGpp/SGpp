bug fix in pdf computation

datadriven/python/uq/dists/MultivariateNormal.py
@@ -56,7 +56,7 @@ class MultivariateNormal(Dist):
         self.L = np.linalg.cholesky(self.corr)
 
     def pdf(self, x):
-        if self.__a <= x <= self.__b:
+        if all([self.__a <= xi <= self.__b for xi in x]):
             z = x - self.__mu
             return self.norm * np.exp(-0.5 * np.dot(z, np.dot(self.cov_inv, z)))
         else:

datadriven/python/uq/plot/plot2d.py
@@ -17,7 +17,7 @@ def plotDensity2d(U, n=50, addContour=True):
     xv, yv = np.meshgrid(x, y, sparse=False, indexing='xy')
     for i in xrange(len(x)):
         for j in xrange(len(y)):
-            Z[j, i] = U.pdf([xv[j, i], yv[j, i]])
+            Z[j, i] = U.pdf(np.array([xv[j, i], yv[j, i]]))
 
     # np.savetxt('density2d.csv', z.reshape(n * n, 3), delimiter=' ')
 