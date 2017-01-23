9527a1e783140f7fea6ae9fec63c3a6c81e9293d
Michael Lettrich <m.lettrich@tum.de>
2017-01-23 19:41:26 +0100

    DataSourceBuilder documentation and slight refactoring

--- datadriven/src/sgpp/datadriven/datamining/builder/DataSourceBuilder.hpp
+++ datadriven/src/sgpp/datadriven/datamining/builder/DataSourceBuilder.hpp
@@ -20,22 +20,78 @@
 namespace sgpp {
 namespace datadriven {
 
+/**
+ * Generate an instance of #sgpp::datadriven::DataSource using the Builder Pattern.
+ */
 class DataSourceBuilder {
  public:
-  DataSourceBuilder();
-  virtual ~DataSourceBuilder();
+  /**
+   * Default constructor
+   */
+  DataSourceBuilder() = default;
+
+  /**
+   * Optionally specify a valid path to a file that should be read if files are used.
+   * @param filePath valid path to a file that should be read by the data source.
+   * @return Reference to this object, used for chaining.
+   */
   DataSourceBuilder& withPath(const std::string& filePath);
+
+  /**
+   * Optionally Specify if the file used is gz compressed. If data source does not use any files,
+   * this is set to false by default.
+   * @param isCompressed true if the file is compressed, false otherwise.
+   * @return Reference to this object, used for chaining.
+   */
   DataSourceBuilder& withCompression(bool isCompressed);
-  DataSourceBuilder& withFileType(const std::string& fileType);
+
+  /**
+   * Optionally Specify the file type if files are used. If data source does not use any files,
+   * this is set to none by default.
+   * @param fileType value of
+   * @return Reference to this object, used for chaining.
+   */
+  DataSourceBuilder& withFileType(DataSourceFileType& fileType);
+
+  /**
+   * Optionally Specify the amount of batches if batch learning is used. If no batch learning is
+   * used, all data is returned as a single batch (same as howMany=1).
+   * @param howMany amount of batches used in batch learning scenario.
+   * @return Reference to this object, used for chaining.
+   */
   DataSourceBuilder& inBatches(size_t howMany);
+
+  /**
+   * Optionally Specify the batch size if batch learning is used. If no batch learning is used this
+   * value defaults to 0 (all samples).
+   * @param batchSize size of batches used in batch learning scenario.
+   * @return Reference to this object, used for chaining.
+   */
   DataSourceBuilder& withBatchSize(size_t batchSize);
-  DataSource* assemble();
+
+  /**
+   * Based on the currently specified configuration, build and configure an instance of a data
+   * source object.
+   * @return Fully configured instance of #sgpp::datadriven::DataSource object.
+   */
+  DataSource* assemble() const;
+
+  /**
+   * Factory method used to build an instance of a #sgpp::datadriven::DataSource object based on the
+   * passed configuration.
+   * @return Fully configured instance of #sgpp::datadriven::DataSource object.
+   */
   DataSource* fromConfig(const DataSourceConfig& config);
 
  private:
+  /**
+   * Extract file type and compression based on the file extensions.
+   */
   void grabTypeInfoFromFilePath();
-  const std::string gz = "gz";
 
+  /**
+   * Current state of the object is stored inside this configuration object.
+   */
   DataSourceConfig config;
 };
 
