78d9b6d3b485fb1e4e2a9267846cdb269e64dd89
Michael Lettrich <m.lettrich@tum.de>
2017-01-24 20:53:25 +0100

    renaming metric operator() to measure

--- datadriven/src/sgpp/datadriven/datamining/modules/scoring/MSE.cpp
+++ datadriven/src/sgpp/datadriven/datamining/modules/scoring/MSE.cpp
@@ -17,7 +17,7 @@ namespace datadriven {
 
 Metric* MSE::clone() const { return new MSE(*this); }
 
-double MSE::operator()(const DataVector& predictedValues, const DataVector& trueValues) const {
+double MSE::measure(const DataVector& predictedValues, const DataVector& trueValues) const {
   DataVector tmp(predictedValues);
   tmp.sub(trueValues);
   const double error = tmp.l2Norm();
