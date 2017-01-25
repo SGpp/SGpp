3b188051c459d446a605768cb40261f1e3aac8fe
Michael Lettrich <m.lettrich@tum.de>
2017-01-25 12:04:45 +0100

    More documentation for DataSource and DataSourceConfig.

--- datadriven/src/sgpp/datadriven/datamining/modules/dataSource/DataSource.hpp
+++ datadriven/src/sgpp/datadriven/datamining/modules/dataSource/DataSource.hpp
@@ -25,37 +25,50 @@ namespace datadriven {
 class DataSourceIterator;
 
 /**
- * Configurable high level object that controlls
+ * DataSource is a high level, easy to use interface for accessing data provided by a all kinds of
+ * #sgpp::datadriven::SampleProvider. Should be used by end users.
  */
 class DataSource {
  public:
   /**
-   *
-   * @param config
-   * @param sampleProvider
+   * Constructor
+   * @param config configuration object used for the data source
+   * @param sampleProvider the sample provider to operate on.
    */
   DataSource(DataSourceConfig config, SampleProvider* sampleProvider);
 
   /**
-   *
+   * Read only access to the configuration used by DataSource and underlying SampleProvider.
+   * @return Current configuration object.
    */
   const DataSourceConfig& getConfig() const;
 
   /**
-   *
+   * Request data from the underlying SampleProvider as specified in the provided configuration
+   * object upon construction.
+   * @return #sgpp::datadriven::Dataset containing requested amount of samples (if available).
    */
   Dataset* getNextSamples();
+
   /**
-   *
+   * Return an iterator object pointing to the first batch of this DataSource. Can be used to obtain
+   * new batches in batch learning scenarios as often as specified inside the configuration. Allows
+   * convenient range based for loops for batch learning.
+   * @return iterator object pointing to the first batch.
    */
   DataSourceIterator begin();
+
   /**
-   *
+   * Return an iterator object pointing to the last possible batch of this DataSource. Required for
+   * range based for loops.
+   * @return iterator object pointing to the last possible batch.
    */
   DataSourceIterator end();
 
   /**
-   *
+   * Return how many batches have already been requested from this DataSource. Required for
+   * range based for loops using the DataSourceIterator.
+   * @return the amount of batches that have already been requested.
    */
   size_t getCurrentIteration() const;
 
@@ -71,7 +84,7 @@ class DataSource {
   size_t currentIteration;
 
   /**
-   * pointer to sample provider that actually returns the data.
+   * pointer to sample provider that actually handles data aquisition.
    */
   std::unique_ptr<SampleProvider> sampleProvider;
 };
