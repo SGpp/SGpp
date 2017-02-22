cf4a9b7862bcab2db514a524ef88fdaaa7d8051e
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2017-02-22 20:05:27 +0100

    Updated mpi example

--- datadriven/examplesMPI/mpi_examples.cpp
+++ datadriven/examplesMPI/mpi_examples.cpp
@@ -27,8 +27,10 @@
 // arg 2: Dataset
 // arg 3: Gridlevel
 int main(int argc, char *argv[]) {
-  // Init MPI enviroment - always has to be done first
+  // Init MPI enviroment - always has to be done first - capture slaves
   sgpp::datadriven::clusteringmpi::MPIEnviroment::init(argc, argv, true);
+
+  // Measure times
   std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
   start = std::chrono::system_clock::now();
 
@@ -46,22 +48,8 @@ int main(int argc, char *argv[]) {
   sgpp::base::DataMatrix& dataset = data.getData();
   int dim = data.getDimension();
   int level = std::stoi(argv[3]);
-  // int packagesize_master = std::stoi(argv[3]);
-  // int packagesize_leut = std::stoi(argv[4]);
 
-  // Create Sample config
-  // sgpp::base::OperationConfiguration conf = sgpp::datadriven::clusteringmpi::
-  //     MPIEnviroment::createMPIConfiguration(2, 4);
-  // conf.serialize("testconf.cfg");
-  // sgpp::base::OCLOperationConfiguration ocl_conf("MyOCLConf.cfg");
-  // sgpp::base::OperationConfiguration conf_ocl = sgpp::datadriven::clusteringmpi::
-  //     MPIEnviroment::createMPIConfiguration(2,
-  //                                           ocl_conf);
-  // conf_ocl.serialize("testconf2.cfg");
-
-  // MPI_Init(&argc, &argv);
   sgpp::base::OperationConfiguration network_conf(argv[1]);
-  //testnode["PREFERED_PACKAGESIZE"].setInt(packagesize_master);
   sgpp::datadriven::clusteringmpi::MPIEnviroment::connect_nodes(network_conf);
 
   // Create Grid
@@ -73,64 +61,35 @@ int main(int argc, char *argv[]) {
   std::cin.get();
 
   sgpp::datadriven::clusteringmpi::OperationDensityMultMPI mult_op(*grid, 0.001);
-
   sgpp::base::DataVector alpha(gridsize);
   sgpp::base::DataVector result(gridsize);
   alpha.setAll(1.0);
-  mult_op.mult(alpha, result);
-  std::cout << std::endl << std::endl;
-  for (size_t i = 0; i < 100; ++i) {
-    std::cout << result[i] << " ";
-  }
-  std::cout << std::endl << std::endl;
 
   // Create right hand side vector
   sgpp::base::DataVector rhs(gridsize);
   sgpp::datadriven::clusteringmpi::OperationDensityRhsMPI rhs_op(*grid, dataset);
   rhs_op.generate_b(rhs);
-  for (auto i = 0; i < 100; ++i) {
-    std::cout << rhs[i] << " ";
-  }
 
   // Solve for alpha vector via CG solver
   alpha.setAll(1.0);
   sgpp::solver::ConjugateGradients solver(1000, 0.001);
-  //sgpp::datadriven::clusteringmpi::OperationDensityMultMPI mult_op(*grid, 0.001);
   solver.solve(mult_op, alpha, rhs, false, true);
   double max = alpha.max();
   double min = alpha.min();
-
   for (size_t i = 0; i < gridsize; i++)
     alpha[i] = alpha[i]*1.0/(max-min);
-  end = std::chrono::system_clock::now();
-
-  // Write result into file
-  // std::ofstream ofs;
-  // ofs.open (argv[5], std::ofstream::out | std::ofstream::app);
-  // ofs << dim << ";" << level << ";" << packagesize_master << ";" << packagesize_leut
-  //     << ";" << elapsed_seconds.count() << ";"
-  //     << gridsize % ((sgpp::datadriven::clusteringmpi::MPIEnviroment::get_node_count() -1) *
-  //                    packagesize_master)<< std::endl;
-  // ofs.close();
 
   // Create and prune knn graph
   std::cout << "Graph Creation/Pruning:" << std::endl;
   sgpp::datadriven::clusteringmpi::OperationPrunedGraphCreationMPI graph_op(*grid, alpha,
-                                                                            dataset, 12, 0.7);
+                                                                            dataset, 12, 1.4);
   std::vector<int> knn_graph;
   graph_op.create_graph(knn_graph);
-  std::cout << "knn graph size: " << knn_graph.size() / 12 << std::endl;
-  for (size_t i = 0; i < 100; ++i) {
-    for (size_t node = 0; node < 12; ++node) {
-      std::cout << knn_graph[i * 12 + node] << " ";
-    }
-    std::cout << "\n";
-  }
   sgpp::datadriven::DensityOCLMultiPlatform::
       OperationCreateGraphOCL::find_clusters(knn_graph, 12);
 
-
   // Calc time
+  end = std::chrono::system_clock::now();
   std::chrono::duration<double> elapsed_seconds = end-start;
   std::time_t end_time = std::chrono::system_clock::to_time_t(end);
   std::cout << "finished computation at " << std::ctime(&end_time)
