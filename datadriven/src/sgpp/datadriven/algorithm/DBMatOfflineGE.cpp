d572d94537ddee01612c34c2c55cbb8c6c1ba3e3
Michael Lettrich <m.lettrich@tum.de>
2017-03-02 15:02:25 +0100

    DBMatOfflineGE as superclass for all GE based decompositions
    ###Base for 
    * LU
    * Chol
    * iChol

---
+++ datadriven/src/sgpp/datadriven/algorithm/DBMatOfflineGE.cpp
@@ -0,0 +1,47 @@
+/*
+ * DBMatOfflineGE.cpp
+ *
+ *  Created on: 02.03.2017
+ *      Author: michael
+ */
+
+#include <sgpp/datadriven/algorithm/DBMatOfflineGE.hpp>
+
+#include <sgpp/base/exception/operation_exception.hpp>
+
+#include <list>
+
+namespace sgpp {
+namespace datadriven {
+
+using sgpp::base::operation_exception;
+using sgpp::base::DataMatrix;
+
+DBMatOfflineGE::DBMatOfflineGE(DBMatDensityConfiguration& oc) : DBMatOffline(oc) {}
+
+void DBMatOfflineGE::buildMatrix() {
+  // build matrix
+  DBMatOffline::buildMatrix();
+
+  // then add regularization term
+  auto size = grid->getStorage().getSize();
+
+  // Construct matrix lambda * C (just use identity for C)
+  DataMatrix lambdaC(size, size);
+  if (config.regularization_ == RegularizationType::Identity) {
+    lambdaC.setAll(0.);
+    for (size_t i = 0; i < size; i++) {
+      lambdaC.set(i, i, config.lambda_);
+    }
+  } else {
+    throw operation_exception("Unsupported regularization type");
+  }
+
+  // Compute A + lambda * C:
+  lhsMatrix.add(lambdaC);
+
+  isConstructed = true;
+}
+
+} /* namespace datadriven */
+} /* namespace sgpp */
---
+++ datadriven/src/sgpp/datadriven/algorithm/DBMatOfflineGE.hpp
@@ -0,0 +1,29 @@
+/*
+ * DBMatOfflineIGE.hpp
+ *
+ *  Created on: 02.03.2017
+ *      Author: michael
+ */
+
+#pragma once
+
+#include <sgpp/datadriven/algorithm/DBMatOffline.hpp>
+
+namespace sgpp {
+namespace datadriven {
+
+class DBMatOfflineGE : public DBMatOffline {
+  DBMatOfflineGE() = default;
+
+  explicit DBMatOfflineGE(DBMatDensityConfiguration& oc);
+
+  /**
+   * Builds the right hand side matrix with or without the regularization term
+   * depending
+   * on the type of decomposition
+   */
+  void buildMatrix() override;
+};
+
+} /* namespace datadriven */
+} /* namespace sgpp */
