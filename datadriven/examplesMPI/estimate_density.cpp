da4b0bdab8d3304be529e2ae0ca04a66ed5080b5
David Pfander <David.Pfander@ipvs.uni-stuttgart.de>
2017-03-02 14:55:07 +0100

    added more timing information

--- datadriven/examplesMPI/estimate_density.cpp
+++ datadriven/examplesMPI/estimate_density.cpp
@@ -112,53 +112,7 @@ int main(int argc, char *argv[]) {
                 << std::endl;
   }
 
-  double max = alpha.max();
-  double min = alpha.min();
-  for (size_t i = 0; i < gridsize; i++)
-    alpha[i] = alpha[i] * 1.0 / (max - min);
 
-  // Create and prune knn graph
-  std::chrono::time_point<std::chrono::high_resolution_clock> create_knn_start,
-      create_knn_end;
-  create_knn_start = std::chrono::system_clock::now();
-  std::cout << "Graph Creation/Pruning:" << std::endl;
-  sgpp::datadriven::clusteringmpi::OperationPrunedGraphCreationMPI graph_op(
-      *grid, alpha, dataset, 12, 0.2, "MyOCLConf.cfg");
-  std::vector<int> knn_graph;
-  graph_op.create_graph(knn_graph);
-
-  create_knn_end = std::chrono::system_clock::now();
-  if (rank == 0) {
-      std::cout << "create knn operation duration: "
-                << std::chrono::duration_cast<std::chrono::seconds>(create_knn_end - create_knn_start).count() << "s" << std::endl;
-  }
-
-  std::chrono::time_point<std::chrono::high_resolution_clock>
-      find_clusters_start, find_clusters_end;
-  find_clusters_start = std::chrono::system_clock::now();
-
-  std::vector<size_t> cluster_assignments =
-      sgpp::datadriven::DensityOCLMultiPlatform::OperationCreateGraphOCL::
-          find_clusters(knn_graph, 12);
-
-  find_clusters_end = std::chrono::system_clock::now();
-  if (rank == 0) {
-      std::cout << "find clusters duration: "
-                << std::chrono::duration_cast<std::chrono::seconds>(find_clusters_end - find_clusters_start).count() << "s"
-                << std::endl;
-  }
-
-  std::ofstream out("cluster_erg.txt");
-  for (size_t datapoint : cluster_assignments) {
-    out << datapoint << " ";
-  }
-
-  // Calc time
-  end = std::chrono::system_clock::now();
-  std::chrono::duration<double> elapsed_seconds = end - start;
-  std::time_t end_time = std::chrono::system_clock::to_time_t(end);
-  std::cout << "finished computation at " << std::ctime(&end_time)
-            << "elapsed time: " << elapsed_seconds.count() << "s\n";
   // Cleanup MPI enviroment
   sgpp::datadriven::clusteringmpi::MPIEnviroment::release();
   return 0;
