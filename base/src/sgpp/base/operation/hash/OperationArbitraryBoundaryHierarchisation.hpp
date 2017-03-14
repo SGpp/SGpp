f209f0d19590d073212dfa6e81eb8fbab402672f
Fabian Franzelin <fabian.franzelin@ipvs.uni-stuttgart.de>
2017-03-14 21:11:58 +0100

    doxygen warnings removed

--- base/src/sgpp/base/operation/hash/OperationArbitraryBoundaryHierarchisation.hpp
+++ base/src/sgpp/base/operation/hash/OperationArbitraryBoundaryHierarchisation.hpp
@@ -35,7 +35,7 @@ class OperationArbitraryBoundaryHierarchisation : public OperationHierarchisatio
   /**
    * Implements the hierarchisation on a sparse grid
    *
-   * @param node_values the function's values in the nodal basis
+   * @param nodal_values the function's values in the nodal basis
    */
   void doHierarchisation(DataVector& nodal_values);
 
