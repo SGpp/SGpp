5bea4591bb998ff0a67e95497be2202cc1de09cf
David Pfander <David.Pfander@ipvs.uni-stuttgart.de>
2017-04-19 16:59:31 +0200

    More doxygen fixes

--- datadriven/src/sgpp/datadriven/operation/hash/OperationMultipleEvalSubspace/combined/OperationMultipleEvalSubspaceCombined.hpp
+++ datadriven/src/sgpp/datadriven/operation/hash/OperationMultipleEvalSubspace/combined/OperationMultipleEvalSubspaceCombined.hpp
@@ -91,24 +91,78 @@ class OperationMultipleEvalSubspaceCombined : public AbstractOperationMultipleEv
  public:
 #include "OperationMultipleEvalSubspaceCombined_calculateIndexCombined.hpp"
 
+  /**
+   * Creates a new instance of the OperationMultipleEvalSubspaceCombined class.
+   *
+   * @param grid grid to be evaluated
+   * @param dataset set of evaluation points
+   */
   OperationMultipleEvalSubspaceCombined(sgpp::base::Grid& grid, sgpp::base::DataMatrix& dataset);
 
+  /**
+   * Destructor
+   */
   ~OperationMultipleEvalSubspaceCombined();
 
+  /**
+   * Updates the internal data structures to reflect changes to the grid, e.g. due to refinement.
+   *
+   */
   void prepare() override;
 
+  /**
+   * Internal eval operator, should not be called directly.
+   *
+   * @see OperationMultipleEval
+   *
+   * @param alpha surplusses of the grid
+   * @param result will contain the evaluation results for the given range.
+   * @param start_index_data beginning of the range to evaluate
+   * @param ebd_index_data end of the range to evaluate
+   */
   void multTransposeImpl(sgpp::base::DataVector& alpha, sgpp::base::DataVector& result,
                          const size_t start_index_data, const size_t end_index_data) override;
 
+  /**
+   * Internal mult operator, should not be called directly.
+   *
+   * @see OperationMultipleEval
+   *
+   * @param source source operand for the operator
+   * @param result stores the result
+   * @param start_index_data beginning of the range to process
+   * @param ebd_index_data end of the range to process
+   */
   void multImpl(sgpp::base::DataVector& source, sgpp::base::DataVector& result,
                 const size_t start_index_data, const size_t end_index_data) override;
 
+  /**
+   * Pads the dataset.
+   *
+   * @param dataset dataset to be padded
+   * @result padded dataset
+   */
   sgpp::base::DataMatrix* padDataset(sgpp::base::DataMatrix& dataset);
 
+  /**
+   * Alignment required by the vector instruction set SG++ is compiled with.
+   *
+   * @result alignment requirement
+   */
   size_t getAlignment() override;
 
+  /**
+   * Name of the implementation, useful for benchmarking different implementation approaches.
+   *
+   * @result name of the implementation
+   */
   std::string getImplementationName() override;
 
+  /**
+   * Size of the dataset after padding.
+   *
+   * @result size of the padded dataset>
+   */
   size_t getPaddedDatasetSize() override;
 };
 }
