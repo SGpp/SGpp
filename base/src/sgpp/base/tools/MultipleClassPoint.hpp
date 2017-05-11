79b687cb1b604a4ff39f92d022df3c5ab6329f35
katrin <katrin@SGpp-VM>
2017-05-11 17:02:31 +0200

    last commit before merge preperations

--- base/src/sgpp/base/tools/MultipleClassPoint.hpp
+++ base/src/sgpp/base/tools/MultipleClassPoint.hpp
@@ -15,33 +15,39 @@ namespace sgpp {
 namespace base {
 class MultipleClassPoint{
     public:
-        explicit MultipleClassPoint(int classes);
+        explicit MultipleClassPoint(size_t classes);
         MultipleClassPoint(base::HashGridPoint& gp, std::vector<base::Grid*> grids,
                     std::vector<base::DataVector*> alphas);
         virtual ~MultipleClassPoint() {};
 
-        int getDominateClass() const;
-        double getDensity(int classId) const;
-        void updateClass(int classId, double newDen, bool hasPoint);
+        size_t getDominateClass() const;
+        double getDensity(size_t classId) const;
+        void updateClass(size_t classId, double newDen, bool hasPoint);
 
-        void addNeighbor(int neighbor, size_t dim, bool isLeft);
-        std::vector<std::tuple<int, size_t, bool>> getNeighbors();
+        void addNeighbor(size_t neighbor, size_t dim, bool isLeft);
+        std::vector<std::tuple<size_t, size_t, bool>> getNeighbors() const;
+
+        void addBorder(size_t dim, size_t level, bool isLeft);
+        double getBorderScore() const;
+        std::vector<std::tuple<size_t, size_t, bool>> getBorders() const;
 
         void resortClasses();
-        bool isClassSet(int classId);
-        std::vector<std::tuple<double, int, bool>> getTopClasses(double percent);
+        bool isClassSet(size_t classId) const;
+        std::vector<std::tuple<double, size_t, bool>> getTopClasses(double percent) const;
 
         std::string toString();
 
     private:
         int classes;
         // tuple: density, classId, points exits in class
-        std::vector<std::tuple<double, int, bool> *> classById;
-        std::vector<std::tuple<double, int, bool>> classByDensity;
-        // sequence number and dimension of neighbors with a change in dominate classes
-        std::vector<std::tuple<int, size_t, bool>> neighbors;
+        std::vector<std::tuple<double, size_t, bool> *> classById;
+        std::vector<std::tuple<double, size_t, bool>> classByDensity;
+        // sequence number and dimension of neighbors with a change in dominate classes, isLeft
+        std::vector<std::tuple<size_t, size_t, bool>> neighbors;
+        // dimension, level of point, direction isLeft
+        std::vector<std::tuple<size_t, size_t, bool>> borders;
 
-        void insertDensitySorted(std::tuple<double, int, bool>* density);
+        void insertDensitySorted(std::tuple<double, size_t, bool>* density);
 };
 
 
--- datadriven/examples/multipleClassExample.cpp
+++ datadriven/examples/multipleClassExample.cpp
@@ -18,10 +18,14 @@
 
 
 #include <cmath>
+#include <ctime>
 #include <sstream>
 #include <iomanip>
 #include <string>
 #include <vector>
+#include <locale>
+#include <chrono>
+#include <sys/resource.h>
 
 #include "../../base/src/sgpp/base/tools/MultipleClassPoint.hpp"
 
@@ -38,7 +42,8 @@ sgpp::datadriven::LearnerSGDE createSGDELearner(size_t dim, size_t level,
 std::vector<std::string> doClassification(std::vector<sgpp::base::Grid*> grids,
                                           std::vector<sgpp::base::DataVector*> alphas,
                                           sgpp::base::DataMatrix& testData,
-                                          sgpp::base::DataVector& testLabel);
+                                          sgpp::base::DataVector& testLabel,
+                                          size_t classes);
 /**
  * TODO
  * \page example_classificationRefinementExample_cpp Classification Example
@@ -53,40 +58,95 @@ std::vector<std::string> doClassification(std::vector<sgpp::base::Grid*> grids,
  */
 
 
-int main() {
-
-    std::string filename2 = "../../datadriven/tests/data/mulitpleClassesTest2.arff";
+int main(int argc, char* argv[]) {
+/*
+    std::string filename2 = "mulitpleClassesTest";
+    size_t classes = 4;
+    size_t dim = 2;
+    size_t level = 3;
+    double lambda = 1e-2;
+    size_t numSteps = 5;
+    size_t numRefinements = 3;
+    size_t partCombined = 0;
+//*/
+/*
+    std::string filename2 = "mulitpleClassesTest2";
+    size_t classes = 4;
+    size_t dim = 2;
+    size_t level = 3;
+    double lambda = 1e-2;
+    size_t numSteps = 6;
+    size_t numRefinements = 4;
+    size_t partCombined = 1;
+//*/
+///*
+    std::string filename2 = "starsgalaxies";
+    size_t classes = 3;
+    size_t dim = 4;
+    size_t level = 5;
+    double lambda = 1e-7;
+    size_t numSteps = 8;
+    size_t numRefinements = 4;
+    size_t partCombined = 2;
+//*/
+/*
+    std::string filename2 = "star_test";
+    size_t classes = 2;
+    size_t dim = 4;
+    size_t level = 3;
+    double lambda = 1e-5;
+    size_t numSteps = 8;
+    size_t numRefinements = 3;
+    size_t partCombined = 0;
+//*/
+    bool useZCRF = false;
+    if ( argc == 4 ) {
+        useZCRF = std::atoi(argv[1]) == 1;
+        numRefinements = std::atoi(argv[2]);
+        partCombined = std::atoi(argv[3]);
+    }
+    // additional star_test.arff star_train.arff
+    
+    //------------- only calculation after -------------------------
+    std::time_t starttime = std::time(nullptr);
+    
     sgpp::datadriven::Dataset dataset =
-            sgpp::datadriven::ARFFTools::readARFF(filename2);
+            sgpp::datadriven::ARFFTools::readARFF(
+            "../../datadriven/tests/data/" + filename2 + ".arff");
     sgpp::base::DataMatrix dataTrain = dataset.getData();
     sgpp::base::DataVector targetTrain = dataset.getTargets();
+
+/*
+    // write log to file
+    std::string filename = "/home/katrin/Desktop/log_"
+            + filename2 + "_" + std::to_string(numRefinements)
+            + "_" + std::to_string(partCombined)
+            + "_" + std::to_string(useZCRF) +  ".log";
+    std::ofstream out(filename);
+    std::cout.rdbuf(out.rdbuf());
+    */
+
     std::cout << "Read training data: " << dataTrain.getNrows() << std::endl;
-    int classes = 4;
 
     std::vector<sgpp::base::DataMatrix> dataCl;
     std::vector<sgpp::datadriven::LearnerSGDE> learner;
-    for ( int i = 0 ; i < classes ; i++ ) {
+    for ( size_t i = 0 ; i < classes ; i++ ) {
         dataCl.push_back(sgpp::base::DataMatrix(0.0, dataTrain.getNcols()));
     }
 
     sgpp::base::DataVector row(dataTrain.getNcols());
     for ( size_t i = 0 ; i < dataTrain.getNrows() ; i++ ) {
         dataTrain.getRow(i, row);
-        dataCl.at((int)targetTrain.get(i)).appendRow(row);
-    }
-    for ( int i = 0 ; i < classes ; i++ ) {
-        std::cout << "Data points of class " << i << ": ";
-        std::cout << dataCl.at(i).getNrows() << std::endl;
+        dataCl.at((size_t)targetTrain.get(i)).appendRow(row);
     }
 
     /**
     * Approximate a probability density function for the class data using
     * LearnerSGDE, one for each class. Initialize the learners with the data
     */
-    size_t dim = 2;
-    int level = 3;
-    double lambda = 1e-1;//größer (start 5)
-    for ( int i = 0 ; i < classes ; i++ ) {
+    for ( size_t i = 0 ; i < classes ; i++ ) {
+        std::cout << "Data points of class " << std::setw(3) << std::right << i << ": ";
+        std::cout << std::setw(14) << std::right << dataCl.at(i).getNrows() << "   | ";
         learner.push_back(createSGDELearner(dim, level, lambda));
         learner.back().initialize(dataCl.at(i));
     }
@@ -97,55 +157,127 @@ int main() {
      */
     std::vector<sgpp::base::Grid*> grids2;
     std::vector<sgpp::base::DataVector*> alphas2;
-    for ( int i = 0 ; i < classes ; i++ ) {
+    for ( size_t i = 0 ; i < classes ; i++ ) {
         grids2.push_back(learner.at(i).getGrid().get());
         alphas2.push_back(learner.at(i).getSurpluses().get());
     }
-    for ( int i = 0 ; i < classes ; i++ ) {
+
+    std::time_t midtime = std::time(nullptr);
+    std::cout << "needed time to read data: " << midtime - starttime  << "s" << std::endl;
+    std::cout << "Start refining" << std::endl;
+
+    std::cout << "DATA SETTINGS:" << std::endl;
+    std::cout << "  filename:    " << filename2 << std::endl;
+    std::cout << "  classes:     " << classes << std::endl;
+    std::cout << "  dimensions:  " << dim << std::endl;
+    std::cout << "  start level: " << level << std::endl;
+    std::cout << "  lambda:      " << lambda << std::endl;
+    std::cout << "  numRef:      " << numRefinements << std::endl;
+    std::cout << "    partComb:  " << partCombined << std::endl;
+    if ( useZCRF ) {
+        std::cout << "Using the ZeroCrossingRefinementFunctor" << std::endl;
+    } else {
+        std::cout << "Using the MultipleClassRefinementFunctor" << std::endl;
+    }
+    std::cout << "---------------------------------------------" << std::endl;
+
+    auto c_start = std::chrono::high_resolution_clock::now();
+    auto c_ref = std::chrono::high_resolution_clock::now();
+    std::cout << "Sizes:";
+    for (size_t i = 0; i < classes ; i++) {
+        std::cout << " (" << i << "->" << grids2.at(i)->getStorage().getSize() << ") ";
+    }
+    std::cout << std::endl;
+    auto c_learn = std::chrono::high_resolution_clock::now();
+    for ( size_t i = 0 ; i < classes ; i++ ) {
         learner.at(i).train(*grids2.at(i), *alphas2.at(i), dataCl.at(i), lambda);
     }
-
-    size_t numSteps = 5;
-    std::vector<std::string> eval = doClassification(grids2, alphas2, dataTrain, targetTrain);
-
-    std::cout << std::endl << "Start" << std::endl;
-    size_t numRefinements = 3;
-    // amount of points refined in the single classes vs combined grid
-    // single classes: numRefinements - partCombined
-    // multigrid: partCombined
-    size_t partCombined = 0;
-    bool levelPenalize = false;
-    bool preCompute = false;
-    double thresh = 0; // .001;
-
-    sgpp::datadriven::MultipleClassRefinementFunctor mcrf(grids2, alphas2,
-            numRefinements, levelPenalize, preCompute, thresh);
-    sgpp::datadriven::MultiGridRefinementFunctor* multifun = &mcrf;
-    std::cout << std::endl;
-
-    std::cout << "Size: " << mcrf.getCombinedGrid()->getStorage().getSize() << std::endl;
-    std::cout << "Dim: " << mcrf.getCombinedGrid()->getDimension() << std::endl;
-    std::cout << std::endl;
-
-    std::cout << "Evaluation:" << std::endl << std::endl;
-    std::cout << "------------------------------" << std::endl;
+    auto c_eval = std::chrono::high_resolution_clock::now();
+    std::vector<std::string> eval = doClassification(grids2, alphas2,
+                    dataTrain, targetTrain, classes);
     std::cout << "   0   | " << eval.at(0) << " | " << eval.at(1) << std::endl;
+    auto c_end = std::chrono::high_resolution_clock::now();
+    std::cout << "time: 0: sum: "
+          << std::chrono::duration_cast<std::chrono::milliseconds>(c_end-c_start).count()
+          << " ms  | step: "
+          << std::chrono::duration_cast<std::chrono::milliseconds>(c_end-c_ref).count()
+          << " ms  | ref: "
+          << std::chrono::duration_cast<std::chrono::milliseconds>(c_learn-c_ref).count()
+          << " ms  | learn: "
+          << std::chrono::duration_cast<std::chrono::milliseconds>(c_eval-c_learn).count()
+          << " ms  | eval: "
+          << std::chrono::duration_cast<std::chrono::milliseconds>(c_end-c_eval).count()
+          << " ms" << std::endl;
+    // amount of points refined in the single classes vs combined grid
+    // class grid: numRefinements - partCombined
+    // combined grid: partCombined
+    bool levelPenalize = true;
+    bool preCompute = false;
+    double thresh = 0;
+    
+    // Zero-crossing-based refinement
+    sgpp::datadriven::MultiGridRefinementFunctor* fun = nullptr;
+    sgpp::datadriven::ZeroCrossingRefinementFunctor funZrcr(grids2, alphas2,
+            numRefinements, levelPenalize, preCompute);
+    fun = &funZrcr;
+
+    sgpp::datadriven::MultipleClassRefinementFunctor* multifun = nullptr;
+    sgpp::datadriven::MultipleClassRefinementFunctor mcrf(grids2, alphas2,
+            numRefinements, thresh);
+    multifun = &mcrf;
+
     for ( size_t i = 1; i < numSteps + 1; i++ ) {
-        mcrf.refine(partCombined);
-        std::cout << "refined: " << i << std::endl;
-        // Re-train the grids. This has to happend AFTER all grids are refined
-        for ( int i = 0 ; i < classes ; i++ ) {
-            learner.at(i).train(*grids2.at(i), *alphas2.at(i), dataCl.at(i), lambda);
+        std::cout << "---------------------------------------------" << std::endl;
+        c_ref = std::chrono::high_resolution_clock::now();
+        if ( useZCRF ) {
+            for ( size_t i = 0 ; i < classes ; i++ ) {
+                fun->setGridIndex(i);
+                grids2.at(i)->getGenerator().refine(*fun);
+            }
+        } else {
+            multifun->refine(partCombined);
+        }
+        std::cout << "Sizes:";
+        for (size_t i = 0; i < classes ; i++) {
+            std::cout << " (" << i << "->" << grids2.at(i)->getStorage().getSize() << ") ";
         }
         std::cout << std::endl;
-        eval = doClassification(grids2, alphas2, dataTrain, targetTrain);
+
+        c_learn = std::chrono::high_resolution_clock::now();
+        // Re-train the grids. This has to happend AFTER all grids are refined
+        for ( size_t i = 0 ; i < classes ; i++ ) {
+            learner.at(i).train(*grids2.at(i), *alphas2.at(i), dataCl.at(i), lambda);
+        }
+
+        c_eval = std::chrono::high_resolution_clock::now();
+        eval = doClassification(grids2, alphas2, dataTrain, targetTrain, classes);
         std::cout << "   " << i << "   | " << eval.at(0) << " | " << eval.at(1)
              << std::endl;
+
+        c_end = std::chrono::high_resolution_clock::now();
+        std::cout << "time: " << i << ": sum: "
+              << std::chrono::duration_cast<std::chrono::milliseconds>(c_end-c_start).count()
+              << " ms  | step: "
+              << std::chrono::duration_cast<std::chrono::milliseconds>(c_end-c_ref).count()
+              << " ms  | ref: "
+              << std::chrono::duration_cast<std::chrono::milliseconds>(c_learn-c_ref).count()
+              << " ms  | learn: "
+              << std::chrono::duration_cast<std::chrono::milliseconds>(c_eval-c_learn).count()
+              << " ms  | eval: "
+              << std::chrono::duration_cast<std::chrono::milliseconds>(c_end-c_eval).count()
+              << " ms" << std::endl;
     }
 
-    // double totalScore = multifun->getTotalRefinementValue(mcrf.getCombinedGrid()->getStorage());
-    // std::cout << "TotalRefinementValue: " << totalScore << std::endl;
-
+    // if (dim == 2) {
+    //     multifun->printPointsPlott();
+    // }
+    // multifun->printPointsPlott();
+    struct rusage usage;
+    getrusage(RUSAGE_SELF, &usage);
+    std::cout << "rusage: memory: " << usage.ru_maxrss
+        << " kB  | time user: " << usage.ru_utime.tv_sec
+        << " s  | time system: " << usage.ru_stime.tv_sec
+        << " s" << std::endl;
     return 0;
 }
 
@@ -199,8 +331,9 @@ sgpp::datadriven::LearnerSGDE learner(gridConfig, adaptConfig,
 std::vector<std::string> doClassification(std::vector<sgpp::base::Grid*> grids,
                   std::vector<sgpp::base::DataVector*> alphas,
                   sgpp::base::DataMatrix& testData,
-                  sgpp::base::DataVector& testLabel) {
-    double best_eval = 0.0;
+                  sgpp::base::DataVector& testLabel,
+                  size_t classes) {
+    double best_eval = -1000.0;
     double eval = 0.0;
     sgpp::base::DataVector p(testData.getNcols());
     sgpp::base::DataVector indices(testData.getNrows());
@@ -213,12 +346,10 @@ std::vector<std::string> doClassification(std::vector<sgpp::base::Grid*> grids,
     }
 
     // Get predictions and save to evals (confidence) and indices (class)
+    std::vector<std::vector<int> > gridEval(classes+1, std::vector<int>(classes+1, 0));
     for (size_t i = 0; i < testData.getNrows(); i++) {
       testData.getRow(i, p);
-      indices.set(i, 0.0);
-      best_eval = evalOps.at(0)->eval(*alphas.at(0), p);
-      evals.set(i, best_eval);
-      for (size_t j = 1; j < grids.size(); j++) {
+      for (size_t j = 0; j < grids.size(); j++) {
         eval = evalOps.at(j)->eval(*alphas.at(j), p);
         if (eval > best_eval) {
           best_eval = eval;
@@ -226,6 +357,10 @@ std::vector<std::string> doClassification(std::vector<sgpp::base::Grid*> grids,
           evals.set(i, best_eval);
         }
       }
+      // set array
+      gridEval[static_cast<size_t>(floor(testLabel.get(i)))]
+                    [static_cast<size_t>(indices.get(i))] += 1;
+      best_eval = -1000.0;
     }
 
     // Count the error for all classes
@@ -242,6 +377,22 @@ std::vector<std::string> doClassification(std::vector<sgpp::base::Grid*> grids,
       }
     }
 
+    // print array
+    std::cout << std::setw(10) << std:: left << "classes" << " | "; 
+    std::cout << std::setw(10) << std:: left << "all Points" << " | "; 
+    for (size_t a = 0; a < grids.size(); a++) {
+      std::cout << "map in " << std::setw(3) << std:: left << a << " | "; 
+    }
+    std::cout << std::setw(10) << std:: left << "wrong map" << std::endl;
+    for (size_t a = 0; a < grids.size(); a++) {
+      std::cout << " Class: " << std::setw(2) << std:: left << a << " | "; 
+      std::cout << std::setw(10) << std::right << classCounts.at(a) << " | "; 
+      for (size_t b = 0; b < grids.size(); b++) {
+        std::cout << std::setw(10) << std::right << gridEval[a][b] << " | ";
+      }
+      std::cout << std::setw(10) << std::right << classErrorCounts.at(a) << std::endl;
+    }
+
     // Format and return the classification percentages
     std::stringstream ss;
     for (size_t i = 0; i < grids.size(); i++) {
@@ -256,6 +407,7 @@ std::vector<std::string> doClassification(std::vector<sgpp::base::Grid*> grids,
     ss2 << std::fixed << std::setprecision(3);
     ss2 << (100.0 * (1.0 - (static_cast<double>(totalCount) /
                             static_cast<double>(testData.getNrows()))));
+
     std::vector<std::string> result;
     result.push_back(ss.str());
     result.push_back(ss2.str());
