99f04afa5a73094baa91ff53c0ae5bf3d4cc043b
Michael Lettrich <m.lettrich@tum.de>
2017-06-02 22:31:05 +0200

    Documentation for DBMatOfflineFactory

--- datadriven/src/sgpp/datadriven/algorithm/DBMatOfflineFactory.hpp
+++ datadriven/src/sgpp/datadriven/algorithm/DBMatOfflineFactory.hpp
@@ -15,14 +15,24 @@
 
 namespace sgpp {
 namespace datadriven {
+
+/**
+ * factories to build the specialization of the DBMatOffline objects.
+ */
 namespace DBMatOfflineFactory {
 
 /**
- * TODO(lettrich) : write documentation
+ * Based on the configuration file, build the appropriate DBMatOffline object and return it
+ * @param config Configuration file with parameters set
+ * @return new instance of DBMatOffline implementor owned by caller.
  */
-
 DBMatOffline* buildOfflineObject(const DBMatDensityConfiguration& config);
 
+/**
+ * Read a serialized DBMatOffline object and construct a new object with the information.
+ * @param fname Path to the serialized DBMatOffline object.
+ * @return new instance of DBMatOffline implementor owned by caller.
+ */
 DBMatOffline* buildFromFile(const std::string& fname);
 
 } /* namespace DBMatOfflineFactory */
