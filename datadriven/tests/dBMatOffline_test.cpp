ca6118e176936530a3a4ee300c00d00e93ec2093
Michael Lettrich <m.lettrich@tum.de>
2017-06-23 08:42:21 +0200

    removed dead test case

--- datadriven/tests/dBMatOffline_test.cpp
+++ datadriven/tests/dBMatOffline_test.cpp
@@ -18,7 +18,6 @@
 #include <sgpp/datadriven/algorithm/DBMatOfflineEigen.hpp>
 #include <sgpp/datadriven/algorithm/DBMatOfflineFactory.hpp>
 #include <sgpp/datadriven/algorithm/DBMatOfflineLU.hpp>
-#include <sgpp/datadriven/algorithm/DBMatOfflineSparseIChol.hpp>
 #include <sgpp/datadriven/application/RegularizationConfiguration.hpp>
 #include <sgpp/globaldef.hpp>
 
@@ -163,39 +162,6 @@ BOOST_AUTO_TEST_CASE(testReadWriteLU) {
     BOOST_CHECK_CLOSE(newMatrix[i], oldMatrix[i], 10e-5);
   }
 }
-
-// BOOST_AUTO_TEST_CASE(testDBMatOfflineIcholBuildMatrix) {
-//  // this test only works if decomposition checks are disabled manually.
-//
-//  sgpp::datadriven::DBMatDensityConfiguration config;
-//  config.grid_dim_ = 2;
-//  config.grid_level_ = 3;
-//  config.grid_type_ = sgpp::base::GridType::Linear;
-//  config.regularization_ = sgpp::datadriven::RegularizationType::Identity;
-//  config.lambda_ = 0.1;
-//  config.decomp_type_ = sgpp::datadriven::DBMatDecompostionType::Chol;
-//
-//  sgpp::datadriven::DBMatOfflineChol chol(config);
-//  chol.buildMatrix();
-//  auto& cholMat = chol.getDecomposedMatrix();
-//  for (size_t i = 0; i < cholMat.getNrows() - 1; i++) {
-//    for (size_t j = i + 1; j < cholMat.getNcols(); j++) {
-//      cholMat.set(i, j, 0.0);
-//    }
-//  }
-//
-//  std::cout << cholMat.toString() << "\n\n";
-//
-//  sgpp::datadriven::DBMatOfflineIChol ichol(config);
-//  ichol.buildMatrix();
-//  auto& icholMat = ichol.getDecomposedMatrix();
-//
-//  std::cout << icholMat.toString() << "\n\n";
-//
-//  for (size_t i = 0; i < cholMat.getSize(); i++) {
-//    BOOST_CHECK_CLOSE(cholMat[i], icholMat[i], 10e-5);
-//  }
-//}
 
 BOOST_AUTO_TEST_SUITE_END()
 
