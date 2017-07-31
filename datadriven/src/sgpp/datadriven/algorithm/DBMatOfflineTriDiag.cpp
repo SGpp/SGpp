fc130050d61d1f93bb3279a3d3a342496f085b6d
boschkdj <dboschko@gmail.com>
2017-07-31 14:05:39 +0200

    adding DBMatOfflineTriDiag.hpp and .cpp to src/datadriven/algorithm

---
+++ datadriven/src/sgpp/datadriven/algorithm/DBMatOfflineTriDiag.cpp
@@ -0,0 +1,1 @@
+#include <DBMatOfflineTriDiag.hpp>
---
+++ datadriven/src/sgpp/datadriven/algorithm/DBMatOfflineTriDiag.hpp
@@ -0,0 +1,9 @@
+/*
+ * This class is used to get the hessenberg decomposition of the system matrix 
+ * of the sparse grid. Also this class can invert the tridiagonal matrix in 
+ * linear time.
+*/
+
+#ifdef USE_GSL
+
+#endif /* USE_GSL */
