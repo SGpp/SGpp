a772a6f7725851eee11e812abeec3423c3239eca
Tim Waegemans <waegeman@in.tum.de>
2017-08-25 18:45:08 +0200

    minor changes

--- datadriven/examples/geomlearnerSGDEOnOffTest.cpp
+++ datadriven/examples/geomlearnerSGDEOnOffTest.cpp
@@ -33,211 +33,215 @@ using sgpp::base::DataVector;
  */
 
 int main() {
-      /**
-       * Get the training, test and validation data
-       */
-      std::string filename = "../../datasets/house_numbers/SVHN_32x32.train.arff";
-      // load training samples
-      std::cout << "# loading file: " << filename << std::endl;
-      sgpp::datadriven::Dataset trainDataset = sgpp::datadriven::ARFFTools::readARFF(filename);
+  /**
+   * Get the training, test and validation data
+   */
+  std::string filename = "../../datasets/artificialDatasets/TrianglesCircles/tricircle_0.25-0.75_16x16_800000images.train";
+  // load training samples
+  std::cout << "# loading file: " << filename << std::endl;
+  sgpp::datadriven::Dataset trainDataset = sgpp::datadriven::ARFFTools::readARFF(filename);
 
-      filename = "../../datasets/house_numbers/SVHN_32x32.test.arff";
-      // load test samples
-      std::cout << "# loading file: " << filename << std::endl;
-      sgpp::datadriven::Dataset testDataset = sgpp::datadriven::ARFFTools::readARFF(filename);
+  filename = "../../datasets/artificialDatasets/TrianglesCircles/tricircle_0.25-0.75_16x16.test";
+  // load test samples
+  std::cout << "# loading file: " << filename << std::endl;
+  sgpp::datadriven::Dataset testDataset = sgpp::datadriven::ARFFTools::readARFF(filename);
 
-      // if fixed validation data should be used (required for convergence
-      // monitor):
-      /*filename = "";  // specify file containing validation data here
-      // load validation samples
-      std::cout << "# loading file: " << filename << std::endl;
-      sgpp::datadriven::Dataset validationDataset =
-          sgpp::datadriven::ARFFTools::readARFF(filename); */
+  // if fixed validation data should be used (required for convergence
+  // monitor):
+  /*filename = "";  // specify file containing validation data here
+  // load validation samples
+  std::cout << "# loading file: " << filename << std::endl;
+  sgpp::datadriven::Dataset validationDataset =
+      sgpp::datadriven::ARFFTools::readARFF(filename); */
 
-      /**
-       * Specify the number of classes and the corresponding class labels.
-       */
-      size_t classNum = 2;
-      sgpp::base::DataVector classLabels(classNum);
-      classLabels[0] = 6;
-      classLabels[1] = 7;
-      /*
-      classLabels[2] = 2;
-      classLabels[3] = 3;
-      classLabels[4] = 4;
-      classLabels[5] = 5;
-      classLabels[6] = 6;
-      classLabels[7] = 7;
-      classLabels[8] = 8;
-      classLabels[9] = 9;
-      */
-      /**
-       * The grid configuration.
-       */
-      std::cout << "# create grid config" << std::endl;
-      sgpp::base::RegularGridConfiguration gridConfig;
-      gridConfig.dim_ = trainDataset.getDimension();
-      gridConfig.level_ = 3;
-      //gridConfig.type_ = sgpp::base::GridType::Linear;
-      gridConfig.type_ = sgpp::base::GridType::ModLinear;
+  /**
+   * Specify the number of classes and the corresponding class labels.
+   */
+  size_t classNum = 2;
+  sgpp::base::DataVector classLabels(classNum);
+  classLabels[0] = 0;
+  classLabels[1] = 1;
+  /*
+  classLabels[2] = 2;
+  classLabels[3] = 3;
+  classLabels[4] = 4;
+  classLabels[5] = 5;
+  classLabels[6] = 6;
+  classLabels[7] = 7;
+  classLabels[8] = 8;
+  classLabels[9] = 9;
+  */
+  /**
+   * The grid configuration.
+   */
+  std::cout << "# create grid config" << std::endl;
+  sgpp::base::RegularGridConfiguration gridConfig;
+  gridConfig.dim_ = trainDataset.getDimension();
+  gridConfig.level_ = 3;
+  gridConfig.type_ = sgpp::base::GridType::Linear;
+  //gridConfig.type_ = sgpp::base::GridType::ModLinear;
 
-      /**
-       * Configure regularization.
-       */
-      std::cout << "# create regularization config" << std::endl;
-      sgpp::datadriven::RegularizationConfiguration regularizationConfig;
-      regularizationConfig.regType_ = sgpp::datadriven::RegularizationType::Identity;
+  /**
+   * Configure regularization.
+   */
+  std::cout << "# create regularization config" << std::endl;
+  sgpp::datadriven::RegularizationConfiguration regularizationConfig;
+  regularizationConfig.regType_ = sgpp::datadriven::RegularizationType::Identity;
 
-      /**
-       * Select the desired decomposition type for the offline step.
-       * Note: Refinement/Coarsening only possible for Cholesky decomposition.
-       */
-      sgpp::datadriven::DBMatDecompostionType dt;
-      std::string decompType;
-      // choose "LU decomposition"
-      // dt = DBMatDecompostionType::DBMatDecompLU;
-      // decompType = "LU decomposition";
-      // choose"Eigen decomposition"
-      // dt = DBMatDecompostionType::DBMatDecompEigen;
-      // decompType = "Eigen decomposition";
-      // choose "Cholesky decomposition"
-      dt = sgpp::datadriven::DBMatDecompostionType::Chol;
-      decompType = "Cholesky decomposition";
-      //      dt = sgpp::datadriven::DBMatDecompostionType::IChol;
-      //      decompType = "Incomplete Cholesky decomposition";
-      //	  dt = sgpp::datadriven::DBMatDecompostionType::DenseIchol;
-      //	  decompType = "Incomplete Cholesky decomposition on Dense Matrix";
-      std::cout << "Decomposition type: " << decompType << std::endl;
+  /**
+   * Select the desired decomposition type for the offline step.
+   * Note: Refinement/Coarsening only possible for Cholesky decomposition.
+   */
+  sgpp::datadriven::DBMatDecompostionType dt;
+  std::string decompType;
+  // choose "LU decomposition"
+  // dt = DBMatDecompostionType::DBMatDecompLU;
+  // decompType = "LU decomposition";
+  // choose"Eigen decomposition"
+  // dt = DBMatDecompostionType::DBMatDecompEigen;
+  // decompType = "Eigen decomposition";
+  // choose "Cholesky decomposition"
+  dt = sgpp::datadriven::DBMatDecompostionType::Chol;
+  decompType = "Cholesky decomposition";
+  //      dt = sgpp::datadriven::DBMatDecompostionType::IChol;
+  //      decompType = "Incomplete Cholesky decomposition";
+  //	  dt = sgpp::datadriven::DBMatDecompostionType::DenseIchol;
+  //	  decompType = "Incomplete Cholesky decomposition on Dense Matrix";
+  std::cout << "Decomposition type: " << decompType << std::endl;
 
-      /**
-       * Configure adaptive refinement (if Cholesky is chosen). As refinement
-       * monitor the periodic monitor or the convergence monitor
-       * can be chosen. Possible refinement indicators are
-       * surplus refinement, data-based refinement, zero-crossings-based
-       * refinement.
-       */
-      std::cout << "# create adaptive refinement configuration" << std::endl;
-      std::string refMonitor;
-      // select periodic monitor - perform refinements in fixed intervals
-      refMonitor = "periodic";
-      size_t refPeriod = 1;  // the refinement interval
-      // select convergence monitor - perform refinements if algorithm has
-      // converged
-      // (convergence measured with respect to changes of the classification
-      // accuracy)
-      // refMonitor = "convergence";
-      // the convergence threshold
-      double accDeclineThreshold = 0.001;
-      // number of accuracy measurements which
-      // are considered for convergence check
-      size_t accDeclineBufferSize = 140;
-      // minimum number of iterations before next refinement
-      // is allowed to be performed
-      size_t minRefInterval = 10;
-      std::cout << "Refinement monitor: " << refMonitor << std::endl;
-      std::string refType;
-      // select surplus refinement
-      // refType = "surplus";
-      // select data-based refinement
-      // refType = "data";
-      // select zero-crossings-based refinement
-      refType = "zero";
-      std::cout << "Refinement type: " << refType << std::endl;
-      sgpp::base::AdpativityConfiguration adaptConfig;
-      /**
-       * Specify number of refinement steps and the max number
-       * of grid points to refine each step.
-       */
-      adaptConfig.numRefinements_ = 0;
-      adaptConfig.noPoints_ = 5;
-      adaptConfig.threshold_ = 0.0;  // only required for surplus refinement
+  /**
+   * Configure adaptive refinement (if Cholesky is chosen). As refinement
+   * monitor the periodic monitor or the convergence monitor
+   * can be chosen. Possible refinement indicators are
+   * surplus refinement, data-based refinement, zero-crossings-based
+   * refinement.
+   */
+  std::cout << "# create adaptive refinement configuration" << std::endl;
+  std::string refMonitor;
+  // select periodic monitor - perform refinements in fixed intervals
+  refMonitor = "periodic";
+  size_t refPeriod = 1;  // the refinement interval
+  // select convergence monitor - perform refinements if algorithm has
+  // converged
+  // (convergence measured with respect to changes of the classification
+  // accuracy)
+  // refMonitor = "convergence";
+  // the convergence threshold
+  double accDeclineThreshold = 0.001;
+  // number of accuracy measurements which
+  // are considered for convergence check
+  size_t accDeclineBufferSize = 140;
+  // minimum number of iterations before next refinement
+  // is allowed to be performed
+  size_t minRefInterval = 10;
+  std::cout << "Refinement monitor: " << refMonitor << std::endl;
+  std::string refType;
+  // select surplus refinement
+  // refType = "surplus";
+  // select data-based refinement
+  // refType = "data";
+  // select zero-crossings-based refinement
+  refType = "zero";
+  std::cout << "Refinement type: " << refType << std::endl;
+  sgpp::base::AdpativityConfiguration adaptConfig;
+  /**
+   * Specify number of refinement steps and the max number
+   * of grid points to refine each step.
+   */
+  adaptConfig.numRefinements_ = 0;
+  adaptConfig.noPoints_ = 5;
+  adaptConfig.threshold_ = 0.0;  // only required for surplus refinement
 
-      // initial regularization parameter lambda
-      double lambda = 0.01;
-      // initial weighting factor
-      double beta = 0.0;
-      // configuration
-      sgpp::datadriven::DBMatDensityConfiguration dconf(gridConfig, adaptConfig,
-                                                        regularizationConfig.regType_, lambda, dt);
-      // specify if prior should be used to predict class labels
-      bool usePrior = false;
+  // initial regularization parameter lambda
+  double lambda = 0.01;
+  // initial weighting factor
+  double beta = 0.0;
+  // configuration
+  sgpp::datadriven::DBMatDensityConfiguration dconf(gridConfig, adaptConfig,
+                                                    regularizationConfig.regType_, lambda, dt);
+  // specify if prior should be used to predict class labels
+  bool usePrior = false;
 
-      dconf.icholParameters.sweepsDecompose = 2;
-      dconf.icholParameters.sweepsRefine = 2;
+  dconf.icholParameters.sweepsDecompose = 2;
+  dconf.icholParameters.sweepsRefine = 2;
 
-      //std::string matrixfile = "32x32_NN_Inter_lvl3_Chol.out";
+  std::string matrixfile = "mats/16x16_NN_Inter_lvl3_Chol.out";
 
-      /**
-       * Create the learner.
-       */
-      std::cout << "# create learner" << std::endl;
-      //sgpp::datadriven::LearnerSGDEOnOff learner(dconf, trainDataset, testDataset, nullptr,
-      //                                           classLabels, classNum, usePrior, beta, lambda, matrixfile);
-      sgpp::datadriven::LearnerSGDEOnOff learner(dconf, trainDataset, testDataset, nullptr,
-                                                 classLabels, classNum, usePrior, beta, lambda);
-      std::cout << "learner set up!" << std::endl;
+  /**
+   * Create the learner.
+   */
+  std::cout << "# create learner" << std::endl;
+  //Use precomputed matrix
+  sgpp::datadriven::LearnerSGDEOnOff learner(dconf, trainDataset, testDataset, nullptr,
+                                             classLabels, classNum, usePrior, beta, lambda, matrixfile);
 
-      /**
-       * Configure cross-validation.
-       * Set enableCv=true to perform cross-validation
-       * during the learning process.
-       */
-      bool enableCv = false;
-      // set cv configuration if cv enabled
-      size_t nextCvStep = 50;
-      double cvLambdaStart = 1e-1;
-      double cvLambdaEnd = 1e-10;
-      int cvLambdaSteps = 10;
-      bool cvLogScale = true;
-      sgpp::base::DataMatrix* cvTestData = &testDataset.getData();
-      sgpp::base::DataMatrix* cvTestDataRes = nullptr;  // needed?
-      learner.setCrossValidationParameters(cvLambdaSteps, cvLambdaStart, cvLambdaEnd, cvTestData,
-                                           cvTestDataRes, cvLogScale);
 
-      // specify batch size
-      // (set to 1 for processing only a single data point each iteration)
-      size_t batchSize = 10;
-      // specify max number of passes over traininig data set
-      size_t maxDataPasses = 1;
+  //Build matrix
+  //sgpp::datadriven::LearnerSGDEOnOff learner(dconf, trainDataset, testDataset, nullptr,
+  //                                           classLabels, classNum, usePrior, beta, lambda);
+  std::cout << "learner set up!" << std::endl;
 
-      /**
-       * Learn the data.
-       */
-      std::cout << "# start to train the learner" << std::endl;
-      learner.train(batchSize, maxDataPasses, refType, refMonitor, refPeriod, accDeclineThreshold,
-                    accDeclineBufferSize, minRefInterval, enableCv, nextCvStep);
+  /**
+   * Configure cross-validation.
+   * Set enableCv=true to perform cross-validation
+   * during the learning process.
+   */
+  bool enableCv = false;
+  // set cv configuration if cv enabled
+  size_t nextCvStep = 50;
+  double cvLambdaStart = 1e-1;
+  double cvLambdaEnd = 1e-10;
+  int cvLambdaSteps = 10;
+  bool cvLogScale = true;
+  sgpp::base::DataMatrix* cvTestData = &testDataset.getData();
+  sgpp::base::DataMatrix* cvTestDataRes = nullptr;  // needed?
+  learner.setCrossValidationParameters(cvLambdaSteps, cvLambdaStart, cvLambdaEnd, cvTestData,
+                                       cvTestDataRes, cvLogScale);
 
-      auto xTest = testDataset.getData();
-  	  auto yTest = testDataset.getTargets();
+  // specify batch size
+  // (set to 1 for processing only a single data point each iteration)
+  size_t batchSize = 80000;
+  // specify max number of passes over traininig data set
+  size_t maxDataPasses = 1;
 
-      std::cout << "Start prediction..." << std::endl;
+  /**
+   * Learn the data.
+   */
+  std::cout << "# start to train the learner" << std::endl;
+  learner.train(batchSize, maxDataPasses, refType, refMonitor, refPeriod, accDeclineThreshold,
+                accDeclineBufferSize, minRefInterval, enableCv, nextCvStep);
 
-      DataVector predictTest;
-	  learner.predict(xTest, predictTest);
-      
-      std::cout << "Build confusion matrix..." << std::endl;
+  //learner.storeResults();
 
-	  size_t confusion [10][10] = {0};
+  auto xTest = testDataset.getData();
+  auto yTest = testDataset.getTargets();
 
-	  for(size_t i = 0; i < testDataset.getNumberInstances(); i++){
-		confusion[(size_t)predictTest.get(i)][(size_t)yTest.get(i)]++;
-	  }
+  std::cout << "Start prediction..." << std::endl;
 
-	  for(size_t real = 0; real < 10; real++){
+  DataVector predictTest;
+  learner.predict(xTest, predictTest);
+  
+  std::cout << "Build confusion matrix..." << std::endl;
+
+  size_t confusion [10][10] = {0};
+
+  for(size_t i = 0; i < testDataset.getNumberInstances(); i++){
+	  confusion[(size_t)predictTest.get(i)][(size_t)yTest.get(i)]++;
+  }
+
+  for(size_t real = 0; real < 10; real++){
 		for(size_t pred = 0; pred < 10; pred++){
 		  std::cout << confusion[pred][real] << "\t";
 		}
-		std::cout << std::endl;
-	  }
+	  std::cout << std::endl;
+  }
 
+  /**
+   * Accuracy on test data.
+   */
+  double acc = learner.getAccuracy();
+  std::cout << "# accuracy (test data): " << acc << std::endl;
 
-      /**
-       * Accuracy on test data.
-       */
-      double acc = learner.getAccuracy();
-      std::cout << "# accuracy (test data): " << acc << std::endl;
-
-      // store results (classified data, grids, density functions)
-      // learner.storeResults();
-
+  // store results (classified data, grids, density functions)
+  // learner.storeResults();
 }
