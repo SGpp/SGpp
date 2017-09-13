9529f6d175ebee62a0a539b002aee56ebf38c5ad
Michael Rehme <michael.rehme@ipvs.uni-stuttgart.de>
2017-09-13 13:33:29 +0200

    made Coefficientstorages templates. Next ToDo: create Bspline SLE in SLECoefficientStorage

--- combigrid/src/sgpp/combigrid/operation/multidim/fullgrid/AbstractBasisCoefficientsStorage.hpp
+++ combigrid/src/sgpp/combigrid/operation/multidim/fullgrid/AbstractBasisCoefficientsStorage.hpp
@@ -5,16 +5,18 @@
 
 #pragma once
 
-#include <sgpp/combigrid/storage/AbstractMultiStorage.hpp>
+#include <sgpp/combigrid/algebraic/FloatScalarVector.hpp>
 #include <sgpp/combigrid/storage/AbstractCombigridStorage.hpp>
+#include <sgpp/combigrid/storage/AbstractMultiStorage.hpp>
 
-#include <vector>
-#include <memory>
 #include <map>
+#include <memory>
+#include <vector>
 
 namespace sgpp {
 namespace combigrid {
 
+template <class V>
 class AbstractBasisCoefficientsStorage {
  public:
   AbstractBasisCoefficientsStorage();
@@ -23,14 +25,34 @@ class AbstractBasisCoefficientsStorage {
   virtual void computeCoefficients(MultiIndex const& level,
                                    std::shared_ptr<AbstractCombigridStorage>& storage,
                                    MultiIndex& multiBounds,
-                                   std::vector<bool>& orderingConfiguration) = 0;
+                                   std::vector<bool>& orderingConfiguration,
+                                   std::vector<std::vector<V>> const& basisValues) = 0;
   std::shared_ptr<std::vector<double>> getCoefficients(
       MultiIndex const& level, std::shared_ptr<AbstractCombigridStorage>& storage,
-      MultiIndex& multiBounds, std::vector<bool>& orderingConfiguration);
+      MultiIndex& multiBounds, std::vector<bool>& orderingConfiguration,
+      std::vector<std::vector<V>> const& basisValues);
 
  protected:
   std::map<MultiIndex, std::shared_ptr<std::vector<double>>> coefficients;
+  base::DataVector functionValues;
 };
+
+template <class V>
+AbstractBasisCoefficientsStorage<V>::AbstractBasisCoefficientsStorage() {}
+
+template <class V>
+AbstractBasisCoefficientsStorage<V>::~AbstractBasisCoefficientsStorage() {}
+
+template <class V>
+std::shared_ptr<std::vector<double>> AbstractBasisCoefficientsStorage<V>::getCoefficients(
+    MultiIndex const& level, std::shared_ptr<AbstractCombigridStorage>& storage,
+    MultiIndex& multiBounds, std::vector<bool>& orderingConfiguration,
+    std::vector<std::vector<V>> const& basisValues) {
+  if (coefficients.find(level) == coefficients.end()) {
+    computeCoefficients(level, storage, multiBounds, orderingConfiguration, basisValues);
+  }
+  return coefficients[level];
+}
 
 } /* namespace combigrid */
 } /* namespace sgpp */
