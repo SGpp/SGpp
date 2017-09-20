043568b589c4c9f0a0b945961d9935aaf63b28c1
Tim Waegemans <waegeman@in.tum.de>
2017-09-20 10:43:01 +0200

    Added MNIST dataset

---
+++ datasets/mnist/README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2229be646212f51aff34beabe140a68f6b6262b866a942a7efeb40f84778245a
+size 217
---
+++ datasets/mnist/to_arrf.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:63de9f02b680489e5c252a215f2d067c4cc725cce3cc9c556a9336681dcbbe02
+size 3397
