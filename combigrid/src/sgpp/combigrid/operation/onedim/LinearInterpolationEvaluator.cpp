8b6e8d293561b7e12234256a069c56b7cbbd5c44
Michael Rehme <michael.rehme@ipvs.uni-stuttgart.de>
2017-09-28 10:36:47 +0200

    B-Spline prototype works, interpolating the Bspline way works for polynomials and linear as well

--- combigrid/src/sgpp/combigrid/operation/onedim/LinearInterpolationEvaluator.cpp
+++ combigrid/src/sgpp/combigrid/operation/onedim/LinearInterpolationEvaluator.cpp
@@ -32,66 +32,33 @@ void LinearInterpolationEvaluator::computeBasisValues() {
   }
 
   // returns linear interpolation between function values in evaluationPoint
-  if (basistype == BasisCoefficientsComputationType::FUNCTION_VALUES) {
-    if (evaluationPoint <= xValues[0]) {
-      if (std::abs(xValues[0]) > 1e-14) {
-        basisValues[0] = FloatScalarVector(evaluationPoint / xValues[0]);
-      }
+  if (evaluationPoint <= xValues[0]) {
+    if (std::abs(xValues[0]) > 1e-14) {
+      basisValues[0] = FloatScalarVector(evaluationPoint / xValues[0]);
+    } else {
+      basisValues[0] = 1.0;
+    }
+    return;
+  }
+
+  // TODO(holzmudd): could be optimized by using binary search
+  for (size_t i = 1; i < numPoints; ++i) {
+    if (evaluationPoint <= xValues[i]) {
+      double x0 = xValues[i - 1];
+      double x1 = xValues[i];
+
+      double weight = (x1 - evaluationPoint) / (x1 - x0);
+      basisValues[i - 1] = FloatScalarVector(weight);
+      basisValues[i] = FloatScalarVector(1 - weight);
+
       return;
     }
+  }
 
-    // TODO(holzmudd): could be optimized by using binary search
-    for (size_t i = 1; i < numPoints; ++i) {
-      if (evaluationPoint <= xValues[i]) {
-        double x0 = xValues[i - 1];
-        double x1 = xValues[i];
-
-        double weight = (x1 - evaluationPoint) / (x1 - x0);
-        basisValues[i - 1] = FloatScalarVector(weight);
-        basisValues[i] = FloatScalarVector(1 - weight);
-
-        return;
-      }
-    }
-
-    // if we did not return in the loop, then evaluationPoint > all xValues...
-    if (std::abs(xValues[numPoints - 1] - 1.0) > 1e-14) {
-      basisValues[numPoints - 1] =
-          FloatScalarVector((evaluationPoint - 1.0) / (xValues[numPoints - 1] - 1.0));
-    }
-
-    // returns evaluation of corresponding B-Spline in evaluationPoint
-  } else if (basistype == BasisCoefficientsComputationType::SLE) {
-    //    std::cout << "current level: " << level << "  evaluation Point: " << evaluationPoint
-    //              << " grid: [";
-
-    //    for (size_t i = 0; i < numPoints; ++i) {
-    //      std::cout << xValues[i] << " ";
-    //    }
-    //    std::cout << "]" << std::endl;
-
-    // BasisCoeffCompType to SLE
-    size_t degree = 3;
-
-    std::unique_ptr<base::BsplineBasis<int, int>> bsplineBasis;
-    //    bsplineBasis = std::unique_ptr<base::SBsplineBase>(new sgpp::base::SBsplineBase(degree));
-
-    //    std::cout << "basis values: ";
-    // hInv = 2^level
-    const double hInv = static_cast<double>(1 << (level + 1));
-    for (int i = 0; i < (int)numPoints; i++) {
-      // cardinal B-Spline:  b(x/h - k)  with x the evaluation point, h the gridwidth and k the
-      // index of the B-Spline.
-      // the index of the B-Spline that has its midpoint in 0 is -2, the one that has it in h has
-      // index -1, 2h has 0, 3h has 1 and so on...
-
-      // Choosing the index like this sets the first Bspline the point h
-      double index = i - 1;
-      basisValues[i] = bsplineBasis->uniformBSpline(evaluationPoint * hInv - index, degree);
-      //      std::cout << bsplineBasis->uniformBSpline(evaluationPoint * hInv - index, degree) <<
-      //      ", ";
-    }
-    //    std::cout << "\n\n";
+  // if we did not return in the loop, then evaluationPoint > all xValues...
+  if (std::abs(xValues[numPoints - 1] - 1.0) > 1e-14) {
+    basisValues[numPoints - 1] =
+        FloatScalarVector((evaluationPoint - 1.0) / (xValues[numPoints - 1] - 1.0));
   }
 }
 
