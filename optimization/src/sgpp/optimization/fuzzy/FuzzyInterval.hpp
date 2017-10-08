19c324e9cf4fadfc0469b65bd7ba4ddab1821ff2
Julian Valentin <julian.valentin@ipvs.uni-stuttgart.de>
2017-10-08 09:27:36 +0200

    Add MC calculation of fuzzy interval norms/errors

--- optimization/src/sgpp/optimization/fuzzy/FuzzyInterval.hpp
+++ optimization/src/sgpp/optimization/fuzzy/FuzzyInterval.hpp
@@ -13,11 +13,30 @@ namespace optimization {
 
 class FuzzyInterval {
  public:
-  virtual ~FuzzyInterval() {}
+  const size_t DEFAULT_NUMBER_OF_INTEGRAL_SAMPLES = 10000;
+
+  FuzzyInterval();
+  virtual ~FuzzyInterval();
 
   virtual double evaluateMembershipFunction(double x) const = 0;
   virtual double evaluateConfidenceIntervalLowerBound(double alpha) const = 0;
   virtual double evaluateConfidenceIntervalUpperBound(double alpha) const = 0;
+
+  double approximateL1Norm() const;
+  double approximateL2Norm() const;
+  double approximateLinfNorm() const;
+  double approximateL1Error(const FuzzyInterval& other) const;
+  double approximateL2Error(const FuzzyInterval& other) const;
+  double approximateLinfError(const FuzzyInterval& other) const;
+  double approximateRelativeL1Error(const FuzzyInterval& other) const;
+  double approximateRelativeL2Error(const FuzzyInterval& other) const;
+  double approximateRelativeLinfError(const FuzzyInterval& other) const;
+
+  size_t getNumberOfIntegralSamples() const;
+  void setNumberOfIntegralSamples(size_t numberOfIntegralSamples);
+
+ protected:
+  size_t numberOfIntegralSamples;
 };
 
 }  // namespace optimization
