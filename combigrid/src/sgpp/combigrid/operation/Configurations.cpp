b0d6eafc18c299142f252ad36d3afdc1d9d786bb
David Holzmüller <david.holzmueller@online.de>
2017-10-11 17:52:42 +0200

    fixed error in FloatTensorVector, implemented CombigridTensorOperation, implemented pce example

--- combigrid/src/sgpp/combigrid/operation/Configurations.cpp
+++ combigrid/src/sgpp/combigrid/operation/Configurations.cpp
@@ -23,6 +23,7 @@
 #include <sgpp/combigrid/operation/onedim/ArrayEvaluator.hpp>
 #include <sgpp/combigrid/operation/onedim/BSplineInterpolationEvaluator.hpp>
 #include <sgpp/combigrid/operation/onedim/CubicSplineInterpolationEvaluator.hpp>
+#include <sgpp/combigrid/operation/onedim/InterpolationCoefficientEvaluator.hpp>
 #include <sgpp/combigrid/operation/onedim/LinearInterpolationEvaluator.hpp>
 #include <sgpp/combigrid/operation/onedim/PolynomialInterpolationEvaluator.hpp>
 #include <sgpp/combigrid/operation/onedim/QuadratureEvaluator.hpp>
@@ -194,6 +195,11 @@ std::shared_ptr<AbstractLinearEvaluator<FloatArrayVector>> CombiEvaluators::mult
     SingleFunction func, bool normalizeWeights) {
   return std::make_shared<ArrayEvaluator<QuadratureEvaluator>>(
       false, QuadratureEvaluator(func, normalizeWeights));
+}
+
+std::shared_ptr<AbstractLinearEvaluator<FloatTensorVector>> CombiEvaluators::tensorInterpolation(
+    std::shared_ptr<AbstractInfiniteFunctionBasis1D> functionBasis) {
+  return std::make_shared<InterpolationCoefficientEvaluator>(functionBasis);
 }
 
 } /* namespace combigrid */
