846b5587955bf4ebccccd06ca77bc7dafb7635b5
Fabian Franzelin <fabian.franzelin@ipvs.uni-stuttgart.de>
2017-10-12 17:56:02 +0200

    made sure that SGpp builds without DAKOTA

--- combigrid/src/sgpp/combigrid/functions/LegendreBasis1D.cpp
+++ combigrid/src/sgpp/combigrid/functions/LegendreBasis1D.cpp
@@ -5,22 +5,33 @@
 
 #include <sgpp/combigrid/functions/LegendreBasis1D.hpp>
 
+#include <iostream>
+
 #ifdef USE_DAKOTA
 #include <BasisPolynomial.hpp>
+#endif
 
 namespace sgpp {
 namespace combigrid {
 
+#ifdef USE_DAKOTA
 LegendreBasis1D::LegendreBasis1D() : basisPoly(Pecos::LEGENDRE_ORTHOG) {}
+#else
+LegendreBasis1D::LegendreBasis1D() {}
+#endif
 
 LegendreBasis1D::~LegendreBasis1D() {}
 
 double LegendreBasis1D::evaluate(size_t basisIndex, double xValue) {
+#ifdef USE_DAKOTA
   double invNorm = 1. / std::sqrt(basisPoly.norm_squared(basisIndex));
   return invNorm * basisPoly.type1_value(2.0 * xValue - 1.0, basisIndex);
+#else
+  std::cerr << "Error in LegendreBasis1D::evaluate: "
+            << "SG++ was compiled without DAKOTAsupport!\n";
+  return false;
+#endif
 }
 
 } /* namespace combigrid */
 } /* namespace sgpp */
-
-#endif
