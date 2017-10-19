6db4eb8eefdcfa9a3cd2953ef805c895381e1cc6
Michael Rehme <michael.rehme@ipvs.uni-stuttgart.de>
2017-10-19 17:25:22 +0200

    working 1D B-Spline integration prototype

--- combigrid/src/sgpp/combigrid/operation/onedim/BSplineQuadratureEvaluator.cpp
+++ combigrid/src/sgpp/combigrid/operation/onedim/BSplineQuadratureEvaluator.cpp
@@ -7,9 +7,8 @@
 #include <sgpp/base/tools/GaussLegendreQuadRule1D.hpp>
 #include <sgpp/combigrid/integration/GaussLegendreQuadrature.hpp>
 #include <sgpp/combigrid/operation/Configurations.hpp>
-#include <sgpp/combigrid/operation/onedim/BSplineInterpolationEvaluator.hpp>
 #include <sgpp/combigrid/operation/onedim/BSplineQuadratureEvaluator.hpp>
-#include <sgpp/combigrid/operation/onedim/PolynomialInterpolationEvaluator.hpp>
+#include <sgpp/combigrid/operation/onedim/BSplineRoutines.hpp>
 
 #include <cmath>
 #include <iomanip>
@@ -26,37 +25,42 @@ namespace combigrid {
  */
 double BSplineQuadratureEvaluator::getWeight(std::vector<double>& points, size_t index) {
   // performing Gauss-Legendre integration
-  size_t numGaussPoints = (degree + 1) / 2 + numAdditionalPoints + 50;
+  size_t numGaussPoints = (degree + 1) / 2 + numAdditionalPoints;
   base::DataVector roots;
-  base::DataVector quadweights;
+  base::DataVector quadratureweights;
   auto& quadRule = base::GaussLegendreQuadRule1D::getInstance();
   quadRule.getLevelPointsAndWeightsNormalized(
-      std::min(numGaussPoints, quadRule.getMaxSupportedLevel()), roots, quadweights);
-  //  std::cout << "numpoints: " << numGaussPoints << " roots: " << roots.toString()  //<<
-  //  std::endl;
-  //            << " weights: " << quadweights.toString() << std::endl;
+      std::min(numGaussPoints, quadRule.getMaxSupportedLevel()), roots, quadratureweights);
+
   double sum = 0.0;
+  std::vector<double> xi;
+  createNakKnots(xValues, degree, xi);
 
-  // BSplineInterpolationEvaluator -> createKnots ->nonuniformBspline
-  auto evaluator = sgpp::combigrid::CombiEvaluators::BSplineInterpolation(degree);
-  for (size_t i = 0; i < roots.getSize(); ++i) {
-    double b = 1.0;
-    double a = 0.0;
+  // multiply weights by length_old_interval / length_new_interval
+  for (size_t i = 0; i < quadratureweights.size(); i++) {
+    quadratureweights[i] *= 1.0 / (points.size() - 1.0);
+  }
+
+  //  std::cout << "index: " << index << std::endl;
+  size_t offset = (degree + 1) / 2;
+  size_t first_segment = std::max(offset, index);
+  size_t last_segment = std::min(xi.size() - offset - 1, index + degree + 1);
+  //    std::cout << first_segment << " " << last_segment << std::endl;
+  for (size_t segmentIndex = first_segment; segmentIndex < last_segment; segmentIndex++) {
+    double a = std::max(0.0, xi[segmentIndex]);
+    double b = std::min(1.0, xi[segmentIndex + 1]);
     double width = b - a;
-    double x = a + width * roots[i];
-    FloatScalarVector param(x);
-    evaluator->setGridPoints(points);
-    evaluator->setParameter(param);
-    std::vector<FloatScalarVector> bsplineevaluation = evaluator->getBasisValues();
-    std::vector<double> bsplineevaluationdouble(bsplineevaluation.size());
-    for (size_t i = 0; i < bsplineevaluation.size(); i++) {
-      bsplineevaluationdouble[i] = bsplineevaluation[i].value();
-    }
-    // ToDo (rehmemk) alle Basisfunktionene werden ausgerechnet, aber nur eine wird genutzt,
-    // vektorisieren!
+    //    std::cout << "a: " << a << " b: " << b << std::endl;
 
-    double funceval = bsplineevaluationdouble[index] * this->weight_function(x);
-    sum += quadweights[i] * funceval;
+    for (size_t i = 0; i < roots.getSize(); ++i) {
+      double x = a + width * roots[i];
+      double bsplinevalue = nonUniformBSpline(x, degree, index, xi);
+      //      std::cout << " x: " << x << " b(x): " << bsplinevalue << std::endl;
+
+      double integrand = bsplinevalue * this->weight_function(x);
+      sum += integrand * quadratureweights[i];
+    }
+    //    std::cout << "\n";
   }
   return sum;
 }
@@ -72,14 +76,9 @@ double BSplineQuadratureEvaluator::getWeight(std::vector<double>& points, size_t
  */
 void BSplineQuadratureEvaluator::calculateWeights(std::vector<double>& points,
                                                   std::vector<FloatScalarVector>& weights) {
-  // calc weight for each point
-  //  std::cout << "points: ";
-  //  for (size_t i = 0; i < points.size(); i++) {
-  //    std::cout << points[i] << " ";
-  //  }
-  //  std::cout << "\n";
-  for (size_t i = 0; i < points.size(); ++i) {
-    weights.push_back(FloatScalarVector(getWeight(points, i)));
+  // "weights" here are the integrals!
+  for (size_t index = 0; index < points.size(); ++index) {
+    weights.push_back(FloatScalarVector(getWeight(points, index)));
   }
 }
 
