5708098ed2a40fc13e96d9ef0b320d6fbaf9e1aa
Tim Waegemans <waegeman@in.tum.de>
2017-10-23 13:42:23 +0200

    fixed typo

--- datasets/mnist/to_arrf.py
+++ datasets/mnist/to_arrf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1402e584ea4452f2f1f7aaaa059af883d9063fc6cc1a7dde9994825768778a61
-size 3396
+oid sha256:627c2fcf2e90063df9407778f303d1bbba172d0a006b1bf7c3fce90148936a91
+size 3395
