887540f8d7ef27d916f07eb1e98476449fc4e6b1
koepkeec <eric.koepke@online.de>
2017-12-11 03:41:40 +0100

    Files added.

---
+++ datadriven/src/sgpp/datadriven/datamining/modules/dataSource/CSVFileSampleProvider.cpp
@@ -0,0 +1,113 @@
+/* Copyright (C) 2008-today The SG++ project
+ * This file is part of the SG++ project. For conditions of distribution and
+ * use, please see the copyright notice provided with SG++ or at
+ * sgpp.sparsegrids.org
+ *
+ * CSVFileSampleProvider.cpp
+ *
+ *  Created on: Feb 8, 2016
+ *      Author: perun, Michael Lettrich
+ */
+
+#include <sgpp/datadriven/datamining/modules/dataSource/CSVFileSampleProvider.hpp>
+
+#include <sgpp/base/datatypes/DataMatrix.hpp>
+#include <sgpp/base/datatypes/DataVector.hpp>
+#include <sgpp/base/exception/data_exception.hpp>
+#include <sgpp/base/exception/file_exception.hpp>
+#include <sgpp/datadriven/tools/CSVTools.hpp>
+
+#include <string>
+
+namespace sgpp {
+
+namespace datadriven {
+
+CSVFileSampleProvider::CSVFileSampleProvider()
+    : FileSampleProvider{}, dataset(Dataset{}), counter(0) {}
+
+SampleProvider* CSVFileSampleProvider::clone() const {
+  return dynamic_cast<SampleProvider*>(new CSVFileSampleProvider{*this});
+}
+
+size_t CSVFileSampleProvider::getDim() const {
+  if (dataset.getDimension() != 0) {
+    return dataset.getDimension();
+  } else {
+    throw base::file_exception{"No dataset loaded."};
+  }
+}
+
+size_t CSVFileSampleProvider::getNumSamples() const {
+  if (dataset.getDimension() != 0) {
+    return dataset.getNumberInstances();
+  } else {
+    throw base::file_exception{"No dataset loaded."};
+  }
+}
+
+void CSVFileSampleProvider::readFile(const std::string& fileName) {
+  try {
+    dataset = CSVTools::readCSV(fileName, true);
+  } catch (...) {
+    // TODO(lettrich): catching all exceptions is bad design. Replace call to CSVTools with
+    // exception safe implementation.
+    const std::string msg{"Failed to parse CSV File " + fileName + "."};
+    throw base::data_exception{msg.c_str()};
+  }
+}
+
+Dataset* CSVFileSampleProvider::getNextSamples(size_t howMany) {
+  if (dataset.getDimension() != 0) {
+    return splitDataset(howMany);
+  } else {
+    throw base::file_exception("No dataset loaded.");
+  }
+}
+
+Dataset* CSVFileSampleProvider::getAllSamples() {
+  if (dataset.getDimension() != 0) {
+    return new Dataset{dataset};
+  } else {
+    throw base::file_exception{"No dataset loaded."};
+  }
+}
+
+void CSVFileSampleProvider::readString(const std::string& input) {
+ // try {
+ //   dataset = CSVTools::readCSVFromString(input);
+ // } catch (...) {
+    // TODO(lettrich): catching all exceptions is bad design. Replace call to CSVTools with
+    // exception safe implementation.
+    throw base::data_exception{"Failed to parse CSV data."};
+ // }
+}
+
+Dataset* CSVFileSampleProvider::splitDataset(size_t howMany) {
+  const size_t size = counter + howMany < dataset.getNumberInstances()
+                          ? howMany
+                          : dataset.getNumberInstances() - counter;
+  auto tmpDataset = std::make_unique<Dataset>(size, dataset.getDimension());
+
+  base::DataMatrix& srcSamples = dataset.getData();
+  base::DataVector& srcTargets = dataset.getTargets();
+
+  base::DataMatrix& destSamples = tmpDataset->getData();
+  base::DataVector& destTargets = tmpDataset->getTargets();
+
+  base::DataVector tmpRow{srcSamples.getNcols()};
+
+  // copy "size" rows beginning from "counter" to the new dataset.
+  for (size_t i = counter; i < counter + size; ++i) {
+    srcSamples.getRow(i, tmpRow);
+    destSamples.setRow(i - counter, tmpRow);
+
+    destTargets[i - counter] = srcTargets[i];
+  }
+  counter = counter + size;
+
+  return tmpDataset.release();
+}
+
+} /* namespace datadriven */
+} /* namespace sgpp */
