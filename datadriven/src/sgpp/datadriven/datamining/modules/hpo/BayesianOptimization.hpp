add7e337adeddda5e0ab786ed6652777d0c78ed5
ericonlaptop <eric.koepke@online.de>
2018-02-02 19:31:12 +0100

    classes for BO created

---
+++ datadriven/src/sgpp/datadriven/datamining/modules/hpo/BayesianOptimization.hpp
@@ -0,0 +1,33 @@
+/*
+ * BayesianOptimization.hpp
+ *
+ *  Created on: Feb 2, 2018
+ *      Author: polarbart
+ */
+
+#ifndef DATADRIVEN_SRC_SGPP_DATADRIVEN_DATAMINING_MODULES_HPO_BAYESIANOPTIMIZATION_HPP_
+#define DATADRIVEN_SRC_SGPP_DATADRIVEN_DATAMINING_MODULES_HPO_BAYESIANOPTIMIZATION_HPP_
+
+#include <sgpp/base/datatypes/DataMatrix.hpp>
+#include <sgpp/base/datatypes/DataVector.hpp>
+
+
+namespace sgpp {
+namespace datadriven {
+
+class BayesianOptimization {
+public:
+	BayesianOptimization();
+	double mean(base::DataVector knew);
+	double var(base::DataVector knew, double kself);
+	void updateGP(base::DataVector knew, base::DataVector y);
+	double kernel(base::DataVector x1, base::DataVector x2);
+protected:
+	base::DataMatrix kernelmatrix;
+	base::DataVector transformedOutput;
+};
+
+} /* namespace datadriven */
+} /* namespace sgpp */
+
+#endif /* DATADRIVEN_SRC_SGPP_DATADRIVEN_DATAMINING_MODULES_HPO_BAYESIANOPTIMIZATION_HPP_ */
