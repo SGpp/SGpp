84acf805ac96bdbe1a0c4991aaf55170e9addc79
Fabian Franzelin <fabian.franzelin@ipvs.uni-stuttgart.de>
2018-02-22 10:52:55 +0100

    bug fix in parameter set: missing imports added
    Former-commit-id: c5ae7059a8859e82bbf1f08b812a8a6ab3f4d954

--- datadriven/python/uq/parameters/ParameterSet.py
+++ datadriven/python/uq/parameters/ParameterSet.py
@@ -21,7 +21,10 @@ from pysgpp import PolynomialChaosExpansion, \
     singleFunc, \
     ProbabilityDensityFunction1DConfiguration, \
     ProbabilityDensityFunctionParameters, \
-    ProbabilityDensityFunction1D
+    ProbabilityDensityFunction1D, \
+    ProbabilityDensityFunctionType_BOUNDED_LOGNORMAL, \
+    ProbabilityDensityFunctionType_BETA, \
+    ProbabilityDensityFunctionType_UNIFORM
 
 from pysgpp.extensions.datadriven.uq.dists import J, Beta, Lognormal, TLognormal, Uniform
 from pysgpp.extensions.datadriven.uq.transformation import (JointTransformation,
