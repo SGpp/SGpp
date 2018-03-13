142e2fdfd0f88217e494e769abdfa42c22550aaf
Fabian Franzelin <fabian.franzelin@ipvs.uni-stuttgart.de>
2018-03-13 10:31:42 +0100

    documentation added
    Former-commit-id: 5d38836f33c42ed85d6b8fed040c21ccd2d04c10

--- datadriven/python/uq/plot/colors.py
+++ datadriven/python/uq/plot/colors.py
@@ -12,10 +12,22 @@ except:
 
 
 def get_username():
+    """
+    returns the user name
+    """
     return pwd.getpwuid(os.getuid())[0]
 
 
 def load_custom_pgf_preamble(dtype="standard", macros="thesis"):
+    """
+    Load preamble for latex style plots. It includes amsmath, scientific units,
+    font style for springer publications, etc. If the macros parameter
+    is defined, then all the provided latex commands in there will be loaded
+    in the preamble as well.
+
+    @param dtype: string that specifies the math style (standard, springer)
+    @param macros: path to latex file that contains latex commands
+    """
     pysgpp_uq_font = load_font()
 
     pgf_preamble = {"font.family": pysgpp_uq_font["family"],  # use serif/main font for text elements
@@ -33,7 +45,7 @@ def load_custom_pgf_preamble(dtype="standard", macros="thesis"):
                     'xtick.labelsize': pysgpp_uq_font["size"],
                     'ytick.labelsize': pysgpp_uq_font["size"],
                     'axes.unicode_minus': True,
-                    'figure.figsize': (5, 4.5),
+                    #                     'figure.figsize': (5, 4.5),
                     'image.cmap': load_default_color_map(dtype="string")
                     #                     'axes.titlepad': 25
                     }
@@ -73,11 +85,20 @@ def load_custom_pgf_preamble(dtype="standard", macros="thesis"):
     return pgf_preamble
 
 
-def initialize_plotting_style(dtype="standard", macros="thesis"):
+def initialize_plotting_style(dtype="standard",
+                              macros="thesis",
+                              style="seaborn-paper"):
+    """
+    Initialize the plotting style for the current python session.
+
+    @param dtype: font style type (standard, springer)
+    @param macros: path to commands latex file -> containing (newcommand)
+    @param style: plot style
+    """
     import matplotlib as mpl
     import matplotlib.pyplot as plt
 
-    plt.style.use('seaborn-paper')
+    plt.style.use(style)
 
     # Include packages `amssymb` and `amsmath` in LaTeX preamble
     # as they include extended math support (symbols, envisonments etc.)
@@ -100,6 +121,10 @@ def rgbTpInt(rgb):
 
 
 def load_default_color_map(dtype="cmap"):
+    """
+    Load default color map -> viridis
+    @param dtype: color map identifier
+    """
     if dtype == "cmap":
         return plt.get_cmap('viridis')
     else:
@@ -107,26 +132,47 @@ def load_default_color_map(dtype="cmap"):
 
 
 def load_color(i):
+    """
+    Provide color via index
+    @param i: index
+    """
     colors = list(plt.rcParams['axes.prop_cycle'])
     return colors[i % len(colors)]["color"]
 
 
 def load_marker(i):
+    """
+    Provide marker via index
+    @param i: index
+    """
     markers = ["o", "v", "D", "s", "*", "d", "^", "x", "+"]
     return markers[i % len(markers)]
 
 
 def load_linestyle(i):
+    """
+    Provide linestyle via index.
+    @param i: index
+    """
     linestyles = ["-", ":", "-.", "--"]
     return linestyles[i % len(linestyles)]
 
 
 def load_bw_color(i, nmax=11):
+    """
+    Load black and white color via index
+
+    @param i: index
+    @param nmax: maximum number of cycle
+    """
     colors = np.linspace(0, 0.75, nmax, endpoint=True, dtype=str)
     return colors[i % len(colors)]
 
 
 def load_font():
+    """
+    Load standard font
+    """
     return {'family': 'serif',
             'size': 20}
 
@@ -147,7 +193,27 @@ def load_font_properties(size=None,
 #     return colors
 
 
-def savefig(fig, filename, lgd=None, tikz=False, mpl3d=False, crop=False):
+def savefig(fig,
+            filename,
+            lgd=None,
+            tikz=False,
+            mpl3d=False,
+            crop=False,
+            sizes=(5.25, 5)):
+    """
+    Save a figure in various formats.
+
+    @param fig: figure object
+    @param filename: filename (without extension) where the file should be stored
+    @param lgd: legend object
+    @param tikz: save result as tikz file using matplotlib2tikz
+    @param mpl3d: is it a 3d plot? Then one should not use a tight layout
+    @param crop: crop the resulting pdf -> pdfcrop needs to be installed
+    @param sizes: tuple containing the figures size in x and y direction in inches
+    """
+    if sizes is not None:
+        sizex, sizey = sizes
+        fig.set_size_inches(sizex, sizey, forward=True)
     if mpl3d:
         fig.savefig("%s.png" % filename)
         fig.savefig("%s.pdf" % filename)
@@ -164,10 +230,10 @@ def savefig(fig, filename, lgd=None, tikz=False, mpl3d=False, crop=False):
                         bbox_extra_artists=(lgd,),
                         bbox_inches='tight')
         if tikz:
-            #             try:
-            tikz_save("%s.tex" % filename, fig)
-#             except:
-#                 pass
+            try:
+                tikz_save("%s.tex" % filename, fig)
+            except:
+                pass
 
     if crop:
         subprocess.call(["pdfcrop",
@@ -177,27 +243,37 @@ def savefig(fig, filename, lgd=None, tikz=False, mpl3d=False, crop=False):
     plt.close(fig)
 
 
-def insert_legend(fig, loc="right", ncol=3, has_axis=True, shift=0.0):
+def insert_legend(loc="right", ncol=3, has_axis=True, shift=0.0):
+    """
+    Inserts a legend outside of the plotting area. It is possible that the
+    exact position (bbox_to_anchor) needs to be adjusted but once the
+    size of the figures is fixed, this works fine.
+
+    @param loc: location of the legend (right, bottom, top, left)
+    @param ncol: number of columns for the labels
+    @param has_axis: defines if xticks labels are shown
+    @param shift: additional offset of the legend from the inner plotting area
+    """
     if loc == "right":
         lgd = plt.legend(loc='upper left',
-                         bbox_to_anchor=(1.02, 1),
+                         bbox_to_anchor=(1.02 + shift, 1),
                          borderaxespad=0,
                          prop=load_font_properties())
     elif loc == "bottom":
         lgd = plt.legend(loc='upper center',
                          ncol=ncol,
-                         bbox_to_anchor=(0.5, -0.3 + shift) if has_axis else (0.5, -0.08),
+                         bbox_to_anchor=(0.5, -0.3 + shift) if has_axis else (0.5, -0.08 + shift),
                          borderaxespad=0,
                          prop=load_font_properties())
     elif loc == "top":
-        lgd = ax.legend(loc='upper center',
-                        bbox_to_anchor=(0.5, 1.25),
-                        ncol=ncol,
-                        borderaxespad=0,
-                        prop=load_font_properties())
+        lgd = plt.legend(loc='upper center',
+                         bbox_to_anchor=(0.5, 1.25 + shift),
+                         ncol=ncol,
+                         borderaxespad=0,
+                         prop=load_font_properties())
     elif loc == "left":
         lgd = plt.legend(loc='upper right',
-                         bbox_to_anchor=(-0.33, 1),
+                         bbox_to_anchor=(-0.33 + shift, 1),
                          borderaxespad=0,
                          prop=load_font_properties())
     else:
