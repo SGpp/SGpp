24fedf18c07be97a52ddbe1928371bd3df7e0ce9
Julian Valentin <julian.valentin@ipvs.uni-stuttgart.de>
2018-05-21 11:04:55 +0200

    Consistent header banner in GeneralGridTypeParser.hpp

--- datadriven/src/sgpp/datadriven/datamining/configuration/GeneralGridTypeParser.hpp
+++ datadriven/src/sgpp/datadriven/datamining/configuration/GeneralGridTypeParser.hpp
@@ -1,15 +1,7 @@
-
-/*
- * Copyright (C) 2008-today The SG++ project
- * This file is part of the SG++ project. For conditions of distribution and
- * use, please see the copyright notice provided with SG++ or at
- * sgpp.sparsegrids.org
- *
- * GeneralGridTypeParser.hpp
- *
- * Created on: 23.04.18
- *     Author: Dominik Fuchsgruber
- */
+// Copyright (C) 2008-today The SG++ project
+// This file is part of the SG++ project. For conditions of distribution and
+// use, please see the copyright notice provided with SG++ or at
+// sgpp.sparsegrids.org
 
 #ifndef DATADRIVEN_SRC_SGPP_DATADRIVEN_DATAMINING_CONFIGURATION_GENERALGRIDTYPEPARSER_HPP_
 #define DATADRIVEN_SRC_SGPP_DATADRIVEN_DATAMINING_CONFIGURATION_GENERALGRIDTYPEPARSER_HPP_
