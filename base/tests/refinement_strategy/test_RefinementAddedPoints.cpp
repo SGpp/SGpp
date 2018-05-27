17393ff0e8c320b23531050079792c81c5a8df9d
Sebastian Kreisel <kreisel.sebastian@gmail.com>
2018-05-27 23:23:30 +0200

    Add: Missing boundary test cases

--- base/tests/refinement_strategy/test_RefinementAddedPoints.cpp
+++ base/tests/refinement_strategy/test_RefinementAddedPoints.cpp
@@ -11,6 +11,7 @@
 #include <sgpp/base/grid/GridStorage.hpp>
 #include <sgpp/base/grid/generation/GridGenerator.hpp>
 #include <sgpp/base/grid/generation/hashmap/HashRefinement.hpp>
+#include <sgpp/base/grid/generation/hashmap/HashRefinementBoundaries.hpp>
 #include <sgpp/base/grid/generation/hashmap/AbstractRefinement.hpp>
 #include <sgpp/base/grid/generation/functors/SurplusRefinementFunctor.hpp>
 #include <sgpp/base/grid/generation/refinement_strategy/SubspaceRefinement.hpp>
@@ -24,6 +25,7 @@ using sgpp::base::GridGenerator;
 using sgpp::base::GridPoint;
 using sgpp::base::GridStorage;
 using sgpp::base::HashRefinement;
+using sgpp::base::HashRefinementBoundaries;
 using sgpp::base::SurplusRefinementFunctor;
 using sgpp::base::SubspaceRefinement;
 using sgpp::base::AbstractRefinement;
@@ -33,21 +35,9 @@ using sgpp::base::RefinementFunctor;
 BOOST_AUTO_TEST_SUITE(TestRefinementAddedPoints)
 
 /*
-  Utility for linear grid tests
+  Utility for tests
  */
-void linearGridTest(AbstractRefinement& ref) {
-  size_t dim = 4;
-  size_t level = 3;
-  std::unique_ptr<Grid> grid(Grid::createLinearGrid(dim));
-  GridStorage& gridStorage = grid->getStorage();
-  grid->getGenerator().regular(level);
-
-  DataVector alpha(gridStorage.getSize());
-  alpha.setAll(0.0);
-  // arbitrary choice
-  alpha[3] = 1.0; // level 3 1 1 1
-  alpha[37] = 1.0; // level 2 1 1 2
-
+void gridTest(GridStorage& gridStorage, DataVector& alpha, AbstractRefinement& ref) {
   std::vector<size_t> oldPoints; // points before refinement
   std::vector<size_t> newPoints; // points that are new after refinement
   std::vector<size_t> addedPoints; // points that are added by refinement
@@ -76,15 +66,39 @@ void linearGridTest(AbstractRefinement& ref) {
   }
 }
 
-BOOST_AUTO_TEST_CASE(TestLinearHash) {
+BOOST_AUTO_TEST_CASE(TestBasicHash) {
+  size_t dim = 4;
+  size_t level = 3;
+  std::unique_ptr<Grid> grid(Grid::createLinearGrid(dim));
+  GridStorage& gridStorage = grid->getStorage();
+  grid->getGenerator().regular(level);
+
+  DataVector alpha(gridStorage.getSize());
+  alpha.setAll(0.0);
+  // arbitrary choice
+  alpha[3] = 1.0; // level 3 1 1 1
+  alpha[37] = 1.0; // level 2 1 1 2
+
   HashRefinement refHash;
-  linearGridTest(refHash);
+  gridTest(gridStorage, alpha, refHash);
 }
 
-BOOST_AUTO_TEST_CASE(TestLinearSubspace) {
+BOOST_AUTO_TEST_CASE(TestBasicSubspace) {
+  size_t dim = 4;
+  size_t level = 3;
+  std::unique_ptr<Grid> grid(Grid::createLinearGrid(dim));
+  GridStorage& gridStorage = grid->getStorage();
+  grid->getGenerator().regular(level);
+
+  DataVector alpha(gridStorage.getSize());
+  alpha.setAll(0.0);
+  // arbitrary choice
+  alpha[3] = 1.0; // level 3 1 1 1
+  alpha[37] = 1.0; // level 2 1 1 2
+
   HashRefinement refHash;
   SubspaceRefinement refSub(&refHash);
-  linearGridTest(refSub);
+  gridTest(gridStorage, alpha, refSub);
 }
 
 BOOST_AUTO_TEST_CASE(TestLinearPredictive) {
@@ -92,11 +106,38 @@ BOOST_AUTO_TEST_CASE(TestLinearPredictive) {
 }
 
 BOOST_AUTO_TEST_CASE(TestBoundaryHash) {
-  // TODO
+  size_t dim = 3;
+  size_t level = 3;
+  std::unique_ptr<Grid> grid(Grid::createLinearBoundaryGrid(dim));
+  GridStorage& gridStorage = grid->getStorage();
+  grid->getGenerator().regular(level);
+
+  DataVector alpha(gridStorage.getSize());
+  alpha.setAll(0.0);
+  // arbitrary choice
+  alpha[30] = 1.0; // level 1 3 0
+  alpha[180] = 1.0; // level 1 3 1
+
+  HashRefinementBoundaries refHash;
+  gridTest(gridStorage, alpha, refHash);
 }
 
 BOOST_AUTO_TEST_CASE(TestBoundarySubspace) {
-  // TODO
+  size_t dim = 3;
+  size_t level = 3;
+  std::unique_ptr<Grid> grid(Grid::createLinearBoundaryGrid(dim));
+  GridStorage& gridStorage = grid->getStorage();
+  grid->getGenerator().regular(level);
+
+  DataVector alpha(gridStorage.getSize());
+  alpha.setAll(0.0);
+  // arbitrary choice
+  alpha[30] = 1.0; // level 1 3 0
+  alpha[180] = 1.0; // level 1 3 1
+
+  HashRefinementBoundaries refHash;
+  SubspaceRefinement refSub(&refHash);
+  gridTest(gridStorage, alpha, refSub);
 }
 
 BOOST_AUTO_TEST_CASE(TestBoundaryPredictive) {
