f81294e27e11d59eca58694c6acddf8d4f6aaca6
Michael Rehme <michael.rehme@ipvs.uni-stuttgart.de>
2018-05-28 16:24:48 +0200

    modified nak basis files i forgot in last commit

---
+++ base/src/sgpp/base/grid/type/NotAKnotBsplineModifiedGrid.cpp
@@ -0,0 +1,52 @@
+// Copyright (C) 2008-today The SG++ project
+// This file is part of the SG++ project. For conditions of distribution and
+// use, please see the copyright notice provided with SG++ or at
+// sgpp.sparsegrids.org
+
+#include <sgpp/base/grid/Grid.hpp>
+#include <sgpp/base/grid/GridStorage.hpp>
+#include <sgpp/base/grid/type/NotAKnotBsplineModifiedGrid.hpp>
+
+#include <sgpp/base/exception/factory_exception.hpp>
+
+#include <sgpp/globaldef.hpp>
+
+namespace sgpp {
+namespace base {
+
+NotAKnotBsplineModifiedGrid::NotAKnotBsplineModifiedGrid(std::istream& istr)
+    : Grid(istr), generator(storage), degree(1 << 16) {
+  istr >> degree;
+  basis_.reset(new SBsplineModifiedBase(degree));
+}
+
+NotAKnotBsplineModifiedGrid::NotAKnotBsplineModifiedGrid(size_t dim, size_t degree)
+    : Grid(dim), generator(storage), degree(degree), basis_(new SBsplineModifiedBase(degree)) {}
+
+NotAKnotBsplineModifiedGrid::~NotAKnotBsplineModifiedGrid() {}
+
+sgpp::base::GridType NotAKnotBsplineModifiedGrid::getType() {
+  return sgpp::base::GridType::NotAKnotBsplineModified;
+}
+
+const SBasis& NotAKnotBsplineModifiedGrid::getBasis() { return *basis_; }
+
+size_t NotAKnotBsplineModifiedGrid::getDegree() { return this->degree; }
+
+Grid* NotAKnotBsplineModifiedGrid::unserialize(std::istream& istr) {
+  return new NotAKnotBsplineModifiedGrid(istr);
+}
+
+void NotAKnotBsplineModifiedGrid::serialize(std::ostream& ostr, int version) {
+  this->Grid::serialize(ostr, version);
+  ostr << degree << std::endl;
+}
+
+/**
+ * Creates new GridGenerator
+ * This must be changed if we add other storage types
+ */
+GridGenerator& NotAKnotBsplineModifiedGrid::getGenerator() { return generator; }
+
+}  // namespace base
+}  // namespace sgpp
---
+++ base/src/sgpp/base/operation/hash/OperationQuadratureNotAKnotBsplineModified.hpp
@@ -0,0 +1,49 @@
+// Copyright (C) 2008-today The SG++ project
+// This file is part of the SG++ project. For conditions of distribution and
+// use, please see the copyright notice provided with SG++ or at
+// sgpp.sparsegrids.org
+
+#pragma once
+
+#include <sgpp/base/grid/Grid.hpp>
+#include <sgpp/base/operation/hash/OperationQuadrature.hpp>
+#include <sgpp/base/operation/hash/common/basis/NotAKnotBsplineModifiedBasis.hpp>
+
+#include <sgpp/globaldef.hpp>
+
+namespace sgpp {
+namespace base {
+
+/**
+ * Quadrature on sparse grid, not a knot B-spline grid created by transformation of a not a knot
+ * B-spline combigrid
+ */
+class OperationQuadratureNotAKnotBsplineModified : public OperationQuadrature {
+ public:
+  /**
+   * Constructor of OperationQuadratureNotAKnotBsplineModified
+   *
+   * @param storage Pointer to the grid's GridStorage object
+   * @param degree the B-spline degree
+   */
+  OperationQuadratureNotAKnotBsplineModified(GridStorage& storage, size_t degree)
+      : storage(storage), base(degree) {}
+
+  ~OperationQuadratureNotAKnotBsplineModified() override {}
+
+  /**
+   * Quadrature for not a knot B-spline basis functions
+   *
+   * @param alpha Coefficient vector for current grid
+   */
+  double doQuadrature(DataVector& alpha) override;
+
+ protected:
+  // Pointer to the grid's GridStorage object
+  GridStorage& storage;
+  /// NakBsplineBoundaryCombigrid Basis object
+  SNotAKnotBsplineModifiedBase base;
+};
+
+}  // namespace base
+}  // namespace sgpp
