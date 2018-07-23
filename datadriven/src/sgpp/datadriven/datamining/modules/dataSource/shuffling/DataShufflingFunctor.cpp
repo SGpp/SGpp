c9c4d5eeb83cb301ac4efe4ba005033317e8795e
WodkaRHR <domi.erdnuss2@gmx.de>
2018-07-23 13:21:41 +0200

    Shuffling (not building)

---
+++ datadriven/src/sgpp/datadriven/datamining/modules/dataSource/shuffling/DataShufflingFunctor.cpp
@@ -0,0 +1,22 @@
+/* Copyright (C) 2008-today The SG++ project
+ * This file is part of the SG++ project. For conditions of distribution and
+ * use, please see the copyright notice provided with SG++ or at
+ * sgpp.sparsegrids.org
+ *
+ * DataShufflingFunctor.cpp
+ *
+ *  Created on: Jul 23, 2018
+ *      Author: dominik
+ */
+#include <sgpp/datadriven/datamining/modules/dataSource/shuffling/DataShufflingFunctor.hpp>
+
+namespace sgpp {
+namespace datadriven {
+DataShufflingFunctor::DataShufflingFunctor() { }
+} /* namespace datadriven */
+} /* namespace sgpp */
+
+
+
+
+
