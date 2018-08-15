0e44561e61f659a1ba5619b8b69bd57ab12e2134
Julian Valentin <julian.valentin@ipvs.uni-stuttgart.de>
2018-08-15 14:42:52 +0200

    Remove broken debugging code from HashGenerator
    Fixes #59.

--- base/src/sgpp/base/grid/generation/hashmap/HashGenerator.hpp
+++ base/src/sgpp/base/grid/generation/hashmap/HashGenerator.hpp
@@ -16,16 +16,6 @@
 #include <vector>
 #include <iterator>
 
-template <typename T>
-std::ostream& operator<<(std::ostream& out, const std::vector<T>& v) {
-  if (!v.empty()) {
-    out << '[';
-    std::copy(v.begin(), v.end(), std::ostream_iterator<T>(out, ", "));
-    out << "\b\b]";
-  }
-  return out;
-}
-
 namespace sgpp {
 namespace base {
 
