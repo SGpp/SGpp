3a3304cfd1b55e9a973093d573dba7b7006a8412
Dirk Pflueger <dirk.pflueger@ipvs.uni-stuttgart.de>
2018-08-22 09:40:14 +0200

    Release310patches

--- tools/create_release/git_scripts/01_create_new_release_from_tag.sh
+++ tools/create_release/git_scripts/01_create_new_release_from_tag.sh
@@ -8,17 +8,17 @@
 # clone Github release repository
 git clone git@github.com:SGpp/SGpp.git SGpp_github
 
-# clone SG++ without checkout
-git clone --no-checkout git@simsgs.informatik.uni-stuttgart.de:SGpp/SGpp.git
+# clone SG++ for certain release tag
+git clone -b ${RELEASE} git@simsgs.informatik.uni-stuttgart.de:SGpp/SGpp.git
 cd SGpp
-# create new release branch
-git checkout -b ${RELEASE} ${REVISION}
-# checkout based on whitelist
-mv .git ../
-rm -rf * .*
-mv ../.git/ ./
-(git checkout ${REVISION} -- `cat ../../RELEASE_CONTENTS.sh | grep -v "#"`) || exit 1
-git commit -am "New release branch ${RELEASE}"
+# # create new release branch
+# git checkout -b ${RELEASE} ${REVISION}
+# # checkout based on whitelist
+# mv .git ../
+# rm -rf * .*
+# mv ../.git/ ./
+# (git checkout ${RELEASE}) || exit 1
+# git commit -am "New release branch ${RELEASE}"
 
 # write new state for Github release repository
 rsync --exclude=.git -av --delete-after . ../SGpp_github/
@@ -26,6 +26,7 @@ cd ../SGpp_github/
 git add .
 # checkin
 git commit -m "New release ${RELEASE}"
+git tag -a ${RELEASE}
 cd ..
 
 echo "Now test (at least SGpp)!"
