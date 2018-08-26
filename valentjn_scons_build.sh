b53a8eaeabae2ba735eef67ec079a8ffa3f840c7
Julian Valentin <julian.valentin@ipvs.uni-stuttgart.de>
2018-08-26 17:07:19 +0200

    Use REAL_HOME in valentjn_scons_build
    Don't use R2018a! Otherwise I get linker errors:
    
    g++ -o optimization/examples/optimization -fopenmp -Wl,-rpath=/data/scratch-ssd1/valentjn/local/share/matlab/R2018a/bin/glnxa64 optimization/examples/optimization.o -L/data/scratch-ssd1/valentjn/local/share/matlab/R2018a/bin/glnxa64 -L/usr/lib/x86_64-linux-gnu -Llib/sgpp -lsgppoptimizationstatic -lsgppbasestatic -leng -lmat -lmx -lmex
    /data/scratch-ssd1/valentjn/local/share/matlab/R2018a/bin/glnxa64/libCppMicroServices.so.3.1.1: Nicht definierter Verweis auf `std::thread::_State::~_State()@GLIBCXX_3.4.22'
    /data/scratch-ssd1/valentjn/local/share/matlab/R2018a/bin/glnxa64/libCppMicroServices.so.3.1.1: Nicht definierter Verweis auf `std::thread::_M_start_thread(std::unique_ptr<std::thread::_State, std::default_delete<std::thread::_State> >, void (*)())@GLIBCXX_3.4.22'
    /data/scratch-ssd1/valentjn/local/share/matlab/R2018a/bin/glnxa64/libCppMicroServices.so.3.1.1: Nicht definierter Verweis auf `typeinfo for std::thread::_State@GLIBCXX_3.4.22'
    collect2: error: ld returned 1 exit status
    scons: *** [optimization/examples/optimization] Error 1
    scons: building terminated because of errors.

--- valentjn_scons_build.sh
+++ valentjn_scons_build.sh
@@ -2,6 +2,6 @@
 
 scons -j $(nproc) VERBOSE=1 OPT=1 SG_ALL=0 SG_BASE=1 SG_OPTIMIZATION=1 SG_PYTHON=1 \
     USE_MATLAB=1 BUILD_STATICLIB=1 USE_PYTHON3_FOR_PYSGPP=1 RUN_PYTHON_TESTS=0 \
-    CPPPATH=/home/valentjn/local/share/matlab/R2017a/extern/include \
-    LIBPATH=/home/valentjn/local/share/matlab/R2017a/bin/glnxa64 \
-    RPATH=/home/valentjn/local/share/matlab/R2017a/bin/glnxa64
+    CPPPATH=$REAL_HOME/local/share/matlab/R2017a/extern/include \
+    LIBPATH=$REAL_HOME/local/share/matlab/R2017a/bin/glnxa64 \
+    RPATH=$REAL_HOME/local/share/matlab/R2017a/bin/glnxa64
