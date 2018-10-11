c3942199e85e7a2fc223158d467762cee206ec19
Michael Rehme <michael.rehme@ipvs.uni-stuttgart.de>
2018-10-11 13:10:30 +0200

    removed Rand_double and went with RandomNumberGenerator for MC

--- optimization/src/sgpp/optimization/activeSubspaces/ASMatrixGradientMC.cpp
+++ optimization/src/sgpp/optimization/activeSubspaces/ASMatrixGradientMC.cpp
@@ -7,36 +7,18 @@
 
 #include <sgpp/optimization/activeSubspaces/ASMatrixGradientMC.hpp>
 
-class Rand_double {
- public:
-  Rand_double(double low, double high)
-      : r(std::bind(std::uniform_real_distribution<>(low, high), std::default_random_engine())) {}
-
-  double operator()() { return r(); }
-
- private:
-  std::function<double()> r;
-};
-
 namespace sgpp {
 namespace optimization {
 
 void ASMatrixGradientMC::createMatrixMonteCarlo(size_t numPoints) {
-  //  RandomNumberGenerator::getInstance().setSeed();
+  RandomNumberGenerator::getInstance().setSeed();
   C.resize(numDim, numDim);
   C.setZero();
   evaluationPoints.resizeZero(numPoints, numDim);
   functionValues.resizeZero(numPoints);
-  Rand_double rd{0, 1};
   for (size_t i = 0; i < numPoints; ++i) {
     sgpp::base::DataVector randomVector(numDim, 1);
-    // todo (rehmemk) somehow the randomnumbergenerator results are much worse than the Rand_double
-    // result
     RandomNumberGenerator::getInstance().getUniformRV(randomVector, 0.0, 1.0);
-    //    for (size_t d = 0; d < numDim; ++d) {
-    //      randomVector[d] = rd();
-    //    }
-    //    std::cout << randomVector.toString() << std::endl;
     sgpp::base::DataVector gradient(numDim);
     functionValues[i] = objectiveFuncGradient.eval(randomVector, gradient);
     evaluationPoints.setRow(i, randomVector);
--- optimization/src/sgpp/optimization/activeSubspaces/ASMatrixNakBspline.cpp
+++ optimization/src/sgpp/optimization/activeSubspaces/ASMatrixNakBspline.cpp
@@ -7,17 +7,6 @@
 
 #include <sgpp/optimization/activeSubspaces/ASMatrixNakBspline.hpp>
 
-class Rand_double {
- public:
-  Rand_double(double low, double high)
-      : r(std::bind(std::uniform_real_distribution<>(low, high), std::default_random_engine())) {}
-
-  double operator()() { return r(); }
-
- private:
-  std::function<double()> r;
-};
-
 namespace sgpp {
 namespace optimization {
 void ASMatrixNakBspline::buildRegularInterpolant(size_t level) {
@@ -55,19 +44,12 @@ void ASMatrixNakBspline::createMatrixMonteCarlo(size_t numPoints) {
   sgpp::optimization::InterpolantScalarFunctionGradient interpolantGradient(*(this->grid),
                                                                             this->coefficients);
 
-  //  RandomNumberGenerator::getInstance().setSeed();
+  RandomNumberGenerator::getInstance().setSeed();
   C.resize(numDim, numDim);
   C.setZero();
-  Rand_double rd{0, 1};
   for (size_t i = 0; i < numPoints; ++i) {
     sgpp::base::DataVector randomVector(numDim, 1);
-    // todo (rehmemk) somehow the randomnumbergenerator results are much worse than the Rand_double
-    // result
     RandomNumberGenerator::getInstance().getUniformRV(randomVector, 0.0, 1.0);
-    //    for (size_t d = 0; d < numDim; ++d) {
-    //      randomVector[d] = rd();
-    //    }
-    //    std::cout << randomVector.toString() << std::endl;
     sgpp::base::DataVector gradient(numDim);
     interpolantGradient.eval(randomVector, gradient);
     Eigen::VectorXd e = DataVectorToEigen(gradient);
