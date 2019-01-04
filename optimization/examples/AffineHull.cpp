dffa2a897876010fd1a74b14f92b46bc8bed243d
Michael Rehme <michael.rehme@ipvs.uni-stuttgart.de>
2019-01-04 15:26:52 +0100

    affine hull demo works in general now but volume results are terrible!

--- optimization/examples/AffineHull.cpp
+++ optimization/examples/AffineHull.cpp
@@ -39,35 +39,32 @@ dd_MatrixPtr createHPolytopeDescription(size_t dim, Eigen::MatrixXd A0, Eigen::V
   return hMatrix;
 }
 
-Eigen::MatrixXd affineHull(dd_MatrixPtr vRep, Eigen::MatrixXd A0) {
-  dd_Amatrix vRepMatrix = vRep->matrix;
-  Eigen::MatrixXd M(vRep->rowsize, vRep->colsize - 1);
-  for (unsigned int i = 0; i < vRep->rowsize; i++) {
-    for (unsigned int j = 0; j < vRep->colsize - 1; j++) {
-      M(i, j) = vRepMatrix[i][j + 1][0];
+Eigen::MatrixXd hypercubeCorners(size_t dimension) {
+  size_t twoDim = static_cast<size_t>(std::pow(2, dimension));
+  Eigen::MatrixXd corners = Eigen::MatrixXd::Zero(
+      static_cast<unsigned int>(dimension), static_cast<unsigned int>(std::pow(2, dimension)));
+  if (dimension <= 0) {
+    std::cerr << "ASResponseSurfaceNakBspline: dimension must be > 0!";
+  } else if (dimension == 1) {
+    corners(0, 0) = 1;
+    corners(0, 1) = 0;
+    return corners;
+  }
+  size_t jump = static_cast<size_t>(std::pow(2, dimension - 1));
+  for (size_t i = 0; i < dimension; i++) {
+    size_t j = 0;
+    while (j < twoDim) {
+      for (size_t n = 0; n < jump; n++) {
+        if (j + n >= twoDim) {
+          break;
+        }
+        corners(i, j + n) = 1;
+      }
+      j = j + 2 * jump;
     }
+    jump = jump / 2;
   }
-  dd_FreeMatrix(vRep);
-
-  std::cout << "M:\n";
-  std::cout << M << "\n\n";
-
-  // move to origin
-  for (Eigen::Index i = M.rows(); i-- > 0;) {
-    M.row(i) -= M.row(0);
-  }
-  // rotate s.t. normal vector (=active subspace) is parallel to axis
-  Eigen::MatrixXd Q;
-  Eigen::HouseholderQR<Eigen::MatrixXd> qr(A0);
-  Q = qr.householderQ();
-
-  //  std::cout << "Q:\n" << Q << "\n\n";
-  //  std::cout << "Q*A\n" << Q * A0 << "\n\n";
-
-  M = (Q * M.transpose()).transpose();
-  std::cout << "(Q * M^T)^T\n";
-  std::cout << M << "\n---\n";
-  return M;
+  return corners;
 }
 
 double hPolytopeVolume(dd_MatrixPtr hRep) {
@@ -126,63 +123,80 @@ double hPolytopeVolume(dd_MatrixPtr hRep) {
   return vol2;
 }
 
-int main() {
-  dd_set_global_constants(); /* First, this must be called. */
+Eigen::MatrixXd affineHull(dd_MatrixPtr vRep, Eigen::MatrixXd A0) {
+  dd_Amatrix vRepMatrix = vRep->matrix;
+  Eigen::MatrixXd M(vRep->rowsize, vRep->colsize - 1);
+  for (unsigned int i = 0; i < vRep->rowsize; i++) {
+    for (unsigned int j = 0; j < vRep->colsize - 1; j++) {
+      M(i, j) = vRepMatrix[i][j + 1][0];
+    }
+  }
+  dd_FreeMatrix(vRep);
 
-  size_t originalDim = 2;
-  Eigen::MatrixXd A0(2, 1);
-  A0(0, 0) = 1.0 / sqrt(2.0);
-  A0(1, 0) = 1.0 / sqrt(2.0);
-  Eigen::VectorXd point(originalDim);
-  point(0) = 1.0 / 3.0;
-  point(1) = 0.0;
-  dd_MatrixPtr hMatrix = createHPolytopeDescription(originalDim, A0, point);
+  //  std::cout << "M:\n";
+  //  std::cout << M << "\n\n";
 
-  //  size_t originalDim = 3;
-  //  Eigen::MatrixXd A0(originalDim, 1);
-  //  A0(0, 0) = -1.0 / sqrt(3.0);
-  //  A0(1, 0) = 1.0 / sqrt(3.0);
-  //  A0(2, 0) = 1.0 / sqrt(3.0);
-  //  Eigen::VectorXd point(originalDim);
-  //  point(0) = 1;
-  //  point(1) = 0;
-  //  point(2) = 1;
-  //  dd_MatrixPtr hMatrix = createHPolytopeDescription(originalDim, A0, point);
+  // move to origin
+  for (Eigen::Index i = M.rows(); i-- > 0;) {
+    M.row(i) -= M.row(0);
+  }
+  // rotate s.t. normal vector (=active subspace) is parallel to axis
+  Eigen::MatrixXd Q;
+  Eigen::HouseholderQR<Eigen::MatrixXd> qr(A0);
+  Q = qr.householderQ();
 
+  //  std::cout << "Q:\n" << Q << "\n\n";
+  //  std::cout << "Q*A\n" << Q * A0 << "\n\n";
+
+  M = (Q * M.transpose()).transpose();
+  //  std::cout << "(Q * M^T)^T\n";
+  //  std::cout << M << "\n---\n";
+  return M;
+}
+
+bool reduceHrepresentation(dd_MatrixPtr& hMatrix, Eigen::MatrixXd A0) {
   dd_ErrorType err;
   dd_PolyhedraPtr poly = dd_DDMatrix2Poly(hMatrix, &err);
+  std::cout << "err: " << err << "\n";
   dd_FreeMatrix(hMatrix);
-  dd_Amatrix A = poly->A;
   std::cout << "H-Representation:\n";
   for (dd_rowrange m = 0; m < poly->m; m++) {
     for (dd_colrange d = 0; d < poly->d; d++) {
-      std::cout << A[m][d][0] << " ";
+      std::cout << poly->A[m][d][0] << " ";
     }
     std::cout << "\n";
   }
+  std::cout << "\n";
   dd_MatrixPtr vRep = dd_CopyGenerators(poly);
   dd_FreePolyhedra(poly);
-  //  std::cout << "V-Representation:\n";
-  //  dd_Amatrix vRepMatrix = vRep->matrix;
-  //  for (size_t i = 0; i < vRep->rowsize; i++) {
-  //    for (size_t j = 0; j < vRep->colsize; j++) {
-  //      std::cout << vRepMatrix[i][j][0] << " ";
-  //    }
-  //    std::cout << "\n";
-  //  }
+  std::cout << "V-Representation:\n";
+  dd_Amatrix vRepMatrix = vRep->matrix;
+  for (size_t i = 0; i < vRep->rowsize; i++) {
+    for (size_t j = 0; j < vRep->colsize; j++) {
+      std::cout << vRepMatrix[i][j][0] << " ";
+    }
+    std::cout << "\n";
+  }
+  std::cout << "*****\n";
 
-  std::cout << "--------------\n";
+  // if the cut object is lower dimensional than the reuced space return false resulting in volume
+  // 0. (Only a corner point, nonexistent at all)
+  if (vRep->rowsize < hMatrix->colsize - 1) {
+    std::cout
+        << "reduceHrepresentation: polytope does not have and affine hull of desired dimension\n";
+    return false;
+  }
 
   Eigen::MatrixXd M = affineHull(vRep, A0);
 
   // V -> H
   // delete axis parallel coordinate which is now obsolete (axes are enumbered 0,1,..)
   size_t obsoleteCoordinate = 0;
-  std::cout << "removing " << obsoleteCoordinate << "'th coordinate.\n";
+  //  std::cout << "removing " << obsoleteCoordinate << "'th coordinate.\n";
   dd_MatrixPtr vMatrix = dd_CreateMatrix(M.rows(), M.cols());
-  for (size_t i = 0; i < M.rows(); i++) {
+  for (unsigned int i = 0; i < M.rows(); i++) {
     vMatrix->matrix[i][0][0] = 1.0;
-    for (size_t j = 0; j < M.cols(); j++) {
+    for (unsigned int j = 0; j < M.cols(); j++) {
       if (j < obsoleteCoordinate) {
         vMatrix->matrix[i][j + 1][0] = M(i, j);
       } else if (j > obsoleteCoordinate) {
@@ -190,34 +204,51 @@ int main() {
       }
     }
   }
-
-  std::cout << "vMatrix\n";
-  for (size_t i = 0; i < M.rows(); i++) {
-    for (size_t j = 0; j < M.cols(); j++) {
-      std::cout << vMatrix->matrix[i][j][0] << " ";
-    }
-    std::cout << "\n";
-  }
-
+  //  std::cout << "vMatrix\n";
+  //  for (unsigned int i = 0; i < M.rows(); i++) {
+  //    for (unsigned int j = 0; j < M.cols(); j++) {
+  //      std::cout << vMatrix->matrix[i][j][0] << " ";
+  //    }
+  //    std::cout << "\n";
+  //  }
   vMatrix->numbtype = dd_Real;
   vMatrix->representation = dd_Generator;
   dd_PolyhedraPtr vPoly = dd_DDMatrix2Poly(vMatrix, &err);
   dd_FreeMatrix(vMatrix);
-
-  // Eigentlich unnötig. Volesti kann auch direkt aus V-Darstellung das Volumen berechnen!
-  std::cout << "H-Representation:\n";
   dd_MatrixPtr hRep = dd_CopyInequalities(vPoly);
+  hMatrix = hRep;
   dd_FreePolyhedra(vPoly);
-  dd_Amatrix hRepMatrix = hRep->matrix;
-  for (dd_rowrange i = 0; i < hRep->rowsize; i++) {
-    for (dd_colrange j = 0; j < hRep->colsize; j++) {
-      std::cout << hRepMatrix[i][j][0] << " ";
+  return true;
+}
+
+int main() {
+  dd_set_global_constants(); /* First, this must be called. */
+
+  size_t dim = 2;
+  Eigen::MatrixXd A0(dim, 1);
+  A0(0, 0) = 1.0 / sqrt(2.0);
+  A0(1, 0) = 1.0 / sqrt(2.0);
+
+  //  size_t dim = 3;
+  //  Eigen::MatrixXd A0(dim, 1);
+  //  A0(0, 0) = -1.0 / sqrt(3.0);
+  //  A0(1, 0) = 1.0 / sqrt(3.0);
+  //  A0(2, 0) = 1.0 / sqrt(3.0);
+
+  Eigen::MatrixXd corners = hypercubeCorners(dim);
+  for (size_t c = 0; c < corners.cols(); c++) {
+    Eigen::VectorXd corner = corners.col(c);
+    dd_MatrixPtr hMatrix = createHPolytopeDescription(dim, A0, corner);
+
+    // Unnötig, dass reduzierte V-Darstellung nochmal in H umgewandelt wird. Volesti kann auch
+    // direkt aus V-Darstellung das Volumen berechnen!
+    bool succ = reduceHrepresentation(hMatrix, A0);
+    double vol = 0.0;
+    if (succ == true) {
+      vol = hPolytopeVolume(hMatrix);
     }
-    std::cout << "\n";
+    std::cout << "Volume through (" << corner.transpose() << ") = " << vol << std::endl;
   }
 
-  std::cout << "--------------\n";
-  double vol2 = hPolytopeVolume(hRep);
-  std::cout << "Computed volume (alg.2) = " << vol2 << std::endl;
   return 0;
 }
