d929fdc510dbb9e48f86be03bb2e5ebafe780e4c
nico roesel <ga42tak@mytum.de>
2019-01-09 14:56:08 +0100

    saving

--- datadriven/src/sgpp/datadriven/datamining/modules/fitting/ModelFittingBase.cpp
+++ datadriven/src/sgpp/datadriven/datamining/modules/fitting/ModelFittingBase.cpp
@@ -52,8 +52,6 @@ Grid *ModelFittingBase::buildGrid(const RegularGridConfiguration &gridConfig) co
     tmpGrid = Grid::createLinearBoundaryGrid(gridConfig.dim_);
   } else if (gridConfig.type_ == GridType::ModLinear) {
     tmpGrid = Grid::createModLinearGrid(gridConfig.dim_);
-  } else if(gridConfig.type_ == GridType::AnisotropicFullGrid){
-	tmpGrid = Grid::createAnisotropicFullGrid(gridConfig.dim_, gridConfig.levelVector_);
  } else {
     throw factory_exception("ModelFittingBase::createRegularGrid: grid type is not supported");
   }
