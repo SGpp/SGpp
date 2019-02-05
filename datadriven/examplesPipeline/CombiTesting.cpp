8e68530c08e61b377e202324072785665a46ea71
nico roesel <ga42tak@mytum.de>
2019-02-05 14:17:07 +0100

    ..

---
+++ datadriven/examplesPipeline/CombiTesting.cpp
@@ -0,0 +1,38 @@
+/*
+ * CombiTesting.cpp
+ *
+ *  Created on: Jan 29, 2019
+ *      Author: nico
+ */
+
+#include <iostream>
+#include <sgpp/base/grid/Grid.hpp>
+#include <sgpp/datadriven/datamining/configuration/CombiConfigurator.hpp>
+#include <sgpp/datadriven/datamining/configuration/DataMiningConfigParser.hpp>
+#include <sgpp/datadriven/datamining/modules/fitting/FitterConfigurationDensityEstimation.hpp>
+#include <sgpp/datadriven/datamining/modules/fitting/ModelFittingDensityEstimationCombiGrid.hpp>
+
+using std::cout;
+using std::vector;
+using sgpp::datadriven::combiConfig;
+
+int main(int argc, char **argv) {
+  const std::string path = [argc, &argv]() {
+    if (argc != 2) {
+      std::cout << "No or bad path given, aborting\n";
+      exit(1);
+      return std::string{};
+    } else {
+      return std::string{argv[1]};
+    }
+  }();
+
+  const sgpp::datadriven::DataMiningConfigParser parser(path);
+
+  sgpp::datadriven::FitterConfigurationDensityEstimation config{};
+  config.readParams(parser);
+  auto fittingbase = new sgpp::datadriven::ModelFittingDensityEstimationCombiGrid(config);
+  cout << "ModelFittingDensityEstimationCombiGrid created!" << std::endl;
+
+  return 0;
+}
