23dfb323ff6b1c09191e270dc25d5412aee0da36
Julian Valentin <julian.valentin@ipvs.uni-stuttgart.de>
2019-03-11 18:10:42 +0100

    Remove datasets and Git LFS

--- datasets/house_numbers/README.txt
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ea66a5c44d7108f767db88ca1ab2c11ec46e53a06b417f622744f33360deee65
-size 707
--- datasets/parabola/parabolasimple.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8042a058b7e719f5e68ae1a4e0248de95376d2b5c80dd892a61c06e05b46064a
-size 1105
