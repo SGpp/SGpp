3d9ad91e7741c932d56ca2ec51b77fa6e6d39db0
Julian Valentin <julian.valentin@ipvs.uni-stuttgart.de>
2019-03-14 12:39:15 +0100

    Fix style errors (lines too long)

--- datadriven/tests/streamingMultTransposeTest.cpp
+++ datadriven/tests/streamingMultTransposeTest.cpp
@@ -32,8 +32,10 @@ struct FilesNamesAndErrorFixture {
   ~FilesNamesAndErrorFixture() {}
 
   std::vector<std::tuple<std::string, double>> fileNamesErrorDouble = {
-      std::tuple<std::string, double>("datadriven/datasets/friedman/friedman2_4d_10000.arff.gz", 1E-19),
-      std::tuple<std::string, double>("datadriven/datasets/friedman/friedman1_10d_2000.arff.gz", 1E-26)};
+      std::tuple<std::string, double>(
+        "datadriven/datasets/friedman/friedman2_4d_10000.arff.gz", 1E-19),
+      std::tuple<std::string, double>(
+        "datadriven/datasets/friedman/friedman1_10d_2000.arff.gz", 1E-26)};
 
   uint32_t level = 5;
 };
