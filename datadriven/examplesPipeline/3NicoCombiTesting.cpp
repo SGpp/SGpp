b58a20b7fba9701ced9fd09e2398d743294d392d
nico roesel <ga42tak@mytum.de>
2019-04-04 10:11:08 +0200

    adding stuff

---
+++ datadriven/examplesPipeline/3NicoCombiTesting.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <sgpp/datadriven/datamining/configuration/CombiConfigurator.hpp>
+#include <stdexcept>
+#include <string>
+
+using std::cout;
+using std::vector;
+using sgpp::datadriven::combiConfig;
+
+int main(int argc, char** argv) {
+  size_t dim, level;
+
+  if (argc > 2) {
+    dim = std::stoi(argv[1]);
+    level = std::stoi(argv[2]);
+  } else {
+    cout << "setting defaults: dim = 2, level = 3" << std::endl;
+    dim = 2;
+    level = 3;
+  }
+
+  sgpp::datadriven::CombiConfigurator configurator;
+  configurator.initAdaptiveScheme(dim, level);
+
+  vector<combiConfig> vec;
+
+  configurator.getCombiScheme(vec);
+
+  for (int i = 0; i < vec.size(); i++) {
+    if (configurator.isRefinable(vec.at(i))) {
+      cout << "TRUE" << std::endl;
+    } else {
+      cout << "FALSE" << std::endl;
+    }
+  }
+  configurator.initAdaptiveScheme(dim, level);
+  configurator.refineComponent(vec.at(1));
+
+  configurator.getCombiScheme(vec);
+  /*
+    for (int i = 0; i < vec.size(); i++) {
+      if (configurator.isRefinable(vec.at(i))) {
+        cout << "TRUE" << std::endl;
+      } else {
+        cout << "FALSE" << std::endl;
+      }
+    }
+  */
+  cout << std::endl << "Test \"3NicoCombiTesting\" terminated successfully!" << std::endl;
+}
