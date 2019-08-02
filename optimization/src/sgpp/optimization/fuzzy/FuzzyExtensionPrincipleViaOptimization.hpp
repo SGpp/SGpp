41c896b2954f102b5bcb812528e4968cd04fd33e
Julian Valentin <julian.valentin@ipvs.uni-stuttgart.de>
2019-08-02 15:35:50 +0200

    Add docs and comments for fuzzy extension principle

--- optimization/src/sgpp/optimization/fuzzy/FuzzyExtensionPrincipleViaOptimization.hpp
+++ optimization/src/sgpp/optimization/fuzzy/FuzzyExtensionPrincipleViaOptimization.hpp
@@ -22,33 +22,82 @@
 namespace sgpp {
 namespace optimization {
 
+/**
+ * Zadeh's fuzzy extension principle by solving optimization problems
+ * for each \f$\alpha\f$ level.
+ */
 class FuzzyExtensionPrincipleViaOptimization : public FuzzyExtensionPrinciple {
  public:
+  /**
+   * Constructor.
+   * By default, MultiStart is used as optimization algorithm.
+   *
+   * @param f                       function through which to propagate the uncertainties
+   * @param numberOfAlphaSegments   number of \f$\alpha\f$ segments
+   */
   explicit FuzzyExtensionPrincipleViaOptimization(
       const ScalarFunction& f,
       size_t numberOfAlphaSegments = DEFAULT_NUMBER_OF_ALPHA_SEGMENTS);
 
+  /**
+   * Constructor with custom optimization algorithm.
+   *
+   * @param optimizer               optimization algorithm and
+   *                                function through which to propagate the uncertainties
+   * @param numberOfAlphaSegments   number of \f$\alpha\f$ segments
+   */
   explicit FuzzyExtensionPrincipleViaOptimization(
       const optimizer::UnconstrainedOptimizer& optimizer,
       size_t numberOfAlphaSegments = DEFAULT_NUMBER_OF_ALPHA_SEGMENTS);
 
+  /**
+   * Copy constructor.
+   *
+   * @param other   other fuzzy extension principle
+   */
   FuzzyExtensionPrincipleViaOptimization(const FuzzyExtensionPrincipleViaOptimization& other);
 
+  /**
+   * Destructor.
+   */
   ~FuzzyExtensionPrincipleViaOptimization() override;
 
+  /**
+   * @param[out]  clone pointer to cloned object
+   */
   void clone(std::unique_ptr<FuzzyExtensionPrinciple>& clone) const override;
 
  protected:
+  /// default optimization algorithm
   optimizer::MultiStart defaultOptimizer;
+  /// optimization algorithm
   std::unique_ptr<optimizer::UnconstrainedOptimizer> optimizer;
+  /// objective function gradient
   std::unique_ptr<ScalarFunctionGradient> fGradient;
+  /// objective function Hessian
   std::unique_ptr<ScalarFunctionHessian> fHessian;
+  /// scaled objective function (confidence interval to unit hyper-cube)
   std::unique_ptr<ScalarFunction> fScaled;
+  /// scaled objective gradient (confidence interval to unit hyper-cube)
   std::unique_ptr<ScalarFunctionGradient> fGradientScaled;
+  /// scaled objective Hessian (confidence interval to unit hyper-cube)
   std::unique_ptr<ScalarFunctionHessian> fHessianScaled;
 
+  /**
+   * Custom preparation method that is called before the parallelized
+   * optimizeForSingleAlphaLevel calls.
+   */
   void prepareApply() override;
 
+  /**
+   * Solve the minimization/maximization problem for a single \f$\alpha\f$ level.
+   *
+   * @param[in]   j             index of \f$\alpha\f$ level
+   * @param[out]  minimumPoint  minimum point
+   * @param[out]  minimumValue  minimum function value
+   * @param[out]  maximumPoint  maximum point
+   * @param[out]  maximumValue  maximum function value
+   */
   void optimizeForSingleAlphaLevel(
       size_t j, base::DataVector& minimumPoint, double& minimumValue,
       base::DataVector& maximumPoint, double& maximumValue) override;
--- optimization/src/sgpp/optimization/fuzzy/FuzzyExtensionPrincipleViaVertexMethod.hpp
+++ optimization/src/sgpp/optimization/fuzzy/FuzzyExtensionPrincipleViaVertexMethod.hpp
@@ -15,24 +15,60 @@
 namespace sgpp {
 namespace optimization {
 
+/**
+ * Zadeh's fuzzy extension principle by the vertex method, where the optimization
+ * problems are solved by simply taking the best corners of the confidence intervals.
+ */
 class FuzzyExtensionPrincipleViaVertexMethod : public FuzzyExtensionPrincipleViaOptimization {
  public:
+  /**
+   * Constructor.
+   *
+   * @param f                       function through which to propagate the uncertainties
+   * @param numberOfAlphaSegments   number of \f$\alpha\f$ segments
+   */
   explicit FuzzyExtensionPrincipleViaVertexMethod(
       const ScalarFunction& f,
       size_t numberOfAlphaSegments = DEFAULT_NUMBER_OF_ALPHA_SEGMENTS);
 
+  /**
+   * Copy constructor.
+   *
+   * @param other   other fuzzy extension principle
+   */
   FuzzyExtensionPrincipleViaVertexMethod(const FuzzyExtensionPrincipleViaVertexMethod& other);
 
+  /**
+   * Destructor.
+   */
   ~FuzzyExtensionPrincipleViaVertexMethod() override;
 
+  /**
+   * @param[out]  clone pointer to cloned object
+   */
   void clone(std::unique_ptr<FuzzyExtensionPrinciple>& clone) const override;
 
  protected:
+  /// precomputed storage for the powers of two
   std::vector<size_t> powersOfTwo;
+  /// temporary vector used in optimizeForSingleAlphaLevel
   base::DataVector xTmp;
 
+  /**
+   * Custom preparation method that is called before the parallelized
+   * optimizeForSingleAlphaLevel calls.
+   */
   void prepareApply() override;
 
+  /**
+   * Solve the minimization/maximization problem for a single \f$\alpha\f$ level.
+   *
+   * @param[in]   j             index of \f$\alpha\f$ level
+   * @param[out]  minimumPoint  minimum point
+   * @param[out]  minimumValue  minimum function value
+   * @param[out]  maximumPoint  maximum point
+   * @param[out]  maximumValue  maximum function value
+   */
   void optimizeForSingleAlphaLevel(
       size_t j, base::DataVector& minimumPoint, double& minimumValue,
       base::DataVector& maximumPoint, double& maximumValue) override;
