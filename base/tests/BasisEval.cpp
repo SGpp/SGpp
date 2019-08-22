70b0643544541ae78b1e3aa7f4397255bb4c7531
Julian Valentin <julian.valentin@ipvs.uni-stuttgart.de>
2019-08-22 15:30:07 +0200

    Remove unnecessary limits include

--- base/tests/BasisEval.cpp
+++ base/tests/BasisEval.cpp
@@ -6,8 +6,6 @@
 #define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
 
-#include <limits>
-
 #include "BasisEval.hpp"
 
 double basisEvalDx(sgpp::base::SBasis& basis, sgpp::base::level_t l,
