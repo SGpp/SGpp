2e4f499f4e2f1a82cb91f1f337bbb83930ed008e
Michael Rehme <michael.rehme@ipvs.uni-stuttgart.de>
2019-10-01 18:58:06 +1000

    fixed missing regularByPoints option

--- MR_Python/Extended/dataHandling.py
+++ MR_Python/Extended/dataHandling.py
@@ -4,33 +4,42 @@ import os
 
 def createFileName(gridType, model, refineType, maxPoints, maxLevel, degree, objFunc):
     if refineType == 'regular':
-        saveName = '{}_{}{}_{}{}.pkl'.format(objFunc.getName(), refineType, maxLevel, gridType, degree)
+        saveName = '{}_{}{}_{}{}.pkl'.format(
+            objFunc.getName(), refineType, maxLevel, gridType, degree)
+    elif refineType == 'regularByPoints':
+        saveName = '{}_{}{}_{}{}.pkl'.format(
+            objFunc.getName(), refineType, maxPoints, gridType, degree)
     elif refineType == 'mc':
         saveName = '{}_mc_{}.pkl'.format(objFunc.getName(), maxPoints)
     elif refineType == 'surplus':
-        saveName = '{}_{}{}_{}{}.pkl'.format(objFunc.getName(), refineType, maxPoints, gridType, degree)
+        saveName = '{}_{}{}_{}{}.pkl'.format(
+            objFunc.getName(), refineType, maxPoints, gridType, degree)
     return saveName
 
 
 def saveData(data, gridType, model, refineType, maxPoints, maxLevel, degree, objFunc):
-    directory = os.path.join('/home/rehmemk/git/SGpp/MR_Python/Extended/data/', model, objFunc.getName())
+    directory = os.path.join(
+        '/home/rehmemk/git/SGpp/MR_Python/Extended/data/', model, objFunc.getName())
     if not os.path.exists(directory):
         os.makedirs(directory)
-       
-    saveName = createFileName(gridType, model, refineType, maxPoints, maxLevel, degree, objFunc)
+
+    saveName = createFileName(
+        gridType, model, refineType, maxPoints, maxLevel, degree, objFunc)
     datapath = os.path.join(directory, saveName)
     with open(datapath, 'wb') as fp:
         pickle.dump(data, fp)
-        print('saved data to {}'.format(datapath))   
+        print('saved data to {}'.format(datapath))
 
-        
+
 def loadData(gridType, model, refineType, maxPoints, maxLevel, degree, objFunc):
-    directory = os.path.join('/home/rehmemk/git/SGpp/MR_Python/Extended/data/', model, objFunc.getName())
-    saveName = createFileName(gridType, model, refineType, maxPoints, maxLevel, degree, objFunc)
+    directory = os.path.join(
+        '/home/rehmemk/git/SGpp/MR_Python/Extended/data/', model, objFunc.getName())
+    saveName = createFileName(
+        gridType, model, refineType, maxPoints, maxLevel, degree, objFunc)
     datapath = os.path.join(directory, saveName)
     with open(datapath, 'rb') as fp:
         # encoding necessary because the data was stored with cPickle from python2.7
-        # data = pickle.load(fp, encoding='latin1')  
+        # data = pickle.load(fp, encoding='latin1')
         data = pickle.load(fp, encoding='latin1')
-    print('loaded data from {}'.format(datapath))   
+    print('loaded data from {}'.format(datapath))
     return data
