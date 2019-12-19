971395690e73f605d78a485673ca354b98a6ddbf
Peter Schober <schober@finance.uni-frankfurt.de>
2019-12-19 13:57:46 +0100

    Update compile script for Goethe-HLR for R2019b

--- schobepr_scons_build_goethe.sh
+++ schobepr_scons_build_goethe.sh
@@ -6,6 +6,6 @@ module load comp/gcc/8.2.0
 
 scons -j $(nproc) VERBOSE=1 OPT=1 SG_ALL=0 SG_BASE=1 SG_OPTIMIZATION=1 SG_PYTHON=0 \
     USE_MATLAB=1 BUILD_STATICLIB=1 USE_PYTHON3_FOR_PYSGPP=0 RUN_PYTHON_TESTS=0 \
-    CPPPATH=/home/portfolio/public/MATLAB/R2017b/extern/include \
-    LIBPATH=/home/portfolio/public/MATLAB/R2017b/bin/glnxa64 \
-    RPATH=/home/portfolio/public/MATLAB/R2017b/bin/glnxa64
+    CPPPATH=/home/ww_matlab/MATLAB/R2019b/extern/include \
+    LIBPATH=/home/ww_matlab/MATLAB/R2019b/bin/glnxa64 \
+    RPATH=/home/ww_matlab/MATLAB/R2019b/bin/glnxa64
