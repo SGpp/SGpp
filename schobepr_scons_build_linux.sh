7d80fb1069436f29993e78a129520f4d2893d04f
Peter Schober <schober@finance.uni-frankfurt.de>
2020-01-20 12:53:47 +0100

    Update linux compile script for R2019b

--- schobepr_scons_build_linux.sh
+++ schobepr_scons_build_linux.sh
@@ -2,6 +2,6 @@
 
 scons -j $(nproc) VERBOSE=1 OPT=1 SG_ALL=0 SG_BASE=1 SG_OPTIMIZATION=1 SG_PYTHON=1 \
     USE_MATLAB=1 BUILD_STATICLIB=1 USE_PYTHON3_FOR_PYSGPP=1 RUN_PYTHON_TESTS=0 \
-    CPPPATH=/opt/MATLAB/R2017b/extern/include \
-    LIBPATH=/opt/MATLAB/R2017b/bin/glnxa64 \
-    RPATH=/opt/MATLAB/R2017b/bin/glnxa64
+    CPPPATH=/opt/MATLAB/R2019b/extern/include \
+    LIBPATH=/opt/MATLAB/R2019b/bin/glnxa64 \
+    RPATH=/opt/MATLAB/R2019b/bin/glnxa64
