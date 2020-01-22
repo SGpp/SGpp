aa39768689b3f1230b3c8e316e690587beb2188c
Peter Schober <schober@finance.uni-frankfurt.de>
2020-01-22 13:59:44 +0100

    Use gcc 6.3.0 in compatibility to R2019b

--- schobepr_scons_build_goethe.sh
+++ schobepr_scons_build_goethe.sh
@@ -2,7 +2,7 @@
 module load boost-1.69.0-gcc-4.8.5-gbslxeg
 module load python-2.7.15-gcc-4.8.5-7ok6o2a
 module load scons-2.5.1-gcc-4.8.5-doh7q4i
-module load comp/gcc/8.2.0
+module load gcc-6.3.0-gcc-4.8.5-qvxdvyq
 
 scons -j $(nproc) VERBOSE=1 OPT=1 SG_ALL=0 SG_BASE=1 SG_OPTIMIZATION=1 SG_PYTHON=0 \
     USE_MATLAB=1 BUILD_STATICLIB=1 USE_PYTHON3_FOR_PYSGPP=0 RUN_PYTHON_TESTS=0 \
