5de4c39caac0c734be53d84b42d99d43049b45bb
Michael Rehme <michael.rehme@ipvs.uni-stuttgart.de>
2020-01-31 18:12:10 +0100

    moved MR_Python into its own repo

--- MR_Python/Vector/execute.sh
+++
@@ -1,3 +0,0 @@
-# this should be 7681 grid points. With 5s per point this takes 10.6h
-time python3.6 comparingBSplinesVector.py --model='okushiri' --dim=4 --out=451 --gridType='nakbsplineboundary' --degree=3 --refineType=regular --maxLevel=3
-
