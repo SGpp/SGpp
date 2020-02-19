9d30887d4489cf1d168e47a76674941704e828af
Paul Sarbu <paul.sarbu@in.tum.de>
2020-02-19 16:50:54 +0100

    Fix some redundant and chaining includes

--- datadriven/src/sgpp/datadriven/operation/hash/simple/OperationInverseRosenblattTransformation1DModBspline.cpp
+++ datadriven/src/sgpp/datadriven/operation/hash/simple/OperationInverseRosenblattTransformation1DModBspline.cpp
@@ -8,14 +8,12 @@
 #include <sgpp/base/exception/operation_exception.hpp>
 #include <sgpp/base/grid/type/ModBsplineGrid.hpp>
 #include <sgpp/base/operation/hash/OperationEval.hpp>
-#include <sgpp/base/tools/GaussLegendreQuadRule1D.hpp>
 #include <sgpp/base/tools/HermiteBasis.hpp>
 #include <sgpp/datadriven/DatadrivenOpFactory.hpp>
+#include <sgpp/base/operation/BaseOpFactory.hpp>
 #include <sgpp/datadriven/operation/hash/simple/OperationInverseRosenblattTransformation1DModBspline.hpp>
 #include <sgpp/datadriven/operation/hash/simple/OperationRosenblattTransformation1DModBspline.hpp>
 #include <sgpp/base/function/scalar/WrapperScalarFunction.hpp>
-#include <sgpp/globaldef.hpp>
-#include <sgpp_datadriven.hpp>
 #include <sgpp_optimization.hpp>
 
 #include <algorithm>
@@ -39,14 +37,16 @@ OperationInverseRosenblattTransformation1DModBspline::
 OperationInverseRosenblattTransformation1DModBspline::
     ~OperationInverseRosenblattTransformation1DModBspline() {}
 
-void OperationInverseRosenblattTransformation1DModBspline::init(base::DataVector* alpha1d) {
+void OperationInverseRosenblattTransformation1DModBspline::init(
+    base::DataVector* alpha1d) {
   patch_areas.clear();
   is_negative_patch.clear();
   ordered_grid_points.clear();
   patch_functions.clear();
   coord_cdf.clear();
   sum = 0;
-  opEval = std::unique_ptr<base::OperationEval>(op_factory::createOperationEvalNaive(*grid));
+  opEval = std::unique_ptr<base::OperationEval>(
+      op_factory::createOperationEvalNaive(*grid));
 
   base::DataVector coord(1);
   std::multimap<double, double>::iterator it1;
@@ -56,7 +56,8 @@ void OperationInverseRosenblattTransformation1DModBspline::init(base::DataVector
   size_t p = dynamic_cast<sgpp::base::ModBsplineGrid*>(grid)->getDegree();
   quadOrder = (p + 1) / 2;
 
-  gauss.getLevelPointsAndWeightsNormalized(quadOrder, gauss_coordinates, weights);
+  gauss.getLevelPointsAndWeightsNormalized(quadOrder, gauss_coordinates,
+                                           weights);
 
   // need an ordered list of the grid points
   for (size_t i = 0; i < gs->getSize(); i++) {
@@ -84,7 +85,8 @@ void OperationInverseRosenblattTransformation1DModBspline::init(base::DataVector
     double gaussQuadSum = 0.;
     double left = left_coord;
     double scaling = coord[0] - left;
-    // this will always be initialized to false except if we are in the first patch
+    // this will always be initialized to false except if we are in the first
+    // patch
     // and the first value equals 0
     bool negative_value_encountered = (i == 1 && negative_start);
     for (size_t c = 0; c < quadOrder; c++) {
@@ -106,7 +108,9 @@ void OperationInverseRosenblattTransformation1DModBspline::init(base::DataVector
         right_coord = ordered_grid_points[j];
         coord[0] = right_coord;
         right_function_value = opEval->eval(*alpha1d, coord);
-        if (right_function_value >= 0 && right_function_value != left_function_value) break;
+        if (right_function_value >= 0 &&
+            right_function_value != left_function_value)
+          break;
       }
 
       right_coord = ordered_grid_points[j];
@@ -130,25 +134,29 @@ void OperationInverseRosenblattTransformation1DModBspline::init(base::DataVector
       double function_values[3];
       function_values[0] = left_function_value;
       function_values[1] = right_function_value;
-      secants[0] = (right_function_value - left_function_value) / (right_coord - left_coord);
+      secants[0] = (right_function_value - left_function_value) /
+                   (right_coord - left_coord);
       tangents[0] = secants[0];
       if (j != ordered_grid_points.size() - 1) {
         coord[0] = ordered_grid_points[j + 1];
         function_values[2] = opEval->eval(*alpha1d, coord);
       } else {
-        // if j is the last grid point choose the next one with the same step size
+        // if j is the last grid point choose the next one with the same step
+        // size
         // and set it's function value to zero
         // and set it's function value to the last value
         coord[0] = 1 + ordered_grid_points[j] - ordered_grid_points[j - 1];
         function_values[2] = right_function_value;
       }
-      secants[1] = (function_values[2] - function_values[1]) / (coord[0] - ordered_grid_points[j]);
+      secants[1] = (function_values[2] - function_values[1]) /
+                   (coord[0] - ordered_grid_points[j]);
       tangents[2] = secants[1];
       // secants left and right of current point
       if (secants[0] == 0 || secants[1] == 0)
         // if one of the secants is zero
         tangents[1] = 0;
-      else if ((secants[0] > 0 && secants[1] < 0) || (secants[0] < 0 && secants[1] > 0))
+      else if ((secants[0] > 0 && secants[1] < 0) ||
+               (secants[0] < 0 && secants[1] > 0))
         // if the secants dont have the same sign
         tangents[1] = 0;
       else
@@ -166,9 +174,9 @@ void OperationInverseRosenblattTransformation1DModBspline::init(base::DataVector
       }
       // interpolation that can be evaluated between left_coord and right_coord
 
-      std::function<double(double)> interpolation = [right_coord, left_coord, left_function_value,
-                                                     right_function_value,
-                                                     tangents](double x) -> double {
+      std::function<double(double)> interpolation =
+          [right_coord, left_coord, left_function_value, right_function_value,
+           tangents](double x) -> double {
         double h = right_coord - left_coord;
         double t = (x - left_coord) / h;
         return left_function_value * base::HermiteBasis::h_0_0(t) +
@@ -218,8 +226,8 @@ void OperationInverseRosenblattTransformation1DModBspline::init(base::DataVector
   }
 }
 
-double OperationInverseRosenblattTransformation1DModBspline::sample(base::DataVector* alpha1d,
-                                                                    double coord1d) {
+double OperationInverseRosenblattTransformation1DModBspline::sample(
+    base::DataVector* alpha1d, double coord1d) {
   if (coord1d == 0.0) return 0.0;
   if (sum == 0) return 0;
   base::DataVector coord(1);
@@ -239,7 +247,8 @@ double OperationInverseRosenblattTransformation1DModBspline::sample(base::DataVe
   }
   --patch_nr;
   --negative_patch_counter;
-  // std::cout << "negative_patch_counter: " << negative_patch_counter << std::endl;
+  // std::cout << "negative_patch_counter: " << negative_patch_counter <<
+  // std::endl;
   // std::cout << "patch_nr: " << patch_nr << std::endl;
   // std::cout << "PAs size: " << patch_areas.size() << std::endl;
   // std::cout << "PFs size : " << patch_functions.size() << std::endl;
@@ -250,7 +259,8 @@ double OperationInverseRosenblattTransformation1DModBspline::sample(base::DataVe
   if (is_negative_patch[patch_nr]) {
     for (size_t c = 0; c < quadOrder; c++) {
       coord[0] = left + scaling * gauss_coordinates[c];
-      gaussQuadSum += weights[c] * patch_functions[negative_patch_counter](coord[0]);
+      gaussQuadSum +=
+          weights[c] * patch_functions[negative_patch_counter](coord[0]);
     }
   } else {
     for (size_t c = 0; c < quadOrder; c++) {
@@ -264,9 +274,10 @@ double OperationInverseRosenblattTransformation1DModBspline::sample(base::DataVe
 double OperationInverseRosenblattTransformation1DModBspline::doTransformation1D(
     base::DataVector* alpha1d, double coord1d) {
   init(alpha1d);
-  // std::cout << "PFs size after exit: " << patch_functions.size() << std::endl;
-  std::function<double(const base::DataVector&)> optFunc = [this, coord1d, alpha1d](
-      const base::DataVector& x) -> double {
+  // std::cout << "PFs size after exit: " << patch_functions.size() <<
+  // std::endl;
+  std::function<double(const base::DataVector&)> optFunc =
+      [this, coord1d, alpha1d](const base::DataVector& x) -> double {
     double F_x = sample(alpha1d, x[0]);
     return (F_x - coord1d) * (F_x - coord1d);
   };
