594dda7c1ea345ece48533594a2fb965d532a948
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2020-02-24 13:29:27 +0100

    Update travis debian test to include more examples

--- tools/travis_scripts/create_deb_install_deb_and_test.sh
+++ tools/travis_scripts/create_deb_install_deb_and_test.sh
@@ -3,25 +3,87 @@
 # exit when any command fails
 set -e
 
-cd tools/create_release/deb_package
-./create_deb.sh testing
+#cd tools/create_release/deb_package
+#./create_deb.sh testing
 
-printf '\n~~~ Installing debian package ~~~\n\n'
-sudo dpkg -i libsgpp-test-package_0.0-0.deb
-sudo dpkg -i libsgpp-python-test-package_0.0-0.deb
+#printf '\n~~~ Installing debian package ~~~\n\n'
+#sudo dpkg -i libsgpp-test-package_0.0-0.deb
+#sudo dpkg -i libsgpp-python-test-package_0.0-0.deb
 
-printf '\n~~~ Testing debian cpp package ~~~\n\n'
-cd ../../..
+printf '\n\n\n~~~ Testing debian cpp package ~~~\n\n'
+#cd ../../..
 mkdir -p "$HOME/testing_package"
-cp "base/examples/quadrature.cpp" "$HOME/testing_package/quadrature.cpp"
-cp "base/examples/quadrature.py" "$HOME/testing_package/quadrature.py"
-cd "$HOME/testing_package"
-printf "\nBuilding quadrature cpp example in directory $(pwd) \n"
-g++ quadrature.cpp -std=c++11 -o quad -l sgppbase
-./quad 
+declare -a cpp_examples_to_test=(
+                                    "base/examples/quadrature.cpp"
+                                    "base/examples/tutorial.cpp"
+                                    "base/examples/gridTExample.cpp"
+                                    "base/examples/JSONExample.cpp"
+                                    "base/examples/gridInteractionExample.cpp"
+                                    "base/examples/unserializeGrid.cpp"
+                                    "base/examples/gridTypes.cpp"
+                                    "base/examples/dataVectorSerializeDemo.cpp"
+                                    "base/examples/refinement.cpp"
+                                    "base/examples/predictiveRefinement.cpp"
+                                    "solver/examples/fistaExample.cpp"
+                                    "optimization/examples/optimization.cpp"
+                                    "optimization/examples/constrainedOptimization.cpp"
+                                    #"combigrid/examples/combigrid.cpp"
+                                )
+arraylength=${#cpp_examples_to_test[@]}
+for (( i=1; i<${arraylength}+1; i++ ));
+do
+  echo ""
+  echo "-------------------------------------------------------------"
+  echo "CPP examples test" $i "of" ${arraylength} "(to be copied from " ${cpp_examples_to_test[$i-1]} "into test folder) : "
+  source_file=${cpp_examples_to_test[$i-1]}
+  file_name=$(basename $source_file)
+  target_file=$HOME/testing_package/${file_name}
+  echo "... copying $file_name from" $source_file " into $target_file "
+  cp ${source_file} ${target_file}
+  exec_name=${file_name%.*}
+  echo "... switching into test folder"
+  cd "$HOME/testing_package"
+  echo "... building ${target_file} into $exec_name "
+  g++ ${file_name} -std=c++11 -o ${exec_name} -l sgppbase -l sgppsolver -l sgppoptimization
+  echo "... running ./$exec_name "
+  ./${exec_name}
+  echo "... switching back into SGpp root directory"
+  cd -
+done
 
-printf '\n~~~ Testing debian python package ~~~\n\n'
-printf "\nBuilding quadrature cpp example in directory $(pwd) \n"
-python3 quadrature.py
+printf '\n\n\n~~~ Testing debian python package ~~~\n\n'
+mkdir -p "$HOME/testing_python_package"
+declare -a python_examples_to_test=(
+                                    "base/examples/quadrature.py"
+                                    "base/examples/tutorial.py"
+                                    "base/examples/gridTExample.py"
+                                    "base/examples/dataVectorSerializeDemo.py"
+                                    "base/examples/refinement.py"
+                                    "base/examples/predictiveRefinement.py"
+                                    "base/examples/predictiveANOVARefinement.py"
+                                    "base/examples/subspaceRefinement.py"
+                                    "optimization/examples/optimization.py"
+                                    "optimization/examples/fuzzy.py"
+                                    "combigrid/examples/combigrid.py"
+                                    "pde/examples/LTwoDotTest.py"
+                                )
+arraylength=${#python_examples_to_test[@]}
+for (( i=1; i<${arraylength}+1; i++ ));
+do
+  echo ""
+  echo "-------------------------------------------------------------"
+  echo "Python examples test" $i "of" ${arraylength} "(to be copied from " ${python_examples_to_test[$i-1]} "into test folder) : "
+  source_file=${python_examples_to_test[$i-1]}
+  file_name=$(basename $source_file)
+  target_file=$HOME/testing_python_package/${file_name}
+  echo "... copying $file_name from" $source_file " into $target_file "
+  cp ${source_file} ${target_file}
+  echo "... switching into python test folder"
+  cd "$HOME/testing_python_package"
+  echo "... running python3 ${file_name} "
+  #python3 ${file_name}
+  echo "... switching back into SGpp root directory"
+  cd -
+done
 
 # Cleanup not required on travis
