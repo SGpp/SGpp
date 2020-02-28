82e2b66da85c0da6e614636e2fe006861b7b5b59
Michael Rehme <michael.rehme@ipvs.uni-stuttgart.de>
2020-02-28 14:58:05 +0100

    eval and evalDx can no longer fall through

--- base/src/sgpp/base/operation/hash/common/basis/NakPBsplineBasis.hpp
+++ base/src/sgpp/base/operation/hash/common/basis/NakPBsplineBasis.hpp
@@ -63,9 +63,7 @@ class NakPBsplineBasis : public Basis<LT, IT> {
       case 1:
         if (l == 1) {
           // Lagrange level 1
-          if (i == 1) {
-            return 1.0;
-          }
+          return 1.0;
         } else if ((i > 1) && (i < hInv - 1)) {
           // l >=2, 1 < i < 2^l - 1
           return bsplineBasis.eval(l, i, x);
@@ -74,33 +72,28 @@ class NakPBsplineBasis : public Basis<LT, IT> {
             i = hInv - i;
             t *= -1.0;
           }
-          if (i == 1) {
-            // l >=2, i = 1
-            if ((t < -1.0) || (t > 1.0)) {
-              return 0.0;
-            } else {
-              t += 1.0;
-              double result = -1.0 / 2.0;
-              result = 1.0 + result * t;
-              return result;
-            }
+
+          // l >=2, i = 1
+          if ((t < -1.0) || (t > 1.0)) {
+            return 0.0;
+          } else {
+            t += 1.0;
+            double result = -1.0 / 2.0;
+            result = 1.0 + result * t;
+            return result;
           }
         }
-
       case 3:
         if (l == 1) {
           // Lagrange leve 1
-          if (i == 1) {
-            // l = 1, i = 1
-            // return 4 * x - 4 * x * x;
-            return 1.0;
-          }
+          // l = 1, i = 1
+          return 1.0;
         } else if (l == 2) {
           // Lagrange leve 2
           if (i == 1) {
             // l = 2, i = 1
             return 8 * (x - 0.5) * (x - 0.75);
-          } else if (i == 3) {
+          } else {
             // l = 2, i = 3
             return 8 * (x - 0.25) * (x - 0.5);
           }
@@ -125,7 +118,7 @@ class NakPBsplineBasis : public Basis<LT, IT> {
               result = 9.0 / 20.0 + result * t;
               return result;
             }
-          } else if (i == 3) {
+          } else {
             // l >= 3, i = 3
             if ((t < -3.0) || (t > 2.0)) {
               return 0.0;
@@ -155,17 +148,15 @@ class NakPBsplineBasis : public Basis<LT, IT> {
       case 5:
         if (l == 1) {
           // Lagrange leve 1
-          if (i == 1) {
-            // l = 1, i = 1
-            // return 4 * x - 4 * x * x;
-            return 1.0;
-          }
+          // l = 1, i = 1
+          // return 4 * x - 4 * x * x;
+          return 1.0;
         } else if (l == 2) {
           // Lagrange leve 2
           if (i == 1) {
             // l = 2, i = 1
             return 8 * (x - 0.5) * (x - 0.75);
-          } else if (i == 3) {
+          } else {
             // l = 2, i = 3
             return 8 * (x - 0.25) * (x - 0.5);
           }
@@ -192,7 +183,7 @@ class NakPBsplineBasis : public Basis<LT, IT> {
                 result = 16.0 / 105.0 + result * t;
                 return result;
               }
-            } else if (i == 3) {
+            } else {
               // l=3, i=3
               if ((t < -3.0) || (t > 5.0)) {
                 return 0.0;
@@ -263,7 +254,7 @@ class NakPBsplineBasis : public Basis<LT, IT> {
                 result = 1.0 / 672.0 + result * t;
                 return result;
               }
-            } else if (i == 5) {
+            } else {
               // l >=4, i = 5
               if ((t < -5.0) || (t > 3.0)) {
                 return 0.0;
@@ -315,12 +306,10 @@ class NakPBsplineBasis : public Basis<LT, IT> {
             }
           }
         }
-
       default:
         return 0.0;
     }
   }
-
   /**
    * @param l     level of basis function
    * @param i     index of basis function
@@ -328,8 +317,242 @@ class NakPBsplineBasis : public Basis<LT, IT> {
    * @return      value of derivative of B-spline basis function
    */
   inline double evalDx(LT l, IT i, double x) {
-    std::cout << "NakPBsplineBasis evalDx not implemented";
-    return 0.0;
+    const IT hInv = static_cast<IT>(1) << l;
+    const double hInvDbl = static_cast<double>(hInv);
+    double innerDeriv = hInvDbl;
+    double t = x * hInvDbl - static_cast<double>(i);
+    switch (getDegree()) {
+      case 1:
+        if (l == 1) {
+          // Lagrange level 1
+          return 0.0;
+        } else if ((i > 1) && (i < hInv - 1)) {
+          // l >=2, 1 < i < 2^l - 1
+          return bsplineBasis.eval(l, i, x);
+        } else {
+          if (i > hInv / 2) {
+            i = hInv - i;
+            t *= -1.0;
+            innerDeriv *= -1.0;
+          }
+          // l >=2, i = 1
+          if ((t < -1.0) || (t > 1.0)) {
+            return 0.0;
+          } else {
+            return -1.0 / 2.0;
+          }
+        }
+      case 3:
+        if (l == 1) {
+          // Lagrange leve 1
+          // l = 1, i = 1
+          return 0.0;
+        } else if (l == 2) {
+          // Lagrange leve 2
+          if (i == 1) {
+            // l = 2, i = 1
+            return 16 * (x - 0.625);
+          } else {
+            // l = 2, i = 3
+            return 16 * (x - 0.375);
+          }
+        } else if ((i > 3) && (i < hInv - 3)) {
+          // l >= 4, 3 < i < 2^l - 3
+          return bsplineBasis.evalDx(l, i, x);
+        } else {
+          if (i > hInv / 2) {
+            i = hInv - i;
+            t *= -1.0;
+            innerDeriv *= -1.0;
+          }
+
+          if (i == 1) {
+            // l >=3, i = 1
+            if ((t < -1.0) || (t > 2.0)) {
+              return 0.0;
+            } else {
+              t += 1.0;
+              double result = -1.0 / 20.0;
+              result = 3.0 / 10.0 + result * t;
+              result = -9.0 / 20.0 + result * t;
+              return innerDeriv * result;
+            }
+          } else {
+            // l >= 3, i = 3
+            if ((t < -3.0) || (t > 2.0)) {
+              return 0.0;
+            } else if (t < 0.0) {
+              t += 3.0;
+              double result = -3.0 / 20.0;
+              result = 3.0 / 10.0 + result * t;
+              result = 3.0 / 20.0 + result * t;
+              return result;
+            } else if (t < 1.0) {
+              double result = 3.0 / 5.0;
+              result = -3.0 / 5.0 + result * t;
+              result = -3.0 / 10.0 + result * t;
+              return result;
+            } else {
+              t -= 1.0;
+              double result = -3.0 / 10.0;
+              result = 3.0 / 5.0 + result * t;
+              result = -3.0 / 10.0 + result * t;
+              return innerDeriv * result;
+            }
+          }
+        }
+      case 5:
+        if (l == 1) {
+          // Lagrange leve 1
+          // l = 1, i = 1
+          return 0.0;
+        } else if (l == 2) {
+          // Lagrange leve 2
+          if (i == 1) {
+            // l = 2, i = 1
+            return 16 * (x - 0.625);
+          } else {
+            // l = 2, i = 3
+            return 16 * (x - 0.375);
+          }
+        } else if ((i > 5) && (i < hInv - 5)) {
+          // l >= 4, 5 < i < 2^l - 5
+          return bsplineBasis.eval(l, i, x);
+        } else {
+          if (i > hInv / 2) {
+            i = hInv - i;
+            t *= -1.0;
+          }
+          if (l == 3) {
+            if (i == 1) {
+              // l=3, i=1
+              if ((t < -1.0) || (t > 3.0)) {
+                return 0.0;
+              } else {
+                t += 1.0;
+                double result = -1.0 / 1344.0;
+                result = 1.0 / 84.0 + result * t;
+                result = -1.0 / 14.0 + result * t;
+                result = 4.0 / 21.0 + result * t;
+                result = -4.0 / 21.0 + result * t;
+                return result;
+              }
+            } else {
+              // l=3, i=3
+              if ((t < -3.0) || (t > 5.0)) {
+                return 0.0;
+              } else if (t < 1.0) {
+                t += 3.0;
+                double result = -149.0 / 55440.0;
+                result = 38.0 / 1155.0 + result * t;
+                result = -41.0 / 385.0 + result * t;
+                result = -2.0 / 105.0 + result * t;
+                result = 499.0 / 2310.0 + result * t;
+                return result;
+              } else {
+                t -= 1.0;
+                double result = 7.0 / 7920.0;
+                result = -1.0 / 99.0 + result * t;
+                result = 1.0 / 33.0 + result * t;
+                result = 2.0 / 99.0 + result * t;
+                result = -29.0 / 198.0 + result * t;
+                return result;
+              }
+            }
+          } else {
+            if (i == 1) {
+              // l >=4, i = 1
+              if ((t < -1.0) || (t > 3.0)) {
+                return 0.0;
+              } else {
+                t += 1.0;
+                double result = -1.0 / 1344.0;
+                result = 1.0 / 84.0 + result * t;
+                result = -1.0 / 14.0 + result * t;
+                result = 4.0 / 21.0 + result * t;
+                result = -4.0 / 21.0 + result * t;
+                return result;
+              }
+            } else if (i == 3) {
+              // l >=4, i = 3
+              if ((t < -3.0) || (t > 3.0)) {
+                return 0.0;
+              } else if (t < 1.0) {
+                t += 3.0;
+                double result = -5.0 / 672.0;
+                result = 1.0 / 14.0 + result * t;
+                result = -9.0 / 56.0 + result * t;
+                result = -3.0 / 28.0 + result * t;
+                result = 27.0 / 112.0 + result * t;
+                return result;
+              } else if (t < 2.0) {
+                t -= 1.0;
+                double result = 13.0 / 672.0;
+                result = -1.0 / 21.0 + result * t;
+                result = -1.0 / 56.0 + result * t;
+                result = 11.0 / 84.0 + result * t;
+                result = -31.0 / 336.0 + result * t;
+                return result;
+              } else {
+                t -= 2.0;
+                double result = -5.0 / 672.0;
+                result = 5.0 / 168.0 + result * t;
+                result = -5.0 / 112.0 + result * t;
+                result = 5.0 / 168.0 + result * t;
+                result = -5.0 / 672.0 + result * t;
+                return result;
+              }
+
+            } else {
+              // l >=4, i = 5
+              if ((t < -5.0) || (t > 3.0)) {
+                return 0.0;
+              } else if (t < -1.0) {
+                t += 5.0;
+                double result = -5.0 / 1344.0;
+                result = 1.0 / 84.0 + result * t;
+                result = 1.0 / 56.0 + result * t;
+                result = 1.0 / 84.0 + result * t;
+                result = 1.0 / 336.0 + result * t;
+                return result;
+              } else if (t < 0.0) {
+                t += 1.0;
+                double result = 121.0 / 1344.0;
+                result = -1.0 / 21.0 + result * t;
+                result = -11.0 / 56.0 + result * t;
+                result = -19.0 / 84.0 + result * t;
+                result = 7.0 / 48.0 + result * t;
+                return result;
+              } else if (t < 1.0) {
+                double result = -215.0 / 1344.0;
+                result = 5.0 / 16.0 + result * t;
+                result = 45.0 / 224.0 + result * t;
+                result = -45.0 / 112.0 + result * t;
+                result = -15.0 / 64.0 + result * t;
+                return result;
+              } else if (t < 2.0) {
+                t -= 1.0;
+                double result = 145.0 / 1344.0;
+                result = -55.0 / 168.0 + result * t;
+                result = 5.0 / 28.0 + result * t;
+                result = 25.0 / 84.0 + result * t;
+                result = -95.0 / 336.0 + result * t;
+                return result;
+              } else {
+                t -= 2.0;
+                double result = -5.0 / 192.0;
+                result = 5.0 / 48.0 + result * t;
+                result = -5.0 / 32.0 + result * t;
+                result = 5.0 / 48.0 + result * t;
+                result = -5.0 / 192.0 + result * t;
+                return result;
+              }
+            }
+          }
+        }
+      default:
+        return 0.0;
+    }
   }
 
   /**
