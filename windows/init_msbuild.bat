c19c2365582d4cb1e727df4e189f8bb4be9e3015
Christopher Schnick <c.schnick@monospark.org>
2020-10-03 15:22:25 +0200

    Move vs files and add init script

---
+++ windows/init_msbuild.bat
@@ -0,0 +1,8 @@
+copy .\msbuild\sgpp.sln.default .\..\sgpp.sln
+copy .\msbuild\sgpp.vcxproj.default .\..\sgpp.vcxproj
+copy .\msbuild\sgpp.vcxproj.user.default .\..\sgpp.vcxproj.user
+copy .\msbuild\linux_config.props.default .\msbuild\linux_config.props
+copy .\msbuild\run_config.props.default .\msbuild\run_config.props
+copy .\msbuild\build_debug.py.default .\msbuild\build_debug.py
+copy .\msbuild\build_release.py.default .\msbuild\build_release.py
+pause
