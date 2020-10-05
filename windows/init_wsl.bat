e9509d45063b92f40a257fc3448cbd907e60116c
Christopher Schnick <c.schnick@monospark.org>
2020-10-05 12:14:24 +0200

    Fix missing debugpy install

--- windows/init_wsl.bat
+++ windows/init_wsl.bat
@@ -1,2 +1,2 @@
-wsl sudo apt-get update;sudo apt install g++ gdb make rsync zip unzip python3 python3-pip; python3 -m pip install scons;sudo ssh-keygen -A;sudo cp ./wsl/sshd_config /etc/ssh/sshd_config; sudo systemctl enable ssh; sudo service ssh start;
+wsl sudo apt-get update;sudo apt install g++ gdb make rsync zip unzip python3 python3-pip; python3 -m pip install scons debugpy;sudo ssh-keygen -A;sudo cp ./wsl/sshd_config /etc/ssh/sshd_config; sudo systemctl enable ssh; sudo service ssh start;
 pause
