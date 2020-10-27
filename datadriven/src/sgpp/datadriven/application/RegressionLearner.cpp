10411c5ad68d51038e8f82181acc677f2a1cd935
Raphael Leiteritz <leiterrl@ipvs.uni-stuttgart.de>
2020-10-27 19:02:00 +0100

    add default value to satisfy both gcc and clang

--- datadriven/src/sgpp/datadriven/application/RegressionLearner.cpp
+++ datadriven/src/sgpp/datadriven/application/RegressionLearner.cpp
@@ -189,7 +189,8 @@ void RegressionLearner::initializeGrid(const base::RegularGridConfiguration grid
 std::unique_ptr<datadriven::DMSystemMatrixBase> RegressionLearner::createDMSystem(
     base::DataMatrix& trainDataset) {
   using datadriven::RegularizationType;
-  base::OperationMatrix* opMatrix;
+  // initializing this is sadly neccesary to resolve a face-off between gcc and clang warnings
+  base::OperationMatrix* opMatrix = sgpp::op_factory::createOperationIdentity(*grid);
   switch (regularizationConfig.type_) {
     case RegularizationType::Identity:
       opMatrix = sgpp::op_factory::createOperationIdentity(*grid);
@@ -204,7 +205,6 @@ std::unique_ptr<datadriven::DMSystemMatrixBase> RegressionLearner::createDMSyste
     case RegularizationType::GroupLasso:
     case RegularizationType::Lasso:
     case RegularizationType::ElasticNet:
-    default:
       throw base::application_exception(
           "RegressionLearner::createDMSystem: An unsupported regularization type was chosen!");
   }
