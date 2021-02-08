7645e1d217ed518819a3c21948617f79ec762d30
Christopher Schnick <c.schnick@monospark.org>
2021-02-08 08:51:06 +0100

    Add nuget restore target

--- windows/build_msbuild.bat
+++ windows/build_msbuild.bat
@@ -1,2 +1,3 @@
+"%ProgramFiles(x86)%\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\MSBuild.exe" .\..\winsgpp.sln -t:restore
 "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\MSBuild.exe" .\..\winsgpp.sln -property:Configuration=Debug
 pause
